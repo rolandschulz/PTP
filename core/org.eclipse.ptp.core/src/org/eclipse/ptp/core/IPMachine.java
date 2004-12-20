ae0872e5eb62127f9a65765d2194e199f28cbb71
ndebard <ndebard>
2004-12-20 18:44:48 +0000

    Added another layer above Root, removing root and replacing with universe and machine.  Not finished yet.

--- core/org.eclipse.ptp.core/src/org/eclipse/ptp/core/IPMachine.java
+++ core/org.eclipse.ptp.core/src/org/eclipse/ptp/core/IPMachine.java
@@ -6,7 +6,7 @@ import org.eclipse.pdt.mi.MISession;
  * @author Clement
  *
  */
-public interface IPRoot extends IPElement {
+public interface IPMachine extends IPElement {
     public IPNode[] getNodes();
     public IPNode[] getSortedNodes();
     
@@ -15,7 +15,7 @@ public interface IPRoot extends IPElement {
 	
 	public IPProcess findProcess(String nodeNumber, String processNumber);
 	public IPProcess findProcess(String processNumber);
-	
+	 
 	public IPProcess[] getSortedProcesses();
 	public IPProcess[] getProcesses();
 	
@@ -24,5 +24,10 @@ public interface IPRoot extends IPElement {
 	public void removeAllProcesses();
 	
 	public String getOutputStoreDirectory();
-	public int getStoreLine();	
+	public int getStoreLine();
+	
+	/* returns a String representing the architecture in some form */
+	public String getArch();
+	/* sets the architecture, should be used by instantiating classes and such */
+	public void setArch(String a);
 }
