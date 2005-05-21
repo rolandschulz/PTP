1521b2a113ebadc6928767f8ce96ee8f2eaa1b54
crasmussen <crasmussen>
2005-05-21 09:55:38 +0000

    Initial version from cldt.

---
+++ lang/old/org.eclipse.fdt.ui/src/org/eclipse/fdt/internal/ui/editor/FortranDocumentSetupParticipant.java
@@ -0,0 +1,37 @@
+/**********************************************************************
+ * Copyright (c) 2002,2003,2004 QNX Software Systems and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors: 
+ * QNX Software Systems - Initial API and implementation
+ ***********************************************************************/
+
+package org.eclipse.fdt.internal.ui.editor;
+
+import org.eclipse.fdt.internal.ui.text.FortranTextTools;
+import org.eclipse.fdt.ui.FortranUIPlugin;
+import org.eclipse.core.filebuffers.IDocumentSetupParticipant;
+import org.eclipse.jface.text.IDocument;
+
+/**
+ * CDocumentSetupParticipant
+ */
+public class FortranDocumentSetupParticipant implements IDocumentSetupParticipant {
+	/**
+	 * 
+	 */
+	public FortranDocumentSetupParticipant() {
+	}
+	
+	/*
+	 * @see org.eclipse.core.filebuffers.IDocumentSetupParticipant#setup(org.eclipse.jface.text.IDocument)
+	 */
+	public void setup(IDocument document) {
+		FortranTextTools tools= FortranUIPlugin.getDefault().getTextTools();
+		tools.setupFortranDocument(document);
+	}
+
+}
