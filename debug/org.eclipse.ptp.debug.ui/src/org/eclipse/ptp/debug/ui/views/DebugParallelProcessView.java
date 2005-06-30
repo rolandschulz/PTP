719c973f5f3b1f77fe638a10f2facce53f28ef95
kchu <kchu>
2005-06-30 09:32:39 +0000

    *** empty log message ***

--- debug/org.eclipse.ptp.debug.ui/src/org/eclipse/ptp/debug/ui/views/DebugParallelProcessView.java
+++ debug/org.eclipse.ptp.debug.ui/src/org/eclipse/ptp/debug/ui/views/DebugParallelProcessView.java
@@ -41,13 +41,14 @@ import org.eclipse.ptp.debug.ui.actions.SuspendAction;
 import org.eclipse.ptp.debug.ui.actions.TerminateAction;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
-import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.graphics.Region;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -82,22 +83,28 @@ public class DebugParallelProcessView extends AbstractDebugParallelView {
 	protected PProcessGroup cur_process_group = null;
 	
 	//node info
-	protected final int x_offset = 10;
+	protected final int x_offset = 5;
 	protected final int x_spacing = 4;
-	protected final int y_offset = 10;
+	protected final int y_offset = 5;
 	protected final int y_spacing = 4;
 	protected final int node_width = 16;
 	protected final int node_height = 16;
 
 	protected int view_width = 200;
 	protected int view_height = 40;
-	protected int visible_node_num = 10;
+	protected int visible_node_col = 10;
+	protected int visible_node_row = 5;
 	
 	protected ScrolledComposite sc = null;
 	protected Composite drawComp = null;
 	
 	protected Shell toolTipShell = null;
-	protected Timer hovertimer = null;	
+	protected Timer hovertimer = null;
+	
+	//dragging
+	protected int drag_x = 0;
+	protected int drag_y = 0;
+	protected Shell selectionShell = null;
 		
     public static Image[][] statusImages = {
     	{
@@ -120,9 +127,14 @@ public class DebugParallelProcessView extends AbstractDebugParallelView {
 			ImageUtil.getImage(ImageUtil.IMG_PRO_STOPPED),
 			ImageUtil.getImage(ImageUtil.IMG_PRO_STOPPED_SEL),
     	}
-    };
-	
-	/**
+    };	
+
+	protected Listener myMouseDragListener = new Listener() {
+		public void handleEvent(Event e) {
+			mouseDragHandleEvent(e);
+		}
+	};
+    /**
 	 * constructor of DebugParallelProcessView
 	 */
 	public DebugParallelProcessView() {
@@ -194,52 +206,137 @@ public class DebugParallelProcessView extends AbstractDebugParallelView {
 	    vbar.setPageIncrement(node_height + y_spacing);
 	    sc.addListener(SWT.Resize, new Listener() {
 	    	public void handleEvent(Event event) {
-	    		adjustDrawingView(sc.getSize());
+	    		adjustDrawingView();
 	    	}
 	    });
 	
 	    drawComp = new Composite(sc, SWT.NONE);
-	    sc.setContent(drawComp);
 	    drawComp.setLayout(new FillLayout());	
-	    drawComp.setLayoutData(new GridData(GridData.FILL_BOTH));
-	    
+	    drawComp.setLayoutData(new GridData(GridData.FILL_BOTH));	    
+	    sc.setContent(drawComp);
+
+	    /*
 	    Composite bottomComp = new Composite(parent, SWT.BORDER);
 	    bottomComp.setLayout(new FillLayout());
 	    bottomComp.setLayoutData(new GridData(GridData.FILL_BOTH));
 	    new Label(bottomComp, SWT.NONE).setText("Text A");
 	    new Label(bottomComp, SWT.NONE).setText("Text B");
+	    */
 	    
 	    drawComp.addPaintListener(new PaintListener() {
 	    	public void paintControl(PaintEvent e) {
-	    		paintCanvas(e.gc);
+    			paintCanvas(e.gc);
 	    	}
 	    });
 	    
-	    drawComp.addListener(SWT.MouseDown, new Listener() {
-	    	public void handleEvent(Event e) {
-	    		int node_num = findProcessNum(e.x, e.y);
-	    		if (node_num > -1) {
-	    			PProcess process = cur_process_group.getProcess(node_num);
-	    			boolean isSelected = process.isSelected();
-	    			process.setSelected(!isSelected);
-	    			drawComp.redraw();
-	    		}
-	    	}
-	    });
 	    drawComp.addListener(SWT.MouseHover, new Listener() {
 	    	public void handleEvent(Event e) {
 	    		int node_num = findProcessNum(e.x, e.y);
 	    		if (node_num > -1) {
-	    			int process_id = cur_process_group.getProcess(node_num).getProcess_id();
-	    			showToolTip("" + process_id, e.x, e.y);
+	    			PProcess process = cur_process_group.getProcess(node_num);
+   					showToolTip(String.valueOf(process.getProcess_id()), e.x, e.y);
 	    		}
 	    	}
 	    });
-	    drawComp.addListener(SWT.MouseDown, new Listener() {
-	    	public void handleEvent(Event e) {
-	    		int node_num = findProcessNum(e.x, e.y);
-	    	}
-	    });
+	    drawComp.addListener(SWT.MouseDown, myMouseDragListener);
+	    drawComp.addListener(SWT.DragDetect, myMouseDragListener);
+	    drawComp.addListener(SWT.MouseMove, myMouseDragListener);
+	    drawComp.addListener(SWT.MouseUp, myMouseDragListener);
+	    drawComp.addListener(SWT.MouseExit, myMouseDragListener);
+	}
+	
+	protected void mouseDragHandleEvent(Event e) {
+		final int mx = e.x;
+		final int my = e.y;
+		switch (e.type) {
+			case SWT.MouseDown:
+				drag_x = 0;
+				drag_y = 0;					
+				break;
+			case SWT.DragDetect:
+				drag_x = mx;
+				drag_y = my;
+				break;
+			case SWT.MouseMove:
+				/*
+				if ((drag_x > 0 && drag_y > 0) && (mx > 0 && mx < view_width && my > 0 && my < view_height)) {
+					if (selectionShell == null) {
+						selectionShell = new Shell(drawComp.getShell(), SWT.NO_TRIM | SWT.NO_REDRAW_RESIZE| SWT.ON_TOP);
+						Display display = selectionShell.getDisplay();
+						selectionShell.setBackground(display.getSystemColor(SWT.COLOR_DARK_RED));
+						
+						drawSelectedArea(mx, my);
+						selectionShell.setVisible(true);
+						break;
+					}
+					drawSelectedArea(mx, my);
+				}
+				*/
+				break;				
+			case SWT.MouseUp:
+				disposeSelectionArea();
+				if (drag_x > 0 && drag_y > 0) {
+					drag_x = 0;
+					drag_y = 0;					
+					break;
+				} 
+
+				int node_num = findProcessNum(mx, my);
+				if (node_num > -1) {
+					PProcess process = cur_process_group.getProcess(node_num);
+					boolean isSelected = process.isSelected();
+					process.setSelected(!isSelected);
+					drawComp.redraw();
+				}
+				break;
+			default:
+				disposeSelectionArea();
+				drag_x = 0;
+				drag_y = 0;					
+				break;
+		}	
+	}
+	
+	protected void disposeSelectionArea() {
+		if (selectionShell != null) {
+			selectionShell.dispose();
+			selectionShell = null;
+		}		
+	}
+	
+	protected void drawSelectedArea(final int mx, final int my) {
+		int start_x = drag_x;
+		int start_y = drag_y;
+		int new_x = mx;
+		int new_y = my;
+		
+		if (drag_x > mx) {
+			new_x = drag_x;
+			start_x = mx;
+		}
+		if (drag_y > my) {
+			new_y = drag_y;
+			start_y = my;
+		}		
+		
+		Point locpt = getViewActualLocation(drawComp, selectionShell, start_x, start_y);
+		Point mpt = getViewActualLocation(drawComp, selectionShell, new_x, new_y);
+
+		selectionShell.setLocation(locpt);		
+
+		int len_x = Math.abs(mpt.x-locpt.x);
+		int len_y = Math.abs(mpt.y-locpt.y);		
+
+		Region region = new Region();
+		Rectangle pixel = new Rectangle(0, 0, 1, 1);
+		for (int y=0; y<len_y; y+=4) {
+			for (int x=0; x<len_x; x+=4) {
+				pixel.x = x;
+				pixel.y = y;
+				region.add(pixel);
+			}
+		}
+		selectionShell.setRegion(region);
 	}
 	
 	protected void hideToolTip() {
@@ -257,7 +354,7 @@ public class DebugParallelProcessView extends AbstractDebugParallelView {
 		toolTipLabel.setForeground (display.getSystemColor (SWT.COLOR_INFO_FOREGROUND));
 		toolTipLabel.setBackground (display.getSystemColor (SWT.COLOR_INFO_BACKGROUND));
 		toolTipLabel.setText(text);	
-
+		
 		Point labelSize = toolTipLabel.computeSize(SWT.DEFAULT, SWT.DEFAULT);
 		labelSize.x += 2; labelSize.y += 2;
 		toolTipLabel.setSize(labelSize);
@@ -265,17 +362,21 @@ public class DebugParallelProcessView extends AbstractDebugParallelView {
 		
 		Rectangle area = toolTipShell.getClientArea();
 		toolTipLabel.setSize(area.width, area.height);
-		Point inpt = drawComp.getLocation();
-		Point mappedpt = display.map(drawComp, null, inpt);
-		Point pt = new Point(mx, my);
-		pt.x += mappedpt.x - node_width;
-		pt.y += mappedpt.y - node_height;
-		
-		//Point pt = new Point(mx-x_spacing, my-y_spacing);
-		toolTipShell.setLocation(pt);
+				
+		Point newpt = getViewActualLocation(drawComp, toolTipShell, mx, my);
+		toolTipShell.setLocation((newpt.x - node_width), (newpt.y - node_height));
 		toolTipShell.setVisible(true);		
 	
 		createTimer();
+	}
+	
+	protected Point getViewActualLocation(Composite composite, Shell shell, int mx, int my) {
+		Point mappedpt = shell.getDisplay().map(drawComp, null, new Point(0,0));
+		Point newpt = new Point(mx, my);
+		
+		newpt.x += mappedpt.x;
+		newpt.y += mappedpt.y;
+		return newpt;
 	}
 	
 	private void createTimer() {
@@ -284,7 +385,7 @@ public class DebugParallelProcessView extends AbstractDebugParallelView {
 		hovertimer.schedule(new TimerTask() {
 			public void run() {
 				cancelTimer();
-				DebugParallelProcessView.this.getViewSite().getShell().getDisplay().asyncExec(new Runnable() {
+				drawComp.getDisplay().asyncExec(new Runnable() {
 					public void run() {
 						hideToolTip();
 					}
@@ -293,7 +394,7 @@ public class DebugParallelProcessView extends AbstractDebugParallelView {
 		}, 1000);
 	}
 	
-	private void cancelTimer() {
+	private void cancelTimer() {		
 		if(hovertimer != null) {
 			try {
 				hovertimer.cancel();
@@ -303,14 +404,15 @@ public class DebugParallelProcessView extends AbstractDebugParallelView {
 	}
 	
 	protected int findProcessNum(int mx, int my) {
-		int col = Math.round((mx - x_offset) / (node_width + x_spacing) + 0.5f);
+		int col = (mx - x_offset) / (node_width + x_spacing) + 1;
+		col = col>visible_node_col?(col-1):col;
 		int col_loc = x_offset + (node_width + x_spacing) * (col - 1);
 		
 		if (mx > col_loc && mx < (col_loc + node_width)) {
-			int row = Math.round((my - y_offset) / (node_height + y_spacing) + 0.5f);
+			int row = (my - y_offset) / (node_height + y_spacing) + 1;
 			int row_loc = y_offset + (node_height + y_spacing) * (row - 1);
 			if (my > row_loc && my < (row_loc + node_height)) {
-				int node_num = visible_node_num * (row - 1) + col;
+				int node_num = visible_node_col * (row - 1) + col;
 				if (node_num <= cur_process_group.size())
 					return (node_num - 1);
 			}
@@ -318,41 +420,69 @@ public class DebugParallelProcessView extends AbstractDebugParallelView {
 		return -1;
 	}
 	
-	protected void adjustDrawingView(Point point) {
-		view_width = point.x - sc.getVerticalBar().getSize().x;
-		view_height = point.y;
-		visible_node_num = Math.round((view_width - (x_offset * 2) - x_spacing) / (node_width + x_spacing) + 0.5f);		
-		
-		if (visible_node_num > 0) {
-	    	int total_process = cur_process_group.size();
-	    	int new_height = Math.round(total_process/visible_node_num + 0.5f) * (node_height + y_spacing) + (y_offset * 2);
-	    	
-			drawComp.setSize(view_width, new_height);
-			sc.layout();
-		}
+	protected void updateViewSize(boolean isVSrcoll) {
+		Point pt = sc.getSize();
+		view_width = isVSrcoll?(pt.x-sc.getVerticalBar().getSize().x):pt.x;
+		view_height = pt.y;
+		visible_node_col = (view_width - x_offset) / (node_width + x_spacing);
+		visible_node_row = (view_height - y_offset) / (node_height + y_spacing) + 1 ;
 	}
 	
+	protected int calculateHeight() {
+    	if (visible_node_col > 0) {
+        	int total_process = cur_process_group.size();
+	    	int max_node_row = (total_process / visible_node_col) + ((total_process%visible_node_col==0)?0:1);
+	    	return max_node_row * (node_height + y_spacing) + y_offset;
+    	}
+    	return view_height;
+	}
+	
+	/**
+	 * Calculate the size of drawing component 
+	 * 
+	 */
+	protected void adjustDrawingView() {
+		updateViewSize(false);
+		int new_height = calculateHeight();
+    	if (new_height > view_height) {//display v_scroll
+    		updateViewSize(true);
+    		new_height = calculateHeight();
+    	}	    	
+		drawComp.setSize(view_width, new_height);
+		//sc.layout();
+	}
+
+	/**
+	 * Print visible area ONLY
+	 * Find out process numbers of beginning and ending   
+	 * 
+	 * @param g
+	 */
 	protected void paintCanvas(GC g) {
 		PProcess[] processes = cur_process_group.getProcesses();
-		for (int i=0; i<processes.length; i++) {
+	
+		int start_row = Math.round((sc.getOrigin().y - y_offset) / (node_height + y_spacing));
+		int start_num = start_row * visible_node_col;
+
+		int end_num =  ((start_row + visible_node_row + 1) * visible_node_col);
+		end_num = (end_num>processes.length)?processes.length:end_num;
+
+		//System.out.println("("+px+":"+py+"), ("+pw+":"+ph+"), ("+view_width+":"+view_height+"), (Row: " + visible_node_row +", Col: " + visible_node_col+"), (Start: " + start_num + ", End: " + end_num+")");
+		for (int i=start_num; i<end_num; i++) {
 			drawElement(i+1, processes[i].isSelected(), processes[i].getStatus(), g);
 		}
 	}
 	
 	protected void drawElement(int index, boolean isSelected, int status, GC g) {
-		int x;
-		int y;
-		int x_loc;
-		int y_loc;
+		int x = (visible_node_col>index)?index:(index%visible_node_col);
+		x = (x==0)?visible_node_col:x;
+		int x_loc = x_offset + ((x-1) * (node_width + x_spacing)); 
 		
-		x = (visible_node_num>index)?index:(index%visible_node_num);
-		x = (x==0)?visible_node_num:x;
-		x_loc = x_offset + ((x-1) * (node_width + x_spacing)); 
-		
-		y = (x==visible_node_num)?index/visible_node_num:(visible_node_num>index)?1:(Math.round(index/visible_node_num + 0.5f));
-		y_loc = y_offset + ((y-1) * (node_height + y_spacing));
+		int y = (x==visible_node_col)?index/visible_node_col:(visible_node_col>index)?1:((index/visible_node_col) + 1);
+		int y_loc = y_offset + ((y-1) * (node_height + y_spacing));
 
 		g.drawImage(statusImages[status][isSelected?1:0], x_loc, y_loc);
+		//System.out.println("("+x_loc+":"+y_loc+")");
     }
 		
 	/* (non-Javadoc)
@@ -366,7 +496,7 @@ public class DebugParallelProcessView extends AbstractDebugParallelView {
 	 * @see org.eclipse.ui.IWorkbenchPart#setFocus()
 	 */
 	public void setFocus() {
-		
+		drawComp.setFocus();
 	}
 	
 	/* (non-Javadoc)
@@ -408,6 +538,8 @@ public class DebugParallelProcessView extends AbstractDebugParallelView {
 		return cur_process_group;
 	}
 	public void redraw() {
+		adjustDrawingView();
 		drawComp.redraw();
+		System.out.println("Called redraw");
 	}
 }
