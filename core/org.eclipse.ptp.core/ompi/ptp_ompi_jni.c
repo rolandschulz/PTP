13fd8eb7cb1c5fb80dbcdef7b5a8a0d296c5473d
ndebardeleben <ndebardeleben>
2005-07-13 19:25:06 +0000

    Trying out new JNI code.

--- core/org.eclipse.ptp.core/ompi/ptp_ompi_jni.c
+++ core/org.eclipse.ptp.core/ompi/ptp_ompi_jni.c
@@ -1,6 +1,7 @@
 #include <jni.h>
 #include "ptp_ompi_jni.h"
 #include <stdio.h>
+#include <unistd.h>
 
 #include "orte_config.h"
 #include <stdbool.h>
@@ -11,22 +12,30 @@
 #include "mca/gpr/gpr.h"
 #include "mca/rmgr/rmgr.h"
 #include "mca/base/base.h"
+#include "mca/errmgr/errmgr.h"
+#include "mca/rml/rml.h"
+#include "mca/pls/base/base.h"
+
 #include "event/event.h"
 
 #include "threads/condition.h"
 
 char error_msg[256];
 
-bool ptp_exitted;
-ompi_mutex_t ptp_lock;
-ompi_condition_t ptp_cond;
+static int pid;
+static int pgid;
 
 static void job_state_callback(orte_jobid_t jobid, orte_proc_state_t state);
 static int ptp_ompi_spawn(char *app, int num_procs);
+/*
+static void ptp_ompi_sendcmd(uint16_t usercmd);
+*/
 
 void set_error(char *msg);
 
-ompi_mutex_t eclipse_orte_lock; 
+extern int errno;
+
+opal_mutex_t eclipse_orte_lock; 
 
 /***********************************************************************
  * THE JNI FUNCTIONS THAT THE RUNTIME ENVIRONMENT WILL CALL - THESE ARE
@@ -40,27 +49,109 @@ Java_org_eclipse_ptp_rtsystem_ompi_OMPIControlSystem_OMPIGetError(JNIEnv *env, j
     	return (*env)->NewStringUTF(env, error_msg);
 }
 
+JNIEXPORT jint JNICALL Java_org_eclipse_ptp_rtsystem_ompi_OMPIControlSystem_OMPIStartDaemon(JNIEnv *env, jobject obj, jstring jorted_path, jstring jorted_bin, jobjectArray array)
+{
+    	char *orted_path, *orted_bin;
+	char **orted_args;
+	int ret;
+	jsize len;
+	int i;
+
+	printf("JNI (C) OMPI: OMPI_StartDaemon()\n");
+
+	pid = pgid = -1;
+
+	switch(pid = fork()) {
+	    case -1:
+		set_error("Unable to fork() for the orted spawn.");
+		return -1;
+		break;
+	    /* child */
+	    case 0:
+		orted_path = (char *)(*env)->GetStringUTFChars(env, 
+		  jorted_path, NULL);
+		orted_bin = (char *)(*env)->GetStringUTFChars(env, 
+		  jorted_bin, NULL);
+		len = (*env)->GetArrayLength(env, array);
+
+		/* we need 'len + 2' args for len args, 1 for NULL
+		 * termination, and 1 preface for the program name
+		 * (argv[0] essentially) */
+		orted_args = (char **)malloc((len+2) * sizeof(char*));
+		orted_args[0] = strdup(orted_bin);
+		for(i=1; i<len+1; i++) {
+		    jstring astr;
+		    char *bstr;
+
+		    astr = (*env)->GetObjectArrayElement(env, array, i - 1);
+		    bstr = (char *)(*env)->GetStringUTFChars(env, astr, NULL);
+
+		    /* copy the original string from the Java array over
+		     * into a C array element */
+		    orted_args[i] = strdup(bstr);
+		    
+		    (*env)->ReleaseStringUTFChars(env, astr, bstr);
+		}
+		/* exec() requires the array be NULL terminated */
+		orted_args[len+1] = NULL;
+
+		for(i=0; i<len+2; i++) {
+		    if(orted_args[i] != NULL) 
+		    	printf("[C] #%d = '%s'\n", i, orted_args[i]);
+		    else
+			printf("[C] #%d = NULL\n", i);
+		    fflush(stdout);
+		}
+		
+		/* spawn the daemon */
+		ret = execv(orted_path, orted_args);
+
+		//ret = execl("/Users/ndebard/local/bin/orted", "orted", 0);
+		/* release str */
+		(*env)->ReleaseStringUTFChars(env, jorted_path, orted_path);
+		(*env)->ReleaseStringUTFChars(env, jorted_bin, orted_bin);
+		for(i=0; i<len + 1; i++) {
+		    free(orted_args[i]);
+		}
+		free(orted_args);
+		break;
+	    /* parent */
+	    default:
+		printf("PARENT!\n");
+		pgid = getpgid(pid);
+		printf("[p] GROUP PID = %d\n", pgid);
+		sleep(1);
+		pgid = getpgid(pid);
+		printf("[p] GROUP PID = %d\n", pgid);
+		wait(&ret);
+		printf("parent ret from child = %d\n", ret);
+		printf("PID = %d\n", pid);
+		printf("GROUP PID = %d\n", pgid);
+		fflush(stdout);
+		break;
+	}
+}
+
 /* returns  0 if orted was already started and we connected to it
- * returns  1 if orted was NOT started but we went ahead and started it
+ * returns  1 if orted was NOT started so the caller needs to start it
  * returns -1 if there was some error of any kind - call OMPIGetError()
  *            to get the error string
  */
-JNIEXPORT int JNICALL 
-Java_org_eclipse_ptp_rtsystem_ompi_OMPIControlSystem_OMPIInit(JNIEnv *env, jobject obj, jstring orted_bin_str) 
+JNIEXPORT jint JNICALL Java_org_eclipse_ptp_rtsystem_ompi_OMPIControlSystem_OMPIInit(JNIEnv *env, jobject obj)
 {
     	int rc, id;
-	const char *orted_bin = (*env)->GetStringUTFChars(env, 
-	  orted_bin_str, 0);
 
-    	printf("JNI (C) OMPI: OMPIInit(%s)\n", orted_bin);
+    	printf("JNI (C) OMPI: OMPIInit()\n");
 	set_error("no error");
 	fflush(stdout);
-	(*env)->ReleaseStringUTFChars(env, orted_bin_str, orted_bin);
 
 	/* this makes the orte_init() below fail if the orte daemon
 	 * isn't running */
+	putenv("OMPI_MCA_orte_univ_exist=1");
+	/*
 	id = mca_base_param_register_int("orte", "univ", "exist",NULL,0);
 	mca_base_param_set_int(id, 1);
+	*/
 	
 	/* setup the runtime environment */
 	rc = orte_init();
@@ -68,7 +159,8 @@ Java_org_eclipse_ptp_rtsystem_ompi_OMPIControlSystem_OMPIInit(JNIEnv *env, jobje
 	    /* unreachable orted */
 	    set_error("ORTED unreachable!  Sorry!");
 	    printf("ORTED unreachable!  Sorry!\n");
-	    return 666;
+	    fflush(stdout);
+	    return 1;
 	}
 
 	/*
@@ -81,8 +173,8 @@ Java_org_eclipse_ptp_rtsystem_ompi_OMPIControlSystem_OMPIInit(JNIEnv *env, jobje
 	printf("Registry initted.\n");
 	fflush(stdout);
 
-	OBJ_CONSTRUCT(&eclipse_orte_lock, ompi_mutex_t); 
-	return 777;
+	OBJ_CONSTRUCT(&eclipse_orte_lock, opal_mutex_t); 
+	return 0;
 }
 
 JNIEXPORT void JNICALL 
@@ -90,9 +182,9 @@ Java_org_eclipse_ptp_rtsystem_ompi_OMPIControlSystem_OMPIFinalize(JNIEnv *env, j
 {
     	printf("JNI (C) OMPI: OMPIFinalize()\n");
 	fflush(stdout);
-	ompi_mutex_lock(&ompi_event_lock);
+	opal_mutex_lock(&opal_event_lock);
 	orte_finalize();
-	ompi_mutex_unlock(&ompi_event_lock);
+	opal_mutex_unlock(&opal_event_lock);
 
 	printf("Registry finalized.\n");
 	fflush(stdout);
@@ -104,9 +196,9 @@ Java_org_eclipse_ptp_rtsystem_ompi_OMPIControlSystem_OMPIProgress(JNIEnv *env, j
     	printf("JNI (C) OMPI: OMPIProgress() starting . . .\n");
 	fflush(stdout);
 
-	ompi_mutex_lock(&ompi_event_lock);
-	ompi_event_loop(0); 
-	ompi_mutex_unlock(&ompi_event_lock);
+	opal_mutex_lock(&opal_event_lock);
+	opal_event_loop(0); 
+	opal_mutex_unlock(&opal_event_lock);
 #if 0
 	while(1) {
 	    ompi_mutex_lock(&eclipse_orte_lock);
@@ -122,16 +214,50 @@ Java_org_eclipse_ptp_rtsystem_ompi_OMPIControlSystem_OMPIProgress(JNIEnv *env, j
 }
 
 JNIEXPORT void JNICALL 
-Java_org_eclipse_ptp_rtsystem_ompi_OMPIControlSystem_OMPIRun(JNIEnv *env, jobject obj) 
+Java_org_eclipse_ptp_rtsystem_ompi_OMPIControlSystem_OMPIRun(JNIEnv *env, jobject obj, jobjectArray array)
 {
 	printf("JNI (C) OMPI: OMPIRun() starting . . .\n");
 	fflush(stdout);
 
-	ompi_mutex_lock(&eclipse_orte_lock);
+	jstring jstr;
+	char *str;
+	int i;
+
+	jsize len = (*env)->GetArrayLength(env, array);
+	
+	for(i=0; i<len; i++) {
+	    jstr = (*env)->GetObjectArrayElement(env, array, i);
+	    str = (char *)(*env)->GetStringUTFChars(env, jstr, NULL);
+
+	    printf("string[%d] = %s\n", i, str);
+
+	    /* release str */
+	    (*env)->ReleaseStringUTFChars(env, jstr, str);
+	}
+
+	opal_mutex_lock(&eclipse_orte_lock);
 	ptp_ompi_spawn("/Users/ndebard/ompi-test/test-mpi", 2);
-	ompi_mutex_unlock(&eclipse_orte_lock); 
+	opal_mutex_unlock(&eclipse_orte_lock); 
 	
 	return;
+}
+
+JNIEXPORT void JNICALL 
+Java_org_eclipse_ptp_rtsystem_ompi_OMPIControlSystem_OMPIShutdown(JNIEnv *env, jobject obj)
+{
+    	int ret;
+
+    	printf("JNI (C) OMPI: OMPIShutdown()\n");
+	printf("PID = %d\n", pid);
+	printf("PGID = %d\n", pgid);
+	if(pid != -1) {
+	   // ret = killpg(pgid, SIGHUP);
+	    printf("return from killpg = %d\n", ret);
+	    if(ret == -1) {
+		printf("errormessage = %s\n", strerror(errno));
+	    }
+	}
+    	///ptp_ompi_sendcmd(ORTE_DAEMON_EXIT_CMD);
 }
 
 /**********************************************************************
@@ -205,11 +331,11 @@ int ptp_ompi_spawn(char *app, int num_procs)
 	fflush(stdout);
 
 	/* spawn the job */
-	ompi_mutex_lock(&ompi_event_lock);
+	opal_mutex_lock(&opal_event_lock);
 	rc = orte_rmgr.spawn(apps, num_apps, &jobid, job_state_callback);
-	ompi_mutex_unlock(&ompi_event_lock);
+	opal_mutex_unlock(&opal_event_lock);
 	if(rc != ORTE_SUCCESS) {
-	    ompi_output(0, "%s: failed with errno=%d\n", app,
+	    opal_output(0, "%s: failed with errno=%d\n", app,
 	      rc);
 	    return rc;
 	}
@@ -256,3 +382,32 @@ static void job_state_callback(orte_jobid_t jobid, orte_proc_state_t state)
 	}
 	fflush(stdout);
 }
+
+/*
+static void ptp_ompi_sendcmd(uint16_t usercmd)
+{
+	orte_buffer_t *cmd;
+	orte_daemon_cmd_flag_t command;
+	int rc;
+	orte_process_name_t seed={0,0,0};
+
+	cmd = OBJ_NEW(orte_buffer_t);
+	if (NULL == cmd) {
+	    fprintf(stderr, "console: comm failure\n");
+	    return;
+	}
+	command = usercmd;
+
+	if (ORTE_SUCCESS != (rc = orte_dps.pack(cmd, &command, 1, 
+	    ORTE_DAEMON_CMD))) 
+	{
+	    ORTE_ERROR_LOG(rc);
+	    return;
+	}
+	if (0 > orte_rml.send_buffer(&seed, cmd, ORTE_RML_TAG_DAEMON, 0)) {
+	    ORTE_ERROR_LOG(ORTE_ERR_COMM_FAILURE);
+	    return;
+	}
+	OBJ_RELEASE(cmd);
+}
+*/
