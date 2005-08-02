b166945cab3a1ccb71ed232eea422008143a7640
dkurniawan <dkurniawan>
2005-08-02 21:37:15 +0000

    DebugSimulator Enhancement : added a mini state machine

--- debug/org.eclipse.ptp.debug.sdm.core/simulator/org/eclipse/ptp/debug/external/simulator/SimThread.java
+++ debug/org.eclipse.ptp.debug.sdm.core/simulator/org/eclipse/ptp/debug/external/simulator/SimThread.java
@@ -1,16 +1,34 @@
 package org.eclipse.ptp.debug.external.simulator;
 
+import org.eclipse.ptp.debug.external.DebugSession;
+import org.eclipse.ptp.debug.external.event.EBreakpointHit;
+
 public class SimThread {
 
+	final int RUNNING = 10;
+	final int SUSPENDED = 11;
+	final int TERMINATED = 12;
+	
+	int state;
+	
 	int curLine;
 	int breakLine;
 	SimStackFrame[] stackFrames;
 	int threadId;
+	String processId;
 	
-	public SimThread(int id) {
+	DebugSimulator dSim;
+	DebugSession dSes;
+	
+	public SimThread(int id, String pId, DebugSimulator debugger, DebugSession dSession) {
+		dSim = debugger;
+		dSes = dSession;
+		
+		state = RUNNING;
 		curLine = 1;
 		breakLine = 0;
 		threadId = id;
+		processId = pId;
 		int numStackFrames = 1;
 		
 		stackFrames = new SimStackFrame[numStackFrames];
@@ -30,19 +48,23 @@ public class SimThread {
 	public SimStackFrame[] getStackFrames() {
 		return stackFrames;
 	}
-	
-	public void incrementCurrentLine() {
-		curLine++;
-		if (curLine == breakLine) {
-			// Do Something
+
+	public void runCommand(SimInputStream in, String cmd, String arg) {
+		if (cmd.equals("print")) {
+			checkBreakpoint();
+			in.printString(arg + " from process " + processId + " & thread " + threadId);
+			curLine++;
+		} else	if (cmd.equals("break")) {
+			breakLine = Integer.parseInt(arg);
 		}
 	}
 	
-	public int getCurrentLine() {
-		return curLine;
-	}
 	
-	public void setBreakLine(int l) {
-		breakLine = l;
+	public void checkBreakpoint() {
+		if (curLine == breakLine) {
+			state = SUSPENDED;
+			dSim.fireEvent(new EBreakpointHit(dSes));
+			// Do Something
+		}
 	}
 }
