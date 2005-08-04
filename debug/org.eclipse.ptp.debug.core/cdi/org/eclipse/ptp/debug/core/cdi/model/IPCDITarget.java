21d1509d57d5cacd149adfa0b25408c7733f2543
dkurniawan <dkurniawan>
2005-08-04 21:07:55 +0000

    Removed unnecessary interfaces

--- debug/org.eclipse.ptp.debug.core/cdi/org/eclipse/ptp/debug/core/cdi/model/IPCDITarget.java
+++ debug/org.eclipse.ptp.debug.core/cdi/org/eclipse/ptp/debug/core/cdi/model/IPCDITarget.java
@@ -1,13 +1,6 @@
 package org.eclipse.ptp.debug.core.cdi.model;
 
-import org.eclipse.cdt.debug.core.cdi.CDIException;
 import org.eclipse.cdt.debug.core.cdi.model.ICDITarget;
-import org.eclipse.cdt.debug.core.cdi.model.ICDIThread;
 
 public interface IPCDITarget extends ICDITarget {
-	
-	public Process[] getProcesses();
-	public Process getProcess(int num);
-	
-	public ICDIThread[] getThreads(int procNumber) throws CDIException;
 }
