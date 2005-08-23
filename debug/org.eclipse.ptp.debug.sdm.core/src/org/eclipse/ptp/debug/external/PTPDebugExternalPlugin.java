31ce5249aa8c6a14eb7bc7dfdf8ebec078eea477
gwatson <gwatson>
2005-08-23 01:05:24 +0000

    Debugger configuration page

--- debug/org.eclipse.ptp.debug.sdm.core/src/org/eclipse/ptp/debug/external/PTPDebugExternalPlugin.java
+++ debug/org.eclipse.ptp.debug.sdm.core/src/org/eclipse/ptp/debug/external/PTPDebugExternalPlugin.java
@@ -27,6 +27,10 @@ import java.util.*;
  * The main plugin class to be used in the desktop.
  */
 public class PTPDebugExternalPlugin extends AbstractUIPlugin {
+	/**
+	 * The plug-in identifier (value <code>"org.eclipse.ptp.debug.external.ui"</code>).
+	 */
+	public static final String PLUGIN_ID = "org.eclipse.ptp.debug.external.ui" ; //$NON-NLS-1$
 	//The shared instance.
 	private static PTPDebugExternalPlugin plugin;
 	//Resource bundle.
@@ -99,4 +103,20 @@ public class PTPDebugExternalPlugin extends AbstractUIPlugin {
 	public static ImageDescriptor getImageDescriptor(String path) {
 		return AbstractUIPlugin.imageDescriptorFromPlugin("org.eclipse.ptp.debug.external", path);
 	}
+	
+	/**
+	 * Convenience method which returns the unique identifier of this plugin.
+	 * 
+	 * @return the unique identifier of this plugin
+	 */
+	public static String getUniqueIdentifier() {
+		if ( getDefault() == null ) {
+			// If the default instance is not yet initialized,
+			// return a static identifier. This identifier must
+			// match the plugin id defined in plugin.xml
+			return PLUGIN_ID; //$NON-NLS-1$
+		}
+		return getDefault().getBundle().getSymbolicName();
+	}
+
 }
