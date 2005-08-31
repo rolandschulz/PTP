85b3ed51f89bbbc9fc32136a12c14bc34ec61be1
dkurniawan <dkurniawan>
2005-08-31 00:51:45 +0000

    Moved the breakpoint management to Session

--- debug/org.eclipse.ptp.debug.sdm.core/cdi/org/eclipse/ptp/debug/external/cdi/Session.java
+++ debug/org.eclipse.ptp.debug.sdm.core/cdi/org/eclipse/ptp/debug/external/cdi/Session.java
@@ -18,17 +18,29 @@
  *******************************************************************************/
 package org.eclipse.ptp.debug.external.cdi;
 
+import java.math.BigInteger;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Properties;
 
 import org.eclipse.cdt.core.IBinaryParser.IBinaryObject;
 import org.eclipse.cdt.debug.core.cdi.CDIException;
+import org.eclipse.cdt.debug.core.cdi.ICDIAddressLocation;
+import org.eclipse.cdt.debug.core.cdi.ICDICondition;
 import org.eclipse.cdt.debug.core.cdi.ICDIEventManager;
+import org.eclipse.cdt.debug.core.cdi.ICDIFunctionLocation;
+import org.eclipse.cdt.debug.core.cdi.ICDILineLocation;
 import org.eclipse.cdt.debug.core.cdi.ICDISession;
 import org.eclipse.cdt.debug.core.cdi.ICDISessionConfiguration;
 import org.eclipse.cdt.debug.core.cdi.ICDISessionObject;
+import org.eclipse.cdt.debug.core.cdi.model.ICDIAddressBreakpoint;
+import org.eclipse.cdt.debug.core.cdi.model.ICDIBreakpoint;
+import org.eclipse.cdt.debug.core.cdi.model.ICDIBreakpointManagement;
+import org.eclipse.cdt.debug.core.cdi.model.ICDIExceptionpoint;
+import org.eclipse.cdt.debug.core.cdi.model.ICDIFunctionBreakpoint;
+import org.eclipse.cdt.debug.core.cdi.model.ICDILineBreakpoint;
 import org.eclipse.cdt.debug.core.cdi.model.ICDITarget;
+import org.eclipse.cdt.debug.core.cdi.model.ICDIWatchpoint;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.DebugPlugin;
@@ -40,6 +52,7 @@ import org.eclipse.ptp.core.IPTPLaunchConfigurationConstants;
 import org.eclipse.ptp.debug.core.IPLaunch;
 import org.eclipse.ptp.debug.core.PCDIDebugModel;
 import org.eclipse.ptp.debug.core.cdi.IPCDISession;
+import org.eclipse.ptp.debug.core.cdi.model.IPCDIDebugProcessSet;
 import org.eclipse.ptp.debug.core.cdi.model.IPCDITarget;
 import org.eclipse.ptp.debug.external.IDebugger;
 import org.eclipse.ptp.debug.external.cdi.event.TargetRegisteredEvent;
@@ -47,7 +60,7 @@ import org.eclipse.ptp.debug.external.cdi.event.TargetUnregisteredEvent;
 import org.eclipse.ptp.debug.external.cdi.model.DebugProcessSet;
 import org.eclipse.ptp.debug.external.cdi.model.Target;
 
-public class Session implements IPCDISession, ICDISessionObject {
+public class Session implements IPCDISession, ICDISessionObject, ICDIBreakpointManagement {
 	EventManager eventManager;
 	BreakpointManager breakpointManager;
 	ExpressionManager expressionManager;
@@ -280,4 +293,77 @@ public class Session implements IPCDISession, ICDISessionObject {
 	public ICDISession getSession() {
 		return this;
 	}
+
+	/* Breakpoint Management */
+	
+	public ICDIWatchpoint setWatchpoint(int type, int watchType, String expression, ICDICondition condition) throws CDIException {
+		System.out.println("Session.setWatchpoint()");
+		return null;
+	}
+
+	public ICDIExceptionpoint setExceptionBreakpoint(String clazz, boolean stopOnThrow, boolean stopOnCatch) throws CDIException {
+		System.out.println("Session.setExceptionBreakpoint()");
+		return null;
+	}
+
+	public ICDIBreakpoint[] getBreakpoints() throws CDIException {
+		System.out.println("Session.getBreakpoints()");
+		return null;
+	}
+
+	public void deleteBreakpoints(ICDIBreakpoint[] breakpoints) throws CDIException {
+		System.out.println("Session.deleteBreakpoints()");
+	}
+
+	public void deleteAllBreakpoints() throws CDIException {
+		System.out.println("Session.deleteAllBreakpoints()");
+	}
+	
+	public ICDILineBreakpoint setLineBreakpoint(int type, ICDILineLocation location, ICDICondition condition, boolean deferred) throws CDIException {
+		IPCDIDebugProcessSet newSet = new DebugProcessSet(this, 0);
+		return setLineBreakpoint(newSet, type, location, condition, deferred);
+	}
+
+	public ICDIFunctionBreakpoint setFunctionBreakpoint(int type, ICDIFunctionLocation location, ICDICondition condition, boolean deferred) throws CDIException {
+		IPCDIDebugProcessSet newSet = new DebugProcessSet(this, 0);
+		return setFunctionBreakpoint(newSet, type, location, condition, deferred);
+	}
+
+	public ICDIAddressBreakpoint setAddressBreakpoint(int type, ICDIAddressLocation location, ICDICondition condition, boolean deferred) throws CDIException {
+		IPCDIDebugProcessSet newSet = new DebugProcessSet(this, 0);
+		return setAddressBreakpoint(newSet, type, location, condition, deferred);
+	}
+
+	public ICDILineBreakpoint setLineBreakpoint(IPCDIDebugProcessSet bSet, int type, ICDILineLocation location, ICDICondition condition, boolean deferred) throws CDIException {
+		BreakpointManager bMgr = ((Session)getSession()).getBreakpointManager();
+		return bMgr.setLineBreakpoint(bSet, type, location, condition, deferred);
+	}
+
+	public ICDIFunctionBreakpoint setFunctionBreakpoint(IPCDIDebugProcessSet bSet, int type, ICDIFunctionLocation location, ICDICondition condition, boolean deferred) throws CDIException {
+		BreakpointManager bMgr = ((Session)getSession()).getBreakpointManager();
+		return bMgr.setFunctionBreakpoint(bSet, type, location, condition, deferred);
+	}
+
+	public ICDIAddressBreakpoint setAddressBreakpoint(IPCDIDebugProcessSet bSet, int type, ICDIAddressLocation location, ICDICondition condition, boolean deferred) throws CDIException {
+		BreakpointManager bMgr = ((Session)getSession()).getBreakpointManager();
+		return bMgr.setAddressBreakpoint(bSet, type, location, condition, deferred);
+	}
+	
+	/* Location Management */
+	
+	public ICDILineLocation createLineLocation(String file, int line) {
+		BreakpointManager bMgr = ((Session)getSession()).getBreakpointManager();
+		return bMgr.createLineLocation(file, line);
+	}
+
+	public ICDIFunctionLocation createFunctionLocation(String file, String function) {
+		BreakpointManager bMgr = ((Session)getSession()).getBreakpointManager();
+		return bMgr.createFunctionLocation(file, function);
+	}
+
+	public ICDIAddressLocation createAddressLocation(BigInteger address) {
+		BreakpointManager bMgr = ((Session)getSession()).getBreakpointManager();
+		return bMgr.createAddressLocation(address);
+	}
+
 }
