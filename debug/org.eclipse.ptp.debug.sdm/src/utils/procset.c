2e276211730c9dcf5da09a0ddee051871d5a340a
gwatson <gwatson>
2005-09-14 21:52:42 +0000

    added conversion to/from strings

--- debug/org.eclipse.ptp.debug.sdm/src/utils/procset.c
+++ debug/org.eclipse.ptp.debug.sdm/src/utils/procset.c
@@ -17,8 +17,10 @@
  * LA-CC 04-115
  ******************************************************************************/
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "procset.h"
 
@@ -41,7 +43,10 @@ procset_free(procset *p)
 	BITVECTOR_FREE(p->ps_procs);
 	free(p);
 }
-	
+
+/**
+ * Add a process to the set. Processes are numbered from 0.
+ */
 void		
 procset_add_proc(procset *p, int proc)
 {
@@ -64,11 +69,17 @@ static char tohex[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B
 
 /**
  * Return a string representation of a procset. We use hex to compress
- * the string somewhat.
+ * the string somewhat and drop leading zeros.
+ * 
+ * Format is "nnnnnnnn:bbbbbbb..." where "nnnnnnnn" is a hex representation of the
+ * actual number of processes, and "bbbbb...." are bits representing processes in the
+ * set (in hex).
  */
 char *
 procset_to_str(procset *p)
 {
+	int				n;
+	int				nonzero = 0;
 	int				bytes;
 	int				pbit;
 	int				bit;
@@ -80,23 +91,84 @@ procset_to_str(procset *p)
 		return strdup("<null>");
 		
 	/*
-	 * Round nprocs up to nearest byte
+	 * Find out how many bytes needed (rounded up)
 	 */
 	bytes = (p->ps_nprocs >> 3) + 1;
+
+	str = (char *)malloc((bytes * 2) + 8 + 2);
 	
-	str = s = (char *)malloc((bytes * 2) + 1);
+	/*
+	 * Start with actual number of processes (silently truncate to 32 bits)
+	 */
+	n = sprintf(str, "%x", p->ps_nprocs & 0xffff);
 	
-	for (pbit = bytes << 3; pbit >= 7; ) {
-		for (byte = 0, bit = 7; bit >= 0; bit--, pbit--) {
-			if (pbit <= p->ps_nprocs && BITVECTOR_GET(p->ps_procs, pbit))
-				byte |= (1 << bit);			
+	s = str + n;
+	
+	*s++ = ':';
+	
+	for (pbit = (bytes << 3) - 1; pbit > 0; ) {
+		for (byte = 0, bit = 3; bit >= 0; bit--, pbit--) {
+			if (pbit <= p->ps_nprocs && BITVECTOR_GET(p->ps_procs, pbit)) {
+				byte |= (1 << bit);
+				nonzero = 1;
+			}
 		}
 		
-		*s++ = tohex[(byte >> 4) & 0x0f];
-		*s++ = tohex[byte & 0x0f];
+		if (nonzero) {
+			*s++ = tohex[byte & 0x0f];
+		}
 	}
 	
 	*s = '\0';
 	
 	return str;
+}
+
+/**
+ * Convert string into a procset. Inverse of procset_to_str().
+ * 
+ */
+procset *
+str_to_procset(char *str)
+{
+	int			nprocs;
+	int			n;
+	int			pos;
+	int			b;
+	char *		end;
+	procset *	p;
+	
+	if (str == NULL)
+		return NULL;
+		
+	end = &str[strlen(str) - 1];
+	
+	for (nprocs = 0; *str != ':' && *str != '\0' && isxdigit(*str); str++) {
+		nprocs <<= 4;
+		nprocs += digittoint(*str);
+	}
+	
+	if (*str != ':')
+		return NULL;
+		
+	p = procset_new(nprocs);
+	
+	/*
+	 * Easier if we start from end
+	 */
+	for (pos = 0; end != str && isxdigit(*end); end--) {
+		b = digittoint(*end);
+		for (n = 0; n < 4; n++, pos++) {
+			if (b & (1 << n)) {
+				procset_add_proc(p, pos);
+			}
+		}
+	}
+	
+	if (end != str) {
+		procset_free(p);
+		return NULL;
+	}
+	
+	return p;
 }
