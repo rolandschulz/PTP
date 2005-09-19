e94718355fda378c25f7d34b0a6ccdbe6b65c58d
gwatson <gwatson>
2005-09-19 20:34:00 +0000

    Common error code

---
+++ debug/org.eclipse.ptp.debug.sdm/src/utils/error.c
@@ -0,0 +1,59 @@
+/******************************************************************************
+ * Copyright (c) 2005 The Regents of the University of California. 
+ * This material was produced under U.S. Government contract W-7405-ENG-36 
+ * for Los Alamos National Laboratory, which is operated by the University 
+ * of California for the U.S. Department of Energy. The U.S. Government has 
+ * rights to use, reproduce, and distribute this software. NEITHER THE 
+ * GOVERNMENT NOR THE UNIVERSITY MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR 
+ * ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE. If software is modified 
+ * to produce derivative works, such modified software should be clearly 
+ * marked, so as not to confuse it with the version available from LANL.
+ * 
+ * Additionally, this program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * LA-CC 04-115
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "dbg.h"
+
+static int 			dbg_errno = DBGRES_OK;
+static char *		dbg_errstr = NULL;
+
+/*
+ * Error handling
+ */
+void
+DbgSetError(int errnum, char *msg)
+{
+	dbg_errno = errnum;
+	
+	if (dbg_errstr != NULL) {
+		free(dbg_errstr);
+		dbg_errstr = NULL;
+	}
+	
+	if (msg != NULL)
+		dbg_errstr = strdup(msg);
+}
+
+int
+DbgGetError(void)
+{
+	return dbg_errno;
+}
+
+char *
+DbgGetErrorStr(void)
+{
+	if (dbg_errstr == NULL)
+		return "";
+		
+	return dbg_errstr;
+}
