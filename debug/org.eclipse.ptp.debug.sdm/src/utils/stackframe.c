8265ad6c361fc402d9f30cc25e52593254e50505
Greg Watson <g.watson@computer.org>
2005-09-20 20:58:16 +0000

    stackframe support

---
+++ debug/org.eclipse.ptp.debug.sdm/src/utils/stackframe.c
@@ -0,0 +1,48 @@
+/******************************************************************************
+ * Copyright (c) 2005 The Regents of the University of California. 
+ * This material was produced under U.S. Government contract W-7405-ENG-36 
+ * for Los Alamos National Laboratory, which is operated by the University 
+ * of California for the U.S. Department of Energy. The U.S. Government has 
+ * rights to use, reproduce, and distribute this software. NEITHER THE 
+ * GOVERNMENT NOR THE UNIVERSITY MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR 
+ * ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE. If software is modified 
+ * to produce derivative works, such modified software should be clearly 
+ * marked, so as not to confuse it with the version available from LANL.
+ * 
+ * Additionally, this program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * LA-CC 04-115
+ ******************************************************************************/
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "stackframe.h"
+
+stackframe *	
+NewStackframe(int level) {
+	stackframe *	sf = (stackframe *)malloc(sizeof(stackframe));
+	
+	sf->level = level;
+	sf->loc.file = NULL;
+	sf->loc.func = NULL;
+	sf->loc.addr = NULL;
+	sf->loc.line = 0;
+	
+	return sf;
+}
+
+void	
+FreeStackframe(stackframe *sf) {
+	if (sf->loc.file != NULL)
+		free(sf->loc.file);
+	if (sf->loc.func != NULL)
+		free(sf->loc.func);
+	if (sf->loc.addr != NULL)
+		free(sf->loc.addr);
+		
+	free(sf);
+}
