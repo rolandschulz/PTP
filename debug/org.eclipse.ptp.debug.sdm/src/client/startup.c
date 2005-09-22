9e13d7f10cde39f2aec5cf262b9a5c4d56c5838c
Greg Watson <g.watson@computer.org>
2005-09-22 23:58:43 +0000

    added command processing and debug backend

--- debug/org.eclipse.ptp.debug.sdm/src/client/startup.c
+++ debug/org.eclipse.ptp.debug.sdm/src/client/startup.c
@@ -18,17 +18,67 @@
  ******************************************************************************/
 
 #include <mpi.h>
+#include	<getopt.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#include "backend.h"
+
+#define DEFAULT_BACKEND	"gdbmi"
 
 extern void client(int);
-extern void server(int, int);
+extern void server(int, int, dbg_backend *);
 
-MPI_Comm mcast_comm;
+static struct option longopts[] = {
+	{"debugger",	required_argument,	NULL,	'b'},
+	{"port",		required_argument,	NULL, 	'p'}, 
+	{NULL,		0,					NULL,	0}
+};
 
+void
+error_msg(int rank, char *fmt, ...)
+{
+	va_list	ap;
+	
+	if (rank != 0)
+		return;
+		
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+}
+
+/*
+ * Main entry point for MPI parallel debugger.
+ * 
+ * @arg	-b debugger	select backend debugger
+ * @arg	-p port		port number to listen on
+ */
 int
 main(int argc, char *argv[])
 {
-	int rank;
-	int size;
+	int 				rank;
+	int 				size;
+	int				ch;
+	int				port;
+	char *			debugger = DEFAULT_BACKEND;
+	dbg_backend *	d;
+	
+	while ((ch = getopt_long(argc, argv, "b:p:", longopts, NULL)) != -1)
+	switch (ch) {
+	case 'b':
+		debugger = optarg;
+		break;
+	case 'p':
+		port = atoi(optarg);
+		break;
+	default:
+		fprintf(stderr, "sdm [--debugger=value] [--port=list-port]\n");
+		exit(1);
+	}
+	
+	argc -= optind;
+	argv += optind;
 	
 	MPI_Init(&argc, &argv);
 	
@@ -40,10 +90,16 @@ main(int argc, char *argv[])
 	
 	/* Create multicast communicator */
 	
+	if (find_dbg_backend(debugger, &d) < 0) {
+		error_msg(rank, "No such backend: \"%s\"\n", debugger);
+		MPI_Finalize();
+		return 1;
+	}
+	
 	if (rank == size-1) {
 		client(rank);
 	} else {
-		server(size - 1, rank);
+		server(size - 1, rank, d);
 	}
 	
 	MPI_Finalize();
