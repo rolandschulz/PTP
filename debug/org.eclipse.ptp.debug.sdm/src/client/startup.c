f5c30b24a0620e26ee89d6079b1e3d4fca932795
Greg Watson <g.watson@computer.org>
2005-09-24 22:22:14 +0000

    proxy support

--- debug/org.eclipse.ptp.debug.sdm/src/client/startup.c
+++ debug/org.eclipse.ptp.debug.sdm/src/client/startup.c
@@ -23,14 +23,17 @@
 #include <stdarg.h>
 
 #include "backend.h"
+#include "proxy.h"
 
-#define DEFAULT_BACKEND	"gdbmi"
+#define DEFAULT_BACKEND	"gdb-mi"
+#define DEFAULT_PROXY		"tcp"
 
-extern void client(int);
+extern void client(int, int, proxy *);
 extern void server(int, int, dbg_backend *);
 
 static struct option longopts[] = {
 	{"debugger",	required_argument,	NULL,	'b'},
+	{"proxy",	required_argument,	NULL, 	'P'}, 
 	{"port",		required_argument,	NULL, 	'p'}, 
 	{NULL,		0,					NULL,	0}
 };
@@ -61,19 +64,24 @@ main(int argc, char *argv[])
 	int 				size;
 	int				ch;
 	int				port;
-	char *			debugger = DEFAULT_BACKEND;
+	char *			debugger_str = DEFAULT_BACKEND;
+	char *			proxy_str = DEFAULT_PROXY;
+	proxy *			p;
 	dbg_backend *	d;
 	
-	while ((ch = getopt_long(argc, argv, "b:p:", longopts, NULL)) != -1)
+	while ((ch = getopt_long(argc, argv, "b:P:p:", longopts, NULL)) != -1)
 	switch (ch) {
 	case 'b':
-		debugger = optarg;
+		debugger_str = optarg;
+		break;
+	case 'P':
+		proxy_str = optarg;
 		break;
 	case 'p':
 		port = atoi(optarg);
 		break;
 	default:
-		fprintf(stderr, "sdm [--debugger=value] [--port=list-port]\n");
+		fprintf(stderr, "sdm [--debugger=value] [--proxy=proxy] [--port=list-port]\n");
 		exit(1);
 	}
 	
@@ -96,14 +104,20 @@ main(int argc, char *argv[])
 	
 	/* Create multicast communicator */
 	
-	if (find_dbg_backend(debugger, &d) < 0) {
-		error_msg(rank, "No such backend: \"%s\"\n", debugger);
+	if (find_dbg_backend(debugger_str, &d) < 0) {
+		error_msg(rank, "No such backend: \"%s\"\n", debugger_str);
+		MPI_Finalize();
+		return 1;
+	}
+	
+	if (find_proxy(proxy_str, &p) < 0) {
+		error_msg(rank, "No such proxy: \"%s\"\n", proxy_str);
 		MPI_Finalize();
 		return 1;
 	}
 	
 	if (rank == size-1) {
-		client(rank);
+		client(size - 1, rank, p);
 	} else {
 		server(size - 1, rank, d);
 	}
