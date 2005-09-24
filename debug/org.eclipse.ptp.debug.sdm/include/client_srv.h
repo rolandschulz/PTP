495f1c9e7f7da3f3fc11040f7f308817c1b85d13
gwatson <gwatson>
2005-09-24 21:53:06 +0000

    prototypes for client/server commands

---
+++ debug/org.eclipse.ptp.debug.sdm/include/client_srv.h
@@ -0,0 +1,31 @@
+/******************************************************************************
+ * Copyright (c) 2005 The Regents of the University of California.
+ * This material was produced under U.S. Government contract W-7405-ENG-36
+ * for Los Alamos National Laboratory, which is operated by the University
+ * of California for the U.S. Department of Energy. The U.S. Government has
+ * rights to use, reproduce, and distribute this software. NEITHER THE
+ * GOVERNMENT NOR THE UNIVERSITY MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR
+ * ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE. If software is modified
+ * to produce derivative works, such modified software should be clearly  
+ * marked, so as not to confuse it with the version available from LANL.
+ *
+ * Additionally, this program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * LA-CC 04-115
+ ******************************************************************************/
+ 
+#ifndef _CLIENT_SRV_H_
+#define _CLIENT_SRV_H_
+
+#include "procset.h"
+#include "dbg_event.h"
+
+void ClntRegisterCallback(void (*)(dbg_event *, void *));
+void ClntInit(int);
+int ClntSendCommand(procset *, char *, char *);
+int ClntProgressCmds(void);
+
+#endif /* _CLIENT_SRV_H_ */
