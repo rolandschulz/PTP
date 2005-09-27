b53663258b74656846708b30fd3655ebdeaf2d8d
gwatson <gwatson>
2005-09-27 18:20:42 +0000

    linux compatibility

---
+++ debug/org.eclipse.ptp.debug.sdm/src/utils/compat.c
@@ -0,0 +1,37 @@
+/******************************************************************************
+ * Copyright (c) 2005 The Regents of the University of California. 
+ * This material was produced under U.S. Government contract W-7405-ENG-36 
+ * for Los Alamos National Laboratory, which is operated by the University 
+ * of California for the U.S. Department of Energy. The U.S. Government has 
+ * rights to use, reproduce, and distribute this software. NEITHER THE 
+ * GOVERNMENT NOR THE UNIVERSITY MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR 
+ * ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE. If software is modified 
+ * to produce derivative works, such modified software should be clearly 
+ * marked, so as not to confuse it with the version available from LANL.
+ * 
+ * Additionally, this program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * LA-CC 04-115
+ ******************************************************************************/
+
+#ifdef __linux__
+#include <ctype.h>
+
+int
+digittoint(int c)
+{
+	if (c >= '0' && c <= '9')
+		return c - '0';
+		
+	if (c >= 'a' && c <= 'f')
+		return c - 'a';
+		
+	if (c >= 'A' && c <= 'F')
+		return c - 'A';
+	
+	return 0;
+}
+#endif /* __linux__ */
