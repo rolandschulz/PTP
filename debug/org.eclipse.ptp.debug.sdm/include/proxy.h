bd7c1838e5626d0aad43c7079197d00405d59592
gwatson <gwatson>
2005-09-27 17:04:47 +0000

    interace changes

--- debug/org.eclipse.ptp.debug.sdm/include/proxy.h
+++ debug/org.eclipse.ptp.debug.sdm/include/proxy.h
@@ -42,9 +42,8 @@ struct proxy_clnt_funcs {
 	 * Service functions
 	 */
 	int (*init)(struct proxy_clnt_helper_funcs *, void **, char *, va_list);
-	int (*connect)(struct proxy *, void *);
+	int (*connect)(void *);
 	int (*accept)(void *);
-	void (*regeventhandler)(void *, void (*)(dbg_event *, void *), void *);
 	int (*progress)(void *);
 	
 	/*
@@ -99,10 +98,11 @@ struct proxy_svr_helper_funcs {
 typedef struct proxy_svr_helper_funcs	proxy_svr_helper_funcs;
 
 struct proxy_svr_funcs {
-	int (*create)(struct proxy_svr_helper_funcs *, int, void **);
-	int (*connect)(struct proxy_svr_helper_funcs *, char *, int, void **);
-	int (*progress)(struct proxy_svr_helper_funcs *, void *);
-	void (*finish)(struct proxy_svr_helper_funcs *, void *);
+	void (*init)(struct proxy_svr_helper_funcs *, void **);
+	int (*create)(int, void *);
+	int (*connect)(char *, int, void *);
+	int (*progress)(void *);
+	void (*finish)(void *);
 };
 typedef struct proxy_svr_funcs	proxy_svr_funcs;
 
@@ -118,26 +118,30 @@ typedef struct proxy	proxy;
 extern proxy 	proxies[];
 
 extern int	find_proxy(char *, proxy **);
-extern void	proxy_svr_init(proxy *, proxy_svr_helper_funcs *);
-extern int	proxy_svr_create(proxy *, int, void **);
-extern int	proxy_svr_connect(proxy *, char *, int, void **);
+
+extern void	proxy_svr_init(proxy *, proxy_svr_helper_funcs *, void **);
+extern int	proxy_svr_create(proxy *, int, void *);
+extern int	proxy_svr_connect(proxy *, char *, int, void *);
 extern int	proxy_svr_progress(proxy *, void *);
 extern void	proxy_svr_finish(proxy *, void *);
 
-extern int proxy_clnt_init_not_imp(void **, char *, va_list);
-extern int proxy_clnt_setlinebreakpoint_not_imp(void *, struct procset *, char *, int);
-extern int proxy_clnt_setfuncbreakpoint_not_imp(void *, struct procset *, char *, char *);
-extern int proxy_clnt_deletebreakpoint_not_imp(void *, struct procset *, int);
-extern int proxy_clnt_go_not_imp(void *, struct procset *);
-extern int proxy_clnt_step_not_imp(void *, struct procset *, int, int);
-extern int proxy_clnt_liststackframes_not_imp(void *, struct procset *, int);
-extern int proxy_clnt_setcurrentstackframe_not_imp(void*, struct procset *, int);
-extern int proxy_clnt_evaluateexpression_not_imp(void *, struct procset *, char *);
-extern int proxy_clnt_gettype_not_imp(void *, struct procset *, char *);
-extern int proxy_clnt_listlocalvariables_not_imp(void *, struct procset *);
-extern int proxy_clnt_listarguments_not_imp(void *, struct procset *);
-extern int proxy_clnt_listglobalvariables_not_imp(void *, struct procset *);
-extern int proxy_clnt_quit_not_imp(void *);
-extern int proxy_clnt_progress_not_imp(void *, void (*)(dbg_event *));
+extern int proxy_clnt_init(proxy *, proxy_clnt_helper_funcs *, void **, char *, va_list);
+extern int proxy_clnt_connect(proxy *, void *);
+extern int proxy_clnt_progress(proxy *, void *);
+extern int proxy_clnt_startsession(proxy *, void *, char *, char *);
+extern int proxy_clnt_setlinebreakpoint(proxy *, void *, struct procset *, char *, int);
+extern int proxy_clnt_setfuncbreakpoint(proxy *, void *, struct procset *, char *, char *);
+extern int proxy_clnt_deletebreakpoint(proxy *, void *, struct procset *, int);
+extern int proxy_clnt_go(proxy *, void *, struct procset *);
+extern int proxy_clnt_step(proxy *, void *, struct procset *, int, int);
+extern int proxy_clnt_liststackframes(proxy *, void *, struct procset *, int);
+extern int proxy_clnt_setcurrentstackframe(proxy *, void*, struct procset *, int);
+extern int proxy_clnt_evaluateexpression(proxy *, void *, struct procset *, char *);
+extern int proxy_clnt_gettype(proxy *, void *, struct procset *, char *);
+extern int proxy_clnt_listlocalvariables(proxy *, void *, struct procset *);
+extern int proxy_clnt_listarguments(proxy *, void *, struct procset *);
+extern int proxy_clnt_listglobalvariables(proxy *, void *, struct procset *);
+extern int proxy_clnt_quit(proxy *, void *);
+
 
 #endif /* _PROXY_H_*/
