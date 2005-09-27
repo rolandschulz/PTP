2aa36bc80d5601bb22133f79bba39ce08aa2a3c3
Greg Watson <g.watson@computer.org>
2005-09-27 17:44:20 +0000

    fix autogen so it does the right thing

--- debug/org.eclipse.ptp.debug.sdm/autogen.sh
+++ debug/org.eclipse.ptp.debug.sdm/autogen.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
-aclocal
-automake
-autoconf
+ACLOCAL=aclocal
+AUTOMAKE=automake
+AUTOCONF=autoconf
+
+autogen()
+{
+	echo "autogen in $1"
+	$ACLOCAL
+	$AUTOMAKE
+	$AUTOCONF
+}
+	
+DIRS=". bitvector"
+
+for dir in $DIRS
+do
+	(cd $dir; autogen $dir)
+done
+
+./configure
+
+exit 0
