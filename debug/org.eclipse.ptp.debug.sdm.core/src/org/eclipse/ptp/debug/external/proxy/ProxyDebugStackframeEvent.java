1b09776a574948fd0a7684b6926e1c3499650470
gwatson <gwatson>
2005-10-07 03:25:49 +0000

    stackframe support

--- debug/org.eclipse.ptp.debug.sdm.core/src/org/eclipse/ptp/debug/external/proxy/ProxyDebugStackframeEvent.java
+++ debug/org.eclipse.ptp.debug.sdm.core/src/org/eclipse/ptp/debug/external/proxy/ProxyDebugStackframeEvent.java
@@ -1,23 +1,32 @@
 package org.eclipse.ptp.debug.external.proxy;
 
-import org.eclipse.cdt.debug.core.cdi.model.ICDIStackFrame;
 import org.eclipse.ptp.core.proxy.event.AbstractProxyEvent;
 import org.eclipse.ptp.core.proxy.event.IProxyEvent;
 import org.eclipse.ptp.core.util.BitList;
 
 public class ProxyDebugStackframeEvent extends AbstractProxyEvent implements IProxyEvent {
-	private ICDIStackFrame[]	frames;
+	private ProxyDebugStackframe[] frames;
 	
-	public ProxyDebugStackframeEvent(BitList set, ICDIStackFrame[] frames) {
+	public ProxyDebugStackframeEvent(BitList set, ProxyDebugStackframe[] frames) {
 		super(EVENT_DBG_FRAMES, set);
 		this.frames = frames;
 	}
 	
-	public ICDIStackFrame[] getStackframes() {
+	public ProxyDebugStackframe[] getFrames() {
 		return this.frames;
 	}
 	
 	public String toString() {
-		return "EVENT_DBG_FRAMES " + this.getBitSet().toString() + " <" + frames.length + " frames>";
+		String res = "EVENT_DBG_FRAMES " + this.getBitSet().toString();
+		for (int i = 0; i < frames.length; i++) {
+			res += "\n " + frames[i].getLevel() + " file=\"" + frames[i].getFile() + "\"";
+			if (frames[i].getFunc().compareTo("") != 0)
+				res += " func=" + frames[i].getFunc();
+			if (frames[i].getLine() != 0)
+				res += " line=" + frames[i].getLine();
+			if (frames[i].getAddr().compareTo("") != 0)
+				res += " addr=" + frames[i].getAddr();	
+		}
+		return res;
 	}
 }
