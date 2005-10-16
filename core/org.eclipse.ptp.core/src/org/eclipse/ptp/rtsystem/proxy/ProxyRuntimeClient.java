6054509ff76db05bd565c7cc30db019924c162c4
Greg Watson <g.watson@computer.org>
2005-10-16 16:01:11 +0000

    start of proxy runtime protocol

--- core/org.eclipse.ptp.core/src/org/eclipse/ptp/rtsystem/proxy/ProxyRuntimeClient.java
+++ core/org.eclipse.ptp.core/src/org/eclipse/ptp/rtsystem/proxy/ProxyRuntimeClient.java
@@ -17,18 +17,13 @@
  * LA-CC 04-115
  *******************************************************************************/
 
-package org.eclipse.ptp.internal.core.proxy;
+package org.eclipse.ptp.rtsystem.proxy;
 
 import org.eclipse.ptp.core.proxy.AbstractProxyClient;
-import org.eclipse.ptp.core.proxy.event.IProxyEvent;
 
 public class ProxyRuntimeClient extends AbstractProxyClient {
 
 	public ProxyRuntimeClient(String host, int port) {
 		super(host, port);
-	}
-
-	public IProxyEvent convertEvent(String str) {
-		return null;
 	}
 }
---
+++ core/org.eclipse.ptp.core/src/org/eclipse/ptp/rtsystem/proxy/event/ProxyRuntimeEvent.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2005 The Regents of the University of California. 
+ * This material was produced under U.S. Government contract W-7405-ENG-36 
+ * for Los Alamos National Laboratory, which is operated by the University 
+ * of California for the U.S. Department of Energy. The U.S. Government has 
+ * rights to use, reproduce, and distribute this software. NEITHER THE 
+ * GOVERNMENT NOR THE UNIVERSITY MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR 
+ * ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE. If software is modified 
+ * to produce derivative works, such modified software should be clearly marked, 
+ * so as not to confuse it with the version available from LANL.
+ * 
+ * Additionally, this program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * LA-CC 04-115
+ *******************************************************************************/
+
+package org.eclipse.ptp.rtsystem.proxy.event;
+
+import org.eclipse.ptp.core.proxy.event.IProxyEvent;
+import org.eclipse.ptp.core.proxy.event.ProxyErrorEvent;
+import org.eclipse.ptp.core.proxy.event.ProxyEvent;
+
+public class ProxyRuntimeEvent extends ProxyEvent {
+	public static IProxyEvent toEvent(String str) {
+		IProxyRuntimeEvent evt = null;
+		String[] args = str.split(" ");
+		
+		int type = Integer.parseInt(args[0]);
+		
+		switch (type) {
+		case IProxyEvent.EVENT_OK:
+			evt = new ProxyRuntimeOKEvent();
+			break;
+			
+		case IProxyEvent.EVENT_ERROR:
+			int errCode = Integer.parseInt(args[1]);
+			evt = new ProxyRuntimeErrorEvent(errCode, decodeString(args[2]));
+			break;
+			
+		default:
+			evt = new ProxyRuntimeErrorEvent(ProxyErrorEvent.EVENT_ERR_EVENT, "Invalid event type");
+			break;
+		}
+		
+		return evt;
+	}
+}
