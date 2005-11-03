1c7aa4bac3e9478ed0c617bb7a46b4bdd889ef84
Kam Man Chu <clement.chu@gmail.com>
2005-11-03 01:00:05 +0000

    finished the simulator2

--- debug/org.eclipse.ptp.debug.sdm.core/simulator/org/eclipse/ptp/debug/external/simulator2/DebugSimulation2.java
+++ debug/org.eclipse.ptp.debug.sdm.core/simulator/org/eclipse/ptp/debug/external/simulator2/DebugSimulation2.java
@@ -16,15 +16,16 @@
  * 
  * LA-CC 04-115
  *******************************************************************************/
-package org.eclipse.ptp.debug.external.simulator;
+package org.eclipse.ptp.debug.external.simulator2;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Random;
 import org.eclipse.cdt.debug.core.cdi.model.ICDIArgument;
 import org.eclipse.cdt.debug.core.cdi.model.ICDIBreakpoint;
 import org.eclipse.cdt.debug.core.cdi.model.ICDIFunctionBreakpoint;
@@ -32,49 +33,82 @@ import org.eclipse.cdt.debug.core.cdi.model.ICDIGlobalVariable;
 import org.eclipse.cdt.debug.core.cdi.model.ICDILineBreakpoint;
 import org.eclipse.cdt.debug.core.cdi.model.ICDILocalVariable;
 import org.eclipse.cdt.debug.core.cdi.model.ICDIStackFrame;
+import org.eclipse.cdt.debug.core.cdi.model.ICDITarget;
+import org.eclipse.cdt.debug.core.cdi.model.ICDIThread;
 import org.eclipse.ptp.core.IPJob;
 import org.eclipse.ptp.core.util.BitList;
 import org.eclipse.ptp.debug.core.IDebugger;
 import org.eclipse.ptp.debug.core.cdi.PCDIException;
 import org.eclipse.ptp.debug.external.AbstractDebugger;
+import org.eclipse.ptp.debug.external.cdi.model.StackFrame;
+import org.eclipse.ptp.debug.external.cdi.model.Target;
+import org.eclipse.ptp.debug.external.cdi.model.variable.Argument;
+import org.eclipse.ptp.debug.external.cdi.model.variable.LocalVariable;
 
 /**
  * @author Clement chu
  * 
  */
 public class DebugSimulation2 extends AbstractDebugger implements IDebugger, Observer {
-	private final boolean EVENT_BY_EACH_PROC = false;
-	private static final String HIT_BPT_STATE = "HIT_BPT";
-	private static final String EXIT_STATE = "EXIT";
-	private static final String STEP_END_STATE = "STEP_END";
+	public static final String HIT_BPT_STATE = "HIT_BPT";
+	public static final String EXIT_STATE = "EXIT";
+	public static final String STEP_END_STATE = "STEP_END";
 	private static final String APP_NAME = "../main.c";
+	private final boolean EVENT_BY_EACH_PROC = false;
 	private List sim_list = new ArrayList();
-	private final long time_range = 5;
-	private long current_time = 0;
+	private final long TIME_RANGE = 10;
 	private int total_process = 0;
-	private Thread timer = null;
-	private boolean running_timer = true;
 	private InternalEventQueue intQueue = null;
+	private Map variables = new HashMap();
+	private Map arguments = new HashMap();
 
 	public DebugSimulation2() {
-		intQueue = new InternalEventQueue();
+		intQueue = new InternalEventQueue(TIME_RANGE);
+		intQueue.addObserver(this);
+		createArguments();
+		createVariables();
 	}
+	private void createArguments() {
+		arguments.put("argc", new SimVariable("argc", "char **", ""));
+		arguments.put("argv", new SimVariable("argv", "int", "-"));
+	}
+	private void createVariables() {
+		variables.put("name", new SimVariable("name", "string", "hello"));
+		variables.put("number", new SimVariable("number", "int", "101"));
+	}
+	private SimVariable findVariable(String name) {
+		return (SimVariable)variables.get(name);
+	}
+	private SimVariable findArgument(String name) {
+		return (SimVariable)arguments.get(name);
+	}
+	private SimVariable[] getArguments() {
+		return (SimVariable[])arguments.values().toArray(new SimVariable[0]);
+	}
+	private SimVariable[] getVariables() {
+		return (SimVariable[])variables.values().toArray(new SimVariable[0]);
+	}
+	
 	public void startDebugger(IPJob job) {
 		total_process = job.size();
 		for (int i = 0; i < total_process; i++) {
-			SimulateProgram sim_program = new SimulateProgram(i);
+			SimulateProgram sim_program = new SimulateProgram(i, APP_NAME);
 			sim_program.addObserver(DebugSimulation2.this);
 			sim_list.add(sim_program);
 			sim_program.startProgram();
 		}
-		start_time_range();
+		if (!EVENT_BY_EACH_PROC)
+			intQueue.startTimer();
 	}
 	public void stopDebugger() {
-		stop_time_range();
+		if (!EVENT_BY_EACH_PROC)
+			intQueue.stopTimer();
+
 		for (Iterator i = sim_list.iterator(); i.hasNext();) {
 			SimulateProgram sim_program = (SimulateProgram) i.next();
 			sim_program.deleteObservers();
 		}
+		intQueue.deleteObservers();
 		sim_list.clear();
 	}
 	/***************************************************************************************************************************************************************************************************
@@ -101,7 +135,7 @@ public class DebugSimulation2 extends AbstractDebugger implements IDebugger, Obs
 		new Thread(new Runnable() {
 			public void run() {
 				for (int i=0; i<taskArray.length; i++) {
-					((SimulateProgram)sim_list.get(taskArray[i])).go();
+					getSimProg(taskArray[i]).go();
 				}
 			}
 		}).start();
@@ -111,7 +145,7 @@ public class DebugSimulation2 extends AbstractDebugger implements IDebugger, Obs
 		new Thread(new Runnable() {
 			public void run() {
 				for (int i=0; i<taskArray.length; i++) {
-					((SimulateProgram)sim_list.get(taskArray[i])).stopProgram();
+					getSimProg(taskArray[i]).stopProgram();
 				}
 			}
 		}).start();
@@ -121,7 +155,7 @@ public class DebugSimulation2 extends AbstractDebugger implements IDebugger, Obs
 		new Thread(new Runnable() {
 			public void run() {
 				for (int i=0; i<taskArray.length; i++) {
-					((SimulateProgram)sim_list.get(taskArray[i])).suspend();
+					getSimProg(taskArray[i]).suspend();
 				}
 			}
 		}).start();
@@ -131,7 +165,7 @@ public class DebugSimulation2 extends AbstractDebugger implements IDebugger, Obs
 		new Thread(new Runnable() {
 			public void run() {
 				for (int i=0; i<taskArray.length; i++) {
-					((SimulateProgram)sim_list.get(taskArray[i])).stepLine();
+					getSimProg(taskArray[i]).stepLine();
 				}
 			}
 		}).start();
@@ -141,7 +175,7 @@ public class DebugSimulation2 extends AbstractDebugger implements IDebugger, Obs
 		new Thread(new Runnable() {
 			public void run() {
 				for (int i=0; i<taskArray.length; i++) {
-					((SimulateProgram)sim_list.get(taskArray[i])).stepOverLine();
+					getSimProg(taskArray[i]).stepOverLine();
 				}
 			}
 		}).start();
@@ -177,30 +211,71 @@ public class DebugSimulation2 extends AbstractDebugger implements IDebugger, Obs
 	}
 	public ICDIStackFrame[] listStackFrames(BitList tasks) throws PCDIException {
 		int[] taskArray = tasks.toArray();
+		List frameList = new ArrayList();
 		for (int i=0; i<taskArray.length; i++) {
-			//((SimulateProgram)sim_list.get(taskArray[i])).stopProgram();
+			ICDITarget target = getSession().getTarget(taskArray[i]);
+		    ICDIThread thread = new org.eclipse.ptp.debug.external.cdi.model.Thread((Target) target, 0);
+		    SimulateFrame[] frames = getSimProg(taskArray[i]).getSimStackFrames();
+		    for (int j=0; j<frames.length; j++) {
+		    	frameList.add(new StackFrame((org.eclipse.ptp.debug.external.cdi.model.Thread) thread, frames[j].getLevel(), frames[j].getFile(), frames[j].getFunc(), frames[j].getLine(), frames[j].getAddr()));
+		    }
 		}
-		throw new PCDIException(PCDIException.NOT_IMPLEMENTED, "listStackFrames");
+		return (ICDIStackFrame[]) frameList.toArray(new ICDIStackFrame[0]);
 	}
 	
 	public void setCurrentStackFrame(BitList tasks, ICDIStackFrame frame) throws PCDIException {
-		int[] taskArray = tasks.toArray();
-		for (int i=0; i<taskArray.length; i++) {
-			//((SimulateProgram)sim_list.get(taskArray[i])).stopProgram();
-		}
-		throw new PCDIException(PCDIException.NOT_IMPLEMENTED, "setCurrentStackFrame");
+		//int[] taskArray = tasks.toArray();
+		//for (int i=0; i<taskArray.length; i++) {
+		//}
+		//throw new PCDIException(PCDIException.NOT_IMPLEMENTED, "setCurrentStackFrame");
 	}
 	public String evaluateExpression(BitList tasks, String expression) throws PCDIException {
-		throw new PCDIException(PCDIException.NOT_IMPLEMENTED, "evaluateExpression");
+		SimVariable variable = findVariable(expression);
+		if (variable != null)
+			return variable.getValue();
+
+		variable = findArgument(expression);
+		if (variable != null)
+			return variable.getValue();
+
+		return "";
 	}
 	public String getVariableType(BitList tasks, String varName) throws PCDIException {
-		throw new PCDIException(PCDIException.NOT_IMPLEMENTED, "getVariableType");
+		SimVariable variable = findVariable(varName);
+		if (variable != null)
+			return variable.getType();
+
+		variable = findArgument(varName);
+		if (variable != null)
+			return variable.getType();
+		
+		return "";
 	}
 	public ICDIArgument[] listArguments(BitList tasks, ICDIStackFrame frame) throws PCDIException {
-		throw new PCDIException(PCDIException.NOT_IMPLEMENTED, "listArguments");
+		int[] taskArray = tasks.toArray();
+		List argList = new ArrayList();
+		for (int i=0; i<taskArray.length; i++) {
+			ICDITarget target = getSession().getTarget(taskArray[i]);
+		    ICDIThread thread = new org.eclipse.ptp.debug.external.cdi.model.Thread((Target) target, 0);
+		    SimVariable[] args = getArguments();
+		    for (int j=0; j<args.length; j++) {
+				argList.add(new Argument((Target) target, (org.eclipse.ptp.debug.external.cdi.model.Thread) thread, (StackFrame)frame, args[j].getVariable(), args[j].getVariable(), args.length - j, frame.getLevel(), null));
+		    }
+		}
+		return (ICDIArgument[]) argList.toArray(new ICDIArgument[0]);
 	}
 	public ICDILocalVariable[] listLocalVariables(BitList tasks, ICDIStackFrame frame) throws PCDIException {
-		throw new PCDIException(PCDIException.NOT_IMPLEMENTED, "listLocalVariables");
+		int[] taskArray = tasks.toArray();
+		List varList = new ArrayList();
+		for (int i=0; i<taskArray.length; i++) {
+			ICDITarget target = getSession().getTarget(taskArray[i]);
+		    ICDIThread thread = new org.eclipse.ptp.debug.external.cdi.model.Thread((Target) target, 0);
+		    SimVariable[] vars = getVariables();
+		    for (int j=0; j<vars.length; j++) {
+		    	varList.add(new LocalVariable((Target) target, (org.eclipse.ptp.debug.external.cdi.model.Thread) thread, (StackFrame)frame, vars[j].getVariable(), vars[j].getVariable(), vars.length - j, frame.getLevel(), null));
+		    }
+		}
+		return (ICDILocalVariable[]) varList.toArray(new ICDILocalVariable[0]);
 	}
 	public ICDIGlobalVariable[] listGlobalVariables(BitList tasks) throws PCDIException {
 		throw new PCDIException(PCDIException.NOT_IMPLEMENTED, "listGlobalVariables");
@@ -215,41 +290,21 @@ public class DebugSimulation2 extends AbstractDebugger implements IDebugger, Obs
 				String file = args[2];
 				int line = convertInt(args[3]);
 				if (EVENT_BY_EACH_PROC) {
-					updateEvent(new QueueItem(state, file, line, task)); 
+					updateEvent(new QueueItem(total_process, state, file, line, task)); 
 				}
 				else {
-					intQueue.addItem(new QueueItem(state, file, line, task));
+					intQueue.addItem(new QueueItem(total_process, state, file, line, task));
 				}
 			}
 		}
-	}
-	private SimulateProgram getSimProg(int id) {
-		return (SimulateProgram)sim_list.get(id);		
-	}
-	private void start_time_range() {
-		if (timer == null && !EVENT_BY_EACH_PROC) {
-			running_timer = true;
-			current_time = System.currentTimeMillis();
-			Runnable runnable = new Runnable() {
-				public synchronized void run() {
-					while (running_timer) {
-						if (System.currentTimeMillis() - current_time >= time_range) {
-							current_time = System.currentTimeMillis();
-							updateEvent();
-						}
-					}
-				}
-			};
-			timer = new Thread(runnable);
-			timer.start();
+		else if (obs instanceof InternalEventQueue) {
+			updateEvent((QueueItem)obj);
 		}
 	}
-	private void stop_time_range() {
-		running_timer = false;
-		if (timer != null) {
-			timer.interrupt();
-		} 
-		timer = null;
+	private SimulateProgram getSimProg(int id) {
+		synchronized (sim_list) {
+			return (SimulateProgram)sim_list.get(id);
+		}
 	}
 	private synchronized void updateEvent(QueueItem qItem) {
 		String state = qItem.getState();
@@ -262,223 +317,39 @@ public class DebugSimulation2 extends AbstractDebugger implements IDebugger, Obs
 			handleEndSteppingEvent(qItem.getTasks(), qItem.getLine(), qItem.getFile());
 		}
 	}
-	private synchronized void updateEvent() {
-		if (!intQueue.isEmpty())
-			updateEvent(intQueue.getItem());
-	}
-	private int convertInt(String s_id) {
+	public static int convertInt(String s_id) {
 		try {
 			return Integer.parseInt(s_id);
 		} catch (NumberFormatException e) {
 			return -1;
 		}
 	}
+	public static int random(int min, int max) {
+	    Random generator = new Random();
+	    long range = (long)max - (long)min + 1;
+	    long fraction = (long)(range * generator.nextDouble());
+	    return (int)(fraction + min);
+	}
 	
-	private class InternalEventQueue {
-		List queue = null;
-		InternalEventQueue() {
-			queue = Collections.synchronizedList(new LinkedList());
+	//Currently, all tasks are using the same variable
+	private class SimVariable {
+		private String var = "";
+		private String type = "";
+		private String val = "";
+		
+		public SimVariable(String var, String type, String val) {
+			this.var = var;
+			this.type = type;
+			this.val = val;
 		}
-		public QueueItem getItem() {
-			synchronized (queue) {
-				return (QueueItem)queue.remove(0);
-			}
+		public String getVariable() {
+			return var;
 		}
-		public void addItem(QueueItem item) {
-			synchronized (queue) {
-				if (same(item)) {
-					queue.add(item);
-				} else {
-					updateEvent();
-				}
-			}
+		public String getType() {
+			return type;
 		}
-		public boolean same(QueueItem qItem) {
-			synchronized (queue) {
-				if (queue.isEmpty())
-					return true;
-				QueueItem item = getItem();
-				if (item.equals(qItem)) {
-					qItem.getTasks().or(item.getTasks());
-					return true;
-				}
-				return false;
-			}
-		}
-		public boolean isEmpty() {
-			synchronized (queue) {
-				return queue.isEmpty();
-			}
-		}
-	}
-	private class QueueItem {
-		private String file = "";
-		private String state = "";
-		private int line = -1;
-		private BitList tasks = new BitList(total_process);
-		public QueueItem(String state, String file, int line, int task) {
-			this.state = state;
-			this.file = file;
-			this.line = line;
-			tasks.set(task);
-		}
-		public boolean equals(QueueItem qItem) {
-			return (state.equals(qItem.getState()) && file.equals(qItem.getFile()) && line == qItem.getLine());
-		}
-		public String getFile() {
-			return file;
-		}
-		public String getState() {
-			return state;
-		}
-		public int getLine() {
-			return line;
-		}
-		public BitList getTasks() {
-			return tasks;
-		}
-	}
-
-	private class SimulateProgram extends Observable implements Runnable {
-		private final int sim_program_line = 37;
-		private final int main_method_line = 5;
-		private final int end_step_line = 15;
-		private int start_step_line = 0;
-		private int current_line = 0;
-		private boolean isStepping = false;
-		private int tid = -1;
-		private List bpts = new ArrayList();
-		private boolean isStopInMain = false;
-		private boolean isPause = false;
-
-		public SimulateProgram(int tid) {
-			this.tid = tid;
-		}
-		private synchronized void waitForNotify() {
-			try {
-				SimulateProgram.this.wait();
-			} catch (InterruptedException e) {
-				System.out.println("----- Err in waiting: " + e.getMessage());
-			}
-		}
-		private void waitForWhile(long timeout) {
-			try {
-				Thread.sleep(timeout);
-			} catch (InterruptedException e) {
-				System.out.println("----- Err in waiting: " + e.getMessage());
-			}
-		}
-		public synchronized void run() {
-			SimulateProgram.this.notifyAll();
-			waitForNotify();
-			while (current_line < sim_program_line) {
-				if (isStopInMain) {
-					isStopInMain = false;
-					gotoLine(main_method_line);
-				}
-				else if (isPause) {
-					setChanged();
-					notifyObservers(new String[] { String.valueOf(tid), STEP_END_STATE, APP_NAME, String.valueOf(current_line) });
-					waitForNotify();
-				}
-				else if (isHitBreakpoint(current_line)) {
-					isPause = true;
-					printMessage();
-					setChanged();
-					notifyObservers(new String[] { String.valueOf(tid), HIT_BPT_STATE, APP_NAME, String.valueOf(current_line) });
-					waitForNotify();
-				}
-				else if (isStepping) {
-					printMessage();
-					setChanged();
-					notifyObservers(new String[] { String.valueOf(tid), STEP_END_STATE, APP_NAME, String.valueOf(current_line) });					
-					waitForNotify();
-				}
-				else {
-					printMessage();
-					nextLine();
-					waitForWhile(50);
-				}
-			}
-			System.out.println("==== finished: " + tid);
-			setChanged();
-			notifyObservers(new String[] { String.valueOf(tid), EXIT_STATE, APP_NAME, String.valueOf(current_line) });
-		}
-		public synchronized void startProgram() {
-			new Thread(this).start();
-			try {
-				//wait the program is ready to start
-				SimulateProgram.this.wait(5000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		public void stopProgram() {
-			current_line = sim_program_line;
-			if (isPause || isStepping) {
-				go();
-			}
-		}
-		private synchronized void printMessage() {
-			//System.out.println("Task: " + tid + " - Message at current line: " + current_line);
-		}
-		public synchronized void gotoLine(int line) {
-			this.current_line = line;
-		}
-		public synchronized void go() {
-			isPause = false;
-			isStepping = false;
-			SimulateProgram.this.notifyAll();
-			nextLine();
-		}
-		public synchronized void stepFinish() {
-			if (isStepping) {
-				if (start_step_line > 0) {
-					gotoLine(start_step_line);
-					start_step_line = 0;
-				}
-				isPause = true;
-				isStepping = false;
-				SimulateProgram.this.notifyAll();					
-			}
-		}
-		public synchronized void stepOverLine() {
-			if (isStepping && start_step_line > 0 && current_line == end_step_line) {
-				stepFinish();
-			}
-			else {
-				isPause = false;
-				isStepping = true;
-				SimulateProgram.this.notifyAll();
-				nextLine();
-			}
-		}		
-		public synchronized void stepLine() {
-			if (!isStepping) {
-				start_step_line = current_line;
-			}
-			isPause = false;
-			isStepping = true;
-			SimulateProgram.this.notifyAll();
-			nextLine();
-		}
-		public void suspend() {
-			isPause = true;
-		}
-		public synchronized void nextLine() {
-			current_line++;
-		}
-		public boolean isHitBreakpoint(int line) {
-			return bpts.contains(new Integer(line));
-		}
-		public void setBpt(int bpt_line) {
-			if (!isHitBreakpoint(bpt_line)) {
-				bpts.add(new Integer(bpt_line));
-			}
-		}
-		public void setStopInMain() {
-			setBpt(main_method_line);
-			isStopInMain = true;
+		public String getValue() {
+			return val;
 		}
 	}
 }
