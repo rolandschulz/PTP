277cd5199434cf50acc3a62c07349f2267b1b70d
kchu <kchu>
2005-11-06 15:47:33 +0000

    added the startDebuggerListener() back

--- debug/org.eclipse.ptp.debug.sdm.core/src/org/eclipse/ptp/debug/external/IAbstractDebugger.java
+++ debug/org.eclipse.ptp.debug.sdm.core/src/org/eclipse/ptp/debug/external/IAbstractDebugger.java
@@ -37,6 +37,8 @@ public interface IAbstractDebugger extends ICommonActions, IDebugger {
 	public final static String TERMINATED_PROC_KEY = "terminated";
 	public final static String SUSPENDED_PROC_KEY = "suspended";
 	
+	public void startDebuggerListener();
+	
 	/* Debugger Initialization/Termination */
 	public void initialize(IPJob job);
 	public void exit();
