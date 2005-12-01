7f8c141712284268898223342b75ad56099e4106
ndebardeleben <ndebardeleben>
2005-12-01 17:33:41 +0000

    Adding documentation to interfaces.  More to come.

--- core/org.eclipse.ptp.core/src/org/eclipse/ptp/rtsystem/IControlSystem.java
+++ core/org.eclipse.ptp.core/src/org/eclipse/ptp/rtsystem/IControlSystem.java
@@ -23,28 +23,104 @@ import org.eclipse.ptp.core.IPJob;
 import org.eclipse.ptp.core.IPProcess;
 import org.eclipse.ptp.rtsystem.JobRunConfiguration;
 
-/* This is the interface to a control system which controls a runtime system.
- * Things it has to do might including running a job, aborting a job, etc.
+/**
+ * A Control System is a portion of a runtime system that handles controlling jobs.  This includes
+ * starting new jobs, terminating jobs, getting information about running jobs and processes, etc.
+ * Control Systems also can fire events, specifically {@link RuntimeEvent}s.
  */
+
 public interface IControlSystem {	
-	/* returns the new job name string that it spawns, unique */
+	/**
+	 * Performs a job run given the jobRunConfig.  The JobRunConfiguration contains specifics
+	 * about how the user wants to run the job, such as the program name, number of
+	 * processes, etc.
+	 * 
+	 * @param jobRunConfig the configuration information about the job
+	 * @return the job identifier of the created job or -1 on error
+	 * @see JobRunConfiguration
+	 */
 	public int run(JobRunConfiguration jobRunConfig);
 
+	/**
+	 * Terminates a running job.  The {@link IPJob} contains the job identifier used to 
+	 * locate the job by the control system.
+	 * 
+	 * @param job the job to terminate
+	 */
 	public void terminateJob(IPJob job);
 	
+	/**
+	 * Returns an array of {@link String}s of the form [ job1", "job2", "job10" ].
+	 * The jobs should be a concat of the string "job" and the identifier.
+	 * 
+	 * @return the names of the known jobs
+	 */
 	public String[] getJobs();
 
+	/**
+	 * Returns an array of {@link String}s of the form [ "jobXprocessA", "jobXprocessB" ] where
+	 * each process is part of the specified job.
+	 * 
+	 * @param job the job to look for processes on 
+	 * @return the names of the processes
+	 */
 	public String[] getProcesses(IPJob job);
 	
+	/**
+	 * Gets the specified attributes for all processes of the specified job.  The
+	 * attributes are space separated in a string of the form "PID NODE_NUMBER".  The
+	 * attributes are defined in {@link org.eclipse.ptp.core.AttributeConstants}.  The return array has a size
+	 * of num_procs * num_attribs and is of the form [ proc0attrib0, proc0attrib1, ... proc0attribN,
+	 * proc1attrib0 ...]
+	 * 
+	 * @param job the job to get process attributes from
+	 * @param attribs the attribute String where attributes are space separated
+	 * @return an array of the attribute values for each process
+	 * @see org.eclipse.ptp.core.AttributeConstants
+	 */
 	public String[] getAllProcessesAttributes(IPJob job, String attribs);
+	
+	/**
+	 * Gets the specified attributes for a specific process in a specific job.  The
+	 * attributes are space separated in a string of the form "PID NODE_NUMBER".  The
+	 * attributes are defined in {@link org.eclipse.ptp.core.AttributeConstants}.  The
+	 * return array's length is equal to the number of attributes requested.
+	 * 
+	 * @param job the job to get the process attributes from
+	 * @param proc the process contained in the job to get the process attributes from
+	 * @param attribs the attribute String where attributes are space separated
+	 * @return an array of the attribute values
+	 */
 	public String[] getProcessAttributes(IPJob job, IPProcess proc, String attribs);
 
-	/* event stuff */
+	/**
+	 * Adds a listener to the control system.  The control system may fire {@link RuntimeEvent}s
+	 * and will use this list of listeners to determine who to send these events to.
+	 * 
+	 * @param listener someone that wants to listener to {@link RuntimeEvent}s
+	 * @see RuntimeEvent
+	 */
 	public void addRuntimeListener(IRuntimeListener listener);
 
+	/**
+	 * Removes a listener from the list of things listening to {@link RuntimeEvent}s on
+	 * this control system.
+	 * 
+	 * @param listener the listener to remove
+	 * @see RuntimeEvent
+	 */
 	public void removeRuntimeListener(IRuntimeListener listener);
 
+	/**
+	 * Called after the control system is contructed.  The implementation can use this
+	 * to handle any startup functionality.
+	 */
 	public void startup();
 	
+	/**
+	 * Called when the control system is being shutdown.  Usually this is either at
+	 * close of the PTP plug-in or when switching control systems.  The implementation
+	 * needs to handle any cleanup that is required at this time.
+	 */
 	public void shutdown();
 }
