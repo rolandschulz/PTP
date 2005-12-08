ba6d6ccc7d757eea288dd20bec45bd38b8c4b581
Greg Watson <g.watson@computer.org>
2005-12-08 15:42:26 +0000

    session & error handling

--- debug/org.eclipse.ptp.debug.sdm/src/mi/MIError.c
+++ debug/org.eclipse.ptp.debug.sdm/src/mi/MIError.c
@@ -16,14 +16,60 @@
  *
  * LA-CC 04-115
  ******************************************************************************/
-#ifndef _MISESSION_H_
-#define _MISESSION_H_
 
-struct MISession {
-	int	gdb_fd[2];
-	int	gdb_pid;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "MIError.h"
+
+static int 		MIErrorNum = 0;
+static char *	MIErrorStr = NULL;
+
+static char * MIErrorTab[] =
+{
+	"NO_ERROR",
+	"system error: %s",
 };
-typedef struct MISession	MISession;
 
-#endif _MISESSION_H_
+/*
+ * Error handling
+ */
+void
+MISetError(int errnum, char *msg)
+{
+	MIErrorNum = errnum;
+	
+	if (MIErrorStr != NULL) {
+		free(MIErrorStr);
+		MIErrorStr = NULL;
+	}
+	
+	if (MIErrorNum >= sizeof(MIErrorTab)/sizeof(char *)) {
+		if (msg != NULL) {
+			MIErrorStr = strdup(msg);
+		} else {
+			asprintf(&MIErrorStr, "Error %d occurred.", MIErrorNum);
+		}
+	} else {
+		if (msg == NULL)
+			msg = "<null>";
+			
+		asprintf(&MIErrorStr, MIErrorTab[MIErrorNum], msg);
+	}
+}
 
+int
+MIGetError(void)
+{
+	return MIErrorNum;
+}
+
+char *
+MIGetErrorStr(void)
+{
+	if (MIErrorStr == NULL)
+		return "";
+		
+	return MIErrorStr;
+}
