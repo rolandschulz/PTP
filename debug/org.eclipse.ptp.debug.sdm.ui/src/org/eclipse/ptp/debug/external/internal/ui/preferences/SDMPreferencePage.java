65ba54bbe3b963c6de799f9858cb1fdb82c277e4
Kam Man Chu <clement.chu@gmail.com>
2006-01-25 15:00:53 +0000

    changed setValue to setDefault

--- debug/org.eclipse.ptp.debug.sdm.ui/src/org/eclipse/ptp/debug/external/internal/ui/preferences/SDMPreferencePage.java
+++ debug/org.eclipse.ptp.debug.sdm.ui/src/org/eclipse/ptp/debug/external/internal/ui/preferences/SDMPreferencePage.java
@@ -114,10 +114,12 @@ public class SDMPreferencePage extends AbstractPerferencePage {
 				String ipath = path.getAbsolutePath();
 				int idx = ipath.indexOf(PTPDebugCorePlugin.PLUGIN_ID);
 				String ipath2 = ipath.substring(0, idx) + "org.eclipse.ptp.debug.sdm/sdm";
+				preferences.setValue(IPDebugConstants.PREF_PTP_DEBUGGER_FILE, ipath2);
 				preferences.setDefault(IPDebugConstants.PREF_PTP_DEBUGGER_FILE, ipath2);
 			} catch(Exception e) { 
 			}
 		}
+		preferences.setValue(IPDebugConstants.PREF_PTP_DEBUGGER_ARGS, IPDebugConstants.PREF_DEFAULT_DEUBGGER_ARGS);
 		preferences.setDefault(IPDebugConstants.PREF_PTP_DEBUGGER_ARGS, IPDebugConstants.PREF_DEFAULT_DEUBGGER_ARGS);
 	}
 	public void performDefaults() { 
