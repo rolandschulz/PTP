6f7399d1023794df2d45906f2e9a08da49dc20ae
Greg Watson <g.watson@computer.org>
2006-01-26 01:23:38 +0000

    use NIO to guarantee full reads/writes

--- core/org.eclipse.ptp.core/src/org/eclipse/ptp/core/proxy/AbstractProxyClient.java
+++ core/org.eclipse.ptp.core/src/org/eclipse/ptp/core/proxy/AbstractProxyClient.java
@@ -22,8 +22,17 @@ package org.eclipse.ptp.core.proxy;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
@@ -38,15 +47,16 @@ import org.eclipse.ptp.core.util.BitList;
 public abstract class AbstractProxyClient {
 	private String				sessHost = null;
 	private int					sessPort = 0;
-	private ServerSocket			sessSvrSock = null;
-	private Socket				sessSock = null;
-	private OutputStreamWriter	sessOut;
-	private InputStreamReader		sessIn;
+	private ServerSocketChannel	sessSvrSock = null;
+	private SocketChannel			sessSock = null;
 	private boolean				sessConnected = false;
 	private boolean				exitThread;
 	private Thread				eventThread;
 	private Thread				acceptThread;
 	protected List 				listeners = Collections.synchronizedList(new ArrayList());
+	private static Charset		charset = Charset.forName("US-ASCII");
+	private static CharsetEncoder	encoder = charset.newEncoder();
+	private static CharsetDecoder	decoder = charset.newDecoder();
 	
 	private String encodeLength(int val) {
 		char[] res = new char[8];
@@ -66,8 +76,7 @@ public abstract class AbstractProxyClient {
 		if (sessConnected) {
 			String buf = encodeLength(cmd.length()) + " " + cmd;
 			System.out.println("<" + buf + ">");
-			sessOut.write(buf);
-			sessOut.flush();
+			fullWrite(encoder.encode(CharBuffer.wrap(buf)));
 		}
 	}
 
@@ -101,34 +110,24 @@ public abstract class AbstractProxyClient {
 	}
 	
 	public void sessionCreate(int port) throws IOException {
-		System.out.println("sessionCreate()");
-		sessSvrSock = new ServerSocket(port);
-		sessPort = sessSvrSock.getLocalPort();
-		sessHost = sessSvrSock.getLocalSocketAddress().toString();
+		System.out.println("sessionCreate("+port+")");
+		sessSvrSock = ServerSocketChannel.open();
+		InetSocketAddress isa = new InetSocketAddress(port);
+		System.out.println("bind("+isa.toString()+")");
+		sessSvrSock.socket().bind(isa);
+		sessPort = sessSvrSock.socket().getLocalPort();
+		sessHost = sessSvrSock.socket().getLocalSocketAddress().toString();
 		System.out.println("port=" + sessPort);
 		acceptThread = new Thread("Proxy Client Accept Thread") {
 			public void run() {
 				try {
 					System.out.println("accept thread starting...");
 					sessSock = sessSvrSock.accept();
-					sessOut = new OutputStreamWriter(sessSock.getOutputStream());
-					sessIn = new InputStreamReader(sessSock.getInputStream());
 					sessConnected = true;
 					fireProxyEvent(new ProxyConnectedEvent());
 					startEventThread();
 				} catch (IOException e) {
-					try {
-						sessIn.close();
-					} catch (IOException e1) {
-					}
-					try {
-						sessOut.close();
-					} catch (IOException e1) {
-					}
-					try {
-						sessSock.close();
-					} catch (IOException e1) {
-					} 
+					// TODO: what happens if the accept() fails?
 				}
 				try {
 					sessSvrSock.close();
@@ -161,14 +160,6 @@ public abstract class AbstractProxyClient {
 				} 
 				sessConnected = false;
 				try {
-					sessIn.close();
-				} catch (IOException e) {
-				}
-				try {
-					sessOut.close();
-				} catch (IOException e) {
-				}
-				try {
 					sessSock.close();
 				} catch (IOException e) {
 				} 
@@ -190,38 +181,46 @@ public abstract class AbstractProxyClient {
 		}
 	}
 	
-	private boolean fullRead(char[] buf, int len) throws IOException {
-		int offset = 0;
-		while (len > 0) {
-			int n = sessIn.read(buf, offset, len);
-			if (n <= 0)
+	private boolean fullRead(ByteBuffer buf) throws IOException {
+		buf.clear();
+		while (buf.remaining() > 0) {
+			int n = sessSock.read(buf);
+			if (n < 0)
 				return false;
-			offset += n;
-			len -= n;
+		}
+		buf.flip();
+		return true;
+	}
+	
+	private boolean fullWrite(ByteBuffer buf) throws IOException {
+		while (buf.remaining() > 0) {
+			int n = sessSock.write(buf);
+			if (n < 0)
+				return false;
 		}
 		return true;
 	}
 	
 	private void sessionProgress() throws IOException {
-		char[] len_bytes = new char[9];
+		ByteBuffer len_bytes = ByteBuffer.allocate(9);
 		
-		if (!fullRead(len_bytes, 9)) {
+		if (!fullRead(len_bytes)) {
 			exitThread = true;
 			return;
 		}
 		
-		String len_str = new String(len_bytes, 0, 8);
-		int len = Integer.parseInt(len_str, 16);
+		CharBuffer len_str = decoder.decode(len_bytes);
+		int len = Integer.parseInt(len_str.subSequence(0, 8).toString(), 16);
 		
-		char[] event_bytes = new char[len];
+		ByteBuffer event_bytes = ByteBuffer.allocate(len);
 
-		if (!fullRead(event_bytes, len)) {
+		if (!fullRead(event_bytes)) {
 			exitThread = true;
 			return;
 		}
 
-		String event_str = new String(event_bytes);
-		fireProxyEvent(ProxyEvent.toEvent(event_str));
+		CharBuffer event_str = decoder.decode(event_bytes);
+		fireProxyEvent(ProxyEvent.toEvent(event_str.toString()));
 	}
 
 	public void sessionFinish() throws IOException {
