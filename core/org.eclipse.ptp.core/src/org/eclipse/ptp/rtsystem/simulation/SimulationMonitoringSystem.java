7ce192c343a16ab8eb0526ee639635bb30559b0f
ndebardeleben <ndebardeleben>
2006-02-15 20:14:03 +0000

    Beautification of error messages.

--- core/org.eclipse.ptp.core/src/org/eclipse/ptp/rtsystem/simulation/SimulationMonitoringSystem.java
+++ core/org.eclipse.ptp.core/src/org/eclipse/ptp/rtsystem/simulation/SimulationMonitoringSystem.java
@@ -59,7 +59,7 @@ public class SimulationMonitoringSystem implements IMonitoringSystem {
 
 			PTPCorePlugin.getDefault().savePluginPreferences();
 
-			PTPCorePlugin.errorDialog("Default Simulation Num Machines Sete", "No existing / invalid number of machines to to simulate detected.  Default " + "number of machines set to 1.  Set using the PTP preferences -> simulation page.", null);
+			System.err.println("Noo existing / invalid number of machines to to simulate detected.  Default " + "number of machines set to 1.  Set using the PTP preferences -> simulation page.");
 			numNodes = new int[1];
 			numNodes[0] = 256;
 		}
