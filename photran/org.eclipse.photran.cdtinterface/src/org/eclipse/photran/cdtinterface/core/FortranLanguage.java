1ad4b70e43d50f8c215d6eb4dc151752155892f9
joverbey <joverbey>
2006-04-03 18:53:51 +0000

    Source reflects PDOM changes

--- photran/org.eclipse.photran.cdtinterface/src/org/eclipse/photran/cdtinterface/core/FortranLanguage.java
+++ photran/org.eclipse.photran.cdtinterface/src/org/eclipse/photran/cdtinterface/core/FortranLanguage.java
@@ -3,20 +3,24 @@ package org.eclipse.photran.cdtinterface.core;
 import java.util.Arrays;
 import java.util.Collection;
 
-import org.eclipse.cdt.core.dom.IPDOM;
-import org.eclipse.cdt.core.dom.PDOM;
+import org.eclipse.cdt.core.CCorePlugin;
+import org.eclipse.cdt.core.dom.ICodeReaderFactory;
 import org.eclipse.cdt.core.dom.ast.ASTCompletionNode;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+import org.eclipse.cdt.core.model.ICProject;
 import org.eclipse.cdt.core.model.IContributedModelBuilder;
 import org.eclipse.cdt.core.model.ILanguage;
 import org.eclipse.cdt.core.model.ITranslationUnit;
 import org.eclipse.cdt.core.model.IWorkingCopy;
-import org.eclipse.cdt.internal.core.pdom.PDOMDatabase;
+import org.eclipse.cdt.core.parser.CodeReader;
+import org.eclipse.cdt.internal.core.dom.SavedCodeReaderFactory;
+import org.eclipse.cdt.internal.core.pdom.PDOM;
+import org.eclipse.cdt.internal.core.pdom.PDOMCodeReaderFactory;
 import org.eclipse.cdt.internal.core.pdom.dom.IPDOMLinkageFactory;
 import org.eclipse.cdt.internal.core.pdom.dom.PDOMLinkage;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IStorage;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.PlatformObject;
 import org.eclipse.photran.cdtinterface.core.pdom.PDOMFortranLinkage;
@@ -51,15 +55,15 @@ public class FortranLanguage extends PlatformObject implements ILanguage
 	{
 		System.out.println("getAdapter " + adapter.getName());
 		
-		if (adapter == IPDOMLinkageFactory.class)
+        if (adapter == IPDOMLinkageFactory.class)
 			return new IPDOMLinkageFactory()
 			{
-				public PDOMLinkage getLinkage(PDOMDatabase pdom, int record)
+				public PDOMLinkage getLinkage(PDOM pdom, int record)
 				{
 					return new PDOMFortranLinkage(pdom, record);
 				}
 
-				public PDOMLinkage createLinkage(PDOMDatabase pdom) throws CoreException
+				public PDOMLinkage createLinkage(PDOM pdom) throws CoreException
 				{
 					return new PDOMFortranLinkage(pdom);
 				}
@@ -68,43 +72,54 @@ public class FortranLanguage extends PlatformObject implements ILanguage
 		else
 			return super.getAdapter(adapter);
 	}
-	
-	public IASTTranslationUnit getTranslationUnit(IStorage file, IProject project, int style)
-	{
-		System.out.println("getTranslationUnit 1");
-		
-		return getTranslationUnit(file.getFullPath().toOSString(), project, style);
-	}
-	
-	public IASTTranslationUnit getTranslationUnit(IFile file, int style)
-	{
-		System.out.println("getTranslationUnit 2");
-		
-		return getTranslationUnit(file.getLocation().toOSString(), file.getProject(), style);
-	}
-	
-	public IASTTranslationUnit getTranslationUnit(IWorkingCopy workingCopy, int style)
-	{
-		System.out.println("getTranslationUnit 3");
-		
-		IFile file = (IFile)workingCopy.getResource();
-		String path = file.getLocation().toOSString();
-		return getTranslationUnit(path, file.getProject(), style);
-        // If workingCopy.getContents() returned a String rather than a char[], it could be useful to us
-	}
-
-	protected IASTTranslationUnit getTranslationUnit(String path, IProject project, int style)
-	{
-		IPDOM pdom = PDOM.getPDOM(project);
+    
+    public IASTTranslationUnit getASTTranslationUnit(ITranslationUnit file, int style) {
+        IResource resource = file.getResource();
+        ICProject project = file.getCProject();
+        IProject rproject = project.getProject();
         
-        // Parse the file
-		IASTTranslationUnit ast = new FortranIASTTranslationUnitAdapter(path);
+//        IScannerInfo scanInfo = null;
+//        IScannerInfoProvider provider = CCorePlugin.getDefault().getScannerInfoProvider(rproject);
+//        if (provider != null){
+//            IResource infoResource = resource != null ? resource : rproject; 
+//            IScannerInfo buildScanInfo = provider.getScannerInformation(infoResource);
+//            if (buildScanInfo != null)
+//                scanInfo = buildScanInfo;
+//            else if ((style & ILanguage.AST_SKIP_IF_NO_BUILD_INFO) != 0)
+//                return null;
+//            else
+//                scanInfo = new ScannerInfo();
+//        }
+        
+        PDOM pdom = (PDOM)CCorePlugin.getPDOMManager().getPDOM(project).getAdapter(PDOM.class);
+        ICodeReaderFactory fileCreator;
+        if ((style & ILanguage.AST_SKIP_INDEXED_HEADERS) != 0)
+            fileCreator = new PDOMCodeReaderFactory(pdom);
+        else
+            fileCreator = SavedCodeReaderFactory.getInstance();
 
-        // Tell the translation unit it's been indexed and where to find its index (PDOM)
-		if ((style & AST_USE_INDEX) != 0) ast.setIndex(pdom);
+        String path;
+        CodeReader reader;
+        if (file instanceof IWorkingCopy) {
+            // get the working copy contents
+            IFile rfile = (IFile)file.getResource();
+            path = rfile.getLocation().toOSString();
+            reader = new CodeReader(rfile.getLocation().toOSString(), file.getContents());
+        } else {
+            path = file.getPath().toOSString();
+            reader = fileCreator.createCodeReaderForTranslationUnit(path);
+            if (reader == null)
+                return null;
+        }
+        
+        // Parse
+        IASTTranslationUnit ast = new FortranIASTTranslationUnitAdapter(path);
 
-		return ast;
-	}
+        if ((style & AST_USE_INDEX) != 0) 
+            ast.setIndex(pdom);
+
+        return ast;
+    }
 	
 	public ASTCompletionNode getCompletionNode(IWorkingCopy workingCopy, int offset)
 	{
