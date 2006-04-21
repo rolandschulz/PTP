81ba6944c79f51267bd00b846e9e5e35ea1e21af
kchu <kchu>
2006-04-21 14:26:04 +0000

    Added memory view support.

--- core/org.eclipse.ptp.core/src/org/eclipse/ptp/core/IPTPLaunchConfigurationConstants.java
+++ core/org.eclipse.ptp.core/src/org/eclipse/ptp/core/IPTPLaunchConfigurationConstants.java
@@ -125,5 +125,6 @@ public interface IPTPLaunchConfigurationConstants {
 	 */
 	public static final int ERR_PROGRAM_NOT_EXIST = 104;
 	public static final int ERR_PROGRAM_NOT_BINARY = 107;
-	
+
+	public static final String ATTR_DEBUGGER_MEMORY_BLOCKS = PTP_LAUNCH_ID + ".MEMORY_BLOCKS";
 }
