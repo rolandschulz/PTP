d12c6b457e2da7fec11d6361f98448a752e82ddb
Greg Watson <g.watson@computer.org>
2006-06-08 21:04:40 +0000

    Removed old and unused code (with Randy's help)

--- core/org.eclipse.ptp.ui/src/org/eclipse/ptp/ui/actions/ParallelAction.java
+++ core/org.eclipse.ptp.ui/src/org/eclipse/ptp/ui/actions/ParallelAction.java
@@ -22,8 +22,8 @@ import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ptp.ui.UIUtils;
 import org.eclipse.ptp.ui.model.IElement;
-import org.eclipse.ptp.ui.old.UIUtils;
 import org.eclipse.ptp.ui.views.AbstractParallelElementView;
 import org.eclipse.swt.widgets.Shell;
 
