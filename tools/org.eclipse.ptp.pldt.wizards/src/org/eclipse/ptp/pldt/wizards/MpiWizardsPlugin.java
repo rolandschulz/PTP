21b80822cc92c18a3198857da13ffd9e15e75ba8
btibbitts <btibbitts>
2006-06-13 21:49:03 +0000

    initial version with MPI Project page added to CDT 'new managed make project' wizard

---
+++ tools/org.eclipse.ptp.pldt.wizards/src/org/eclipse/ptp/pldt/wizards/MpiWizardsPlugin.java
@@ -0,0 +1,97 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Research - initial implementation
+ *******************************************************************************/
+
+package org.eclipse.ptp.pldt.wizards;
+
+import org.eclipse.ui.plugin.*;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.osgi.framework.BundleContext;
+import java.util.*;
+
+/**
+ * The main plugin class for MPI Wizards.
+ * 
+ * @author Beth Tibbitts
+ */
+public class MpiWizardsPlugin extends AbstractUIPlugin {
+	//The shared instance.
+	private static MpiWizardsPlugin plugin;
+	//Resource bundle.
+	private ResourceBundle resourceBundle;
+	
+	/**
+	 * The constructor.
+	 */
+	public MpiWizardsPlugin() {
+		super();
+		plugin = this;
+	}
+
+	/**
+	 * This method is called upon plug-in activation
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/**
+	 * This method is called when the plug-in is stopped
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		plugin = null;
+		resourceBundle = null;
+	}
+
+	/**
+	 * Returns the shared instance.
+	 */
+	public static MpiWizardsPlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns the string from the plugin's resource bundle,
+	 * or 'key' if not found.
+	 */
+	public static String getResourceString(String key) {
+		ResourceBundle bundle = MpiWizardsPlugin.getDefault().getResourceBundle();
+		try {
+			return (bundle != null) ? bundle.getString(key) : key;
+		} catch (MissingResourceException e) {
+			return key;
+		}
+	}
+
+	/**
+	 * Returns the plugin's resource bundle,
+	 */
+	public ResourceBundle getResourceBundle() {
+		try {
+			if (resourceBundle == null)
+				resourceBundle = ResourceBundle.getBundle("org.eclipse.cdt.managedbuilder.ui.tests.TestsPluginResources");
+		} catch (MissingResourceException x) {
+			resourceBundle = null;
+		}
+		return resourceBundle;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path.
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return AbstractUIPlugin.imageDescriptorFromPlugin("org.eclipse.cdt.managedbuilder.ui.tests", path);
+	}
+}
---
+++ tools/org.eclipse.ptp.pldt.wizards/src/org/eclipse/ptp/pldt/wizards/wizardPages/MPIProjectWizardPage.java
@@ -0,0 +1,517 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corp. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corp. - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.ptp.pldt.wizards.wizardPages;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.eclipse.cdt.managedbuilder.ui.wizards.MBSCustomPage;
+import org.eclipse.cdt.managedbuilder.ui.wizards.MBSCustomPageManager;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ptp.pldt.mpi.core.MpiIDs;
+import org.eclipse.ptp.pldt.mpi.core.MpiPlugin;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+/**
+ * 
+ * @author Beth Tibbitts
+ * 
+ */
+public class MPIProjectWizardPage extends MBSCustomPage {
+	private static final boolean traceOn=true;
+
+	private Composite composite;
+	public static final String PAGE_ID="org.eclipse.ptp.pldt.wizards.wizardPages.MPIProjectWizardPage";
+
+	// The following are IDs for storing info in MBSPageData so it can be retrived in MpiProjectRunnable
+	// when the wizard is done.
+	/**
+	 * Store in MBSPageData  (with this ID) whether user wants to include MPI info in the project.
+	 */
+	public static final String DO_MPI_INCLUDES = "doMPIincludes";
+	/**
+	 * store in MBSPageData (with this ID) what the include path to MPI will be.
+	 */
+	public static final String INCLUDE_PATH_PROP_ID = "includePath";
+	/**
+	 * store in MBSPageData (with this ID) what the library name for the linker is.
+	 */
+	public static final String LIB_PROP_ID = "lib";
+	/**
+	 * store in MBSPageData (with this ID) what the library search path is.
+	 */
+	public static final String LIBRARY_SEARCH_PATH_PROP_ID = "libPath";
+	
+
+	private String currentMpiIncludePath;
+	private String currentLibName;
+	private String currentLibPath;
+
+	private String defaultMpiIncludePath;
+	private String defaultMpiLibName;
+	private String defaultMpiLibPath;
+
+	private Text includePathField;
+	private Text libNameField;
+	private Text libPathField;
+	
+	private Label includePathLabel, libLabel, libPathLabel;
+
+
+	private Button browseButton;
+	private Button browseButton2;
+
+	private Button useDefaultsButton;
+	private Button useMpiIncludesButton;
+
+	private IPreferenceStore preferenceStore;
+
+	private static final int SIZING_TEXT_FIELD_WIDTH = 250;
+	private boolean useMpiIncludes=true;
+	private String desc = "MPI Project Page";
+
+	/**
+	 * The CDT new project wizard page for MPI projects.  
+	 * Adds the include paths and libary information for an MPI project.
+	 * 
+	 */
+	public MPIProjectWizardPage() {
+		super(PAGE_ID);
+
+		// access the preference store from the MPI plugin
+		preferenceStore = MpiPlugin.getDefault().getPreferenceStore();
+		String mip=preferenceStore.getString(MpiIDs.MPI_INCLUDES);
+		System.out.println("Got mpi include pref from other plugin: "+mip);
+
+		// Set the defaults here in the wizard page constructor and just
+		// overwrite them if the user changes them.
+		defaultMpiIncludePath = preferenceStore.getString(MpiIDs.MPI_INCLUDES);
+		setDefaultOtherNames(defaultMpiIncludePath);
+		// BRT what if the default mpi path is empty?
+		currentMpiIncludePath = defaultMpiIncludePath;
+		// the following sets what will be remembered when we leave the page.
+		setCurrentMpiIncludePath(currentMpiIncludePath);
+
+	}
+
+	/**
+	 * Set the default lib name and lib path based on what the 
+	 * include path is.  We assume something like
+	 * <code>mpiIncludepath=/my/path/include</code>
+	 * in which case we set
+	 * <code>libName="lib" </code>and <code> defaultMpiLibPath=/my/path/lib</code>.
+	 * <p>
+	 * Also, set the initial default values in the MBS page data,
+	 * so if user changes nothing on this page, the default values
+	 * will be picked up.
+	 * 
+	 * @param mpiIncludePath
+	 */
+	private void setDefaultOtherNames(String mpiIncludePath) {
+		defaultMpiLibName="mpi";
+		setCurrentMpiLibName(defaultMpiLibName);
+		IPath path = Path.fromOSString(mpiIncludePath);
+		path=path.removeLastSegments(1);
+		path=path.addTrailingSeparator();
+
+		defaultMpiLibPath=path.toString()+"lib";
+		System.out.println("defaultMpiLibPath="+defaultMpiLibPath);
+		setCurrentMpiLibPath(defaultMpiLibPath);
+		
+		//standardize format for mpi include path, too
+		path = Path.fromOSString(mpiIncludePath);
+		String temp=path.toString();
+		temp=stripTrailingSeparator(temp);
+		defaultMpiIncludePath=temp;
+		setCurrentMpiIncludePath(defaultMpiIncludePath);
+		
+	}
+
+	/**
+	 * This sets what will be remembered for MPI include path when we leave the wizard page
+	 * (so we can retrieve the information from the Runnable to actualy do the change
+	 * to the project info)
+	 * 
+	 * @param path
+	 */
+	private void setCurrentMpiIncludePath(String path) {
+		currentMpiIncludePath = path;
+		MBSCustomPageManager.addPageProperty(pageID, INCLUDE_PATH_PROP_ID, path);
+	}
+	/**
+	 * This sets what will be remembered for library name when we leave the wizard page
+	 * (so we can retrieve the information from the Runnable to actualy do the change
+	 * to the project info)
+	 * 
+	 * @param path
+	 */
+	private void setCurrentMpiLibName(String name) {
+		currentMpiIncludePath = name;
+		MBSCustomPageManager.addPageProperty(pageID, LIB_PROP_ID, name);
+	}
+	/**
+	 * This sets what will be remembered for library search path when we leave the wizard page
+	 * (so we can retrieve the information from the Runnable to actualy do the change
+	 * to the project info)
+	 * 
+	 * @param path
+	 */
+	private void setCurrentMpiLibPath(String path) {
+		currentMpiIncludePath = path;
+		MBSCustomPageManager.addPageProperty(pageID, LIBRARY_SEARCH_PATH_PROP_ID, path);
+	}
+
+
+	public boolean canFlipToNextPage() {
+
+		return (MBSCustomPageManager.getNextPage(pageID) != null);
+	}
+
+	public String getName() {
+		return new String(desc + " name");
+	}
+
+	/**
+	 * Return the path on the "mpi include path" field.
+	 * 
+	 * @return String
+	 */
+	private String getPathFromIncludePathField() {
+		URI fieldURI;
+		try {
+			fieldURI = new URI(includePathField.getText());
+		} catch (URISyntaxException e) {
+			return includePathField.getText();
+		}
+		return fieldURI.getPath();
+	}
+
+	/**
+	 * Update the include path field based on the selected path.
+	 * 
+	 * @param selectedPath
+	 */
+	private void updateIncludePathField(String selectedPath) {
+		System.out.println("APWP.updateLocationField to " + selectedPath);
+		includePathField.setText(selectedPath);
+	}
+
+	/**
+	 * Open an appropriate directory browser and get selected directory <br>
+	 * BRT note: can we use the dialog that CDT uses here? Allows direct typing.
+	 */
+	private void handleLocationBrowseButtonPressed() {
+
+		String selectedDirectory = null;
+		String dirName = getPathFromIncludePathField();
+
+		DirectoryDialog dialog = new DirectoryDialog(includePathField.getShell());
+		dialog.setMessage("MPI Include path directory:");
+
+		dialog.setFilterPath(dirName);
+
+		selectedDirectory = dialog.open();
+
+		if (selectedDirectory != null) {
+			updateIncludePathField(selectedDirectory);
+
+			includePathField.setText(selectedDirectory);
+			System.out.println("Directory found via browse: " + selectedDirectory);
+			// set value to where we can find it in the runnable later
+			setCurrentMpiIncludePath(selectedDirectory);
+		}
+	}
+
+	/**
+	 * Remove any trailing device separator characther (e.g. ; on windows or : on Linux)
+	 * @param str
+	 * @return the string without any trailing separator
+	 */
+	private String stripTrailingSeparator(String str) {
+		char lastChar = str.charAt(str.length() - 1);
+		// BRT how to find ; vs : in a platform-independent manner?
+		if (lastChar == Path.DEVICE_SEPARATOR|| lastChar== ';') {
+			String temp = str.substring(0, str.length() - 1);
+			return temp;
+		}
+		return str;
+
+	}
+
+	/**
+	 * Create the area in which the user can enter MPI include path and other information
+	 * 
+	 * @param composite to put it in
+	 * @param defaultEnabled indicates if the "use defaults" checkbox is to be initially selected.
+	 */
+	private void createUserEntryArea(Composite composite, boolean defaultEnabled) {
+		includePathLabel = new Label(composite, SWT.NONE);
+		includePathLabel.setText("Location:");
+		includePathLabel.setToolTipText("Location of MPI include path(s)");
+
+		// Include path location  entry field
+		includePathField = new Text(composite, SWT.BORDER);
+		GridData data = new GridData(GridData.FILL_HORIZONTAL);
+		data.widthHint = SIZING_TEXT_FIELD_WIDTH;
+		data.horizontalSpan = 2;
+		includePathField.setLayoutData(data);
+		includePathField.setText(defaultMpiIncludePath);
+		includePathField.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				setCurrentMpiIncludePath(includePathField.getText());
+				System.out.println("locationField.modifyText(): " + currentMpiIncludePath);
+			}
+		});
+
+		// browse button
+		browseButton = new Button(composite, SWT.PUSH);
+		browseButton.setText("Browse...");
+		browseButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				System.out.println("Browse button pressed.");
+				handleLocationBrowseButtonPressed();
+
+			}
+		});
+
+		// how do we know when next/finish button pushed? we don't.
+		
+		libLabel=new Label(composite, SWT.NONE);
+		libLabel.setText("Lib name:");
+		libLabel.setToolTipText("Library name:");
+		
+		libNameField=new Text(composite,SWT.BORDER);
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		gd.widthHint=SIZING_TEXT_FIELD_WIDTH;
+		gd.horizontalSpan=2;
+		libNameField.setLayoutData(gd);
+		libNameField.setText(defaultMpiLibName);
+		libNameField.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				setCurrentMpiLibName(libNameField.getText());
+				System.out.println("libNameField.modifyText(): " + currentLibName);
+			}
+		});
+		
+		(new Label(composite,SWT.NONE)).setText(" ");//spacer
+		
+		libPathLabel=new Label(composite, SWT.NONE);
+		libPathLabel.setText("Library search path:");
+		libPathLabel.setToolTipText("Library name:");
+		
+		
+		libPathField=new Text(composite,SWT.BORDER);
+		GridData gd2 = new GridData(GridData.FILL_HORIZONTAL);
+		gd2.widthHint=SIZING_TEXT_FIELD_WIDTH;
+		gd2.horizontalSpan=2;
+		libPathField.setLayoutData(gd2);
+		libPathField.setText(defaultMpiLibPath);
+		libPathField.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				setCurrentMpiLibPath(libPathField.getText());
+				System.out.println("libPathField.modifyText(): " + currentLibPath);
+			}
+		});
+		
+//		 browse button
+
+		browseButton2 = new Button(composite, SWT.PUSH);
+		browseButton2.setText("Browse...");
+		browseButton2.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				System.out.println("Browse button pressed. DO SOMETHING HERE.");
+				//handleLocationBrowseButton2Pressed();
+
+			}
+		});
+		
+	}
+
+
+	private void createContents(Composite composite, boolean defaultEnabled) {
+
+		int columns = 4;
+
+		Composite group = new Composite(composite, SWT.NONE);
+		GridLayout layout = new GridLayout();
+		layout.numColumns = columns;
+		group.setLayout(layout);
+		group.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		
+		useMpiIncludesButton = new Button(group, SWT.CHECK | SWT.RIGHT);
+		useMpiIncludesButton.setText("Add MPI include paths to this project");
+		GridData gd=new GridData();
+		gd.horizontalSpan=columns;
+		useMpiIncludesButton.setLayoutData(gd);
+		useMpiIncludesButton.setSelection(useMpiIncludes);
+		useMpiIncludesButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				useMpiIncludes = useMpiIncludesButton.getSelection();
+				MBSCustomPageManager.addPageProperty(pageID, DO_MPI_INCLUDES, Boolean.toString(useMpiIncludes));
+				setUserAreaEnabled(useMpiIncludes);
+				useDefaultsButton.setEnabled(useMpiIncludes);
+			}
+		});
+
+		useDefaultsButton = new Button(group, SWT.CHECK | SWT.RIGHT);
+		useDefaultsButton.setText("Use default MPI include and lib information");
+		useDefaultsButton.setSelection(defaultEnabled);
+		GridData buttonData = new GridData();
+		buttonData.horizontalSpan = columns;
+		useDefaultsButton.setLayoutData(buttonData);
+		useDefaultsButton.addSelectionListener(new SelectionAdapter() {
+
+			public void widgetSelected(SelectionEvent e) {
+				boolean useDefaults = useDefaultsButton.getSelection();
+
+				if (useDefaults) { // CHANGING to use default MPI locn now...
+					includePathField.setText(defaultMpiIncludePath);
+					setCurrentMpiIncludePath(defaultMpiIncludePath);
+				}
+
+				setUserAreaEnabled(!useDefaults);
+
+			}
+		});
+
+		createUserEntryArea(group, defaultEnabled);
+		setUserAreaEnabled(!defaultEnabled);
+
+	}
+
+	public void createControl(Composite parent) {
+
+		composite = new Composite(parent, SWT.NULL);
+		composite.setLayout(new GridLayout());
+		composite.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+		boolean defaultEnabled = true;
+		createContents(composite, defaultEnabled);
+	}
+
+	public void dispose() {
+		composite.dispose();
+
+	}
+
+	public Control getControl() {
+		return composite;
+	}
+
+	public String getDescription() {
+		String tmp="Select the MPI include path, lib name, and library search path information to be automatically be added to the new project.";
+		return tmp;
+	}
+
+	public String getErrorMessage() {
+		return null;
+		// return new String("My error msg");
+	}
+
+	public Image getImage() {
+		return wizard.getDefaultPageImage();
+	}
+
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getTitle() {
+		return "MPI Project Settings";
+	}
+
+	public void performHelp() {
+		// do nothing
+
+	}
+
+	public void setDescription(String description) {
+		// do nothing
+
+	}
+
+	public void setImageDescriptor(ImageDescriptor image) {
+		// do nothing
+
+	}
+
+	public void setTitle(String title) {
+		// do nothing
+
+	}
+
+	/**
+	 * If you need to init data/widgets on the page when it is displayed based
+	 * on the data set in previous pages, you can override
+	 * IDialogPage.setVisible(boolean visible) and setup everything when visible ==
+	 * true.
+	 * 
+	 * Otherwise, do inits in the ctor.
+	 */
+	public void setVisible(boolean visible) {
+		composite.setVisible(visible);
+		if(traceOn)System.out.println("MPIProjectWizardPage.setVisible: " + visible);
+
+	}
+
+	/**
+	 * Determines the status of the wizard's finish button. If we don't want the
+	 * user to press finish yet, set this to false. <br>
+	 * The Next/Finish/Cancel methods belong to the wizard itself and not the
+	 * pages.
+	 * 
+	 * @return boolean true if finish button should be enabled; false otherwise.
+	 */
+	protected boolean isCustomPageComplete() {
+		// BRT if isDefaults is not checked then includePathField should be
+		// filled in.
+		// BRT if isDefaults is checked then there should actually BE some
+		// defaults to use.
+		return true;
+	}
+
+	/**
+	 * Enable/disable "user area" which is the place user can type and make
+	 * changes (includePathField, and its label and button)
+	 * 
+	 * @param enabled
+	 */
+	private void setUserAreaEnabled(boolean enabled) {
+		includePathField.setEnabled(enabled);
+		browseButton.setEnabled(enabled);
+		browseButton2.setEnabled(enabled);
+		libNameField.setEnabled(enabled);
+		libPathField.setEnabled(enabled);
+		
+		includePathLabel.setEnabled(enabled);
+		libPathLabel.setEnabled(enabled);
+		libLabel.setEnabled(enabled);
+	}
+
+}
