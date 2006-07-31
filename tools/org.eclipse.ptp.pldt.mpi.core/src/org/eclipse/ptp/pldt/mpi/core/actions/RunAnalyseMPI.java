058f0c06c2db4b346e3eec7fb4ad3ec3c5100044
btibbitts <btibbitts>
2006-07-31 13:38:28 +0000

    cleanup

--- tools/org.eclipse.ptp.pldt.mpi.core/src/org/eclipse/ptp/pldt/mpi/core/actions/RunAnalyseMPI.java
+++ tools/org.eclipse.ptp.pldt.mpi.core/src/org/eclipse/ptp/pldt/mpi/core/actions/RunAnalyseMPI.java
@@ -18,67 +18,63 @@ import org.eclipse.ptp.pldt.mpi.core.MpiPlugin;
 import org.eclipse.ptp.pldt.mpi.core.analysis.MpiCASTVisitor;
 import org.eclipse.ptp.pldt.mpi.core.analysis.MpiCPPASTVisitor;
 
-
 /**
  * 
- * Run analysis to create LAPI artifact markers. <br>
+ * Run analysis to create MPI artifact markers. <br>
  * The analysis is done in the doMpiCallAnalysis() method
  * 
  * 
- * IObjectActionDelegate enables popup menu selection IWindowActionDelegate enables toolbar(or menu) selection
+ * IObjectActionDelegate enables popup menu selection IWindowActionDelegate
+ * enables toolbar(or menu) selection
  */
-public class RunAnalyseMPI extends RunAnalyseBase
-{
-    /**
-     * Constructor for the "Run Analysis" action
-     */
-    public RunAnalyseMPI()
-    {
-        super("MPI", new MPIArtifactMarkingVisitor(MpiIDs.MARKER_ID),
-        		MpiIDs.MARKER_ID);
-    }
+public class RunAnalyseMPI extends RunAnalyseBase {
+	/**
+	 * Constructor for the "Run Analysis" action
+	 */
+	public RunAnalyseMPI() {
+		super("MPI", new MPIArtifactMarkingVisitor(MpiIDs.MARKER_ID), MpiIDs.MARKER_ID);
+	}
 
-    /**
-     * Returns LAPI analysis artifacts for file
-     * 
-     * @param file
-     * @param includes_ LAPI include paths
-     * @return
-     */
-    public ScanReturn doArtifactAnalysis(final IFile file, final List /* of String */includes_)
-    {
-        final ScanReturn msr = new ScanReturn();
-        final String fileName = file.getName();
-        ParserLanguage lang = AnalysisUtil.getLanguageFromFile(file);
-        try {
-            IASTTranslationUnit astTransUnit = CDOM.getInstance().getASTService().getTranslationUnit(file,
-                    CDOM.getInstance().getCodeReaderFactory(CDOM.PARSE_SAVED_RESOURCES));
-            if (lang == ParserLanguage.C) {
-                astTransUnit.accept(new MpiCASTVisitor(includes_, fileName, msr));
-            }
-            else if (lang == ParserLanguage.CPP) {
-                astTransUnit.accept(new MpiCPPASTVisitor(includes_, fileName, msr));
-            	
-            }
-        } catch (IASTServiceProvider.UnsupportedDialectException e) {
-        }
+	/**
+	 * Returns MPI analysis artifacts for file
+	 * 
+	 * @param file
+	 * @param includes
+	 *            MPI include paths
+	 * @return
+	 */
+	public ScanReturn doArtifactAnalysis(final IFile file, final List includes) {
+		final ScanReturn msr = new ScanReturn();
+		final String fileName = file.getName();
+		ParserLanguage lang = AnalysisUtil.getLanguageFromFile(file);
+		System.out.println("lang=" + lang);
+		try {
+			IASTTranslationUnit astTransUnit = CDOM.getInstance().getASTService().getTranslationUnit(file,
+					CDOM.getInstance().getCodeReaderFactory(CDOM.PARSE_SAVED_RESOURCES));
+			if (lang == ParserLanguage.C) {
+				astTransUnit.accept(new MpiCASTVisitor(includes, fileName, msr));
+			} else if (lang == ParserLanguage.CPP) {
+				astTransUnit.accept(new MpiCPPASTVisitor(includes, fileName, msr));
 
-        return msr;
-    }
+			}
+		} catch (IASTServiceProvider.UnsupportedDialectException e) {
+			System.out.println("RunAnalyseMPI, UnsupportedDialectException "+e.getMessage());
+		}
 
-    protected List getIncludePath()
-    {
-        return MpiPlugin.getDefault().getMpiIncludeDirs();
-    }
+		return msr;
+	}
 
-    protected void activateArtifactView()
-    {
-        ViewActivater.activateView(MpiIDs.MPI_VIEW_ID);
-    }
+
+	protected List getIncludePath() {
+		return MpiPlugin.getDefault().getMpiIncludeDirs();
+	}
+
+	protected void activateArtifactView() {
+		ViewActivater.activateView(MpiIDs.MPI_VIEW_ID);
+	}
 
 	protected void activateProblemsView() {
 
 	}
-
 
 }
