8eadba1702cdce16478bb752b9b5f85ace772ae2
crasmussen <crasmussen>
2006-11-08 17:21:13 +0000

    Added more variations.

--- lang/org.eclipse.ptp.lang.fortran.core.tests/parser-test-files/program-unit/program.f90
+++ lang/org.eclipse.ptp.lang.fortran.core.tests/parser-test-files/program-unit/program.f90
@@ -1,2 +1,25 @@
+!
+! Strictly not a legal file as there can only be one program
+!
+
 program main
-end program
+end program main
+
+!
+! These programs have unacticipated parse trees, i.e., second main_program has only an
+! end_program_stmt, END PROGRAM, no program_stmt.
+!
+
+PROGRAM main
+END
+
+PROGRAM main
+END PROGRAM
+
+PROGRAM main
+END 
+
+ENDPROGRAM
+
+END
+
