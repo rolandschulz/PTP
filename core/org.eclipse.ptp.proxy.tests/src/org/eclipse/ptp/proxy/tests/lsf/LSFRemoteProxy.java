1cd6e66e8623a8eb447cfe5fa66cb4ce12e5b60e
crasmussen <crasmussen>
2006-12-21 16:15:43 +0000

    Modified to run local copy of ptp_lsf_proxy for tests.

--- core/org.eclipse.ptp.proxy.tests/src/org/eclipse/ptp/proxy/tests/lsf/LSFRemoteProxy.java
+++ core/org.eclipse.ptp.proxy.tests/src/org/eclipse/ptp/proxy/tests/lsf/LSFRemoteProxy.java
@@ -2,17 +2,14 @@ package org.eclipse.ptp.proxy.tests.lsf;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.IOException;
 import org.eclipse.ptp.lsf.core.rtsystem.LSFProxyRuntimeClient;
-
-import org.eclipse.ptp.rtsystem.proxy.ProxyRuntimeClient;
 import org.junit.Test;
 
 public class LSFRemoteProxy {
 
 	@Test public void start_stop() {
 		boolean error = false;
-		String proxy = "../org.eclipse.ptp.lsf.proxy/ptp_lsf_proxy";
+		String proxy = "lsf/ptp_lsf_proxy";
 		
 		LSFProxyRuntimeClient client = new LSFProxyRuntimeClient(proxy, false);
 		
