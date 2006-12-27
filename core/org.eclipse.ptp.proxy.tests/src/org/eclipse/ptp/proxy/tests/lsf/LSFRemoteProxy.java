d73604e04b8ec185019292f80d9b9e43791fe040
crasmussen <crasmussen>
2006-12-27 15:48:15 +0000

    Added events test.

--- core/org.eclipse.ptp.proxy.tests/src/org/eclipse/ptp/proxy/tests/lsf/LSFRemoteProxy.java
+++ core/org.eclipse.ptp.proxy.tests/src/org/eclipse/ptp/proxy/tests/lsf/LSFRemoteProxy.java
@@ -2,6 +2,8 @@ package org.eclipse.ptp.proxy.tests.lsf;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+
 import org.eclipse.ptp.lsf.core.rtsystem.LSFProxyRuntimeClient;
 import org.junit.Test;
 
@@ -9,17 +11,51 @@ public class LSFRemoteProxy {
 
 	@Test public void start_stop() {
 		boolean error = false;
+		boolean launchManually = false;
 		String proxy = "lsf/ptp_lsf_proxy";
 		
-		LSFProxyRuntimeClient client = new LSFProxyRuntimeClient(proxy, false);
+		LSFProxyRuntimeClient client = new LSFProxyRuntimeClient(proxy, launchManually);
 		
 		if (client.startup(null)) {
+			try {
+				client.initialize();
+			} catch (IOException e) {
+				error = true;
+				e.printStackTrace();
+			}
 			client.shutdown();
 		} else {
 			error = true;
 		}
     	      
-		assertEquals("Proxy Client: unsuccessfull initialization",
+		assertEquals("LSFRemoteProxyTest: Proxy Client: FAILURE, unsuccessfull initialization",
+    	              false,
+    	              error);
+
+	}
+	
+	@Test public void events() {
+		boolean error = false;
+		boolean launchManually = false;
+		String proxy = "lsf/ptp_lsf_proxy";
+		
+		LSFProxyRuntimeClient client = new LSFProxyRuntimeClient(proxy, launchManually);
+		
+		if (client.startup(null)) {
+			try {
+				client.initialize();
+				client.sendEvents();
+				client.haltEvents();
+			} catch (IOException e) {
+				error = true;
+				e.printStackTrace();
+			}
+			client.shutdown();
+		} else {
+			error = true;
+		}
+    	      
+		assertEquals("LSFRemoteProxyTest: Proxy Client: FAILURE, unsuccessfull initialization",
     	              false,
     	              error);
 
