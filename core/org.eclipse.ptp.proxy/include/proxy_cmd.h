82b6c3866dbac2bd6c3eddb5fe21aa77ef261be7
crasmussen <crasmussen>
2007-01-08 00:34:50 +0000

    Initial version.  Modified wire protocol to use integer commands rather than strings.

---
+++ core/org.eclipse.ptp.proxy/include/proxy_cmd.h
@@ -0,0 +1,32 @@
+/******************************************************************************
+ * Copyright (c) 2005 The Regents of the University of California.
+ * This material was produced under U.S. Government contract W-7405-ENG-36
+ * for Los Alamos National Laboratory, which is operated by the University
+ * of California for the U.S. Department of Energy. The U.S. Government has
+ * rights to use, reproduce, and distribute this software. NEITHER THE
+ * GOVERNMENT NOR THE UNIVERSITY MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR
+ * ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE. If software is modified
+ * to produce derivative works, such modified software should be clearly  
+ * marked, so as not to confuse it with the version available from LANL.
+ *
+ * Additionally, this program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * LA-CC 04-115
+ ******************************************************************************/
+ 
+ #ifndef PROXY_CMD_H_
+#define PROXY_CMD_H_
+
+/*
+ * CMD codes must EXACTLY match org.eclipse.ptp.core.proxy.AbstractProxyClient
+ */
+
+#define CMD_INIT			1
+#define CMD_QUIT			2
+#define CMD_SEND_EVENTS		3
+#define CMD_HALT_EVENTS		4
+
+#endif /*PROXY_CMD_H_*/
