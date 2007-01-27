e60ca2287fca7e79030a57f057e859ecedb5c627
joverbey <joverbey>
2007-01-27 20:48:52 +0000

    Added line-based scope lookup for debugger

--- photran/org.eclipse.photran.core/parser/org/eclipse/photran/core/SourcePrinter.java
+++ photran/org.eclipse.photran.core/parser/org/eclipse/photran/core/SourcePrinter.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import org.eclipse.photran.internal.core.parser.ASTExecutableProgramNode;
+import org.eclipse.photran.internal.core.parser.ParseTreeNode;
 
 public class SourcePrinter
 {
@@ -13,14 +14,20 @@ public class SourcePrinter
     
     public static String getSourceCodeFromAST(IFortranAST ast)
     {
-        ASTExecutableProgramNode root = (ASTExecutableProgramNode)ast;
-        ByteArrayOutputStream out = new ByteArrayOutputStream(4096);
-        root.printOn(new PrintStream(out));
-        String result = out.toString();
+        String result = getSourceCodeFromASTNode((ASTExecutableProgramNode)ast);
+        
         // When we read in the AST, we use a LineAppendingInputStream so that the
         // user does not have to have a final carriage return in their file.  However,
         // we should chop that off here.
         result = result.substring(0, result.length() - EOL.length());
+        
         return result;
+    }
+    
+    public static String getSourceCodeFromASTNode(ParseTreeNode node)
+    {
+        ByteArrayOutputStream out = new ByteArrayOutputStream(4096);
+        node.printOn(new PrintStream(out));
+        return out.toString();
     }
 }
