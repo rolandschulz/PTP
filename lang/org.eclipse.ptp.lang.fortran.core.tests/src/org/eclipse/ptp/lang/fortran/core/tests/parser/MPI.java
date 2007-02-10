01263e7ca624812b908bd3cae79177b7c84b2c1d
crasmussen <crasmussen>
2007-02-10 01:24:00 +0000

    Initial version.

---
+++ lang/org.eclipse.ptp.lang.fortran.core.tests/src/org/eclipse/ptp/lang/fortran/core/tests/parser/MPI.java
@@ -0,0 +1,36 @@
+package org.eclipse.ptp.lang.fortran.core.tests.parser;
+
+import org.antlr.runtime.*;
+import org.eclipse.ptp.lang.fortran.core.parser.FortranLexer;
+import org.eclipse.ptp.lang.fortran.core.parser.FortranParser;
+import org.eclipse.ptp.lang.fortran.core.parser.FortranStream;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.eclipse.ptp.lang.fortran.core.parser.FortranMain;
+
+public class MPI {
+	
+	public static final String TEST_ROOT = "../org.eclipse.ptp.lang.fortran.core.tests/parser-test-files/";
+
+	private void parse(String file) {
+		Boolean error = Boolean.FALSE;
+		String path = TEST_ROOT + file;
+
+		try {
+			FortranMain fortran = new FortranMain(path);
+			error = fortran.call();
+		} catch(Exception e) {
+			 //        e.printStackTrace();
+	         error = Boolean.TRUE;
+		}
+
+		assertEquals("Fortran Parser: unsuccessfully parsed " + file,
+					 Boolean.FALSE,
+					 error.booleanValue());
+	}
+   
+	@Test public void mpi_send() {
+		parse("mpi/mpi_send.f90");
+	}
+
+}
