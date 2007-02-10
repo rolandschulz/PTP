d32fed31abbc0643f0b27224f74b42e1f9d4cd92
crasmussen <crasmussen>
2007-02-10 01:36:17 +0000

    Added empty modules.

--- lang/org.eclipse.ptp.lang.fortran.core.tests/parser-test-files/misc/test_module.f90
+++ lang/org.eclipse.ptp.lang.fortran.core.tests/parser-test-files/misc/test_module.f90
@@ -5,3 +5,25 @@ contains
   subroutine test_0()
   end subroutine test_0
 end module test_module
+
+module test_module_0
+
+end module test_module_0
+
+module test_module_1
+
+end module test_module_1
+
+module test_module_2
+
+end module test_module_2
+
+module test_module_3
+
+end module test_module_3
+
+module test_module_4
+
+end module test_module_4
+
+
