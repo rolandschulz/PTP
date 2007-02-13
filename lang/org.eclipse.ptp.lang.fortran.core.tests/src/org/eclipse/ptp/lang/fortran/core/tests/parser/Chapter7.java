e1e1a0aace3d2fe2349c428f1eaf33f0f2a28f12
crasmussen <crasmussen>
2007-02-13 21:36:50 +0000

    Added new test cases.

--- lang/org.eclipse.ptp.lang.fortran.core.tests/src/org/eclipse/ptp/lang/fortran/core/tests/parser/Chapter7.java
+++ lang/org.eclipse.ptp.lang.fortran.core.tests/src/org/eclipse/ptp/lang/fortran/core/tests/parser/Chapter7.java
@@ -38,5 +38,57 @@ public class Chapter7 {
    @Test public void note_7_3() {
       parse("chapter_7/note_7.3.f03");
    }
+
+   @Test public void note_7_10() {
+      parse("chapter_7/note_7.10.f03");
+   }
+
+   @Test public void note_7_11() {
+      parse("chapter_7/note_7.11.f03");
+   }
+
+   @Test public void note_7_43() {
+      parse("chapter_7/note_7.43.f03");
+   }
+
+   @Test public void note_7_44() {
+      parse("chapter_7/note_7.44.f03");
+   }
+
+   @Test public void note_7_45() {
+      parse("chapter_7/note_7.45.f03");
+   }
+
+   @Test public void note_7_47() {
+      parse("chapter_7/note_7.47.f03");
+   }
+
+   @Test public void note_7_48() {
+      parse("chapter_7/note_7.48.f03");
+   }
+
+   @Test public void note_7_49() {
+      parse("chapter_7/note_7.49.f03");
+   }
+
+   @Test public void note_7_50() {
+      parse("chapter_7/note_7.50.f03");
+   }
+
+   @Test public void note_7_51() {
+      parse("chapter_7/note_7.51.f03");
+   }
+
+   @Test public void note_7_55() {
+      parse("chapter_7/note_7.55.f03");
+   }
+
+   @Test public void note_7_56() {
+      parse("chapter_7/note_7.56.f03");
+   }
+
+   @Test public void note_7_57() {
+      parse("chapter_7/note_7.57.f03");
+   }
 }// end class Chapter7
 
