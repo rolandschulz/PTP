8723045d80ba0bec7d06b3ad76280ad68eddc5a2
crasmussen <crasmussen>
2007-02-13 22:13:58 +0000

    Added new test cases.

--- lang/org.eclipse.ptp.lang.fortran.core.tests/src/org/eclipse/ptp/lang/fortran/core/tests/parser/Chapter8.java
+++ lang/org.eclipse.ptp.lang.fortran.core.tests/src/org/eclipse/ptp/lang/fortran/core/tests/parser/Chapter8.java
@@ -26,34 +26,61 @@ public class Chapter8 {
 	                   error.booleanValue());
 	   }
 	   
-   
-	@Test public void note_8_1() {
+   	@Test public void note_8_1() {
 		parse("chapter_8/note_8.1.f03");
 	}
+   
+   @Test public void note_8_2() {
+      parse("chapter_8/note_8.2.f03");
+   }
 
-	@Test public void note_8_2() {
-		parse("chapter_8/note_8.2.f03");
-	}
+   @Test public void note_8_4() {
+      parse("chapter_8/note_8.4.f03");
+   }
 
-	@Test public void note_8_6() {
-		parse("chapter_8/note_8.6.f03");
-	}
+   @Test public void note_8_5() {
+      parse("chapter_8/note_8.5.f03");
+   }
 
-	@Test public void note_8_7() {
-		parse("chapter_8/note_8.7.f03");
-	}
+   @Test public void note_8_6() {
+      parse("chapter_8/note_8.6.f03");
+   }
+
+   @Test public void note_8_7() {
+      parse("chapter_8/note_8.7.f03");
+   }
+
+   @Test public void note_8_8() {
+      parse("chapter_8/note_8.8.f03");
+   }
+
+   @Test public void note_8_9() {
+      parse("chapter_8/note_8.9.f03");
+   }
+
+   @Test public void note_8_10() {
+      parse("chapter_8/note_8.10.f03");
+   }
+
+   @Test public void note_8_13() {
+      parse("chapter_8/note_8.13.f03");
+   }
+
+   @Test public void note_8_14() {
+      parse("chapter_8/note_8.14.f03");
+   }
 
 	@Test public void note_8_16() {
 		parse("chapter_8/note_8.16.f03");
 	}
 
-	@Test public void note_8_17() {
-		parse("chapter_8/note_8.17.f03");
-	}
+   @Test public void note_8_17() {
+      parse("chapter_8/note_8.17.f03");
+   }
 
-	@Test public void note_8_18() {
-		parse("chapter_8/note_8.18.f03");
-	}
+   @Test public void note_8_18() {
+      parse("chapter_8/note_8.18.f03");
+   }
 
-} // end class Chapter8
+}// end class Chapter8
 
