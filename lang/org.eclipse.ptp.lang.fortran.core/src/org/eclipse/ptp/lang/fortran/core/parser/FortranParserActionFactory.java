2a6882bb959f06478871cce08c4f57eb77e667e0
crasmussen <crasmussen>
2007-02-21 10:49:07 +0000

    Added FortranParserActionNull.

--- lang/org.eclipse.ptp.lang.fortran.core/src/org/eclipse/ptp/lang/fortran/core/parser/FortranParserActionFactory.java
+++ lang/org.eclipse.ptp.lang.fortran.core/src/org/eclipse/ptp/lang/fortran/core/parser/FortranParserActionFactory.java
@@ -1,11 +1,19 @@
+// remove the following two lines for use outside of Eclipse
 package org.eclipse.ptp.lang.fortran.core.parser;
-
-import org.eclipse.ptp.lang.fortran.internal.core.dom.parser.FortranParserAction;
+import org.eclipse.ptp.lang.fortran.internal.core.dom.parser.FortranParserActionDom;
 
 public class FortranParserActionFactory {
 	
 	static IFortranParserAction newAction(FortranParser parser, String kind) {
-		return new FortranParserAction(parser);
+		IFortranParserAction action = null;
+		if (kind.compareToIgnoreCase("cdt") == 0) {
+			// remove following line for use outside of Eclipse
+			action = new FortranParserActionDom(parser);
+		}
+		if (action == null) {
+			action = new FortranParserActionNull(parser);
+		}
+		return action;
 	}
 
 }
