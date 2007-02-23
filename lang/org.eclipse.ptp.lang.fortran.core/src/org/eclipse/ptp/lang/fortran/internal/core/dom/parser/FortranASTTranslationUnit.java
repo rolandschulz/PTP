88b6a824074580e8a9e9bbce699904f4e5dede48
crasmussen <crasmussen>
2007-02-23 11:51:56 +0000

    Added copyright notice.

--- lang/org.eclipse.ptp.lang.fortran.core/src/org/eclipse/ptp/lang/fortran/internal/core/dom/parser/FortranASTTranslationUnit.java
+++ lang/org.eclipse.ptp.lang.fortran.core/src/org/eclipse/ptp/lang/fortran/internal/core/dom/parser/FortranASTTranslationUnit.java
@@ -1,15 +1,20 @@
 /*******************************************************************************
- * Copyright (c) 2002, 2007 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
+ * Copyright (c) 2005, 2006 Los Alamos National Security, LLC.
+ * This material was produced under U.S. Government contract DE-AC52-06NA25396
+ * for Los Alamos National Laboratory (LANL), which is operated by the Los Alamos
+ * National Security, LLC (LANS) for the U.S. Department of Energy. The U.S. Government has
+ * rights to use, reproduce, and distribute this software. NEITHER THE
+ * GOVERNMENT NOR LANS MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR
+ * ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE. If software is modified
+ * to produce derivative works, such modified software should be clearly marked,
+ * so as not to confuse it with the version available from LANL.
+ *
+ * Additionally, this program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- * IBM Rational Software - Initial API and implementation
- * Markus Schorn (Wind River Systems)
- * Yuan Zhang / Beth Tibbitts (IBM Research)
  *******************************************************************************/
+
 package org.eclipse.ptp.lang.fortran.internal.core.dom.parser;
 
 import org.eclipse.cdt.core.CCorePlugin;
