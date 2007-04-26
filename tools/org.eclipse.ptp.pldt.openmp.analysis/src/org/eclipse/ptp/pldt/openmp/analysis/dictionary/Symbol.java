5989a68f33d59c98a532130cbeee2b4e8c5a7b0b
btibbitts <btibbitts>
2007-04-26 22:41:46 +0000

    cdt 4.0 changes: get ast node other than from IScope

--- tools/org.eclipse.ptp.pldt.openmp.analysis/src/org/eclipse/ptp/pldt/openmp/analysis/dictionary/Symbol.java
+++ tools/org.eclipse.ptp.pldt.openmp.analysis/src/org/eclipse/ptp/pldt/openmp/analysis/dictionary/Symbol.java
@@ -10,8 +10,6 @@
  *******************************************************************************/
 package org.eclipse.ptp.pldt.openmp.analysis.dictionary;
 
-import org.eclipse.cdt.core.dom.ILinkage;
-import org.eclipse.cdt.core.dom.IName;
 import org.eclipse.cdt.core.dom.ast.DOMException;
 import org.eclipse.cdt.core.dom.ast.IASTDeclaration;
 import org.eclipse.cdt.core.dom.ast.IASTDeclarator;
@@ -22,6 +20,10 @@ import org.eclipse.cdt.core.dom.ast.IASTParameterDeclaration;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 import org.eclipse.cdt.core.dom.ast.IBinding;
 import org.eclipse.cdt.core.dom.ast.IScope;
+import org.eclipse.cdt.internal.core.dom.parser.c.CFunction;
+import org.eclipse.cdt.internal.core.dom.parser.c.CParameter;
+import org.eclipse.cdt.internal.core.dom.parser.c.CTypedef;
+import org.eclipse.cdt.internal.core.dom.parser.c.CVariable;
 
 /**
  * Holds a symbol definition
@@ -29,136 +31,96 @@ import org.eclipse.cdt.core.dom.ast.IScope;
  * @author pazel
  * 
  */
-public class Symbol {
-	protected IASTDeclarator declarator_ = null; // what is being defined
-	// below can be either IASTDeclaration or IASTParameterDeclaration
-	protected IASTNode declaration_ = null; // broader containing statement
 
-	/**
-	 * Symbol - constructor
-	 * 
-	 * @param declarator -
-	 *            IASTDeclarator
-	 * @param declaration -
-	 *            IASTDeclartion (for this declarator - and possibly others
-	 */
-	public Symbol(IASTDeclarator declarator, IASTDeclaration declaration) {
-		declarator_ = declarator;
-		declaration_ = declaration;
-	}
+public class Symbol
+{
+	/** what is being defined */
+    protected IASTDeclarator      declarator_  = null;   
+    /**
+     * declaration can be  either IASTDeclaration or IASTParameterDeclaration;
+     * this is the broader containing statement
+     * */
+    protected IASTNode            declaration_ = null;   
+    private static final boolean traceOn=false;
+    
+    /**
+     *  constructor
+     * @param declarator  - IASTDeclarator
+     * @param declaration - IASTDeclartion (for this declarator - and possibly others
+     */
+    public Symbol( IASTDeclarator declarator, IASTDeclaration declaration)
+    {
+        declarator_  = declarator;
+        declaration_ = declaration;
+    }
+    
+    /**
+     *  constructor
+     * @param declarator  - IASTDeclarator
+     * @param declaration - IASTParameterDeclartion (for this declarator - and possibly others
+     */
+    public Symbol( IASTDeclarator declarator, IASTParameterDeclaration declaration)
+    {
+        declarator_  = declarator;
+        declaration_ = declaration;
+    }
+    
+    /**
+     * getName - get the name of the declarator
+     * @return IASTName
+     */
+    public IASTName getName()
+    {
+        return declarator_.getName();
+    }
+    
+    /**
+     * getDeclarator - accessor to declarator
+     * @return - IASTDeclarator
+     */
+    public IASTDeclarator getDeclarator()
+    {
+        return declarator_;
+    }
+    
+    /**
+     * getDeclaration - get the related declaration
+     * @return - IASTNode  (can be IASTDeclaration or IASTParameterDeclaration)
+     */
+    public IASTNode getDeclaration()
+    {
+        return declaration_;
+    }
+    
+    /**
+     * getScope - get the scope for this declarator
+     * @return IScope
+     */
+    public IScope getScope()
+    {
+        // find the unique scope for this name
+        IASTName name = declarator_.getName();
+        IBinding rb=name.resolveBinding();
+        IBinding binding = name.getBinding();
+        if (binding==null)  return null;
+        
+        IScope scope = null;
+        try {
+            scope = binding.getScope();
+        }
+        catch(DOMException e) { System.out.println("SymbolBucket.find: exception "+e);  return null; }
+        
+        return scope;
+    }
 
-	/**
-	 * Symbol - constructor
-	 * 
-	 * @param declarator -
-	 *            IASTDeclarator
-	 * @param declaration -
-	 *            IASTParameterDeclaration (for this declarator - and possibly
-	 *            others
-	 */
-	public Symbol(IASTDeclarator declarator,
-			IASTParameterDeclaration declaration) {
-		declarator_ = declarator;
-		declaration_ = declaration;
-	}
+    
+    /**
+     * getDefiningFunction - get the function in which declarator is defined
+     * @return IASTNode - either IASTTranslationUnit or IASTFunctionDefinition
+     */
 
-	/**
-	 * getName - get the name of the declarator
-	 * 
-	 * @return IASTName
-	 */
-	public IASTName getName() {
-		return declarator_.getName();
-	}
-
-	/**
-	 * getDeclartor - accessor to declarator
-	 * 
-	 * @return - IASTDeclarator
-	 */
-	public IASTDeclarator getDeclarator() {
-		return declarator_;
-	}
-
-	/**
-	 * getDeclaration - get the related declaration
-	 * 
-	 * @return - IASTNode (can be IASTDeclaration or IASTParameterDeclaration)
-	 */
-	public IASTNode getDeclaration() {
-		return declaration_;
-	}
-
-	/**
-	 * getScope - get the scope for this declarator
-	 * 
-	 * @return IScope
-	 */
-	public IScope getScope() {
-		// find the unique scope for this name
-		IASTName name = declarator_.getName();
-		name.resolveBinding();
-		IBinding binding = name.getBinding();
-		if (binding == null)
-			return null;
-
-		IScope scope = null;
-		try {
-			scope = binding.getScope();
-		} catch (DOMException e) {
-			System.out.println("SymbolBucket.find: exception " + e);
-			return null;
-		}
-
-		return scope;
-	}
-
-	/**
-	 * getDefiningFunction - get the function in which declarator is defined
-	 * <br>
-	 * TODO: complete port to CDT 4.0
-	 * 
-	 * @return IASTNode - either IASTTranslationUnit or IASTFunctionDefinition
-	 */
-	public IASTNode getDefiningFunction() {
-		IScope scope = getScope();
-		if (scope == null)
-			return null;
-
-		// cdt40
-		IASTNode node = null;
-		IName sname = null;
-		try {
-			sname = scope.getScopeName();
-			// cdt40 attempt at getting node from scope:
-			if (sname instanceof IASTName) {
-				IASTName astName = (IASTName) sname;
-				IBinding binding = scope.getBinding(astName, true);
-				System.out.println("Symbol: attempting to get node ... TBD");
-				// how to get a node from a binding?
-				// ILinkage linkage = binding.getLinkage();//would this help?
-				// ??? can we get the node from the binding?
-			}
-		} catch (DOMException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		IASTName name = declarator_.getName();
-		name.resolveBinding();
-		IBinding binding = name.getBinding();
-		if (binding == null)
-			return null;
-
-		// cdt 3.1 version: how do we get the node from the scope?
-		// IASTNode node = null;
-		// try {
-		// node = scope.getPhysicalNode();
-		// }
-		// catch(DOMException e) {
-		// System.out.println("Symbol.getFunction exception "+e);
-		// return null;
-		// }
+    public IASTNode getDefiningFunction() {
+		IASTNode node = getPhysicalNode();
 
 		// keep moving up the tree until we find the node
 		while (true) {
@@ -170,5 +132,45 @@ public class Symbol {
 				return node; // our function
 			node = node.getParent();
 		}
+	}
+
+
+    /**
+     * get the physical node (IASTNode) that this symbol is contained in.
+     * <br>
+     * (Formerly supplied by IScope.getPhysicalNode() )
+     * @return
+     */
+	public IASTNode getPhysicalNode() {
+		IASTNode node=null;
+		IASTName dn = declarator_.getName();
+			if (dn instanceof IASTName) {
+			IBinding binding = dn.resolveBinding();
+
+			if (binding instanceof CFunction) {
+				CFunction cf = (CFunction) binding;
+				if(traceOn)System.out.println("Symbol .. CFunction: "+cf.getName());
+				node = cf.getPhysicalNode();
+			}
+			else if (binding instanceof CParameter){
+				CParameter cp = (CParameter)binding;
+				if(traceOn)System.out.println("Symbol .. CParameter: "+cp.getName());
+				 
+			}
+			else if (binding instanceof CVariable){
+				CVariable cv=(CVariable)binding;
+				if(traceOn)System.out.println("Symbol .. CVariable: "+cv.getName());
+				node = cv.getPhysicalNode();
+			}
+			else if (binding instanceof CTypedef){
+				CTypedef ct=(CTypedef)binding;
+				if(traceOn)System.out.println("Symbol .. CTypedef: "+ct.getName());
+				node=ct.getPhysicalNode();
+			}
+			else{//any other types I can get a physical node from???
+				System.out.println("**\n**Symbol.getPhysicalNode(): Binding type unknown. is: "+binding);//ProblemBinding
+			}
+		}
+		return node;
 	}
 }
