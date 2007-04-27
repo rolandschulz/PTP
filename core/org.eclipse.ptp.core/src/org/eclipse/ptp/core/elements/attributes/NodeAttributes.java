3cacc4a23e056fb8b4d9ca4668c4ee505faa5b1f
Greg Watson <g.watson@computer.org>
2007-04-27 21:20:31 +0000

    Added node number attribute.

--- core/org.eclipse.ptp.core/src/org/eclipse/ptp/core/elements/attributes/NodeAttributes.java
+++ core/org.eclipse.ptp.core/src/org/eclipse/ptp/core/elements/attributes/NodeAttributes.java
@@ -3,6 +3,8 @@ package org.eclipse.ptp.core.elements.attributes;
 import org.eclipse.ptp.core.attributes.EnumeratedAttributeDefinition;
 import org.eclipse.ptp.core.attributes.IAttributeDefinition;
 import org.eclipse.ptp.core.attributes.IEnumeratedAttributeDefinition;
+import org.eclipse.ptp.core.attributes.IIntegerAttributeDefinition;
+import org.eclipse.ptp.core.attributes.IntegerAttributeDefinition;
 
 
 /**
@@ -17,15 +19,23 @@ public class NodeAttributes {
 	};
 
 	private static final String STATE_ATTR_ID = "nodeState";
+	private static final String NUMBER_ATTR_ID = "nodeNumber";
 
 	private final static IEnumeratedAttributeDefinition stateAttrDef = 
 		new EnumeratedAttributeDefinition(STATE_ATTR_ID, "state", "Node State", State.UNKNOWN, State.values());
+	
+	private final static IIntegerAttributeDefinition numAttrDef = 
+		new IntegerAttributeDefinition(NUMBER_ATTR_ID, NUMBER_ATTR_ID, "Node Number", 0);
 	
 	public static IEnumeratedAttributeDefinition getStateAttributeDefinition() {
 		return stateAttrDef;
 	}
 	
+	public static IIntegerAttributeDefinition getNumberAttributeDefinition() {
+		return numAttrDef;
+	}
+	
 	public static IAttributeDefinition[] getDefaultAttributeDefinitions() {
-		return new IAttributeDefinition[]{stateAttrDef};
+		return new IAttributeDefinition[]{stateAttrDef, numAttrDef};
 	}
 }
