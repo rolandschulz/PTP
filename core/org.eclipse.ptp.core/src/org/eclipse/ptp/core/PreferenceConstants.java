e1ae34815d34079fdf066e7a97f8feb580823862
Greg Watson <g.watson@computer.org>
2007-04-28 21:31:49 +0000

    Remove deprecated constants.

--- core/org.eclipse.ptp.core/src/org/eclipse/ptp/core/PreferenceConstants.java
+++ core/org.eclipse.ptp.core/src/org/eclipse/ptp/core/PreferenceConstants.java
@@ -20,12 +20,6 @@ package org.eclipse.ptp.core;
 
 public interface PreferenceConstants {
 	public static final String SET_ROOT_ID = "Root";
-	//public static final String JOB_DEBUG_SESSION = "debug_session";
-	public static final String JOB_DEBUG_DIR = "debug_dir";
-	public static final String JOB_APP_NAME = "app_name";
-	public static final String JOB_APP_PATH = "app_path";
-	public static final String JOB_WORK_DIR = "work_dir";
-	public static final String JOB_ARGS = "args";
 	
 	public static final String OUTPUT_DIR = "OUTPUT_DIR";
 	public static final String STORE_LINE = "STORE_LINE";
