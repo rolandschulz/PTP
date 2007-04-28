9e0abf97c2f22e3a98ed9b9fbe71034c465f9bef
Greg Watson <g.watson@computer.org>
2007-04-28 21:32:24 +0000

    Debug attributes.

--- core/org.eclipse.ptp.core/src/org/eclipse/ptp/core/elements/attributes/JobAttributes.java
+++ core/org.eclipse.ptp.core/src/org/eclipse/ptp/core/elements/attributes/JobAttributes.java
@@ -26,38 +26,46 @@ public class JobAttributes {
 	
 	private static final String STATE_ATTR_ID = "jobState";
 	private static final String SUBID_ATTR_ID = "jobSubId";
-	private static final String EXECNAME_ATTR_ID = "execName";
-	private static final String EXECPATH_ATTR_ID = "execPath";
-	private static final String WORKINGDIR_ATTR_ID = "workingDir";
-	private static final String PROGARGS_ATTR_ID = "progArgs";
+	private static final String EXEC_NAME_ATTR_ID = "execName";
+	private static final String EXEC_PATH_ATTR_ID = "execPath";
+	private static final String WORKING_DIR_ATTR_ID = "workingDir";
+	private static final String PROG_ARGS_ATTR_ID = "progArgs";
 	private static final String ENV_ATTR_ID = "env";
-	private static final String DEBUG_PATH_ATTR_ID = "debugPath";
+	private static final String DEBUG_EXEC_NAME_ATTR_ID = "debugExecName";
+	private static final String DEBUG_EXEC_PATH_ATTR_ID = "debugExecPath";
+	private static final String DEBUG_BACKEND_PATH_ATTR_ID = "debugBackendPath";
 	private static final String DEBUG_ARGS_ATTR_ID = "debugArgs";
 	private static final String DEBUG_FLAG_ATTR_ID = "debug";
 
 	private final static IEnumeratedAttributeDefinition stateAttrDef = 
-		new EnumeratedAttributeDefinition(STATE_ATTR_ID, "state", "Job State", State.STARTED, State.values());
+		new EnumeratedAttributeDefinition(STATE_ATTR_ID, "Job State", "State of a job", State.STARTED, State.values());
 
 	private final static IIntegerAttributeDefinition subIdAttrDef = 
-		new IntegerAttributeDefinition(SUBID_ATTR_ID, SUBID_ATTR_ID, "Job Submission ID", 0);
+		new IntegerAttributeDefinition(SUBID_ATTR_ID, "Job Submission ID", "Temporary ID used for job submission", 0);
 
 	private final static IStringAttributeDefinition execNameAttrDef = 
-		new StringAttributeDefinition(EXECNAME_ATTR_ID, EXECNAME_ATTR_ID, "Executable Name", "");
+		new StringAttributeDefinition(EXEC_NAME_ATTR_ID, "Executable Name", "Name of executable to be launched", "");
 
 	private final static IStringAttributeDefinition execPathAttrDef = 
-		new StringAttributeDefinition(EXECPATH_ATTR_ID, EXECPATH_ATTR_ID, "Executable Path", "");
+		new StringAttributeDefinition(EXEC_PATH_ATTR_ID, "Executable Path", "Path of executable to be launched", "");
 
 	private final static IStringAttributeDefinition workingDirAttrDef = 
-		new StringAttributeDefinition(WORKINGDIR_ATTR_ID, WORKINGDIR_ATTR_ID, "Working Directory", "");
+		new StringAttributeDefinition(WORKING_DIR_ATTR_ID, "Working Directory", "Working directory where executable will run", "");
 
 	private final static IArrayAttributeDefinition progArgsAttrDef = 
-		new ArrayAttributeDefinition(PROGARGS_ATTR_ID, PROGARGS_ATTR_ID, "Program Arguments", null);
+		new ArrayAttributeDefinition(PROG_ARGS_ATTR_ID, "Program Arguments", "Command-line arguments supplied to executable", null);
 
 	private final static IArrayAttributeDefinition envAttrDef = 
-		new ArrayAttributeDefinition(ENV_ATTR_ID, ENV_ATTR_ID, "Environment", null);
+		new ArrayAttributeDefinition(ENV_ATTR_ID, "Environment", "Environment to be supplied to executable on launch", null);
 
-	private final static IStringAttributeDefinition debugPathAttrDef = 
-		new StringAttributeDefinition(DEBUG_PATH_ATTR_ID, DEBUG_PATH_ATTR_ID, "Debugger Path", "");
+	private final static IStringAttributeDefinition debugExecNameAttrDef = 
+		new StringAttributeDefinition(DEBUG_EXEC_NAME_ATTR_ID, "Debugger Executable Name", "Name of debugger executable", "");
+
+	private final static IStringAttributeDefinition debugExecPathAttrDef = 
+		new StringAttributeDefinition(DEBUG_EXEC_PATH_ATTR_ID, "Debugger Executable NaPathme", "Path to debugger executable", "");
+
+	private final static IStringAttributeDefinition debugBackendAttrDef = 
+		new StringAttributeDefinition(DEBUG_BACKEND_PATH_ATTR_ID, "Debugger Backend Path", "Path to backend debugger", "");
 
 	private final static IArrayAttributeDefinition debugArgsAttrDef = 
 		new ArrayAttributeDefinition(DEBUG_ARGS_ATTR_ID, DEBUG_ARGS_ATTR_ID, "Debugger Arguments", null);
@@ -93,8 +101,16 @@ public class JobAttributes {
 		return envAttrDef;
 	}
 
-	public static IStringAttributeDefinition getDebuggerPathAttributeDefinition() {
-		return debugPathAttrDef;
+	public static IStringAttributeDefinition getDebuggerExecutableNameAttributeDefinition() {
+		return debugExecNameAttrDef;
+	}
+
+	public static IStringAttributeDefinition getDebuggerExecutablePathAttributeDefinition() {
+		return debugExecPathAttrDef;
+	}
+
+	public static IStringAttributeDefinition getDebuggerBackendPathAttributeDefinition() {
+		return debugBackendAttrDef;
 	}
 
 	public static IArrayAttributeDefinition getDebuggerArgumentsAttributeDefinition() {
@@ -114,7 +130,9 @@ public class JobAttributes {
 					workingDirAttrDef,
 					progArgsAttrDef,
 					envAttrDef,
-					debugPathAttrDef,
+					debugExecNameAttrDef,
+					debugExecPathAttrDef,
+					debugBackendAttrDef,
 					debugArgsAttrDef,
 					debugFlagAttrDef
 				};
