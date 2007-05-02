50dbccb3c4a4ae67cf43a479d4e849ba989f1cfb
gwatson <gwatson>
2007-05-02 23:53:31 +0000

    Missing argument

--- debug/org.eclipse.ptp.debug.sdm.core/src/org/eclipse/ptp/debug/external/core/proxy/command/AbstractProxyDebugCommand.java
+++ debug/org.eclipse.ptp.debug.sdm.core/src/org/eclipse/ptp/debug/external/core/proxy/command/AbstractProxyDebugCommand.java
@@ -1,5 +1,6 @@
 package org.eclipse.ptp.debug.external.core.proxy.command;
 
+import org.eclipse.ptp.core.proxy.AbstractProxyClient;
 import org.eclipse.ptp.core.proxy.IProxyClient;
 import org.eclipse.ptp.core.proxy.command.AbstractProxyCommand;
 import org.eclipse.ptp.core.util.BitList;
@@ -8,9 +9,10 @@ public abstract class AbstractProxyDebugCommand extends AbstractProxyCommand {
 	
 	protected AbstractProxyDebugCommand(IProxyClient client, BitList procs) {
 		super(client);
+		addArgument(procs);
 	}
 
 	protected void addArgument(BitList bits) {
-		addArgument(bits.toString());
+		addArgument(AbstractProxyClient.encodeBitSet(bits));
 	}
 }
