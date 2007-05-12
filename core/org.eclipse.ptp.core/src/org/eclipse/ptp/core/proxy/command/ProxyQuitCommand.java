0d7a41f56c47868f703b4c3305f653a02a41fc65
Greg Watson <g.watson@computer.org>
2007-05-12 12:58:30 +0000

    Missing comments

--- core/org.eclipse.ptp.core/src/org/eclipse/ptp/core/proxy/command/ProxyQuitCommand.java
+++ core/org.eclipse.ptp.core/src/org/eclipse/ptp/core/proxy/command/ProxyQuitCommand.java
@@ -1,7 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - Initial API and implementation
+ *******************************************************************************/
+
 package org.eclipse.ptp.core.proxy.command;
 
 import org.eclipse.ptp.core.proxy.IProxyClient;
-import org.eclipse.ptp.core.proxy.command.AbstractProxyCommand;
 
 public class ProxyQuitCommand extends AbstractProxyCommand {
 
