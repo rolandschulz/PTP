563a54e1fb416ef57c08e991a06b8a39ca39d56c
Greg Watson <g.watson@computer.org>
2007-05-17 18:24:51 +0000

    Added code to clean up model properly when the RM is shut down. All the remove child handlers will now be called.

--- core/org.eclipse.ptp.core/src/org/eclipse/ptp/rmsystem/AbstractResourceManager.java
+++ core/org.eclipse.ptp.core/src/org/eclipse/ptp/rmsystem/AbstractResourceManager.java
@@ -60,15 +60,27 @@ import org.eclipse.ptp.core.elements.attributes.ProcessAttributes;
 import org.eclipse.ptp.core.elements.attributes.QueueAttributes;
 import org.eclipse.ptp.core.elements.attributes.ResourceManagerAttributes;
 import org.eclipse.ptp.core.elements.attributes.ResourceManagerAttributes.State;
+import org.eclipse.ptp.core.elements.events.IJobChangedProcessEvent;
+import org.eclipse.ptp.core.elements.events.IJobNewProcessEvent;
+import org.eclipse.ptp.core.elements.events.IJobRemoveProcessEvent;
 import org.eclipse.ptp.core.elements.events.IMachineChangedEvent;
+import org.eclipse.ptp.core.elements.events.IMachineChangedNodeEvent;
+import org.eclipse.ptp.core.elements.events.IMachineNewNodeEvent;
+import org.eclipse.ptp.core.elements.events.IMachineRemoveNodeEvent;
 import org.eclipse.ptp.core.elements.events.IQueueChangedEvent;
+import org.eclipse.ptp.core.elements.events.IQueueChangedJobEvent;
+import org.eclipse.ptp.core.elements.events.IQueueNewJobEvent;
+import org.eclipse.ptp.core.elements.events.IQueueRemoveJobEvent;
 import org.eclipse.ptp.core.elements.events.IResourceManagerChangedEvent;
 import org.eclipse.ptp.core.elements.events.IResourceManagerChangedMachineEvent;
 import org.eclipse.ptp.core.elements.events.IResourceManagerChangedQueueEvent;
 import org.eclipse.ptp.core.elements.events.IResourceManagerErrorEvent;
 import org.eclipse.ptp.core.elements.events.IResourceManagerNewMachineEvent;
 import org.eclipse.ptp.core.elements.events.IResourceManagerNewQueueEvent;
+import org.eclipse.ptp.core.elements.listeners.IJobProcessListener;
 import org.eclipse.ptp.core.elements.listeners.IMachineListener;
+import org.eclipse.ptp.core.elements.listeners.IMachineNodeListener;
+import org.eclipse.ptp.core.elements.listeners.IQueueJobListener;
 import org.eclipse.ptp.core.elements.listeners.IQueueListener;
 import org.eclipse.ptp.core.elements.listeners.IResourceManagerListener;
 import org.eclipse.ptp.core.elements.listeners.IResourceManagerMachineListener;
@@ -91,7 +103,7 @@ import org.eclipse.ptp.internal.core.elements.events.ResourceManagerNewQueueEven
  * 
  */
 public abstract class AbstractResourceManager extends PElement implements IResourceManager,
-IResourceManagerControl {
+	IResourceManagerControl {
 	
 	private static IAttribute[] getDefaultAttributes(IResourceManagerConfiguration config) {
 		IAttribute nameAttr = null;
@@ -110,10 +122,13 @@ IResourceManagerControl {
 	
 	private final IQueueListener queueListener;
 	private final IMachineListener machineListener;
-
+	private final IQueueJobListener queueJobListener;
+	private final IJobProcessListener jobProcessListener;
+	private final IMachineNodeListener machineNodeListener;
+	
 	private final IResourceManagerConfiguration config;
 	private EnumeratedAttribute<ResourceManagerAttributes.State> state;
-	private AttributeDefinitionManager attrDefManager;
+	private AttributeDefinitionManager attrDefManager = new AttributeDefinitionManager();
 
 	private final HashMap<String, IPJobControl> jobsById = new HashMap<String, IPJobControl>();
 	private final HashMap<String, IPMachineControl> machinesById = new HashMap<String, IPMachineControl>();
@@ -128,23 +143,52 @@ IResourceManagerControl {
 		super(id, universe, P_RESOURCE_MANAGER, getDefaultAttributes(config));
 		this.config = config;
 		this.state = ResourceManagerAttributes.getStateAttributeDefinition().create(State.STOPPED);
-		this.attrDefManager = new AttributeDefinitionManager();
-		this.attrDefManager.setAttributeDefinitions(ElementAttributes.getDefaultAttributeDefinitions());
-		this.attrDefManager.setAttributeDefinitions(JobAttributes.getDefaultAttributeDefinitions());
-		this.attrDefManager.setAttributeDefinitions(MachineAttributes.getDefaultAttributeDefinitions());
-		this.attrDefManager.setAttributeDefinitions(NodeAttributes.getDefaultAttributeDefinitions());
-		this.attrDefManager.setAttributeDefinitions(ProcessAttributes.getDefaultAttributeDefinitions());
-		this.attrDefManager.setAttributeDefinitions(QueueAttributes.getDefaultAttributeDefinitions());
-		this.attrDefManager.setAttributeDefinitions(ResourceManagerAttributes.getDefaultAttributeDefinitions());
 
 		queueListener = new IQueueListener(){
 			public void handleEvent(IQueueChangedEvent e) {
 				fireQueueChanged(e.getSource(), e.getAttributes());				
-			}};
+			}
+		};
 		machineListener = new IMachineListener(){
 			public void handleEvent(IMachineChangedEvent e) {
 				fireMachineChanged(e.getSource(), e.getAttributes());				
-			}};
+			}
+		};
+		queueJobListener = new IQueueJobListener(){
+			public void handleEvent(IQueueChangedJobEvent e) {
+				// OK to ignore
+			}
+			public void handleEvent(IQueueNewJobEvent e) {
+				// OK to ignore
+			}
+			public void handleEvent(IQueueRemoveJobEvent e) {
+				IPJob job = e.getJob();
+				job.removeChildListener(jobProcessListener);
+				jobsById.remove(job.getID());
+			}
+		};
+		jobProcessListener = new IJobProcessListener(){
+			public void handleEvent(IJobChangedProcessEvent e) {
+				// OK to ignore
+			}
+			public void handleEvent(IJobNewProcessEvent e) {
+				// OK to ignore		
+			}
+			public void handleEvent(IJobRemoveProcessEvent e) {
+				processesById.remove(e.getProcess().getID());
+			}			
+		};
+		machineNodeListener = new IMachineNodeListener(){
+			public void handleEvent(IMachineChangedNodeEvent e) {
+				// OK to ignore
+			}
+			public void handleEvent(IMachineNewNodeEvent e) {
+				// OK to ignore
+			}
+			public void handleEvent(IMachineRemoveNodeEvent e) {
+				nodesById.remove(e.getNode().getID());
+			}
+		};
 	}
 
 	/* (non-Javadoc)
@@ -224,6 +268,9 @@ IResourceManagerControl {
 		return attrDefManager.getAttributeDefinition(attrId);
 	}
 
+	/**
+	 * @return
+	 */
 	public AttributeDefinitionManager getAttributeDefinitionManager() {
 		return attrDefManager;
 	}
@@ -252,14 +299,23 @@ IResourceManagerControl {
 		return super.getID();
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.ptp.core.elements.IResourceManager#getMachineById(java.lang.String)
+	 */
 	public synchronized IPMachine getMachineById(String id) {
 		return machinesById.get(id);
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.ptp.core.elementcontrols.IResourceManagerControl#getMachineControls()
+	 */
 	public synchronized IPMachineControl[] getMachineControls() {
 		return (IPMachineControl[]) machinesById.values().toArray(new IPMachineControl[0]);
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.ptp.core.elements.IResourceManager#getMachines()
+	 */
 	public IPMachine[] getMachines() {
 		return getMachineControls();
 	}
@@ -271,18 +327,30 @@ IResourceManagerControl {
 		return config.getName();
 	}
 	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ptp.core.elements.IResourceManager#getQueueById(java.lang.String)
+	 */
 	public synchronized IPQueue getQueueById(String id) {
 		return queuesById.get(id);
 	}
 	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ptp.core.elements.IResourceManager#getQueueByName(java.lang.String)
+	 */
 	public synchronized IPQueue getQueueByName(String name) {
 		return queuesByName.get(name);
 	}
 	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ptp.core.elementcontrols.IResourceManagerControl#getQueueControls()
+	 */
 	public synchronized IPQueueControl[] getQueueControls() {
 		return (IPQueueControl[]) queuesById.values().toArray(new IPQueueControl[0]);
 	}
 	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ptp.core.elements.IResourceManager#getQueues()
+	 */
 	public IPQueue[] getQueues() {
 		return getQueueControls();
 	}
@@ -308,6 +376,9 @@ IResourceManagerControl {
 		return getConfiguration().getUniqueName();
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.ptp.core.elementcontrols.IPElementControl#hasChildren()
+	 */
 	public boolean hasChildren() {
 		return getMachines().length > 0 || getQueues().length > 0;
 	}
@@ -335,8 +406,39 @@ IResourceManagerControl {
 		listeners.remove(listener);
 	}
 	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ptp.core.elements.IResourceManager#removeTerminatedJobs(org.eclipse.ptp.core.elements.IPQueue)
+	 */
 	public void removeTerminatedJobs(IPQueue queue) {
 		doRemoveTerminatedJobs((IPQueueControl)queue);
+	}
+	
+	/**
+	 * Initialize the resource manager. This is called each time the
+	 * resource manager is started.
+	 */
+	private void initialize() {
+		attrDefManager.clear();
+		attrDefManager.setAttributeDefinitions(ElementAttributes.getDefaultAttributeDefinitions());
+		attrDefManager.setAttributeDefinitions(JobAttributes.getDefaultAttributeDefinitions());
+		attrDefManager.setAttributeDefinitions(MachineAttributes.getDefaultAttributeDefinitions());
+		attrDefManager.setAttributeDefinitions(NodeAttributes.getDefaultAttributeDefinitions());
+		attrDefManager.setAttributeDefinitions(ProcessAttributes.getDefaultAttributeDefinitions());
+		attrDefManager.setAttributeDefinitions(QueueAttributes.getDefaultAttributeDefinitions());
+		attrDefManager.setAttributeDefinitions(ResourceManagerAttributes.getDefaultAttributeDefinitions());
+	}
+	
+	/**
+	 * Remove all the model elements below the RM. This is called when the RM
+	 * shuts down and ensures that everything is cleaned up properly.
+	 */
+	private void cleanUp() {
+		for (IPQueueControl queue : getQueueControls()) {
+			removeQueue(queue);
+		}
+		for (IPMachineControl machine : getMachineControls()) {
+			removeMachine(machine);
+		}
 	}
 	
 	/*
@@ -349,6 +451,7 @@ IResourceManagerControl {
 		switch (getState()) {
 		case ERROR:
 			setState(ResourceManagerAttributes.State.STOPPED);
+			cleanUp();
 			break;
 		case STARTED:
 			setState(ResourceManagerAttributes.State.STOPPING);
@@ -363,6 +466,7 @@ IResourceManagerControl {
 			finally {
 				monitor.done();
 				setState(ResourceManagerAttributes.State.STOPPED);
+				cleanUp();
 			}
 		}
 	}
@@ -381,9 +485,10 @@ IResourceManagerControl {
     		monitor.beginTask("Starting Resource Manager " + getName(), 10);
 			SubProgressMonitor subMonitor = new SubProgressMonitor(monitor, 10);
 			try {
+				initialize();
 				boolean started = doStartup(subMonitor);
 				if (started) {
-					setState(ResourceManagerAttributes.State.STARTED);					
+					setState(ResourceManagerAttributes.State.STARTED);
 				} else if (!subMonitor.isCanceled()) {
 					setState(ResourceManagerAttributes.State.ERROR);					
 				} else {
@@ -396,6 +501,9 @@ IResourceManagerControl {
         }
 	}
 	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ptp.core.elements.IResourceManager#submitJob(org.eclipse.ptp.core.attributes.AttributeManager, org.eclipse.core.runtime.IProgressMonitor)
+	 */
 	public IPJob submitJob(AttributeManager attrMgr, IProgressMonitor monitor) throws CoreException {
 		if (monitor == null) {
 			monitor = new NullProgressMonitor();
@@ -403,10 +511,16 @@ IResourceManagerControl {
 		return doSubmitJob(attrMgr, monitor);
 	}
 	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ptp.core.elements.IResourceManager#terminateJob(org.eclipse.ptp.core.elements.IPJob)
+	 */
 	public void terminateJob(IPJob job) throws CoreException {
 		doTerminateJob(job);
 	}
 	
+	/**
+	 * @param attrs
+	 */
 	private void fireResourceManagerChanged(List<IAttribute> attrs) {
 		IResourceManagerChangedEvent e = 
 			new ResourceManagerChangedEvent(this, attrs);
@@ -421,25 +535,43 @@ IResourceManagerControl {
 	 */
 	protected synchronized void addJob(String jobId, IPJobControl job) {
 		job.getQueueControl().addJob(job);
+		job.addChildListener(jobProcessListener);
 		jobsById.put(jobId, job);
 	}
 
+	/**
+	 * @param machineId
+	 * @param machine
+	 */
 	protected synchronized void addMachine(String machineId, IPMachineControl machine) {
 		machinesById.put(machineId, machine);
 		machine.addElementListener(machineListener);
+		machine.addChildListener(machineNodeListener);
 		fireNewMachine(machine);
 	}
 
+	/**
+	 * @param nodeId
+	 * @param node
+	 */
 	protected synchronized void addNode(String nodeId, IPNodeControl node) {
 		node.getMachineControl().addNode(node);
 		nodesById.put(nodeId, node);
 	}
 	
+	/**
+	 * @param processId
+	 * @param process
+	 */
 	protected synchronized void addProcess(String processId, IPProcessControl process) {
 		process.getJobControl().addProcess(process);
 		processesById.put(processId, process);
 	}
 
+	/**
+	 * @param queueId
+	 * @param queue
+	 */
 	protected synchronized void addQueue(String queueId, IPQueueControl queue) {
 		queuesById.put(queueId, queue);
 		/*
@@ -450,6 +582,7 @@ IResourceManagerControl {
 			queuesByName.put(attr.getValue(), queue);
 		}
 		queue.addElementListener(queueListener);
+		queue.addChildListener(queueJobListener);
 		fireNewQueue(queue);
 	}
 	
@@ -469,7 +602,8 @@ IResourceManagerControl {
 	protected abstract void doEnableEvents();
 	
 	/**
-	 * 
+	 * @param queue
+	 * @return
 	 */
 	protected abstract List<IPJob> doRemoveTerminatedJobs(IPQueueControl queue);
 
@@ -514,6 +648,10 @@ IResourceManagerControl {
 		}
     }
 
+	/**
+	 * @param machine
+	 * @param attrs
+	 */
 	protected void fireMachineChanged(IPMachine machine, Collection<IAttribute> attrs) {
 		IResourceManagerChangedMachineEvent e = 
 			new ResourceManagerChangedMachineEvent(this, machine, attrs);
@@ -523,6 +661,9 @@ IResourceManagerControl {
 		}
 	}
 	
+	/**
+	 * @param machine
+	 */
 	protected void fireNewMachine(IPMachine machine) {
 		IResourceManagerNewMachineEvent e = 
 			new ResourceManagerNewMachineEvent(this, machine);
@@ -532,6 +673,9 @@ IResourceManagerControl {
 		}
 	}
 
+	/**
+	 * @param queue
+	 */
 	protected void fireNewQueue(IPQueue queue) {
 		IResourceManagerNewQueueEvent e = 
 			new ResourceManagerNewQueueEvent(this, queue);
@@ -541,6 +685,10 @@ IResourceManagerControl {
 		}
     }
 
+	/**
+	 * @param queue
+	 * @param attrs
+	 */
 	protected void fireQueueChanged(IPQueue queue, Collection<IAttribute> attrs) {
 		IResourceManagerChangedQueueEvent e = 
 			new ResourceManagerChangedQueueEvent(this, queue, attrs);
@@ -550,49 +698,93 @@ IResourceManagerControl {
 		}
 	}
 
+	/**
+	 * @param jobId
+	 * @return
+	 */
 	protected IPJobControl getJobControl(String jobId) {
 		return jobsById.get(jobId);
 	}
 
+	/**
+	 * @param machineId
+	 * @return
+	 */
 	protected IPMachineControl getMachineControl(String machineId) {
 		return machinesById.get(machineId);
 	}
 
+    /**
+     * @param nodeId
+     * @return
+     */
     protected IPNodeControl getNodeControl(String nodeId) {
 		return nodesById.get(nodeId);
 	}
 
+	/**
+	 * @param processId
+	 * @return
+	 */
 	protected IPProcessControl getProcessControl(String processId) {
 		return processesById.get(processId);
 	}
 
+	/**
+	 * @param machineId
+	 * @return
+	 */
 	protected IPQueueControl getQueueControl(String machineId) {
 		return queuesById.get(machineId);
 	}
 	
+	/**
+	 * @param string
+	 * @return
+	 */
 	protected CoreException makeCoreException(String string) {
 		IStatus status = new Status(Status.ERROR, PTPCorePlugin.getUniqueIdentifier(),
 				Status.ERROR, string, null);
 		return new CoreException(status);
 	}
 
-	/*
-	 * Create new model elements.
+	/**
+	 * @param queue
+	 * @param jobId
+	 * @param attrs
+	 * @return
 	 */
 	protected IPJobControl newJob(IPQueueControl queue, String jobId, AttributeManager attrs) {
 		IPJobControl job = new PJob(jobId, queue, attrs.getAttributes());
 		return job;
 	}
 
+	/**
+	 * @param machineId
+	 * @param attrs
+	 * @return
+	 */
 	protected IPMachineControl newMachine(String machineId, AttributeManager attrs) {
 		return new PMachine(machineId, this, attrs.getAttributes());
 	}
 
+	/**
+	 * @param machine
+	 * @param nodeId
+	 * @param attrs
+	 * @return
+	 */
 	protected IPNodeControl newNode(IPMachineControl machine, String nodeId, AttributeManager attrs) {
 		IPNodeControl node = new PNode(nodeId, machine, attrs.getAttributes());
 		return node;
 	}
 
+	/**
+	 * @param job
+	 * @param processId
+	 * @param attrs
+	 * @return
+	 */
 	protected IPProcessControl newProcess(IPJobControl job, String processId, AttributeManager attrs) {
 		IPNodeControl node = null;
 		
@@ -613,40 +805,62 @@ IResourceManagerControl {
 		return process;
 	}
 
+	/**
+	 * @param queueId
+	 * @param attrs
+	 * @return
+	 */
 	protected IPQueueControl newQueue(String queueId, AttributeManager attrs) {
 		return new PQueue(queueId, this, attrs.getAttributes());
 	}
 
+	/**
+	 * @param job
+	 */
 	protected synchronized void removeJob(IPJobControl job) {
 		job.getQueueControl().removeJob(job);
-		jobsById.remove(job.getID());
 	}
 
+	/**
+	 * @param machine
+	 */
 	protected synchronized void removeMachine(IPMachineControl machine) {
-		machinesById.remove(machine.getID());
+		for (IPNodeControl node : machine.getNodeControls()) {
+			machine.removeNode(node);
+		}
 		machine.removeElementListener(machineListener);
+		machine.removeChildListener(machineNodeListener);
+		machinesById.remove(machine.getID());
 	}
 
+	/**
+	 * @param node
+	 */
 	protected synchronized void removeNode(IPNodeControl node) {
 		node.getMachineControl().removeNode(node);
-		nodesById.remove(node.getID());
 	}
 
+	/**
+	 * @param process
+	 */
 	protected synchronized void removeProcess(IPProcessControl process) {
 		process.getJobControl().removeProcess(process);
-		processesById.remove(process.getID());
 	}
 
+	/**
+	 * @param queue
+	 */
 	protected synchronized void removeQueue(IPQueueControl queue) {
+		for (IPJobControl job : queue.getJobControls()) {
+			queue.removeJob(job);
+		}
+		queue.removeElementListener(queueListener);
+		queue.removeChildListener(queueJobListener);
 		queuesById.remove(queue.getID());
-		/*
-		 * Keep a map of queue names, if the queue has a name (and it should).
-		 */
 		StringAttribute attr = (StringAttribute) queue.getAttribute(ElementAttributes.getNameAttributeDefinition());
 		if (attr != null) {
 			queuesByName.remove(attr.getValue());
 		}
-		queue.removeElementListener(queueListener);
 	}
 
 	/**
@@ -659,29 +873,51 @@ IResourceManagerControl {
 		}
 	}
 
-	/*
-	 * Update attribute information in model elements.
+	/**
+	 * @param job
+	 * @param attrs
+	 * @return
 	 */
 	protected boolean updateJob(IPJobControl job, AttributeManager attrs) {
 		job.addAttributes(attrs.getAttributes());
 		return true;
 	}
 
+	/**
+	 * @param machine
+	 * @param attrs
+	 * @return
+	 */
 	protected boolean updateMachine(IPMachineControl machine, AttributeManager attrs) {
 		machine.addAttributes(attrs.getAttributes());
 		return true;
 	}
 
+	/**
+	 * @param node
+	 * @param attrs
+	 * @return
+	 */
 	protected boolean updateNode(IPNodeControl node, AttributeManager attrs) {
 		node.addAttributes(attrs.getAttributes());
 		return true;
 	}
 
+	/**
+	 * @param process
+	 * @param attrs
+	 * @return
+	 */
 	protected boolean updateProcess(IPProcessControl process, AttributeManager attrs) {
 		process.addAttributes(attrs.getAttributes());
 		return true;
 	}
 
+	/**
+	 * @param queue
+	 * @param attrs
+	 * @return
+	 */
 	protected boolean updateQueue(IPQueueControl queue, AttributeManager attrs) {
 		queue.addAttributes(attrs.getAttributes());
 		return true;
@@ -694,5 +930,4 @@ IResourceManagerControl {
 	protected void doAddAttributeHook(List<IAttribute> attrs) {
 		fireResourceManagerChanged(attrs);
 	}
-
 }
