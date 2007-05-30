31ef4177d584bfe95c06114f67b2e8c25f377828
crasmussen <crasmussen>
2007-05-30 14:37:04 +0000

    Trying to merge with CDT.

--- lang/org.eclipse.ptp.lang.fortran.core/src/org/eclipse/ptp/lang/fortran/internal/core/dom/parser/FortranASTIdExpression.java
+++ lang/org.eclipse.ptp.lang.fortran.core/src/org/eclipse/ptp/lang/fortran/internal/core/dom/parser/FortranASTIdExpression.java
@@ -17,24 +17,13 @@
 
 package org.eclipse.ptp.lang.fortran.internal.core.dom.parser;
 
-import org.eclipse.cdt.core.dom.ILinkage;
 import org.eclipse.cdt.core.dom.ast.ASTVisitor;
-import org.eclipse.cdt.core.dom.ast.DOMException;
 import org.eclipse.cdt.core.dom.ast.IASTCompletionContext;
 import org.eclipse.cdt.core.dom.ast.IASTIdExpression;
 import org.eclipse.cdt.core.dom.ast.IASTName;
 import org.eclipse.cdt.core.dom.ast.IBinding;
-import org.eclipse.cdt.core.dom.ast.IScope;
 import org.eclipse.cdt.core.dom.ast.IType;
-import org.eclipse.cdt.core.index.IIndex;
-import org.eclipse.cdt.core.index.IndexFilter;
-import org.eclipse.cdt.core.parser.util.ArrayUtil;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.NullProgressMonitor;
 
-/**
- * @author jcamelon
- */
 public class FortranASTIdExpression extends FortranASTNode implements IASTIdExpression, IASTCompletionContext {
 
     private IASTName name;
@@ -78,34 +67,7 @@ public class FortranASTIdExpression extends FortranASTNode implements IASTIdExpr
 	}
 	
 	public IBinding[] findBindings(IASTName n, boolean isPrefix) {
-		IScope scope = FortranVisitor.getContainingScope(n);
-		
-		IBinding[] b1 = null;
-		if (scope != null) {
-			try {
-				b1 = scope.find(n.toString(), isPrefix);
-			} catch (DOMException e) {
-			}	
-		}
-		
-		IIndex index = getTranslationUnit().getIndex();
-		
-		IBinding[] b2 = null;
-		if (index != null) {
-			try {
-				IndexFilter filter = IndexFilter.getFilter(ILinkage.C_LINKAGE_ID);
-				// TODO isPrefix argument corrent (added)
-				b2 = isPrefix ?
-						index.findBindingsForPrefix(n.toCharArray(), isPrefix, filter) :
-						index.findBindings(n.toCharArray(), filter, new NullProgressMonitor());
-			} catch (CoreException e) {
-			}
-		}
-		
-		int size = (b1 == null ? 0 : b1.length) + (b2 == null ? 0 : b2.length);
-		IBinding[] all = new IBinding[size];
-		if (b1 != null) ArrayUtil.addAll(IBinding.class, all, b1);
-		if (b2 != null) ArrayUtil.addAll(IBinding.class, all, b2);
-		return all;
+		return FortranVisitor.findBindingsForContentAssist(n, isPrefix);
 	}
+
 }
--- lang/org.eclipse.ptp.lang.fortran.core/src/org/eclipse/ptp/lang/fortran/internal/core/dom/parser/FortranScope.java
+++ lang/org.eclipse.ptp.lang.fortran.core/src/org/eclipse/ptp/lang/fortran/internal/core/dom/parser/FortranScope.java
@@ -17,34 +17,44 @@
 
 package org.eclipse.ptp.lang.fortran.internal.core.dom.parser;
 
+import org.eclipse.cdt.core.CCorePlugin;
+import org.eclipse.cdt.core.dom.ILinkage;
 import org.eclipse.cdt.core.dom.IName;
 import org.eclipse.cdt.core.dom.ast.ASTNodeProperty;
 import org.eclipse.cdt.core.dom.ast.DOMException;
 import org.eclipse.cdt.core.dom.ast.IASTCompositeTypeSpecifier;
+import org.eclipse.cdt.core.dom.ast.IASTDeclSpecifier;
 import org.eclipse.cdt.core.dom.ast.IASTDeclarationStatement;
 import org.eclipse.cdt.core.dom.ast.IASTDeclarator;
 import org.eclipse.cdt.core.dom.ast.IASTElaboratedTypeSpecifier;
 import org.eclipse.cdt.core.dom.ast.IASTEnumerationSpecifier;
+import org.eclipse.cdt.core.dom.ast.IASTFunctionDeclarator;
+import org.eclipse.cdt.core.dom.ast.IASTFunctionDefinition;
 import org.eclipse.cdt.core.dom.ast.IASTName;
 import org.eclipse.cdt.core.dom.ast.IASTNode;
 import org.eclipse.cdt.core.dom.ast.IASTStatement;
+import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 import org.eclipse.cdt.core.dom.ast.IBinding;
 import org.eclipse.cdt.core.dom.ast.ICompositeType;
 import org.eclipse.cdt.core.dom.ast.IEnumeration;
+import org.eclipse.cdt.core.dom.ast.IFunction;
 import org.eclipse.cdt.core.dom.ast.IScope;
 import org.eclipse.cdt.core.dom.ast.c.CASTVisitor;
 import org.eclipse.cdt.core.dom.ast.c.ICScope;
+import org.eclipse.cdt.core.index.IIndex;
+import org.eclipse.cdt.core.index.IndexFilter;
 import org.eclipse.cdt.core.parser.util.ArrayUtil;
 import org.eclipse.cdt.core.parser.util.CharArrayObjectMap;
 import org.eclipse.cdt.core.parser.util.CharArrayUtils;
-import org.eclipse.cdt.internal.core.dom.parser.IASTInternalScope;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
 
 /**
  * @author aniefer
  */
-public class FortranScope implements ICScope, IASTInternalScope {
+public class FortranScope implements ICScope  /* , IASTInternalScope */ {
 	/**
-	 * ISO C:99 6.2.3 there are seperate namespaces for various categories of
+	 * ISO C:99 6.2.3 there are separate namespaces for various categories of
 	 * identifiers: - label names ( labels have ICFunctionScope ) - tags of
 	 * structures or unions : NAMESPACE_TYPE_TAG - members of structures or
 	 * unions ( members have ICCompositeTypeScope ) - all other identifiers :
@@ -52,6 +62,7 @@ public class FortranScope implements ICScope, IASTInternalScope {
 	 */
 	public static final int NAMESPACE_TYPE_TAG = 0;
 	public static final int NAMESPACE_TYPE_OTHER = 1;
+	public static final int NAMESPACE_TYPE_BOTH = 2;
 	
     private IASTNode physicalNode = null;
     private boolean isFullyCached = false;
@@ -176,8 +187,22 @@ public class FortranScope implements ICScope, IASTInternalScope {
         int type = getNamespaceType( name );
         Object o = bindings[type].get( name.toCharArray() );
         
-        if( o == null ) 
-            return null;
+        if( o == null ) {
+        	IBinding result= null;
+        	if(physicalNode instanceof IASTTranslationUnit) {
+        		IIndex index= ((IASTTranslationUnit)physicalNode).getIndex();
+        		if(index!=null) {
+        			try {
+        				IBinding[] bindings= index.findBindings(name.toCharArray(), getIndexFilter(type), new NullProgressMonitor());
+        				result= processIndexResults(name, bindings);
+        			} catch(CoreException ce) {
+        				CCorePlugin.log(ce);
+        			}
+        		}
+        	}
+        	return result;
+        }
+            
         
         if( o instanceof IBinding )
             return (IBinding) o;
@@ -188,6 +213,123 @@ public class FortranScope implements ICScope, IASTInternalScope {
         return null;
     }
 
+    /* (non-Javadoc)
+     * @see org.eclipse.cdt.core.dom.ast.c.ICScope#getBinding(org.eclipse.cdt.core.dom.ast.IASTName, boolean)
+     */
+    public IBinding[] getBindings( IASTName name, boolean resolve, boolean prefixLookup ) {
+        char [] c = name.toCharArray();
+        
+        Object[] obj = null;
+        
+        for (int i = 0; i < bindings.length; i++) {
+        	if (prefixLookup) {
+        		Object[] keys = bindings[i].keyArray();
+        		for (int j = 0; j < keys.length; j++) {
+        			char[] key = (char[]) keys[j];
+        			if (CharArrayUtils.equals(key, 0, c.length, c, true)) {
+        				obj = ArrayUtil.append(obj, bindings[i].get(key));
+        			}
+        		}
+        	} else {
+        		obj = ArrayUtil.append(obj, bindings[i].get(c));
+        	}
+        }
+
+       	if(physicalNode instanceof IASTTranslationUnit) {
+        	IIndex index= ((IASTTranslationUnit)physicalNode).getIndex();
+        	if(index!=null) {
+        		try {
+        			IBinding[] bindings = prefixLookup ?
+							index.findBindingsForPrefix(name.toCharArray(), true, getIndexFilter(NAMESPACE_TYPE_BOTH), null) :
+							index.findBindings(name.toCharArray(), getIndexFilter(NAMESPACE_TYPE_BOTH), null);
+					obj = ArrayUtil.addAll(Object.class, obj, bindings);
+        		} catch(CoreException ce) {
+        			CCorePlugin.log(ce);
+        		}
+        	}
+        }
+       	obj = ArrayUtil.trim(Object.class, obj);
+       	IBinding[] result = null;
+        
+       	for (int i = 0; i < obj.length; i++) {
+            if( obj[i] instanceof IBinding )
+            	result = (IBinding[]) ArrayUtil.append(IBinding.class, result, obj[i]);
+
+            if( (resolve || ((IASTName)obj[i]).getBinding() != null) && ( obj[i] != name ) )
+            	result = (IBinding[]) ArrayUtil.append(IBinding.class, result, ((IASTName)obj[i]).resolveBinding());
+       	}
+
+        return (IBinding[]) ArrayUtil.trim(IBinding.class, result);
+    }
+    
+    /**
+     * Index results from global scope, differ from ast results from translation unit scope. This routine
+     * is intended to fix results from the index to be consistent with ast scope behaviour.
+     * @param name the name as it occurs in the ast
+     * @param bindings the set of candidate bindings
+     * @return the appropriate binding, or null if no binding is appropriate for the ast name
+     */
+    private IBinding processIndexResults(IASTName name, IBinding[] bindings) {
+    	if(bindings.length!=1)
+    		return null;
+
+    	IBinding candidate= bindings[0];
+    	if(candidate instanceof IFunction) {
+    		IASTNode parent= name.getParent();
+    		if(parent instanceof IASTFunctionDeclarator) {
+    			IASTNode parent2= parent.getParent();
+    			if(parent2 instanceof IASTFunctionDefinition) {
+    				IASTFunctionDefinition def= (IASTFunctionDefinition) parent2;
+    				if(def.getDeclSpecifier().getStorageClass()==IASTDeclSpecifier.sc_static) {
+    					try {
+    						if(!((IFunction)candidate).isStatic()) {
+    							return null;
+    						}
+    					} catch(DOMException de) {
+    						CCorePlugin.log(de);
+    					}
+    				}
+    			}
+    		}
+    	}
+
+    	return candidate;
+    }
+    
+
+    /**
+     * Returns a C-linkage filter suitable for searching the index for the types of bindings
+     * specified
+     * @param type the types of bindings to search for. One of {@link CScope#NAMESPACE_TYPE_TAG}
+     * or {@link CScope#NAMESPACE_TYPE_OTHER}, otherwise the C-linkage will not be filtered
+     * @return a C-linkage filter suitable for searching the index for the types of bindings
+     * specified
+     */
+    private IndexFilter getIndexFilter(final int type) {
+    	switch(type) {
+    		case NAMESPACE_TYPE_TAG:
+    		return new IndexFilter() {
+    			public boolean acceptBinding(IBinding binding) {
+    				return binding instanceof ICompositeType || binding instanceof IEnumeration;
+    			}
+    			public boolean acceptLinkage(ILinkage linkage) {
+    				return linkage.getID().equals(ILinkage.C_LINKAGE_ID);
+    			}
+    		};
+    		case NAMESPACE_TYPE_OTHER:
+    			return new IndexFilter() {
+        			public boolean acceptBinding(IBinding binding) {
+        				return !(binding instanceof ICompositeType || binding instanceof IEnumeration);
+        			}
+        			public boolean acceptLinkage(ILinkage linkage) {
+        				return linkage.getID().equals(ILinkage.C_LINKAGE_ID);
+        			}
+        		};
+        	default:
+        		return IndexFilter.getFilter(ILinkage.C_LINKAGE_ID);
+    	}
+    }
+    
     /* (non-Javadoc)
      * @see org.eclipse.cdt.core.dom.ast.c.ICScope#setFullyCached(boolean)
      */
@@ -229,4 +371,5 @@ public class FortranScope implements ICScope, IASTInternalScope {
         
 		bindings[type].put(binding.getNameCharArray(), binding);
 	}
+
 }
