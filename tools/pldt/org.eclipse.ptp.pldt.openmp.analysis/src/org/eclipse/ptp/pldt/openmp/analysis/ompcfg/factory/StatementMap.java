35187f50e62d450be5e3825de7590dcc6fca954f
Beth Tibbitts <tibbitts@us.ibm.com>
2007-06-14 21:38:45 +0000

    Utility has moved up to common plugin

--- tools/pldt/org.eclipse.ptp.pldt.openmp.analysis/src/org/eclipse/ptp/pldt/openmp/analysis/ompcfg/factory/StatementMap.java
+++ tools/pldt/org.eclipse.ptp.pldt.openmp.analysis/src/org/eclipse/ptp/pldt/openmp/analysis/ompcfg/factory/StatementMap.java
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (c) 2006 IBM Corporation.
+ * Copyright (c) 2006,2007 IBM Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,7 @@ package org.eclipse.ptp.pldt.openmp.analysis.ompcfg.factory;
 import java.util.Hashtable;
 
 import org.eclipse.cdt.core.dom.ast.IASTNode;
-import org.eclipse.ptp.pldt.openmp.analysis.Utility;
+import org.eclipse.ptp.pldt.common.util.Utility;
 
 /**
  * Interval tree, mapping statement (position, length)->statement
