0f9f68c8977c657abc17846197346b5a9456aae7
Beth Tibbitts <tibbitts@us.ibm.com>
2007-06-14 21:37:30 +0000

    OpenMP specific id information

---
+++ tools/pldt/org.eclipse.ptp.pldt.openmp.analysis/src/org/eclipse/ptp/pldt/openmp/analysis/OpenMpIDs.java
@@ -0,0 +1,30 @@
+/**********************************************************************
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ptp.pldt.openmp.analysis;
+
+/**
+ * IDs e.g. for markers
+ * 
+ * @author tibbitts
+ * 
+ */
+public class OpenMpIDs {
+	/**
+	 * These are the marker type used for showing concurrency
+	 */
+
+	public static final String ConcurrencyType = "org.eclipse.ptp.pldt.openmp.analysis.concurrency";
+	/**
+	 * These are the marker type used for showing non-concurrency
+	 */
+	public static final String NonConcurrencyType = "org.eclipse.ptp.pldt.openmp.analysis.nonconcurrency";
+
+}
