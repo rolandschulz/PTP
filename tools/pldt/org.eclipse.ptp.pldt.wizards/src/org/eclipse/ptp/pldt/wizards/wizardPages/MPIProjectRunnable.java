7a956110cff3411c6607551a40666380f8068c24
Beth Tibbitts <tibbitts@us.ibm.com>
2007-06-18 21:03:29 +0000

    set xlC project options correctly; in other tools, set the FIRST option found of the correct type.

--- tools/pldt/org.eclipse.ptp.pldt.wizards/src/org/eclipse/ptp/pldt/wizards/wizardPages/MPIProjectRunnable.java
+++ tools/pldt/org.eclipse.ptp.pldt.wizards/src/org/eclipse/ptp/pldt/wizards/wizardPages/MPIProjectRunnable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006,2007 IBM Corp. and others.
+ * Copyright (c) 2006,2007 IBM Corp. 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -45,6 +45,8 @@ import org.osgi.framework.Bundle;
  * we have this opportunity to use that information to modify the include paths etc.
  * in the project
  * 
+ * <p>Note: not sure we handle C++ projects correctly yet.
+ * 
  * @author Beth Tibbitts
  *
  */
@@ -58,13 +60,14 @@ public class MPIProjectRunnable implements Runnable {
 	 */
 	public void run() {
 		String pageID = MPIProjectWizardPage.PAGE_ID;
-		String propID=MPIProjectWizardPage.DO_MPI_INCLUDES;
+		String propID = MPIProjectWizardPage.DO_MPI_INCLUDES;
 		Object obj = MBSCustomPageManager.getPageProperty(pageID, propID);
-		boolean doMpiIncludes=MPIProjectWizardPage.getDefaultUseMpiIncludes();
-		if(obj!=null)
-		    doMpiIncludes=Boolean.valueOf((String)obj);
-		if(!doMpiIncludes) {
-			if(traceOn)System.out.println("Do not save MPI info in this project.");
+		boolean doMpiIncludes = MPIProjectWizardPage.getDefaultUseMpiIncludes();
+		if (obj != null)
+			doMpiIncludes = Boolean.valueOf((String) obj);
+		if (!doMpiIncludes) {
+			if (traceOn)
+				System.out.println("Do not save MPI info in this project.");
 			return;
 		}
 		MBSCustomPageData pData = MBSCustomPageManager.getPageData(pageID);
@@ -76,6 +79,10 @@ public class MPIProjectRunnable implements Runnable {
 			if(w instanceof CProjectWizard){
 				wiz = (CProjectWizard)w;
 			}
+			if(wiz==null){
+				System.out.println("Can't get CProjectWizard from MBS page data. Quitting. No MPI info added to project.");
+				return;
+			}
 		}
 		catch (Exception e) {
 			Throwable reason = e.getCause();
@@ -86,6 +93,7 @@ public class MPIProjectRunnable implements Runnable {
 
 		if(traceOn)System.out.println("Project: " + proj.getName());
 
+		// Collect the values that the user entered on the wizard page
 		propID = MPIProjectWizardPage.INCLUDE_PATH_PROP_ID;
 		String newIncludePath = getNewPropValue(pageID, propID,"c:/mpich2/include");
 		if(traceOn)System.out.println("Got prop: "+propID+"="+newIncludePath);
@@ -122,17 +130,15 @@ public class MPIProjectRunnable implements Runnable {
 		
 		// add the include path & linker values to all the configurations
 		IConfiguration[] configs = mProj.getConfigurations();
-
 		for (int i = 0; i < configs.length; i++) {
 			IConfiguration cf = configs[i];
 			if(traceOn)System.out.println("Config " + i + ": " + cf.getName());
 			addIncludePath(cf, newIncludePath);
 			addLinkerOpt(cf,newLib,newLibSearchPath);
-			//addLinkerOptOld(cf,newLib,newLibSearchPath);
 			setCompileCommand(cf,mpiCompileCommand);
 			setLinkCommand(cf,mpiLinkCommand);		
 		}
-		if(traceOn)System.out.println("Runnable, newIncludePath: "+newIncludePath);
+		if(traceOn)System.out.println("MPIProjectRunnable, newIncludePath: "+newIncludePath);
 		if(traceOn)System.out.println("   newLib: "+newLib+"  newLibSrchPth: "+newLibSearchPath);
 		if(traceOn)System.out.println("   compileCmd: "+mpiCompileCommand);
 		if(traceOn)System.out.println("   linkCmd: "+mpiLinkCommand);
@@ -143,21 +149,18 @@ public class MPIProjectRunnable implements Runnable {
 		
 		if(mpiSampleFileInsert.equals("true")){
 			 try {
-				//FileInputStream fis=null;
 				Bundle bundle = Platform.getBundle(MpiWizardsPlugin.getPluginId());
-				Path path = new Path("testMPI.c");
-				//URL fileURL = Platform.find(bundle, path); deprecated
+				String mpiFilename="testMPI.c";
+				Path path = new Path(mpiFilename);
 				URL fileURL = FileLocator.find(bundle, path, null);
-				
-				
 				InputStream mpiFileStream = null;
 				try {
-					String fname="testMPI.c";
 					mpiFileStream = fileURL.openStream();
-					proj.getFile(fname).create(mpiFileStream,false,null);
+					proj.getFile(mpiFilename).create(mpiFileStream,false,null);
 					//System.out.println("file "+fname+" created.");
 				} catch (IOException e) {
-					e.printStackTrace();
+					System.out.println("Error creating file: "+mpiFilename);
+					//e.printStackTrace();
 				}
 			} catch (CoreException e) {
 				System.out.println("Error creating testMPI.c");
@@ -197,18 +200,17 @@ public class MPIProjectRunnable implements Runnable {
 	}
 
 
-	protected List<IOption> getOptionsByType(int type){
-		type=IOption.INCLUDE_PATH; // sample
-		return null;
-	}
 	/**
 	 * Add an include path for C compiling to the existing include paths of the given
-	 * Configuration
+	 * Configuration.  XLC is handled as a special case to find its include path option;
+	 * for other tools, we assume there is a single option for include paths, so we take
+	 * the first one.
 	 * 
 	 * @param cf
 	 *            the configuration of the project (e.g. Release, Debug, etc.)
 	 * @param newIncludePath
-	 *            include path to add
+	 *            include path(s) to add.  If more than one, separate by
+	 *            java.io.File.pathSeparator, which is semicolon for Windows, colon for Mac/Linux
 	 */
 	private void addIncludePath(IConfiguration cf, String newIncludePath) {
 		// note: could be > 1 path in 'newIncludePath'
@@ -216,31 +218,38 @@ public class MPIProjectRunnable implements Runnable {
 		ITool cfTool = cf.getToolFromInputExtension(ext);
 		// do we need to also handle c++ case as well?
 
-		// String optID = "gnu.c.compiler.option.include.paths";
-		// IOption option = cfTool.getOptionById(optID);
-		// run thru ALL options and check type for each
-		int desiredType=IOption.INCLUDE_PATH;
-		IOption[] allOptions=cfTool.getOptions();
-		for (int i = 0; i < allOptions.length; i++) {
-			IOption option = allOptions[i];
-			try {
-				int optionType = option.getValueType();
-				if (desiredType == optionType) {
-					String[] includePaths = option.getIncludePaths();
-					String[] newIncludePaths = add(includePaths, newIncludePath);
-					if(traceOn)System.out.println("add "+newIncludePath+" to existing includePaths: "+includePaths);
-					ManagedBuildManager.setOption(cf, cfTool, option, newIncludePaths);
-				    }
-				} catch (BuildException e) {
-					System.out.println("MPIProjectRunnable, problem getting include paths: "
-									+ e.getMessage());
-					e.printStackTrace();
-				} 
-			}
+		String id = cfTool.getId(); // "cdt.managedbuild.tool.xlc.c.compiler.exe.debug.1423270745"
+		String name = cfTool.getName();// "XL C Compiler"
+		IOption option = null;
+		if (name.startsWith("XL C")) { // special case for XL C compiler
+			option = cfTool.getOptionById("xlc.c.compiler.option.include.paths");
+		} else { // otherwise we assume there is only one include path option.
+			option = getFirstOptionByType(cf, cfTool, IOption.INCLUDE_PATH);
 		}
+		if (option != null) {
+			String[] includePaths = null;
+			try {
+				includePaths = option.getIncludePaths();
+			} catch (BuildException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			String[] newIncludePaths = add(includePaths, newIncludePath);
+			if (traceOn)
+				System.out.println("        add " + newIncludePath + " to existing includePaths: "
+						+ unroll(includePaths));
+			ManagedBuildManager.setOption(cf, cfTool, option, newIncludePaths);
+		}
+		else{
+			System.out.println("MPIProjectRunnable, no option for include paths found.");
+		}
+	}
+
 
 	/**
-	 * Add a new linker option
+	 * Add a new linker option.  Assumes that there is ONE instance of an option
+	 * of type <code>IOption.LIBRARY_PATHS</code> and <code>IOption.LIBRARIES</code>.
+	 * This method adds the libName and libPath to these two options.
 	 * @param cf the Configuration to which we want to add to linker options
 	 * @param libName the lib name (e.g. "lib")
 	 * @param libPath the library search path name (e.g. "c:/mypath/lib")
@@ -249,28 +258,13 @@ public class MPIProjectRunnable implements Runnable {
 	private void addLinkerOpt(IConfiguration cf, String libName, String libPath) {
 		String ext = "o";
 		ITool cfTool = cf.getToolFromInputExtension(ext);
-		IOption[] allOptions = cfTool.getOptions();
-		for (int i = 0; i < allOptions.length; i++) {
-			IOption option = allOptions[i];
-			try {
-				int optionType = option.getValueType();
-				switch (optionType) {
-				case IOption.LIBRARIES:
-					addOptionValue(cf, cfTool, option, libName);
-					break;
-				case IOption.LIBRARY_PATHS:
-					addOptionValue(cf, cfTool, option, libPath);
-				default:
-					break;
-				}
-			} catch (BuildException e) {
-				System.out
-						.println("MPIProjectRunnable, problem getting include paths: "
-								+ e.getMessage());
-				e.printStackTrace();
-			}
-		}
-	}
+
+		IOption lpOpt = getFirstOptionByType(cf, cfTool, IOption.LIBRARY_PATHS);
+		addOptionValue(cf, cfTool, lpOpt, libPath);
+
+		IOption libOpt = getFirstOptionByType(cf, cfTool, IOption.LIBRARIES);
+		addOptionValue(cf, cfTool, libOpt, libName);
+	}                                                                                                                     
 
 	
 	private void setCompileCommand(IConfiguration cf, String buildCmd) {
@@ -307,11 +301,7 @@ public class MPIProjectRunnable implements Runnable {
 				valueList = option.getLibraries();
 				valueList=addNotPath(valueList, value);
 				break;
-			case IOption.STRING_LIST:// this is type for library search path cdt 3.1.2
-				valueList = option.getStringListValue();
-				valueList=addNotPath(valueList,value);
-				break;
-			case IOption.LIBRARY_PATHS:// this is type for library search path cdt 4.0 as on 5/11/07
+			case IOption.LIBRARY_PATHS:// this is type for library search path cdt 4.0 
 				valueList=option.getBasicStringListValue();
 				valueList=addNotPath(valueList,value);
 				break;
@@ -363,8 +353,7 @@ public class MPIProjectRunnable implements Runnable {
 					System.out.println("  Tool " + k + ": " + tool.getName());
 					//boolean rc=tool.setToolCommand("foo");
 					String toolCmd=tool.getToolCommand();
-					String toolID=tool.getId();//"cdt.managedbuild.tool.gnu.c.compiler.cygwin.exe.debug.244391908"
-					                           //"cdt.managedbuild.tool.gnu.c.linker.cygwin.exe.debug.107078755"
+					String toolID=tool.getId();
 					System.out.println("  cmd="+toolCmd+"  toolID="+toolID);
 					
 					
@@ -373,8 +362,9 @@ public class MPIProjectRunnable implements Runnable {
 					for (int j = 0; j < options.length; j++) {
 						IOption opt = options[j];
 						String foundOptionID = opt.getId();
+						int type=opt.getValueType();
 						System.out.println("    option " + j + " " + opt.getName() + " id="
-								+ foundOptionID);
+								+ foundOptionID+"  type="+showType(type));
 						if(opt.getValueType()==IOption.INCLUDE_PATH) {
 							showIncludePaths(opt);
 						}
@@ -389,26 +379,26 @@ public class MPIProjectRunnable implements Runnable {
 				ITool cfTool = cf.getToolFromInputExtension(ext);
 				System.out.println("Tool by ext: " + ext + " is: " + cfTool.getName());
 				
-				// Look for include path when we know the option ID
-				String optID = "gnu.c.compiler.option.include.paths";
-				IOption option = cfTool.getOptionById(optID);
-				System.out.println("Option " + optID + " is " + option.getName());
-
-				IOption[] options = cfTool.getOptions();
-				for (int j = 0; j < options.length; j++) {
-					IOption opt = options[j];
-					String foundOptionID = opt.getId();
-					System.out.println("  option " + j + " " + opt.getName() + " id="
-							+ foundOptionID);
-					if(opt.getValueType()==IOption.INCLUDE_PATH) {
-						showIncludePaths(opt);
-					}
+				// Look for include path
+				IOption option = getFirstOptionByType(cf, cfTool, IOption.INCLUDE_PATH);
+				if (option != null) {
+					String oname = option.getName();
+					System.out.println("Option " + option.getId() + " is " + oname);
+					showIncludePaths(option);
 				}
+				else System.out.println("No include path option found. ");
+
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
+	}
+	private String showType(int type){
+		if(type==IOption.INCLUDE_PATH) return type+" (IOption.INCLUDE_PATH)";
+		if(type==IOption.LIBRARY_PATHS) return type+" (IOption.LIBRARY_PATHS)";
+		if(type==IOption.LIBRARIES)     return type+" (IOption.LIBRARIES)";
+		return type+" ";
 	}
 
 	/**
@@ -431,16 +421,17 @@ public class MPIProjectRunnable implements Runnable {
 	/**
 	 * Add one or more paths to the list of paths
 	 * 
-	 * @param includePaths the existing list of paths to add to
-	 * @param newPath the new path to add; may be >1 directory, with path delimiter
+	 * @param existingPaths the existing list of paths to add to
+	 * @param newPath the new path to add; may be >1 directory, with path delimiter java.io.File.pathSeparator 
+	 * (usually semicolon or colon)
 	 * @return the merged list
 	 */
-	private String[] add(String[] includePaths, String newPath) {
+	private String[] add(String[] existingPaths, String newPath) {
 		String pathSep=java.io.File.pathSeparator;  // semicolon for windows, colon for Mac/Linux
 		List<String> newPathList = new ArrayList<String>();
 		String path;
-		for (int i = 0; i < includePaths.length; i++) {
-			path = includePaths[i];
+		for (int i = 0; i < existingPaths.length; i++) {
+			path = existingPaths[i];
 			newPathList.add(path);
 		}
 		String[] newPathArray=newPath.split(pathSep);
@@ -470,37 +461,123 @@ public class MPIProjectRunnable implements Runnable {
 		newList[len] = newStr;
 		return newList;
 	}
+	/**
+	 * Simple string representation of items in a string array
+	 * @param list
+	 * @return
+	 */
+	private String unroll (String[] list){
+		StringBuffer result=new StringBuffer();
+		//list = new String[]{"one","two"};
+		//System.out.println(list);
+		for (int i = 0; i < list.length; i++) {
+			String string = list[i];
+			result.append(string);
+			result.append(", ");
+			
+			
+		}
+		if (result.length() > 2) {
+			result.delete(result.length() - 2, result.length() - 1);
+		}
+		return result.toString();
+	}
+	/**
+	 * Get the options of a specific type.<br>
+	 * We really need an ITool.getOptionsByType(int) so we don't have to do
+	 * this.
+	 * 
+	 * @param cf
+	 *            the IConfiguration in which to search
+	 * @param cfTool
+	 *            the ITool whose options are being searched
+	 * @param optionType
+	 *            the option type we are looking for, e.g. IOption.SOMETHING
+	 * @return
+	 */
+	private List<IOption> getOptionsByType(IConfiguration cf, ITool cfTool, int optionType) {
+
+		// run thru ALL options and check type for each, returning the ones that match
+		IOption[] allOptions = cfTool.getOptions();
+		List<IOption> foundOptions = new ArrayList<IOption>();
+
+		for (int i = 0; i < allOptions.length; i++) {
+			IOption option = allOptions[i];
+
+			int oType = 0;
+			try {
+				oType = option.getValueType();
+			} catch (BuildException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				continue;
+			}
+			if (optionType == oType) {
+				// add it to the list
+				foundOptions.add(option);
+			}
+
+		}
+		//IOption[] ret= foundOptions.toArray(new IOption[foundOptions.size()]);
+		return foundOptions;
+	}
+	/**
+	 * Returns the <i>first</i> option of the specified type from the tool
+	 * @param cf
+	 * @param cfTool
+	 * @param optionType
+	 * @return
+	 */
+	private IOption getFirstOptionByType(IConfiguration cf, ITool cfTool, int optionType){
+		List<IOption> allOptions=getOptionsByType(cf,cfTool,optionType);
+		if(allOptions.size()>0){
+			return allOptions.get(0);
+		}
+		return null;
+	}
 
 }
 /**
  * 
-  Some sample tool data from showOptions()  on Windows XP with cygwin
-  Tool 1: GCC C Compiler<p>
-
-    option 0 Do not search system directories (-nostdinc) id=gnu.c.compiler.option.preprocessor.nostdinc<br>
-    option 1 Preprocess only (-E) id=gnu.c.compiler.option.preprocessor.preprocess<br>
-    option 2 Defined symbols (-D) id=gnu.c.compiler.option.preprocessor.def.symbols<br>
-    option 3 Undefined symbols (-U) id=gnu.c.compiler.option.preprocessor.undef.symbol<br>
-    option 4 Include paths (-I) id=gnu.c.compiler.option.include.paths<br>
-    option 5 Optimization Level id=gnu.c.compiler.cygwin.exe.release.option.optimization.level<br>
-    option 6 Other optimization flags id=gnu.c.compiler.option.optimization.flags<br>
-    option 7 Debug Level id=gnu.c.compiler.cygwin.exe.release.option.debugging.level<br>
-    option 8 Other debugging flags id=gnu.c.compiler.option.debugging.other<br>
-    option 9 Generate gprof information (-pg) id=gnu.c.compiler.option.debugging.gprof<br>
-    option 10 Generate prof information (-p) id=gnu.c.compiler.option.debugging.prof<br>
-    option 11 Check syntax only (-fsyntax-only) id=gnu.c.compiler.option.warnings.syntax<br>
-    option 12 Pedantic (-pedantic) id=gnu.c.compiler.option.warnings.pedantic<br>
-    option 13 Pedantic warnings as errors (-pedantic-errors) id=gnu.c.compiler.option.warnings.pedantic.error<br>
-    option 14 Inhibit all warnings (-w) id=gnu.c.compiler.option.warnings.nowarn<br>
-    option 15 All warnings (-Wall) id=gnu.c.compiler.option.warnings.allwarn<br>
-    option 16 Warnings as errors (-Werror) id=gnu.c.compiler.option.warnings.toerrors<br>
-    option 17 Other flags id=gnu.c.compiler.option.misc.other<br>
-    option 18 Verbose (-v) id=gnu.c.compiler.option.misc.verbose<br>
-    option 19 Support ANSI programs (-ansi) id=gnu.c.compiler.option.misc.ansi<br>
+ * Some sample tool data from showOptions() on Windows XP with cygwin Tool 1:
+ * GCC C Compiler
+ * <p>
+ * 
+ * option 0 Do not search system directories (-nostdinc)
+ * id=gnu.c.compiler.option.preprocessor.nostdinc<br>
+ * option 1 Preprocess only (-E)
+ * id=gnu.c.compiler.option.preprocessor.preprocess<br>
+ * option 2 Defined symbols (-D)
+ * id=gnu.c.compiler.option.preprocessor.def.symbols<br>
+ * option 3 Undefined symbols (-U)
+ * id=gnu.c.compiler.option.preprocessor.undef.symbol<br>
+ * option 4 Include paths (-I) id=gnu.c.compiler.option.include.paths<br>
+ * option 5 Optimization Level
+ * id=gnu.c.compiler.cygwin.exe.release.option.optimization.level<br>
+ * option 6 Other optimization flags id=gnu.c.compiler.option.optimization.flags<br>
+ * option 7 Debug Level
+ * id=gnu.c.compiler.cygwin.exe.release.option.debugging.level<br>
+ * option 8 Other debugging flags id=gnu.c.compiler.option.debugging.other<br>
+ * option 9 Generate gprof information (-pg)
+ * id=gnu.c.compiler.option.debugging.gprof<br>
+ * option 10 Generate prof information (-p)
+ * id=gnu.c.compiler.option.debugging.prof<br>
+ * option 11 Check syntax only (-fsyntax-only)
+ * id=gnu.c.compiler.option.warnings.syntax<br>
+ * option 12 Pedantic (-pedantic) id=gnu.c.compiler.option.warnings.pedantic<br>
+ * option 13 Pedantic warnings as errors (-pedantic-errors)
+ * id=gnu.c.compiler.option.warnings.pedantic.error<br>
+ * option 14 Inhibit all warnings (-w) id=gnu.c.compiler.option.warnings.nowarn<br>
+ * option 15 All warnings (-Wall) id=gnu.c.compiler.option.warnings.allwarn<br>
+ * option 16 Warnings as errors (-Werror)
+ * id=gnu.c.compiler.option.warnings.toerrors<br>
+ * option 17 Other flags id=gnu.c.compiler.option.misc.other<br>
+ * option 18 Verbose (-v) id=gnu.c.compiler.option.misc.verbose<br>
+ * option 19 Support ANSI programs (-ansi) id=gnu.c.compiler.option.misc.ansi<br>
  * 
  * Tool 2: GCC C Linker <br>
- * option 5 Libraries (-l) id=gnu.c.link.option.libs<br> 
- * option 6 Library search path (-L)  id=gnu.c.link.option.paths<br>
+ * option 5 Libraries (-l) id=gnu.c.link.option.libs<br>
+ * option 6 Library search path (-L) id=gnu.c.link.option.paths<br>
  * 
  */
 
