daf8d5c8fa41b95bf6cb00a463152b3fc88a44e2
gwatson <gwatson>
2007-07-30 15:04:48 +0000

    fix NPE

--- core/org.eclipse.ptp.ui/src/org/eclipse/ptp/internal/ui/adapters/PElementPropertySource.java
+++ core/org.eclipse.ptp.ui/src/org/eclipse/ptp/internal/ui/adapters/PElementPropertySource.java
@@ -40,10 +40,8 @@ public class PElementPropertySource implements IPropertySource {
         for (int i = 0; i < attrDefs.length; i++) {
         	if (attrDefs[i].getDisplay()) {
         		keys[i] = attrDefs[i].getId();
+                addDescriptor(new PropertyDescriptor(keys[i], attrDefs[i].getName()));
         	}
-        }
-        for (int i = 0; i < keys.length; ++i) {
-            addDescriptor(new PropertyDescriptor(keys[i], attrDefs[i].getName()));
         }
 	}
 
