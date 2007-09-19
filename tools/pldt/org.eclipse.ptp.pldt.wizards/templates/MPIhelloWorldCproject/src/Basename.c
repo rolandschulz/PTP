9838334f93630f2d986eb170766f1d005dd08c40
Beth Tibbitts <tibbitts@us.ibm.com>
2007-09-19 22:10:17 +0000

    add templates for new project types

---
+++ tools/pldt/org.eclipse.ptp.pldt.wizards/templates/MPIhelloWorldCproject/src/Basename.c
@@ -0,0 +1,43 @@
+/*
+ ============================================================================
+ Name        : $(baseName).c
+ Author      : $(author)
+ Version     :
+ Copyright   : $(copyright)
+ Description : Hello OpenMP World in C 
+ ============================================================================
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>  
+#include <math.h>    
+#include "openmp.h"    
+
+ 
+// Sample dummy OpenMP program 
+
+
+int main(int argc, char* argv[]){
+	int    i,arraySize;
+	double *x, *y;     /* the arrays                 */
+	printf("Hello OpenMP World.\n");	
+	
+// sample openMP API
+	if (omp_in_parallel()){
+		printf("true"); 
+	}
+	  /* Allocate memory for the arrays. */
+  x = (double *) malloc( (size_t) (  arraySize * sizeof(double) ) );
+  y = (double *) malloc( (size_t) (  arraySize * sizeof(double) ) );
+ 
+  /* Here's the OpenMP pragma that parallelizes the for-loop. */
+#pragma omp parallel for
+  for ( i = 0; i < arraySize; i++ )
+    {
+      y[i] = sin( exp( cos( - exp( sin(x[i]) ) ) ) );
+    }
+	  
+	 
+	return 0;   
+}
+
