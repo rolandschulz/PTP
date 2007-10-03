40f5627659eb6e5a93fe21cc6c46d75ee25a951c
joverbey <joverbey>
2007-10-03 17:10:35 +0000

    No significant speedup using chained visitor; reference collector time-consuming

--- photran/org.eclipse.photran.core.vpg/src/org/eclipse/photran/core/vpg/PhotranVPG.java
+++ photran/org.eclipse.photran.core.vpg/src/org/eclipse/photran/core/vpg/PhotranVPG.java
@@ -34,7 +34,6 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.content.IContentType;
 import org.eclipse.photran.core.FortranAST;
 import org.eclipse.photran.core.IFortranAST;
-import org.eclipse.photran.internal.core.analysis.binding.ChainedVisitor;
 import org.eclipse.photran.internal.core.analysis.binding.Definition;
 import org.eclipse.photran.internal.core.analysis.binding.DefinitionCollector;
 import org.eclipse.photran.internal.core.analysis.binding.ImplicitSpecCollector;
@@ -288,31 +287,28 @@ public class PhotranVPG extends EclipseVPG<IFortranAST, Token>
 		}
 		
 		if (ast == null) return;
-		
-		ast.visitTopDownUsing(new ImplicitSpecCollector());
-		
-		ast.visitBottomUpUsing(new DefinitionCollector(getIFileForFilename(filename)));
-		
-		ast.visitBottomUpUsing(new SpecificationCollector());
 
+        long[] t = new long[6];
+		t[0] = System.currentTimeMillis();
+		ast.visitTopDownUsing(new ImplicitSpecCollector());
+        t[1] = System.currentTimeMillis();
+		ast.visitBottomUpUsing(new DefinitionCollector(getIFileForFilename(filename)));
+        t[2] = System.currentTimeMillis();
+		ast.visitBottomUpUsing(new SpecificationCollector());
+        t[3] = System.currentTimeMillis();
 		ast.visitBottomUpUsing(
 			new ModuleLoader(getIFileForFilename(filename),
 					new NullProgressMonitor()));
 					//PhotranVPG.getInstance().getCurrentProgressMonitor()));
-
 		// TODO: Type check here so derived type components can be resolved
-		
+        t[4] = System.currentTimeMillis();
 		ast.visitBottomUpUsing(new ReferenceCollector());
-		
-		/*
-		ChainedVisitor v = new ChainedVisitor();
-		v.addTopDownVisitor(new ImplicitSpecCollector());
-        v.addBottomUpVisitor(new DefinitionCollector(getIFileForFilename(filename)));
-        v.addBottomUpVisitor(new SpecificationCollector());
-        v.addBottomUpVisitor(new ModuleLoader(getIFileForFilename(filename), new NullProgressMonitor()));
-        v.addBottomUpVisitor(new ReferenceCollector());
-        ast.visitUsing(v);
-        */
+        t[5] = System.currentTimeMillis();
+
+//        String s = "";
+//        for (int i = 1; i < t.length; i++)
+//            s += (t[i] - t[i-1]) + " ";
+//        System.out.println(s);
 	}
 	
 	public IFortranAST acquireTransientAST(IFile file)
