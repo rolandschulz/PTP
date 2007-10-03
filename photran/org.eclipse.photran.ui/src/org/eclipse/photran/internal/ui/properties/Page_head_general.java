e02a29e68caa3f85919ff28162acc5f944272fd7
joverbey <joverbey>
2007-10-03 20:50:39 +0000

    Comment/Uncomment bound to Ctrl+Shift+1; Fortran property pages (including managed build) fixed for CDT 4.0

---
+++ photran/org.eclipse.photran.ui/src/org/eclipse/photran/internal/ui/properties/Page_head_general.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2007 Intel Corporation and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Intel Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.photran.internal.ui.properties;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.dialogs.PropertyPage;
+
+public class Page_head_general extends PropertyPage {
+	protected Control createContents(Composite parent) {
+		noDefaultAndApplyButton();
+		return parent;
+	}
+}
