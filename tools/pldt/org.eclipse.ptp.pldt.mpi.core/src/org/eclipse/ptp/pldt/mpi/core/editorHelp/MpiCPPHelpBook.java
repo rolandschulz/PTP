3c9fd47a8840d64655beed864f446e484b01984f
Beth Tibbitts <tibbitts@us.ibm.com>
2007-10-10 01:52:36 +0000

    implement (finally!) populating function map for C++.  Why didn't I notice this had never been done before now???

--- tools/pldt/org.eclipse.ptp.pldt.mpi.core/src/org/eclipse/ptp/pldt/mpi/core/editorHelp/MpiCPPHelpBook.java
+++ tools/pldt/org.eclipse.ptp.pldt.mpi.core/src/org/eclipse/ptp/pldt/mpi/core/editorHelp/MpiCPPHelpBook.java
@@ -11,9 +11,20 @@
 
 package org.eclipse.ptp.pldt.mpi.core.editorHelp;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+
 import org.eclipse.cdt.ui.ICHelpBook;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.ptp.pldt.common.editorHelp.CHelpBookImpl;
+import org.eclipse.ptp.pldt.common.editorHelp.FunctionSummaryImpl;
 import org.eclipse.ptp.pldt.mpi.core.MpiPlugin;
+import org.osgi.framework.Bundle;
 
 /**
  * Help book for C++ MPI functions
@@ -27,14 +38,34 @@ public class MpiCPPHelpBook extends CHelpBookImpl
     public MpiCPPHelpBook()
     {
 		super(MpiPlugin.getPluginId());
-    	// TODO populate func map for C++
-    	
+		System.out.println("MPI CPP help book ctor()...");
+		// populate func map
+		Bundle bundle = Platform.getBundle(MpiPlugin.getPluginId());
+		Path path = new Path("mpiref.xml");
+
+		URL fileURL = FileLocator.find(bundle, path, null);
+		InputStream xmlIn = null;
+		try {
+			xmlIn = fileURL.openStream();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		List<FunctionSummaryImpl> mpiFuncList = MPIDocXMLParser.parseDOM(xmlIn, "cppname");
+		int temp=0;
+		for (Iterator<FunctionSummaryImpl> it = mpiFuncList.iterator(); it.hasNext();) {
+			FunctionSummaryImpl functionSummary = it.next();
+			if(2>temp++)System.out.println("  "+functionSummary.getName()+"-"+functionSummary.getDescription());
+			funcName2FuncInfo.put(functionSummary.getName(), functionSummary);
+		};
     	// set title
     	setTitle(TITLE);
+
     }
     
     public int getCHelpType()
     {
         return ICHelpBook.HELP_TYPE_CPP;
     }
+
 }
