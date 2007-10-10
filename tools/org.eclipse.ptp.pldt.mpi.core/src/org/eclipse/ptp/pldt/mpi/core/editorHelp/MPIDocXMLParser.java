cf9bca11a36cdbcfef44f3e5b5736198a241d8f0
btibbitts <btibbitts>
2007-10-10 01:54:06 +0000

    convert to some java 5 constructs; provide a flexible argument so can be shared by C and C++ impl's both.

--- tools/org.eclipse.ptp.pldt.mpi.core/src/org/eclipse/ptp/pldt/mpi/core/editorHelp/MPIDocXMLParser.java
+++ tools/org.eclipse.ptp.pldt.mpi.core/src/org/eclipse/ptp/pldt/mpi/core/editorHelp/MPIDocXMLParser.java
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (c) 2005 IBM Corporation.
+ * Copyright (c) 2005, 2007 IBM Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -23,6 +23,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.eclipse.cdt.ui.FunctionPrototypeSummary;
 import org.eclipse.ptp.pldt.common.editorHelp.FunctionPrototypeSummaryImpl;
 import org.eclipse.ptp.pldt.common.editorHelp.FunctionSummaryImpl;
 import org.w3c.dom.Document;
@@ -38,7 +39,7 @@ public class MPIDocXMLParser {
 	{
 		File file = new File(args[0]);
 		try {
-			List functions = parseDOM(new FileInputStream(file));
+			List functions = parseDOM(new FileInputStream(file), "cname");
 			System.out.println("num of functions gathered: "+functions.size());
             System.out.println("first function summary:");
             System.out.println((FunctionSummaryImpl)(functions.iterator().next()));
@@ -47,9 +48,14 @@ public class MPIDocXMLParser {
 		}
 	}
 	
-	
-	public static List /* of FunctionSummary */parseDOM(InputStream xmlIn) {
-		List mpiFuncList = new ArrayList();
+	/**
+	 * 
+	 * @param xmlIn input stream for XML file
+	 * @param cOrCppName the name of the 'name' node: "cname" for C, "cppName" for C++
+	 * @return
+	 */
+	public static List<FunctionSummaryImpl> parseDOM(InputStream xmlIn, String cOrCppName) {
+		List<FunctionSummaryImpl> mpiFuncList = new ArrayList<FunctionSummaryImpl>();
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		Document document = null;
 		try {
@@ -74,23 +80,23 @@ public class MPIDocXMLParser {
 				FunctionPrototypeSummaryImpl prototypeSummary = null;
 				for (int j = 0; j < child.getLength(); j++) {
 					Node sub = child.item(j);
-					if (sub.getNodeName().equals("cname")) {
-//						cname = sub.getTextContent(); // java 5 
-                        cname = sub.getFirstChild().getNodeValue();
+					if (sub.getNodeName().equals(cOrCppName)) {
+ 						cname = sub.getTextContent(); // java 5 
+//                        cname = sub.getFirstChild().getNodeValue();
 					} else if (sub.getNodeName().equals("description")) {
-//						desc = sub.getTextContent(); // java 5
-                        desc = sub.getFirstChild().getNodeValue();
+						desc = sub.getTextContent(); // java 5
+//                     desc = sub.getFirstChild().getNodeValue();
 					} else if (sub.getNodeName().equals("prototype")) {
 						NodeList protoSub = sub.getChildNodes();
                         // java 5
-//						prototypeSummary = new FunctionPrototypeSummary(
-//								protoSub.item(3).getTextContent(), protoSub
-//										.item(1).getTextContent(), protoSub
-//										.item(5).getTextContent());
-                        prototypeSummary = new FunctionPrototypeSummaryImpl(
-                              protoSub.item(3).getFirstChild().getNodeValue(), protoSub
-                                      .item(1).getFirstChild().getNodeValue(), protoSub
-                                      .item(5).getFirstChild().getNodeValue());
+						prototypeSummary = new FunctionPrototypeSummaryImpl(
+								protoSub.item(3).getTextContent(), protoSub
+										.item(1).getTextContent(), protoSub
+										.item(5).getTextContent());
+//                        prototypeSummary = new FunctionPrototypeSummaryImpl(
+//                              protoSub.item(3).getFirstChild().getNodeValue(), protoSub
+//                                      .item(1).getFirstChild().getNodeValue(), protoSub
+//                                      .item(5).getFirstChild().getNodeValue());
 					}
 				}
 				FunctionSummaryImpl functionSummary = new FunctionSummaryImpl(cname,
