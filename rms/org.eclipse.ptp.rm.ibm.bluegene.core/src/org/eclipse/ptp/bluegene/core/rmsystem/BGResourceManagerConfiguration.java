ecd2028655276c35b78e9c1107b136efdfc550d5
gwatson <gwatson>
2007-10-16 01:54:00 +0000

    Initial Blue Gene implementation

---
+++ rms/org.eclipse.ptp.rm.ibm.bluegene.core/src/org/eclipse/ptp/bluegene/core/rmsystem/BGResourceManagerConfiguration.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2006 The Regents of the University of California. 
+ * This material was produced under U.S. Government contract W-7405-ENG-36 
+ * for Los Alamos National Laboratory, which is operated by the University 
+ * of California for the U.S. Department of Energy. The U.S. Government has 
+ * rights to use, reproduce, and distribute this software. NEITHER THE 
+ * GOVERNMENT NOR THE UNIVERSITY MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR 
+ * ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE. If software is modified 
+ * to produce derivative works, such modified software should be clearly marked, 
+ * so as not to confuse it with the version available from LANL.
+ * 
+ * Additionally, this program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * LA-CC 04-115
+ *******************************************************************************/
+/**
+ * 
+ */
+package org.eclipse.ptp.bluegene.core.rmsystem;
+
+import org.eclipse.ptp.remote.AbstractRemoteResourceManagerConfiguration;
+import org.eclipse.ptp.rmsystem.IResourceManagerConfiguration;
+import org.eclipse.ui.IMemento;
+
+final public class BGResourceManagerConfiguration extends AbstractRemoteResourceManagerConfiguration {
+
+	public static IResourceManagerConfiguration load(BGResourceManagerFactory factory,
+			IMemento memento) {
+
+		RemoteConfig remoteConfig = loadRemote(factory, memento);
+		
+		BGResourceManagerConfiguration config = 
+			new BGResourceManagerConfiguration(factory, remoteConfig);
+
+		return config;
+	}
+	
+	public BGResourceManagerConfiguration(BGResourceManagerFactory factory,
+			RemoteConfig remoteConfig) {
+		super(remoteConfig, factory);
+	}
+	
+	public BGResourceManagerConfiguration(BGResourceManagerFactory factory) {
+		this(factory, new RemoteConfig());
+		setDefaultNameAndDesc();
+	}
+	
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ptp.rmsystem.IResourceManagerConfiguration#setDefaultNameAndDesc()
+	 */
+	public void setDefaultNameAndDesc() {
+		String name = "Blue Gene";
+		String conn = getConnectionName();
+		if (conn != null && !conn.equals("")) {
+			name += "@" + conn;
+		}
+		setName(name);
+		setDescription("Blue Gene Resource Manager");
+	}
+}
---
+++ rms/org.eclipse.ptp.rm.ibm.bluegene.core/src/org/eclipse/ptp/bluegene/core/rtsystem/BGProxyRuntimeClient.java
@@ -0,0 +1,11 @@
+package org.eclipse.ptp.bluegene.core.rtsystem;
+
+import org.eclipse.ptp.bluegene.core.rmsystem.BGResourceManagerConfiguration;
+import org.eclipse.ptp.remote.AbstractRemoteProxyRuntimeClient;
+
+public class BGProxyRuntimeClient extends AbstractRemoteProxyRuntimeClient {
+	public BGProxyRuntimeClient(BGResourceManagerConfiguration config, 
+			int baseModelId) {
+		super(config, baseModelId);
+	}
+}
