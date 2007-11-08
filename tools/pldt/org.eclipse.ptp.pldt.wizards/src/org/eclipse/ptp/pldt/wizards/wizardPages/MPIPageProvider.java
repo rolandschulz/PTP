438f9a19653a57c44a36ab10c30be563101421a9
Beth Tibbitts <tibbitts@us.ibm.com>
2007-11-08 02:43:22 +0000

    trying to detect project type before displaying wizard page, but no luck yet

--- tools/pldt/org.eclipse.ptp.pldt.wizards/src/org/eclipse/ptp/pldt/wizards/wizardPages/MPIPageProvider.java
+++ tools/pldt/org.eclipse.ptp.pldt.wizards/src/org/eclipse/ptp/pldt/wizards/wizardPages/MPIPageProvider.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corp. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corp. - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.ptp.pldt.wizards.wizardPages;
 
 import org.eclipse.cdt.ui.templateengine.IPagesAfterTemplateSelectionProvider;
@@ -6,13 +16,18 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchWizard;
 
+/**
+ * 
+ * @author tibbitts
+ *
+ */
 public class MPIPageProvider implements IPagesAfterTemplateSelectionProvider {
 	protected IWizardDataPage[] pages;
 
 	public IWizardDataPage[] createAdditionalPages(IWorkbenchWizard wizard, IWorkbench workbench,
 			IStructuredSelection selection) {
 		System.out.println("MPIPageProvider.createAdditionalPages()...");
-		pages = new IWizardDataPage[0];
+		pages = new IWizardDataPage[0];// empty now
 		
 		return pages;
 	}
