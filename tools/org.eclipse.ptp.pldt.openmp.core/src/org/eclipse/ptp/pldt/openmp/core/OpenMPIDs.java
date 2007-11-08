86f6f6189dc8eb9a14123bf0afe20d4888dd8571
btibbitts <btibbitts>
2007-11-08 22:10:38 +0000

    IDs used in wizard page (to be symmetric with MPI usage)

---
+++ tools/org.eclipse.ptp.pldt.openmp.core/src/org/eclipse/ptp/pldt/openmp/core/OpenMPIDs.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.eclipse.ptp.pldt.openmp.core;
+
+/**
+ * @author tibbitts
+ *
+ */
+public class OpenMPIDs {
+ 
+	public static final String OpenMP_INCLUDES = "OpenMP Includes";
+	public static final String OpenMP_BUILD_CMD = "gcc";
+
+}
