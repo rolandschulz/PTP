2d4e66a6a4bcb80efa204dbfb36c569f3c915f6f
gwatson <gwatson>
2007-12-14 16:03:41 +0000

    Provide default implementation of remote runtime system.

---
+++ core/org.eclipse.ptp.rm.remote/src/org/eclipse/ptp/rm/remote/core/AbstractRemoteRuntimeSystem.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.ptp.rm.remote.core;
+
+import java.io.IOException;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.ptp.core.PTPCorePlugin;
+import org.eclipse.ptp.core.attributes.AttributeDefinitionManager;
+import org.eclipse.ptp.rtsystem.AbstractProxyRuntimeSystem;
+
+public class AbstractRemoteRuntimeSystem extends AbstractProxyRuntimeSystem {
+	private AbstractRemoteProxyRuntimeClient proxy;
+	
+	public AbstractRemoteRuntimeSystem(AbstractRemoteProxyRuntimeClient proxy, AttributeDefinitionManager manager) {
+		super(proxy, manager);
+		this.proxy = proxy;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ptp.rtsystem.AbstractProxyRuntimeSystem#shutdown(org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public void shutdown(IProgressMonitor monitor) throws CoreException {
+		try {
+			proxy.shutdown(monitor);
+		} catch (IOException e) {
+			throw new CoreException(new Status(IStatus.WARNING, PTPCorePlugin.getUniqueIdentifier(), 
+					IStatus.WARNING, e.getMessage(), null));
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ptp.rtsystem.AbstractProxyRuntimeSystem#startup(org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public void startup(IProgressMonitor monitor) throws CoreException {
+		try {
+			proxy.startup(monitor);
+		} catch (IOException e) {
+			throw new CoreException(new Status(IStatus.WARNING, PTPCorePlugin.getUniqueIdentifier(), 
+					IStatus.WARNING, e.getMessage(), null));
+		}
+	}
+}
