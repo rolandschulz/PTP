1f0f11037bf08504a7e864cc4f53cc14b30e4a47
gwatson <gwatson>
2007-12-14 18:51:15 +0000

    Initial commit of ptp service model.

---
+++ org.eclipse.ptp.rdt.services/src/org/eclipse/ptp/services/core/IServiceModelManager.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ptp.services.core;
+
+import java.util.Set;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectNature;
+
+public interface IServiceModelManager {
+	/**
+	 * Get the configuration that is currently active for the project. Each project has
+	 * exactly one active configuration, which describes the mapping from services to
+	 * service providers. By default, the first configuration created for a project will 
+	 * be the active configuration for that project.
+	 * 
+	 * @param project project for which the configuration will be obtained
+	 * @return the service configuration for this project
+	 */
+	public IServiceConfiguration getActiveConfiguration(IProject project);
+	
+	/**
+	 * Get the named configuration for this project.
+	 * 
+	 * @param project project for which the configuration will be obtained
+	 * @param name name of the configuration
+	 * @return the service configuration or null if no configurations with the supplied name exist
+	 */
+	public IServiceConfiguration getConfiguration(IProject project, String name);
+	
+	/**
+	 * Get all the configurations that are known by the project
+	 * 
+	 * @param project project containing the configurations
+	 * @return set of configurations known by the project
+	 */
+	public Set<IServiceConfiguration> getConfigurations(IProject project);
+	
+	/**
+	 * Get all the services that have been registered with the system.
+	 * 
+	 * @return a set of all the services
+	 */
+	public Set<IService> getServices();
+	
+	/**
+	 * Get all the services that are used by a particular project.
+	 * 
+	 * @param project project using the services
+	 * @return set of services
+	 */
+	public Set<IService> getServices(IProject project);
+	
+	/**
+	 * Get all the services that are associated with a project nature.
+	 * 
+	 * @param nature project nature
+	 * @return set of services
+	 */
+	public Set<IService> getServices(IProjectNature nature);
+	
+	/**
+	 * Set the active configuration for a project. By default, the first configuration created
+	 * for a project will be the active configuration for that project.
+	 * 
+	 * @param project project for which the configuration will be obtained
+	 * @param configuration configuration to set as active for this project
+	 */
+	public void setActiveConfiguration(IProject project, IServiceConfiguration configuration);
+}
