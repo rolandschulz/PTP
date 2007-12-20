a7db809e7ce910394d6eb21a5fa9bc6d7c20601c
gwatson <gwatson>
2007-12-20 02:06:02 +0000

    Initial commit

---
+++ rms/org.eclipse.ptp.rm.ibm.ll.core/src/org/eclipse/ptp/ibmll/core/rmsystem/IBMLLResourceManagerConfiguration.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2006 The Regents of the University of California. 
+ * This material was produced under U.S. Government contract W-7405-ENG-36 
+ * for Los Alamos National Laboratory, which is operated by the University 
+ * of California for the U.S. Department of Energy. The U.S. Government has 
+ * rights to use, reproduce, and distribute this software. NEITHER THE 
+ * GOVERNMENT NOR THE UNIVERSITY MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR 
+ * ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE. If software is modified 
+ * to produce derivative works, such modified software should be clearly marked, 
+ * so as not to confuse it with the version available from LANL.
+ * 
+ * Additionally, this program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * LA-CC 04-115
+ *
+ * Copyright (c) 2005, 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+/**
+ * 
+ */
+package org.eclipse.ptp.ibmll.core.rmsystem;
+
+import org.eclipse.ptp.rm.remote.core.AbstractRemoteResourceManagerConfiguration;
+import org.eclipse.ptp.rmsystem.IResourceManagerConfiguration;
+import org.eclipse.ui.IMemento;
+
+final public class IBMLLResourceManagerConfiguration extends AbstractRemoteResourceManagerConfiguration {
+
+	public static IResourceManagerConfiguration load(IBMLLResourceManagerFactory factory,
+			IMemento memento) {
+
+		RemoteConfig remoteConfig = loadRemote(factory, memento);
+		
+		IBMLLResourceManagerConfiguration config = 
+			new IBMLLResourceManagerConfiguration(factory, remoteConfig);
+
+		return config;
+	}
+	
+	public IBMLLResourceManagerConfiguration(IBMLLResourceManagerFactory factory,
+			RemoteConfig remoteConfig) {
+		super(remoteConfig, factory);
+	}
+	
+	public IBMLLResourceManagerConfiguration(IBMLLResourceManagerFactory factory) {
+		this(factory, new RemoteConfig());
+		setDefaultNameAndDesc();
+	}
+	
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ptp.rmsystem.IResourceManagerConfiguration#setDefaultNameAndDesc()
+	 */
+	public void setDefaultNameAndDesc() {
+		String name = "IBMLL";
+		String conn = getConnectionName();
+		if (conn != null && !conn.equals("")) {
+			name += "@" + conn;
+		}
+		setName(name);
+		setDescription("IBMLL Resource Manager");
+	}
+}
