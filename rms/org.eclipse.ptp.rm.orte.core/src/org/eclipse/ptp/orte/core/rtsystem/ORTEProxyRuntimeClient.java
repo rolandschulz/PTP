d287f52362248e16ea9a2fa2a1730b494a7aecd2
gwatson <gwatson>
2008-01-09 17:44:56 +0000

    Use custom command factory to allow arguments to be added to the INIT command.

--- rms/org.eclipse.ptp.rm.orte.core/src/org/eclipse/ptp/orte/core/rtsystem/ORTEProxyRuntimeClient.java
+++ rms/org.eclipse.ptp.rm.orte.core/src/org/eclipse/ptp/orte/core/rtsystem/ORTEProxyRuntimeClient.java
@@ -1,11 +1,12 @@
 package org.eclipse.ptp.orte.core.rtsystem;
 
 import org.eclipse.ptp.orte.core.rmsystem.ORTEResourceManagerConfiguration;
+import org.eclipse.ptp.proxy.runtime.event.ProxyRuntimeEventFactory;
 import org.eclipse.ptp.rm.remote.core.AbstractRemoteProxyRuntimeClient;
 
 public class ORTEProxyRuntimeClient extends AbstractRemoteProxyRuntimeClient {
 	public ORTEProxyRuntimeClient(ORTEResourceManagerConfiguration config, 
 			int baseModelId) {
-		super(config, baseModelId);
+		super(config, baseModelId, new ORTEProxyCommandFactory(config), new ProxyRuntimeEventFactory());
 	}
 }
