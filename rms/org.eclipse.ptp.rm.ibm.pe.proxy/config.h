648814a4d28cd7abd049430e436fb31a8650cedf
gwatson <gwatson>
2008-02-13 14:02:49 +0000

    Reran automake and autoconf.

--- rms/org.eclipse.ptp.rm.ibm.pe.proxy/config.h
+++ rms/org.eclipse.ptp.rm.ibm.pe.proxy/config.h
@@ -1,11 +1,41 @@
 /* config.h.  Generated from config.h.in by configure.  */
 /* config.h.in.  Generated from configure.in by autoheader.  */
 
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
 /* Define to 1 if you have the `proxysvr' library (-lproxysvr). */
 #define HAVE_LIBPROXYSVR 1
 
 /* Define to 1 if you have the `utils' library (-lutils). */
 #define HAVE_LIBUTILS 1
+
+/* Define to 1 if you have the <llapi.h> header file. */
+/* #undef HAVE_LLAPI_H */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
 
 /* Define to 1 if your C compiler doesn't accept -c and -o together. */
 /* #undef NO_MINUS_C_MINUS_O */
@@ -29,7 +59,7 @@
 #define PACKAGE_VERSION "2.0.0"
 
 /* Location of poe program */
-#define POE "no"
+#define POE "/usr/bin/poe"
 
 /* Define as the return type of signal handlers (`int' or `void'). */
 #define RETSIGTYPE void
