b972c4054fc322a8cc63bca73d0d9fefaaf396d0
btibbitts <btibbitts>
2008-04-01 02:57:10 +0000

    move countFilesSelected() up from RunAnalyseHandlerBase to RunAnalyseHandler

--- tools/org.eclipse.ptp.pldt.common/src/org/eclipse/ptp/pldt/common/actions/RunAnalyseHandler.java
+++ tools/org.eclipse.ptp.pldt.common/src/org/eclipse/ptp/pldt/common/actions/RunAnalyseHandler.java
@@ -11,8 +11,15 @@
 
 package org.eclipse.ptp.pldt.common.actions;
 
+import java.util.Iterator;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -72,6 +79,58 @@ public abstract class RunAnalyseHandler extends AbstractHandler {
 		}
 		return selection;
 	
+	}
+
+	/**
+	 * Counts the number of files in the selection (leaf nodes only - Files -
+	 * not the directories/containers) <br>
+	 * Note that this makes no distinction about what type of files.
+	 * 
+	 * @return number of files
+	 */
+	@SuppressWarnings("unchecked")
+	protected int countFilesSelected() {
+		int count = 0;
+		// Get elements of a possible multiple selection
+		Iterator iter = this.selection.iterator();
+		while (iter.hasNext()) {
+			Object obj = (Object) iter.next();
+			// It can be a Project, Folder, File, etc...
+			if (obj instanceof IAdaptable) {
+				final IResource res = (IResource) ((IAdaptable) obj)
+						.getAdapter(IResource.class);
+				count = count + countFiles(res);
+			}
+		}
+		// System.out.println("number of files: " + count);
+		return count;
+	}
+
+	/**
+	 * Count the number of files in this resource (file or container).
+	 * 
+	 * @param res
+	 * @return
+	 */
+	protected int countFiles(IResource res) {
+		if (res instanceof IFile) {
+			return 1;
+		} else if (res instanceof IContainer) {
+			int count = 0;
+	
+			try {
+				IResource[] kids = ((IContainer) res).members();
+				for (int i = 0; i < kids.length; i++) {
+					IResource child = kids[i];
+					count = count + countFiles(child);
+				}
+				return count;
+			} catch (CoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return 0;
 	}
 
 }
