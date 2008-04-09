22b5ccb4793e376f0fecb21099471e35a044c923
btibbitts <btibbitts>
2008-04-09 15:03:39 +0000

    added ToolOptionsConstants

---
+++ tools/perf/org.eclipse.ptp.perf.toolopts/src/org/eclipse/ptp/perf/toolopts/ToolsOptionsConstants.java
@@ -0,0 +1,24 @@
+/**********************************************************************
+ * Copyright (c) 2008 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ptp.perf.toolopts;
+
+/**
+ * Constants used in the toolopts plugin
+ * @author Beth Tibbitts
+ *
+ */
+public class ToolsOptionsConstants {
+
+	public static final String TOOL_CONFIG_ARGUMENT_SUFFIX="_ARGUMENT_SAVED";
+	public static final String TOOL_CONFIG_STATE_SUFFIX="_BUTTON_STATE";
+	public static final String TOOL_CONFIG_DEFAULT_SUFFIX="_ARGUMENT_DEFAULT";
+	
+}
