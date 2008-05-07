59d7eebc9eed3914676fb59fef542e33f39f9206
Beth Tibbitts <tibbitts@us.ibm.com>
2008-05-07 18:05:23 +0000

    use more standard include file name of "omp.h" in templates (sample source files)

--- tools/pldt/org.eclipse.ptp.pldt.wizards/templates/OpenMPhelloWorldCproject/src/Basename.c
+++ tools/pldt/org.eclipse.ptp.pldt.wizards/templates/OpenMPhelloWorldCproject/src/Basename.c
@@ -4,37 +4,37 @@
  Author      : $(author)
  Version     :
  Copyright   : $(copyright)
- Description : Hello OpenMP World in C 
+ Description : Hello OpenMP World in C
  ============================================================================
  */
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>  
-#include <math.h>    
-#include "openmp.h"    
+#include <stdlib.h>
+#include <math.h>
+#include "omp.h"
 
 
 int main(int argc, char* argv[]){
 	int    i,arraySize;
 	double *x, *y;     /* the arrays                 */
-	printf("$(openmp.hello.message)\n");	
-	
+	printf("$(openmp.hello.message)\n");
+
 // sample openMP API
 	if (omp_in_parallel()){
-		printf("true"); 
+		printf("true");
 	}
 	  /* Allocate memory for the arrays. */
   x = (double *) malloc( (size_t) (  arraySize * sizeof(double) ) );
   y = (double *) malloc( (size_t) (  arraySize * sizeof(double) ) );
- 
+
   /* Here's the OpenMP pragma that parallelizes the for-loop. */
 #pragma omp parallel for
   for ( i = 0; i < arraySize; i++ )
     {
       y[i] = sin( exp( cos( - exp( sin(x[i]) ) ) ) );
     }
-	  
-	 
-	return 0;   
+
+
+	return 0;
 }
 
