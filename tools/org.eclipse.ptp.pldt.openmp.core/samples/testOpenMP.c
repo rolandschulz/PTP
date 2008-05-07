9050d78591d99e9ad77421ed21c60d51aa0c8de1
btibbitts <btibbitts>
2008-05-07 18:31:42 +0000

    use more standard include file name of "omp.h"

--- tools/org.eclipse.ptp.pldt.openmp.core/samples/testOpenMP.c
+++ tools/org.eclipse.ptp.pldt.openmp.core/samples/testOpenMP.c
@@ -3,33 +3,33 @@
 #include <stdlib.h>  // for omp sample
 #include <math.h>    // for omp
 // fake header file from OpenMPstub proj
-#include "openmp.h"    
+#include "omp.h"
 
- 
-// Sample dummy OpenMP program 
+
+// Sample dummy OpenMP program
 
 
 int main(int argc, char* argv[]){
 	int    i,arraySize;
 	double *x, *y;     /* the arrays                 */
-	printf("Hello OpenMP World.\n");	
-	
+	printf("Hello OpenMP World.\n");
+
 // sample openMP API
 	if (omp_in_parallel()){
-		printf("true"); 
+		printf("true");
 	}
 	  /* Allocate memory for the arrays. */
   x = (double *) malloc( (size_t) (  arraySize * sizeof(double) ) );
   y = (double *) malloc( (size_t) (  arraySize * sizeof(double) ) );
- 
+
   /* Here's the OpenMP pragma that parallelizes the for-loop. */
 #pragma omp parallel for
   for ( i = 0; i < arraySize; i++ )
     {
       y[i] = sin( exp( cos( - exp( sin(x[i]) ) ) ) );
     }
-	  
-	 
-	return 0;   
+
+
+	return 0;
 }
 
