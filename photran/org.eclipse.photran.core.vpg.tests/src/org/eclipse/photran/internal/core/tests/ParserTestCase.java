8c0c3531f4b89d2cfaf8a94c42b5c307f7b5b834
joverbey <joverbey>
2008-06-09 12:39:36 +0000

    Added comments

--- photran/org.eclipse.photran.core.vpg.tests/src/org/eclipse/photran/internal/core/tests/ParserTestCase.java
+++ photran/org.eclipse.photran.core.vpg.tests/src/org/eclipse/photran/internal/core/tests/ParserTestCase.java
@@ -4,6 +4,12 @@ import java.io.File;
 
 import org.eclipse.photran.internal.core.parser.ASTExecutableProgramNode;
 
+/**
+ * A test case which runs the parser over a file, expecting a successful parse.
+ * Created by {@link ParserTestSuite}.
+ * 
+ * @author joverbey
+ */
 public class ParserTestCase extends AbstractParserTestCase
 {
     public ParserTestCase(File file, boolean isFixedForm, String testCaseDescription)
--- photran/org.eclipse.photran.core.vpg.tests/src/org/eclipse/photran/internal/core/tests/ParserTestSuite.java
+++ photran/org.eclipse.photran.core.vpg.tests/src/org/eclipse/photran/internal/core/tests/ParserTestSuite.java
@@ -4,6 +4,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+/**
+ * Creates a suite of {@link ParserTestCase} tests from Fortran source files in a particular directory.
+ * 
+ * @author joverbey
+ */
 public abstract class ParserTestSuite extends AbstractParserTestSuite
 {
     public ParserTestSuite(String directorySuffix, boolean isFixedForm, boolean mustExist) throws FileNotFoundException, IOException
