164e5a5b69da9a47c14570604b84a61b22d5363d
Greg Watson <g.watson@computer.org>
2008-06-18 18:06:45 +0000

    Switch to phase 1. Remove remaining MPI dependencies.

--- debug/org.eclipse.ptp.debug.sdm/src/client/startup.c
+++ debug/org.eclipse.ptp.debug.sdm/src/client/startup.c
@@ -26,20 +26,20 @@
 #include "backend.h"
 #include "proxy.h"
 #include "proxy_tcp.h"
-#include "runtime.h"
+#include "sdm.h"
 
 #define DEFAULT_BACKEND	"gdb-mi"
 #define DEFAULT_PROXY	"tcp"
 
-extern void client(int, int, char *, char *, int);
-extern void server(int, int, int, dbg_backend *);
+extern void client(char *proxy, char *host, int port);
+extern void server(dbg_backend *dbgr);
 
 static struct option longopts[] = {
 	{"debugger",		required_argument,	NULL,	'b'},
-	{"debugger_path",	required_argument,	NULL, 	'e'}, 
-	{"proxy",			required_argument,	NULL, 	'P'}, 
-	{"port",			required_argument,	NULL, 	'p'}, 
-	{"host",			required_argument,	NULL, 	'h'}, 
+	{"debugger_path",	required_argument,	NULL, 	'e'},
+	{"proxy",			required_argument,	NULL, 	'P'},
+	{"port",			required_argument,	NULL, 	'p'},
+	{"host",			required_argument,	NULL, 	'h'},
 	{"jobid",			required_argument,	NULL, 	'j'},
 #ifdef DEBUG
 	{"debug",			optional_argument,	NULL, 	'd'},
@@ -48,9 +48,9 @@ static struct option longopts[] = {
 };
 
 #ifdef DEBUG
-static char * shortopts = "b:e:P:p:h:d::";
+static char * shortopts = "b:e:P:p:h:j:d::";
 #else /* DEBUG */
-static char * shortopts = "b:e:P:p:h:";
+static char * shortopts = "b:e:P:p:h:j:";
 #endif /* DEBUG */
 
 static int	fatal_error = 0;
@@ -60,11 +60,11 @@ void
 error_msg(char *fmt, ...)
 {
 	va_list	ap;
-	
+
 	va_start(ap, fmt);
 	vasprintf(&error_str, fmt, ap);
 	va_end(ap);
-	
+
 	fatal_error = 1;
 }
 
@@ -79,7 +79,7 @@ print_error_msg()
 
 /*
  * Main entry point for MPI parallel debugger.
- * 
+ *
  * @arg	-b debugger	select backend debugger
  * @arg	-e path		set backend debugger path
  * @arg	-p port		port number to listen on/connect to
@@ -90,10 +90,7 @@ print_error_msg()
 int
 main(int argc, char *argv[])
 {
-	int 			rank;
-	int 			size;
-	int				ch;
-	int				jobid = 0;
+	int 			ch;
 	int				port = PROXY_TCP_PORT;
 	char *			host = NULL;
 	char *			debugger_str = DEFAULT_BACKEND;
@@ -102,101 +99,78 @@ main(int argc, char *argv[])
 	proxy *			p;
 	dbg_backend *	d;
 
-	while ((ch = getopt_long(argc, argv, shortopts, longopts, NULL)) != -1)
-	switch (ch) {
-	case 'b':
-		debugger_str = optarg;
-		break;
-	case 'e':
-		path = optarg;
-		break;
-	case 'P':
-		proxy_str = optarg;
-		break;
-	case 'p':
-		port = (int)strtol(optarg, NULL, 10);
-		break;
-	case 'h':
-		host = optarg;
-		break;
-	case 'j':
-		jobid = (int)strtol(optarg, NULL, 10);
-		break;
-#ifdef DEBUG
-	case 'd':
-		if (optarg == NULL)
-			debug_level = DEBUG_LEVEL_ALL;
-		else
-			debug_level = (int)strtol(optarg, NULL, 10);
-		break;
-#endif /* DEBUG */
-	default:
-		error_msg(
-			"sdm [--debugger=value] [--debugger_path=path]\n"
-			"    [--proxy=proxy]\n"
-			"    [--host=host_name] [--port=port]\n"
-			"    [--jobid=jobid]\n"
-#ifdef DEBUG
-			"    [--debug[=level]]\n"
-#endif /* DEBUG */
-		);
-		goto error_out;
+	while ((ch = getopt_long(argc, argv, shortopts, longopts, NULL)) != -1) {
+		switch (ch) {
+		case 'b':
+			debugger_str = optarg;
+			break;
+		case 'e':
+			path = optarg;
+			break;
+		case 'P':
+			proxy_str = optarg;
+			break;
+		case 'p':
+			port = (int)strtol(optarg, NULL, 10);
+			break;
+		case 'h':
+			host = optarg;
+			break;
+		case 'j':
+			break;
+	#ifdef DEBUG
+		case 'd':
+			if (optarg == NULL)
+				debug_level = DEBUG_LEVEL_ALL;
+			else
+				debug_level = (int)strtol(optarg, NULL, 10);
+			break;
+	#endif /* DEBUG */
+		default:
+			error_msg(
+				"sdm [--debugger=value] [--debugger_path=path]\n"
+				"    [--proxy=proxy]\n"
+				"    [--host=host_name] [--port=port]\n"
+				"    [--jobid=jobid]\n"
+	#ifdef DEBUG
+				"    [--debug[=level]]\n"
+	#endif /* DEBUG */
+			);
+			goto error_out;
+		}
 	}
-	
-	argc -= optind;
-	argv += optind;
-	
+
 	if (find_dbg_backend(debugger_str, &d) < 0) {
 		error_msg("No such backend: \"%s\"\n", debugger_str);
 		goto error_out;
 	}
-	
-	if (path != NULL)
+
+	if (path != NULL) {
 		backend_set_path(d, path);
-	
+	}
+
 	if (find_proxy(proxy_str, &p) < 0) {
 		error_msg("No such proxy: \"%s\"\n", proxy_str);
 		goto error_out;
 	}
-	
-error_out:
-	DEBUG_PRINTS(DEBUG_LEVEL_STARTUP, "starting MPI\n");
-	
-	runtime_init(&size, &rank);
-	
-	DEBUG_SET_RANK(rank);
-	
-	DEBUG_PRINTF(DEBUG_LEVEL_STARTUP, "size=%d rank=%d\n", size, rank);
 
-	if (size < 2) {
-		error_msg("Debugger requires at least 2 processes\n");
-	}
-	
-	if (fatal_error) {
-		if (rank == 0)
-			print_error_msg();
-		runtime_finalize();
+error_out:
+	if (sdm_init(argc, argv) < 0) {
+		DEBUG_PRINTS(DEBUG_LEVEL_STARTUP, "sdm_init failed\n");
 		return 1;
 	}
-		
-	// MPI_Comm_create_errhandler(handle_fatal_errors, &err_handler);
-	// MPI_Comm_set_errhandler(MPI_COMM_WORLD, err_handler);
-	
-	/*
-	 * The client process *must* always have task id 'size' - 1 since it
-	 * does not control a debugged process.
-	 */
-	if (rank == size-1) {
+
+	if (sdm_route_get_id() == SDM_MASTER) {
 		DEBUG_PRINTS(DEBUG_LEVEL_STARTUP, "starting client\n");
-		client(size, rank, proxy_str, host, port);
+		client(proxy_str, host, port);
 	} else {
-		DEBUG_PRINTF(DEBUG_LEVEL_STARTUP, "starting task %d\n", rank);
-		server(size, rank, jobid, d);
+		DEBUG_PRINTF(DEBUG_LEVEL_STARTUP, "starting task %d\n", sdm_route_get_id());
+		server(d);
 	}
 
 	DEBUG_PRINTS(DEBUG_LEVEL_STARTUP, "all finished\n");
-	
-	runtime_finalize();
-	
+
+	sdm_finalize();
+
 	return 0;
 }
