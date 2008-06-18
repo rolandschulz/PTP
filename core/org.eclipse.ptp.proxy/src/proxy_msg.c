66bb9dfa74d6a4746bc3d0e98a36b66d067fe558
Greg Watson <g.watson@computer.org>
2008-06-18 17:59:43 +0000

    Interface change for str_to_bitset.

--- core/org.eclipse.ptp.proxy/src/proxy_msg.c
+++ core/org.eclipse.ptp.proxy/src/proxy_msg.c
@@ -1,19 +1,19 @@
 /******************************************************************************
- * Copyright (c) 2005 The Regents of the University of California. 
- * This material was produced under U.S. Government contract W-7405-ENG-36 
- * for Los Alamos National Laboratory, which is operated by the University 
- * of California for the U.S. Department of Energy. The U.S. Government has 
- * rights to use, reproduce, and distribute this software. NEITHER THE 
- * GOVERNMENT NOR THE UNIVERSITY MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR 
- * ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE. If software is modified 
- * to produce derivative works, such modified software should be clearly 
+ * Copyright (c) 2005 The Regents of the University of California.
+ * This material was produced under U.S. Government contract W-7405-ENG-36
+ * for Los Alamos National Laboratory, which is operated by the University
+ * of California for the U.S. Department of Energy. The U.S. Government has
+ * rights to use, reproduce, and distribute this software. NEITHER THE
+ * GOVERNMENT NOR THE UNIVERSITY MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR
+ * ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE. If software is modified
+ * to produce derivative works, such modified software should be clearly
  * marked, so as not to confuse it with the version available from LANL.
- * 
- * Additionally, this program and the accompanying materials 
+ *
+ * Additionally, this program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * LA-CC 04-115
  ******************************************************************************/
 #ifdef __gnu_linux__
@@ -41,7 +41,7 @@ static char tohex[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B
 
 /*
  * Convert a message to a packet ready to send over the wire.
- * 
+ *
  * Note: packet length is added when the packet is transmitted.
  */
 int
@@ -55,40 +55,40 @@ proxy_serialize_msg(proxy_msg *m, char **result)
 
 	if (m == NULL)
 		return -1;
-		
+
 	/*
-	 * Compute packet length. 
-	 * 
+	 * Compute packet length.
+	 *
 	 * Header length includes leading space and separators.
 	 * Body length include encoded strings and separators.
 	 */
 	hdr_len = MSG_ID_SIZE + MSG_TRANS_ID_SIZE + MSG_NARGS_SIZE + 3;
-	
+
 	for (i = 0; i < m->num_args; i++) {
 		len += strlen(m->args[i]) + MSG_ARG_LEN_SIZE + 2;
 	}
-	
+
 	/*
 	 * Allocate packet
 	 */
 	packet = (char *)malloc(hdr_len + len + 1);
 
-	sprintf(packet, " %0*x:%0*x:%0*x", 
-		MSG_ID_SIZE, m->msg_id & MSG_ID_MASK, 
-		MSG_TRANS_ID_SIZE, m->trans_id & MSG_TRANS_ID_MASK, 
+	sprintf(packet, " %0*x:%0*x:%0*x",
+		MSG_ID_SIZE, m->msg_id & MSG_ID_MASK,
+		MSG_TRANS_ID_SIZE, m->trans_id & MSG_TRANS_ID_MASK,
 		MSG_NARGS_SIZE, m->num_args & MSG_NARGS_MASK);
-	
+
 	len = hdr_len;
-	
+
 	for (i = 0; i < m->num_args; i++) {
 		arg_len = strlen(m->args[i]);
-		sprintf(packet + len, " %0*x:%s", 
+		sprintf(packet + len, " %0*x:%s",
 			MSG_ARG_LEN_SIZE, arg_len & MSG_ARG_LEN_MASK, m->args[i]);
 		len += arg_len + MSG_ARG_LEN_SIZE + 2;
 	}
-	
+
 	*result = packet;
-		
+
 	return 0;
 }
 
@@ -98,12 +98,12 @@ proxy_get_data(char *str, char **data, int *len)
 	int		data_len;
 	char	ch;
 	char *	p;
-		
+
 	data_len = strlen(str) / 2;
 
 	*len = data_len;
 	*data = p = (char *)malloc(sizeof(char) * data_len);
-	
+
 	for (; data_len > 0; data_len--) {
 		ch = digittoint(*str++);
 		ch <<= 4;
@@ -122,12 +122,12 @@ proxy_get_int(char *str, int *val)
 void
 proxy_get_bitset(char *str, bitset **b)
 {
-	*b = str_to_bitset(str);
+	*b = str_to_bitset(str, NULL);
 }
 
 /*
- * Convert wire protocol to message. 
- * 
+ * Convert wire protocol to message.
+ *
  * Packet length has already been removed.
  */
 int
@@ -140,11 +140,11 @@ proxy_deserialize_msg(char *packet, int packet_len, proxy_msg **msg)
 	proxy_msg *	m = NULL;
 	char *		arg;
 	char *		end;
-		
+
 	if (packet == NULL || *packet != ' ' || packet_len < MSG_ID_SIZE + MSG_TRANS_ID_SIZE + MSG_NARGS_SIZE + 3) {
 		return -1;
 	}
-	
+
 	/*
 	 * message ID
 	 */
@@ -160,7 +160,7 @@ proxy_deserialize_msg(char *packet, int packet_len, proxy_msg **msg)
 	end = packet + MSG_TRANS_ID_SIZE;
 	*end++ = '\0';
 	trans_id = strtol(packet, NULL, 16);
-	 
+
 	/*
 	 * number of args
 	 */
@@ -170,9 +170,9 @@ proxy_deserialize_msg(char *packet, int packet_len, proxy_msg **msg)
 	num_args = strtol(packet, NULL, 16);
 
 	m = new_proxy_msg(msg_id, trans_id);
-	
+
 	packet = end;
-	
+
 	for (i = 0; i < num_args; i++) {
 		if (proxy_msg_decode_string(packet, &arg, &packet) < 0) {
 			free_proxy_msg(m);
@@ -181,9 +181,9 @@ proxy_deserialize_msg(char *packet, int packet_len, proxy_msg **msg)
 		proxy_msg_add_string_nocopy(m, arg);
 		packet++; /* skip space */
 	}
-	
+
 	*msg = m;
-	
+
 	return 0;
 }
 
@@ -198,26 +198,26 @@ proxy_msg_decode_string(char *str, char **arg, char **end)
 	int 	str_len = strlen(str);
 	char *	ep;
 	char *	p;
-	
+
 	if (str_len < MSG_ARG_LEN_SIZE + 1) {
 		return -1;
 	}
-	
+
 	ep = str + MSG_ARG_LEN_SIZE;
 	*ep++ = '\0';
 	arg_len = strtol(str, NULL, 16);
-	
+
 	if (str_len < MSG_ARG_LEN_SIZE + arg_len + 1) {
 		return -1;
 	}
-	
+
 	p = (char *)malloc(arg_len + 1);
 	memcpy(p, ep, arg_len);
 	p[arg_len] = '\0';
-	
+
 	*arg = p;
 	*end = ep + arg_len;
-	
+
 	return 0;
 }
 
@@ -238,11 +238,11 @@ add_arg(proxy_msg *m, char *arg, int free_arg)
 		m->args = (char **)realloc(m->args, sizeof(char *) * m->arg_size);
 		m->free_args = (int *)realloc(m->free_args, sizeof(int *) * m->arg_size);
 	}
-	
+
 	m->args[m->num_args] = arg;
 	m->free_args[m->num_args] = free_arg;
 	m->num_args++;
-	
+
 	/*
 	 * Make sure that args are always null terminated
 	 */
@@ -256,25 +256,25 @@ proxy_msg_add_data(proxy_msg *m, char *data, int len)
 	char	ch;
 	char *	arg;
 	char *	p;
-	
+
 	if (data == NULL) {
 		proxy_msg_add_string(m, "00");
 		return;
 	}
-	
+
 	p = arg = (char *)malloc((len * 2) + 8 + 2);
-	
+
 	/*
-	 * Encode data 
+	 * Encode data
 	 */
 	for (i = 0; i < len; i++) {
 		ch = *data++;
 		*p++ = tohex[(ch >> 4) & 0xf];
 		*p++ = tohex[ch & 0xf];
 	}
-	
+
 	*p = '\0';
-	
+
 	proxy_msg_add_string(m, arg);
 }
 
@@ -282,7 +282,7 @@ void
 proxy_msg_add_int(proxy_msg *m, int val)
 {
 	char *	str_val;
-	
+
 	asprintf(&str_val, "%d", val);
 	add_arg(m, str_val, 1);
 }
@@ -309,10 +309,10 @@ void
 proxy_msg_add_args(proxy_msg *m, int nargs, char **args)
 {
 	int i;
-	
+
 	if (nargs == 0)
 		return;
-	
+
 	for (i = 0; i < nargs; i++) {
 		add_arg(m, strdup(args[i]), 1);
 	}
@@ -322,10 +322,10 @@ void
 proxy_msg_add_args_nocopy(proxy_msg *m, int nargs, char **args)
 {
 	int i;
-	
+
 	if (nargs == 0)
 		return;
-	
+
 	for (i = 0; i < nargs; i++) {
 		add_arg(m, args[i], 0);
 	}
@@ -335,7 +335,7 @@ void
 proxy_msg_add_keyval_int(proxy_msg *m, char *key, int val)
 {
 	char *	kv;
-	
+
 	asprintf(&kv, "%s=%d", key, val);
 	add_arg(m, kv, 1);
 }
@@ -344,7 +344,7 @@ void
 proxy_msg_add_keyval_string(proxy_msg *m, char *key, char *val)
 {
 	char *	kv;
-	
+
 	asprintf(&kv, "%s=%s", key, val);
 	add_arg(m, kv, 1);
 }
@@ -376,49 +376,49 @@ proxy_msg_insert_bitset(proxy_msg *m, bitset *b, int idx)
 	if (idx >= m->num_args) {
 		return;
 	}
-	
+
 	/*
 	 * Otherwise rotate last argument into required position
 	 */
-	
+
 	tmp_arg = m->args[m->num_args-1];
 	tmp_free = m->free_args[m->num_args-1];
-	
+
 	for (i = m->num_args-1; i > idx; i--) {
 		m->args[i] = m->args[i-1];
 		m->free_args[i] = m->free_args[i-1];
 	}
-	
+
 	m->args[idx] = tmp_arg;
 	m->free_args[idx] = tmp_free;
 }
 
-proxy_msg *	
-new_proxy_msg(int msg_id, int trans_id) 
+proxy_msg *
+new_proxy_msg(int msg_id, int trans_id)
 {
 	proxy_msg *	m = (proxy_msg *)malloc(sizeof(proxy_msg));
-	
+
 	m->msg_id = msg_id;
 	m->trans_id = trans_id;
 	m->arg_size = 0;
 	m->num_args = 0;
 	m->args = NULL;
 	m->free_args = NULL;
-	
+
 	return m;
 }
 
-void	
-free_proxy_msg(proxy_msg *m) 
+void
+free_proxy_msg(proxy_msg *m)
 {
 	int i;
-	
+
 	for (i = 0; i < m->num_args; i++) {
 		if (m->free_args[i]) {
 			free(m->args[i]);
 		}
 	}
-		
+
 	free(m->args);
 	free(m->free_args);
 	free(m);
@@ -440,10 +440,10 @@ void
 proxy_process_msgs(List *msg_list, void (*callback)(proxy_msg *, void *), void *data)
 {
 	proxy_msg *	m;
-	
+
 	if (msg_list == NULL)
 		return;
-		
+
 	while ((m = (proxy_msg *)RemoveFirst(msg_list)) != NULL) {
 		callback(m, data);
 		free_proxy_msg(m);
