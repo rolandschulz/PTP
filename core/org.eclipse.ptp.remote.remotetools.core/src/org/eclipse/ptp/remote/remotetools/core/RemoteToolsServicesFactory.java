14c6a0de37e8c5bb2f98d8b96cc3c4b2edfe1a5e
Greg Watson <g.watson@computer.org>
2008-07-22 22:01:58 +0000

    Separated ptp.remote.remotetools into ptp.remote.remotetools.core and ptp.remote.remotetools.ui

---
+++ core/org.eclipse.ptp.remote.remotetools.core/src/org/eclipse/ptp/remote/remotetools/core/RemoteToolsServicesFactory.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ptp.remote.remotetools.core;
+
+import org.eclipse.ptp.remote.core.IRemoteServicesDelegate;
+import org.eclipse.ptp.remote.core.IRemoteServicesFactory;
+
+public class RemoteToolsServicesFactory implements IRemoteServicesFactory {
+	/* (non-Javadoc)
+	 * @see org.eclipse.ptp.remote.core.IRemoteServicesFactory#getServices()
+	 */
+	public IRemoteServicesDelegate getServices() {
+		return RemoteToolsServices.getInstance();
+	}
+}
