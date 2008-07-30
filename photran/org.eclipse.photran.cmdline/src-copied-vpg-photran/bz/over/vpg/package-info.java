deb9526132a755d92430306f30abf1bd6e2abc9f
joverbey <joverbey>
2008-07-30 20:23:19 +0000

    Cleared copied code; prep for parser regeneration

--- photran/org.eclipse.photran.cmdline/src-copied-vpg-photran/bz/over/vpg/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Contains base classes for VPGs, dependencies, and edges.
- * <p>
- * Eclipse integration and VPG implementations using various databases are <i>not</i> in this package.
- */
-package bz.over.vpg;
