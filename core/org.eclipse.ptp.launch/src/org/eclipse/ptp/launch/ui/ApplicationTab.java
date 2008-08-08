cea9a1a8caf7a6f38766f1501f9e95ebe481d9d3
dferber <dferber>
2008-08-08 18:21:26 +0000

    Fixed launch option that was not being saved.

--- core/org.eclipse.ptp.launch/src/org/eclipse/ptp/launch/ui/ApplicationTab.java
+++ core/org.eclipse.ptp.launch/src/org/eclipse/ptp/launch/ui/ApplicationTab.java
@@ -95,6 +95,7 @@ public class ApplicationTab extends LaunchConfigurationTab {
 	            updateLaunchConfigurationDialog();
 	        } else if (source == localAppButton) {
 	        	handleLocalApplicationButtonSelected();
+	        	updateLaunchConfigurationDialog();
 	        } else if (source == browseAppButton) {
 	        	handleBrowseLocalApplicationButtonSelected();
 	        }
