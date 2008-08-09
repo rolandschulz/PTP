ab1f1878cd5fffe800c88c14a6582c37b019d431
gwatson <gwatson>
2008-08-09 11:51:52 +0000

    Add comments

--- debug/org.eclipse.ptp.debug.sdm/src/impl/sdm_hello_proto.c
+++ debug/org.eclipse.ptp.debug.sdm/src/impl/sdm_hello_proto.c
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - Initial API and implementation
+ *******************************************************************************/
+
 #include <unistd.h>
 
 #include "helloproto.h"
@@ -11,7 +22,7 @@ is_server_peer_valid(int sockfd)
 	return 1;
 }
 
-int 
+int
 is_client_peer_valid(int sockfd)
 {
 	return 1;
