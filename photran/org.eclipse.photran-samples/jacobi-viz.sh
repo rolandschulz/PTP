69ff4bba8973e0f8e36cba08e5edb1b5500ed8ff
joverbey <joverbey>
2008-08-27 21:45:13 +0000

    Made viz shell script more readable

--- photran/org.eclipse.photran-samples/jacobi-viz.sh
+++ photran/org.eclipse.photran-samples/jacobi-viz.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
+#
+# Runs src-jacobi-3-loops/a.exe, which outputs a 2-D matrix of real numbers,
+# and then feeds this matrix to gnuplot to generate a visualization, which
+# is saved as jacobi-viz.gif.
+#
+# J. Overbey 8/27/08
+#
+
 src-jacobi-3*/a.exe >data
+
 cat <<EOF | gnuplot
+#==GNUPLOT INPUT=================================================================
 set term gif
 set output "jacobi-viz.gif"
 
@@ -8,9 +18,9 @@ set output "jacobi-viz.gif"
  set pm3d map   # 1b. Display a 2-D color gradient rather than the 3-D visualization
 #set pm3d at b  # 1c. Display a 2-D color gradient under the 3-D visualization
 
-                                                                   # 2a. Nothing = Black to yellow via blue and red
+#                                                                  # 2a. Nothing = Black to yellow via blue and red
 #set palette model XYZ functions gray**0.35, gray**0.5, gray**0.8  # 2b. Brown to yellow
- set palette model XYZ functions gray**0.3, gray**0.5, 0          # 2b. Dark red to yellow (Jeff)
+ set palette model XYZ functions gray**0.3, gray**0.5, 0           # 2b. Dark red to yellow (Jeff)
 #set palette model XYZ functions gray**0.8, gray**0.5, gray**0.35  # 2c. Green to yellow
 #set palette rgbformulae 21,22,23                                  # 2d. Black to red to yellow to white
 #set palette rgbformulae 22,13,-31                                 # 2e. Blue to green to yellow to red
@@ -18,9 +28,14 @@ set output "jacobi-viz.gif"
 
  splot 'data' matrix notitle             # 3a. Plot normally
 #splot 'data' matrix notitle with pm3d   # 3b. Colorize the 3-D visualization (set 1a above)
+
+#================================================================================
 EOF
+
 rm -f data
+
 echo "Visualization saved as jacobi-viz.gif"
+
 if [ `uname -s` == "Darwin" ]; then
 	open jacobi-viz.gif
 elif [ `which eog` != "" ]; then
