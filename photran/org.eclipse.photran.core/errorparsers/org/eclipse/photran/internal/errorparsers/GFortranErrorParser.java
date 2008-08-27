36180e43447fcbadd07da2074ac6600e5499727a
joverbey <joverbey>
2008-08-27 21:38:59 +0000

    Wrote new gfortran (4.x) error parser; removed XLF error parser in favor of one in separate plug-in

--- photran/org.eclipse.photran.core/errorparsers/org/eclipse/photran/internal/errorparsers/GFortranErrorParser.java
+++ photran/org.eclipse.photran.core/errorparsers/org/eclipse/photran/internal/errorparsers/GFortranErrorParser.java
@@ -1,175 +1,141 @@
 /*******************************************************************************
- * Copyright (c) 2005 IBM Corporation and others.
+ * Copyright (c) 2008 University of Illinois at Urbana-Champaign and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
+ *     UIUC - Initial API and implementation
  *******************************************************************************/
 package org.eclipse.photran.internal.errorparsers;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.eclipse.cdt.core.ErrorParserManager;
 import org.eclipse.cdt.core.IErrorParser;
 import org.eclipse.cdt.core.IMarkerGenerator;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
+import org.eclipse.core.resources.IResource;
 
 /**
-* gfortran Error Parser -- An error parser for gfortran (GNU GCC Fortran)
-*
-* @author ?
-*/
-public class GFortranErrorParser implements IErrorParser {
-	
-	String fileName = null;
-	int lineNumber = -1;
-	
-	public boolean processLine(String line, ErrorParserManager eoParser) {
-		return processLine(line, eoParser, IMarkerGenerator.SEVERITY_ERROR_RESOURCE);
+ * An error parser for GNU Fortran 4.x
+ */
+public class GFortranErrorParser implements IErrorParser
+{
+    private static final int MAX_LINES_IN_ERROR_MESSAGE = 10;
+
+    /*================================================================================
+    cray-pointers.f90:56.21:
+    
+    subroutine example3()
+                        1
+    cray-pointers.f90:43.21:
+    
+    subroutine example3()
+                        2
+    Error: Global name 'example3' at (1) is already being used as a SUBROUTINE at (2)
+    cray-pointers.f90:58.22:
+    
+        real    :: pointee
+                         1
+    Error: Array 'pointee' at (1) cannot have a deferred shape
+    ================================================================================*/
+    
+    private static final Pattern startLine = Pattern.compile("^(.+):([0-9]+)\\.([0-9]+):$");
+    private static final Pattern errorLine = Pattern.compile("^(Fatal )?Error: .*");
+    private static final Pattern warningLine = Pattern.compile("^Warning: .*");
+    
+    private IErrorParser currentState = new WaitForStartLine();
+
+    /*
+     * N.B. We don't return "true" (meaning we successfully matched an error line)
+     * until we match the final line and generate the marker: The startLine pattern
+     * (string:int.int:) is very general, so we might make a mistake.  If this
+     * happens and we accumulate more than MAX_LINES_IN_ERROR_MESSAGE lines,
+     * we bail.  Returning "false" on every line until the final one guarantees that
+     * another error parser will still have a chance to handle these lines if we
+     * do make a mistake.
+     */
+    
+	public boolean processLine(String line, ErrorParserManager eoParser)
+	{
+	    return currentState.processLine(line, eoParser);
 	}
 
-	public boolean processLine(String line, ErrorParserManager eoParser, int inheritedSeverity) {
-		// Known patterns.
-		// (a)
-		//  In file filename:lineno
-		//
-		// use standard_types
-		//                  1
-		//
-		// Fatal Error: Can't open module file 'standard_types.mod' for reading at (1): No such file or directory
-
-		int firstColon = line.indexOf(':');
-
-		/* Guard against drive in Windows platform.  */
-		if (firstColon == 1) {
-			try {
-				String os = System.getProperty("os.name"); //$NON-NLS-1$
-				if (os != null && os.startsWith("Win")) { //$NON-NLS-1$
-					try {
-						if (Character.isLetter(line.charAt(0))) {
-							firstColon = line.indexOf(':', 2);
-						}
-					} catch (StringIndexOutOfBoundsException e) {
-					}
-				}
-			} catch (SecurityException e) {
-			}
-		}
-
-		if (firstColon != -1) {
-			
-			try {
-			
-				/*
-				 *	In file filename:lineno
-				 */
-				if (line.startsWith("In file ")) {
-					fileName = line.substring(8, firstColon);
-					String lineno = line.substring(firstColon + 1);
-					try {
-						lineNumber = Integer.parseInt(lineno);
-					} catch (NumberFormatException e) {
-						lineNumber = -1;
-						fileName = null;
-						return false;
-					}
-				}
-
-				/*
-				 *	(a) Error: desc
-				 *  (b) Fatal Error: desc
-				 */
-				else if (line.startsWith("Error:") || line.startsWith("Fatal Error:")) {
-					String varName = null;
-					String desc = line.substring(firstColon + 1).trim();
-					int severity = extractSeverity("Error", inheritedSeverity);
-					
-					if (fileName == null || lineNumber == -1) {
-						return false;
-					}
-
-					// The pattern is to generall we have to guard:
-					// Before making this pattern a marker we do one more check
-					// The fileName that we extract __must__ look like a valid file name.
-					// We been having to much bad hits with patterns like
-					//   /bin/sh ../libtool --mode=link gcc -version-info 0:1:0 foo.lo var.lo
-					// Things like libtool that will fool the parser because of "0:1:0"
-					if (!Path.EMPTY.isValidPath(fileName)) {
-						return false;
-					}
-					IFile file = eoParser.findFileName(fileName);
-					if (file != null) {
-						if (eoParser.isConflictingName(fileName)) {
-							desc = "[Conflicting names: " + fileName + " ] " + desc; //$NON-NLS-1$ //$NON-NLS-2$
-							file = null;							
-						}
-					} else {
-						file = eoParser.findFilePath(fileName);
-						if (file == null) {
-							// one last try before bailing out we may be in a wrong
-							// directory.  This will happen, for example in the Makefile:
-							// all: foo.c
-							//    cd src3; gcc -c bar/foo.c
-							// the user do a cd(1).
-							IPath path = new Path(fileName);
-							if (path.segmentCount() > 1) {
-								String name = path.lastSegment();
-								file = eoParser.findFileName(fileName);
-								if (file != null) {
-									if (eoParser.isConflictingName(fileName)) {
-										desc = "[Conflicting names: " + name + " ] " + desc; //$NON-NLS-1$ //$NON-NLS-2$
-										file = null;							
-									}
-								}
-							}
-						}
-					}
-
-					// Display the fileName.
-					if (file == null) {
-						desc = desc +"[" + fileName + "]"; //$NON-NLS-1$ //$NON-NLS-2$
-					}
-
-					// Look for variable name
-					int p = desc.indexOf("Symbol \'");
-				 	if (p != -1) {
-				 		varName = desc.substring(p+8);
-				 		p = varName.indexOf("'");
-				 		if (p != -1) {
-				 			varName = varName.substring(0, p);
-				 		} else {
-				 			varName = null;
-				 		}
-				 	}
-				 	
-				 	// Remove at (error number)
-				 	p = desc.indexOf("at (");
-				 	if (p != -1) {
-				 		String tail = desc.substring(p+4);
-				 		int q = tail.indexOf(")");
-				 		if (q != -1) {
-				 			desc = desc.substring(0, p) + tail.substring(q+1);
-				 		}
-				 	}
-
-					eoParser.generateMarker(file, lineNumber, desc, severity, varName);
-					lineNumber = -1;
-					fileName = null;
-				}
-			} catch (StringIndexOutOfBoundsException e) {
-			}
-		}
-		return false;
+	private class WaitForStartLine implements IErrorParser
+	{
+        public boolean processLine(String line, ErrorParserManager eoParser)
+        {
+            Matcher startLineMatcher = startLine.matcher(line);
+            if (startLineMatcher.matches())
+            {
+                String filename = startLineMatcher.group(1);
+                int lineNumber = Integer.parseInt(startLineMatcher.group(2));
+                currentState = new AccumulateErrorMessageLines(filename, lineNumber, line);
+            }
+            return false;
+        }
 	}
 	
-	private int extractSeverity(String desc, int defaultSeverity) {
-		int severity = defaultSeverity; 
-		if (desc.startsWith("warning") || desc.startsWith("Warning")) { //$NON-NLS-1$ //$NON-NLS-2$
-			severity = IMarkerGenerator.SEVERITY_WARNING;
-		}
-		return severity;
+	private class AccumulateErrorMessageLines implements IErrorParser
+	{
+	    private String filename;
+	    private int lineNumber;
+	    private StringBuffer errorMessage = new StringBuffer();
+	    
+	    private int linesAccumulated = 1;
+	    
+	    public AccumulateErrorMessageLines(String filename, int lineNumber, String line)
+	    {
+	        this.filename = filename;
+	        this.lineNumber = lineNumber;
+	        errorMessage.append(line);
+	    }
+	    
+        public boolean processLine(String line, ErrorParserManager eoParser)
+        {
+            errorMessage.append("\n");
+            errorMessage.append(line);
+            linesAccumulated++;
+            
+            Matcher errorMatcher = errorLine.matcher(line);
+            Matcher warningMatcher = warningLine.matcher(line);
+            
+            if (errorMatcher.matches())
+            {
+                // Matched "Error: Description" or "Fatal Error: Description"
+                addMarker(eoParser, true);
+                return true;
+            }
+            else if (warningMatcher.matches())
+            {
+                // Matched "Warning: Description"
+                addMarker(eoParser, false);
+                return true;
+            }
+            else if (linesAccumulated > MAX_LINES_IN_ERROR_MESSAGE)
+            {
+                // We probably made a mistake matching the first line
+                currentState = new WaitForStartLine();
+                return false;
+            }
+            else
+            {
+                // Still accumulating an error message
+                return false;
+            }
+        }
+
+        private void addMarker(ErrorParserManager eoParser, boolean isError)
+        {
+            IResource file = eoParser.findFileName(filename);
+            eoParser.generateMarker(file,
+                lineNumber,
+                errorMessage.toString(),
+                isError ? IMarkerGenerator.SEVERITY_ERROR_RESOURCE : IMarkerGenerator.SEVERITY_WARNING,
+                null);
+            currentState = new WaitForStartLine();
+        }
 	}
 }
---
+++ photran/org.eclipse.photran.core/errorparsers/org/eclipse/photran/internal/errorparsers/OldGFortranErrorParser.java
@@ -0,0 +1,175 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.photran.internal.errorparsers;
+
+import org.eclipse.cdt.core.ErrorParserManager;
+import org.eclipse.cdt.core.IErrorParser;
+import org.eclipse.cdt.core.IMarkerGenerator;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+
+/**
+* gfortran Error Parser -- An error parser for gfortran (GNU GCC Fortran)
+*
+* @author ?
+*/
+public class OldGFortranErrorParser implements IErrorParser {
+	
+	String fileName = null;
+	int lineNumber = -1;
+	
+	public boolean processLine(String line, ErrorParserManager eoParser) {
+		return processLine(line, eoParser, IMarkerGenerator.SEVERITY_ERROR_RESOURCE);
+	}
+
+	public boolean processLine(String line, ErrorParserManager eoParser, int inheritedSeverity) {
+		// Known patterns.
+		// (a)
+		//  In file filename:lineno
+		//
+		// use standard_types
+		//                  1
+		//
+		// Fatal Error: Can't open module file 'standard_types.mod' for reading at (1): No such file or directory
+
+		int firstColon = line.indexOf(':');
+
+		/* Guard against drive in Windows platform.  */
+		if (firstColon == 1) {
+			try {
+				String os = System.getProperty("os.name"); //$NON-NLS-1$
+				if (os != null && os.startsWith("Win")) { //$NON-NLS-1$
+					try {
+						if (Character.isLetter(line.charAt(0))) {
+							firstColon = line.indexOf(':', 2);
+						}
+					} catch (StringIndexOutOfBoundsException e) {
+					}
+				}
+			} catch (SecurityException e) {
+			}
+		}
+
+		if (firstColon != -1) {
+			
+			try {
+			
+				/*
+				 *	In file filename:lineno
+				 */
+				if (line.startsWith("In file ")) {
+					fileName = line.substring(8, firstColon);
+					String lineno = line.substring(firstColon + 1);
+					try {
+						lineNumber = Integer.parseInt(lineno);
+					} catch (NumberFormatException e) {
+						lineNumber = -1;
+						fileName = null;
+						return false;
+					}
+				}
+
+				/*
+				 *	(a) Error: desc
+				 *  (b) Fatal Error: desc
+				 */
+				else if (line.startsWith("Error:") || line.startsWith("Fatal Error:")) {
+					String varName = null;
+					String desc = line.substring(firstColon + 1).trim();
+					int severity = extractSeverity("Error", inheritedSeverity);
+					
+					if (fileName == null || lineNumber == -1) {
+						return false;
+					}
+
+					// The pattern is to generall we have to guard:
+					// Before making this pattern a marker we do one more check
+					// The fileName that we extract __must__ look like a valid file name.
+					// We been having to much bad hits with patterns like
+					//   /bin/sh ../libtool --mode=link gcc -version-info 0:1:0 foo.lo var.lo
+					// Things like libtool that will fool the parser because of "0:1:0"
+					if (!Path.EMPTY.isValidPath(fileName)) {
+						return false;
+					}
+					IFile file = eoParser.findFileName(fileName);
+					if (file != null) {
+						if (eoParser.isConflictingName(fileName)) {
+							desc = "[Conflicting names: " + fileName + " ] " + desc; //$NON-NLS-1$ //$NON-NLS-2$
+							file = null;							
+						}
+					} else {
+						file = eoParser.findFilePath(fileName);
+						if (file == null) {
+							// one last try before bailing out we may be in a wrong
+							// directory.  This will happen, for example in the Makefile:
+							// all: foo.c
+							//    cd src3; gcc -c bar/foo.c
+							// the user do a cd(1).
+							IPath path = new Path(fileName);
+							if (path.segmentCount() > 1) {
+								String name = path.lastSegment();
+								file = eoParser.findFileName(fileName);
+								if (file != null) {
+									if (eoParser.isConflictingName(fileName)) {
+										desc = "[Conflicting names: " + name + " ] " + desc; //$NON-NLS-1$ //$NON-NLS-2$
+										file = null;							
+									}
+								}
+							}
+						}
+					}
+
+					// Display the fileName.
+					if (file == null) {
+						desc = desc +"[" + fileName + "]"; //$NON-NLS-1$ //$NON-NLS-2$
+					}
+
+					// Look for variable name
+					int p = desc.indexOf("Symbol \'");
+				 	if (p != -1) {
+				 		varName = desc.substring(p+8);
+				 		p = varName.indexOf("'");
+				 		if (p != -1) {
+				 			varName = varName.substring(0, p);
+				 		} else {
+				 			varName = null;
+				 		}
+				 	}
+				 	
+				 	// Remove at (error number)
+				 	p = desc.indexOf("at (");
+				 	if (p != -1) {
+				 		String tail = desc.substring(p+4);
+				 		int q = tail.indexOf(")");
+				 		if (q != -1) {
+				 			desc = desc.substring(0, p) + tail.substring(q+1);
+				 		}
+				 	}
+
+					eoParser.generateMarker(file, lineNumber, desc, severity, varName);
+					lineNumber = -1;
+					fileName = null;
+				}
+			} catch (StringIndexOutOfBoundsException e) {
+			}
+		}
+		return false;
+	}
+	
+	private int extractSeverity(String desc, int defaultSeverity) {
+		int severity = defaultSeverity; 
+		if (desc.startsWith("warning") || desc.startsWith("Warning")) { //$NON-NLS-1$ //$NON-NLS-2$
+			severity = IMarkerGenerator.SEVERITY_WARNING;
+		}
+		return severity;
+	}
+}
