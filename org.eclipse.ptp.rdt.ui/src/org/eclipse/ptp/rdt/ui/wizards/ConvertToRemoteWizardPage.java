d1e46c6815d88c6e3306ed646b21b37b9d1c8be8
crecoskie <crecoskie>
2008-09-03 21:04:45 +0000

    RESOLVED - bug 244829: Conversion wizard to convert CDT projects to RDT projects https://bugs.eclipse.org/bugs/show_bug.cgi?id=244829

---
+++ org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/rdt/ui/wizards/ConvertToRemoteWizardPage.java
@@ -0,0 +1,174 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.ptp.rdt.ui.wizards;
+
+ 
+import java.lang.reflect.InvocationTargetException;
+import java.text.MessageFormat;
+import java.util.Map;
+
+import org.eclipse.cdt.ui.wizards.conversion.ConvertProjectWizardPage;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.viewers.CheckStateChangedEvent;
+import org.eclipse.jface.viewers.ICheckStateListener;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.ptp.rdt.core.resources.RemoteNature;
+import org.eclipse.ptp.rdt.services.core.IServiceProvider;
+import org.eclipse.ptp.rdt.ui.messages.Messages;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+
+/**
+ * Converts existing CDT projects to RDT projects
+ * @author vkong
+ */
+public class ConvertToRemoteWizardPage extends ConvertProjectWizardPage {
+    
+    private static final String WZ_TITLE = "WizardProjectConversion.title"; //$NON-NLS-1$
+    private static final String WZ_DESC = "WizardProjectConversion.description"; //$NON-NLS-1$
+    ConvertToRemoteServiceModelWidget fServiceModelWidget;
+	Group remoteServices;
+	
+	/**
+	 * Constructor for ConvertToRemoteWizardPage.
+	 * @param pageName
+	 */
+	public ConvertToRemoteWizardPage(String pageName) {
+		super(pageName);
+		fServiceModelWidget = new ConvertToRemoteServiceModelWidget();
+	}
+    
+    /**
+     * Method getWzTitleResource returns the correct Title Label for this class
+     * overriding the default in the superclass.
+     */
+    protected String getWzTitleResource(){
+        return Messages.getString(WZ_TITLE);
+    }
+    
+	/**
+     * Method getWzDescriptionResource returns the correct description
+     * Label for this class overriding the default in the superclass.
+     */
+    protected String getWzDescriptionResource(){
+        return Messages.getString(WZ_DESC);
+    }
+       
+    /**
+     * Returns true for:
+     * - non-remote projects
+     * So projects with remote nature will be refused.
+     */
+    public boolean isCandidate(IProject project) {
+    	boolean b = false;
+		try {
+			b = !project.hasNature(RemoteNature.REMOTE_NATURE_ID);
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+		return b; 
+    }
+    
+    /**
+     * Add remote nature and configure remote services for the project
+     */
+    public void convertProject(IProject project, String bsId, IProgressMonitor monitor) throws CoreException{
+		monitor.beginTask(Messages.getString("WizardProjectConversion.monitor.convertingToRemoteProject"), 3); //$NON-NLS-1$
+		try {
+			RemoteNature.addRemoteNature(project, monitor);
+			configureServicesForRemoteProject(project);
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			monitor.done();
+		}
+    }
+    
+    /* (non-Javadoc)
+	 * @see org.eclipse.cdt.ui.wizards.conversion.ConvertProjectWizardPage#convertProject(org.eclipse.core.resources.IProject, org.eclipse.core.runtime.IProgressMonitor, java.lang.String)
+	 */
+	@Override
+	public void convertProject(IProject project, IProgressMonitor monitor, String projectID) throws CoreException {
+		monitor.beginTask(Messages.getString("WizardProjectConversion.monitor.convertingToRemoteProject"), 3); //$NON-NLS-1$
+		try {
+			RemoteNature.addRemoteNature(project, monitor);
+			configureServicesForRemoteProject(project);
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			monitor.done();
+		}
+	}
+
+	@Override
+	protected void addToMainPage(Composite container) {
+		remoteServices = new Group(container, SWT.SHADOW_IN);
+		remoteServices.setText(Messages.getString("WizardProjectConversion.servicesTableLabel")); //$NON-NLS-1$
+		remoteServices.setLayout(new FillLayout());
+		remoteServices.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		
+		//create the table for remote services
+		fServiceModelWidget.createContents(remoteServices);
+		//remove all the services in the table for now and add them back as project gets selected in the project list
+		fServiceModelWidget.emptyTable();
+	}
+
+	public void createControl(Composite parent) {
+		super.createControl(parent);
+		tableViewer.addSelectionChangedListener(new ISelectionChangedListener() {
+            public void selectionChanged(SelectionChangedEvent e) {
+            	IProject project = (IProject) ((IStructuredSelection)e.getSelection()).getFirstElement();
+                if (project != null) {
+	            	//update the table with remote services available for the project selected
+	            	fServiceModelWidget.addServicesToTable(project);
+	            	remoteServices.setText(MessageFormat.format(Messages.getString("WizardProjectConversion.servicesTableForProjectLabel"), new Object[] {project.getName()}));  //$NON-NLS-1$
+                }
+            }
+        });		
+		tableViewer.addCheckStateListener(new ICheckStateListener() {
+			public void checkStateChanged(CheckStateChangedEvent e) {				
+				IProject project = (IProject) e.getElement();
+				if (e.getChecked() && project != null) {
+	            	//update the table with remote services available for the project selected
+	            	fServiceModelWidget.addServicesToTable(project);
+	            	remoteServices.setText(MessageFormat.format(Messages.getString("WizardProjectConversion.servicesTableForProjectLabel"), new Object[] {project.getName()}));  //$NON-NLS-1$
+				}							
+			}			
+		});
+	}
+	
+	private void configureServicesForRemoteProject(IProject project) throws InvocationTargetException,
+			InterruptedException {
+		Map<IProject, Map<String,String>> projectToServices = fServiceModelWidget.getProjectToServices();
+		Map<IProject, Map<String,IServiceProvider>> projectToProviders = fServiceModelWidget.getProjectToProviders();
+		
+		Map<String, String> serviceIDToProviderIDMap = projectToServices.get(project);
+		Map<String, IServiceProvider> providerIDToProviderMap = projectToProviders.get(project);
+		
+		ConfigureRemoteServices.configure(project, serviceIDToProviderIDMap, providerIDToProviderMap);
+	}
+}
--- org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/rdt/ui/wizards/ServiceModelWizardPageOperation.java
+++ org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/rdt/ui/wizards/ServiceModelWizardPageOperation.java
@@ -11,27 +11,15 @@
 package org.eclipse.ptp.rdt.ui.wizards;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Properties;
 
-import org.eclipse.cdt.core.CCorePlugin;
-import org.eclipse.cdt.core.model.ICProject;
-import org.eclipse.cdt.internal.core.model.CModelManager;
-import org.eclipse.cdt.internal.core.pdom.indexer.IndexerPreferences;
-import org.eclipse.cdt.managedbuilder.core.IManagedBuildInfo;
-import org.eclipse.cdt.managedbuilder.core.ManagedBuildManager;
 import org.eclipse.cdt.managedbuilder.ui.wizards.MBSCustomPageManager;
 import org.eclipse.cdt.ui.wizards.CDTCommonProjectWizard;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.wizard.IWizard;
-import org.eclipse.ptp.internal.rdt.core.index.RemoteFastIndexer;
-import org.eclipse.ptp.rdt.services.core.IService;
 import org.eclipse.ptp.rdt.services.core.IServiceProvider;
-import org.eclipse.ptp.rdt.services.core.ServiceConfiguration;
-import org.eclipse.ptp.rdt.services.core.ServiceModelManager;
 
 /**
  * @author crecoskie
@@ -55,8 +43,6 @@ public class ServiceModelWizardPageOperation implements IRunnableWithProgress {
 		
 		IProject project = null;
 		
-		final ServiceModelManager serviceModelManager = ServiceModelManager.getInstance();
-		
 		// go through the mappings of services and set the service model to match
 		Object obj = MBSCustomPageManager.getPageProperty(
 				ServiceModelWizardPage.SERVICE_MODEL_WIZARD_PAGE_ID,
@@ -70,45 +56,14 @@ public class ServiceModelWizardPageOperation implements IRunnableWithProgress {
 			if(wizard instanceof CDTCommonProjectWizard)
 				project = ((CDTCommonProjectWizard) wizard).getLastProject();
 			
-			if(project == null)
-				throw new RuntimeException();
+			Object obj2 = MBSCustomPageManager.getPageProperty(
+					ServiceModelWizardPage.SERVICE_MODEL_WIZARD_PAGE_ID,
+					ServiceModelWizardPage.ID_TO_PROVIDERS_MAP_PROPERTY);
 			
-			IManagedBuildInfo info = ManagedBuildManager.getBuildInfo(project);
-			
-			ServiceConfiguration config = new ServiceConfiguration(info.getConfigurationName());
-			
-			Iterator<String> iterator = serviceIDToProviderIDMap.keySet().iterator();
-			
-			while(iterator.hasNext()) {
-				String serviceID = iterator.next();
-				
-				IService service = serviceModelManager.getService(serviceID);
-				
-				String serviceProviderID = serviceIDToProviderIDMap.get(serviceID);
-				
-				Object obj2 = MBSCustomPageManager.getPageProperty(
-						ServiceModelWizardPage.SERVICE_MODEL_WIZARD_PAGE_ID,
-						ServiceModelWizardPage.ID_TO_PROVIDERS_MAP_PROPERTY);
-				
-				if (obj2 instanceof Map) {
-					Map<String, IServiceProvider> providerIDToProviderMap = (Map<String, IServiceProvider>) obj2;
-					
-					IServiceProvider provider = providerIDToProviderMap.get(serviceProviderID);
-
-					config.setServiceProvider(service, provider);
-
-					// note: we only have one config at this point so it will be
-					// active by default
-					serviceModelManager.addConfiguration(project, config);
-				}
-				
+			if (obj2 instanceof Map) {
+				Map<String, IServiceProvider> providerIDToProviderMap = (Map<String, IServiceProvider>) obj2;				
+				ConfigureRemoteServices.configure(project, serviceIDToProviderIDMap, providerIDToProviderMap);
 			}
 		}
-		
-		ICProject cProject = CModelManager.getDefault().getCModel().getCProject(project);
-		CCorePlugin.getIndexManager().setIndexerId(cProject, RemoteFastIndexer.ID);
-		Properties properties = new Properties();
-		properties.put(IndexerPreferences.KEY_FILES_TO_PARSE_UP_FRONT, ""); //$NON-NLS-1$
-		IndexerPreferences.setProperties(project, IndexerPreferences.SCOPE_PROJECT_PRIVATE, properties);
 	}
 }
