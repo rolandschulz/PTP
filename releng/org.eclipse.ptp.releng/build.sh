6b21a07cf26cdfaf433dbffaa32d2c5790a8e3e5
Greg Watson <g.watson@computer.org>
2008-09-18 14:57:51 +0000

    Build RDT server packages

--- releng/org.eclipse.ptp.releng/build.sh
+++ releng/org.eclipse.ptp.releng/build.sh
@@ -30,3 +30,25 @@ cd ..
 # Let's go!
 java -jar tools/org.eclipse.releng.basebuilder/plugins/org.eclipse.equinox.launcher.jar \
 	-ws gtk -arch ppc -os linux -application org.eclipse.ant.core.antRunner $*
+
+# Build RDT server packages
+cd results/plugins
+
+# Check out rdt.core.remotejars, rdt.core.stubs and cdt.core for source
+export CVSROOT=:pserver:anonymous@dev.eclipse.org:/cvsroot/tools
+cvs co -d org.eclipse.ptp.rdt.core.remotejars org.eclipse.ptp/rdt/org.eclipse.ptp.rdt.core.remotejars
+cvs co -d org.eclipse.ptp.rdt.core.stubs org.eclipse.ptp/rdt/org.eclipse.ptp.rdt.core.stubs
+cvs co -r cdt_5_0 -d org.eclipse.cdt.core org.eclipse.cdt/all/org.eclipse.cdt.core
+
+cd org.eclipse.ptp.rdt.core.remotejars
+
+# run the script in remotejars
+export PATH=/opt/public/common/ibm-java2-ppc-50/bin:$PATH
+echo `java -version`
+java -jar ../../../tools/org.eclipse.releng.basebuilder/plugins/org.eclipse.equinox.launcher.jar \
+	-ws gtk -arch ppc -os linux -application org.eclipse.ant.core.antRunner
+
+cd ../../../
+
+# Copy the build.log to where the latest build is
+cp build.log builds/2.1.0/latest
