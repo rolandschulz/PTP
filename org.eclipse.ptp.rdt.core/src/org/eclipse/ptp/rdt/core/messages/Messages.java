c95a284b5efafd9653d6f215f23c409046d347f6
gwatson <gwatson>
2008-10-01 16:31:40 +0000

    Apply patch for bug #246907.

--- org.eclipse.ptp.rdt.core/src/org/eclipse/ptp/rdt/core/messages/Messages.java
+++ org.eclipse.ptp.rdt.core/src/org/eclipse/ptp/rdt/core/messages/Messages.java
@@ -13,13 +13,17 @@ package org.eclipse.ptp.rdt.core.messages;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
+	
 	private static final String BUNDLE_NAME = "org.eclipse.ptp.rdt.core.messages.messages"; //$NON-NLS-1$
-	public static String RemoteCIndexServiceProvider_0;
+	
+	public static String 
+		RemoteCIndexServiceProvider_0,
+		RemoteRunSiProvider_taskName;
+	
+	
 	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+		initializeMessages(BUNDLE_NAME, Messages.class);
 	}
 
-	private Messages() {
-	}
+	private Messages() {}
 }
