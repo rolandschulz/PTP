bf1ca1568084846f62c9ae2c852d0a1b5364869d
crecoskie <crecoskie>
2008-10-09 13:38:59 +0000

    RESOLVED - bug 247678: Type hierarchy doesn't work https://bugs.eclipse.org/bugs/show_bug.cgi?id=247678

---
+++ org.eclipse.ptp.rdt.core/src/org/eclipse/ptp/internal/rdt/core/model/RemoteCProjectFactory.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ptp.internal.rdt.core.model;
+
+import java.util.Set;
+
+import org.eclipse.cdt.core.model.ICProject;
+import org.eclipse.ptp.internal.rdt.core.miners.ScopeManager;
+
+/**
+ * @author crecoskie
+ *
+ */
+public class RemoteCProjectFactory implements ICProjectFactory {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ptp.internal.rdt.core.model.ICProjectFactory#getProjectForFile(java.lang.String)
+	 */
+	public ICProject getProjectForFile(String filename) {
+		// try to use the scope manager to determine the project
+		Set<String> projectNames = ScopeManager.getInstance().getScopesForFile(filename);
+
+		// use the first project we find
+		for (String projName : projectNames) {
+			return new CProject(projName);
+		}
+		
+		return null;
+	}
+
+}
