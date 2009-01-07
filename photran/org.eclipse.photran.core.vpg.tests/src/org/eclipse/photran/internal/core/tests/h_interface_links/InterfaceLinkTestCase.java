99e40131a20d4edc91dac544ba7effccad9fab37
joverbey <joverbey>
2009-01-07 21:54:41 +0000

    Added copyright notices

--- photran/org.eclipse.photran.core.vpg.tests/src/org/eclipse/photran/internal/core/tests/h_interface_links/InterfaceLinkTestCase.java
+++ photran/org.eclipse.photran.core.vpg.tests/src/org/eclipse/photran/internal/core/tests/h_interface_links/InterfaceLinkTestCase.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2009 University of Illinois at Urbana-Champaign and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     UIUC - Initial API and implementation
+ *******************************************************************************/
 package org.eclipse.photran.internal.core.tests.h_interface_links;
 
 import java.util.Collection;
@@ -10,6 +20,12 @@ import org.eclipse.photran.internal.core.analysis.binding.Definition;
 import org.eclipse.photran.internal.core.lexer.Token;
 import org.eclipse.photran.refactoring.tests.RefactoringTestCase;
 
+/**
+ * Unit tests for {@link Definition#resolveInterfaceBinding()}
+ * and {@link Definition#findMatchingDeclarationsInInterfaces()}
+ * 
+ * @author Jeff Overbey
+ */
 public class InterfaceLinkTestCase extends RefactoringTestCase
 {
     private static final String DIR = "interface-test-code";
--- photran/org.eclipse.photran.core.vpg.tests/src/org/eclipse/photran/refactoring/tests/Activator.java
+++ photran/org.eclipse.photran.core.vpg.tests/src/org/eclipse/photran/refactoring/tests/Activator.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2008 University of Illinois at Urbana-Champaign and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     UIUC - Initial API and implementation
+ *******************************************************************************/
 package org.eclipse.photran.refactoring.tests;
 
 import org.eclipse.core.runtime.Plugin;
