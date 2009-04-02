4f2586f6a88397069a69cf3ceeefe91e5ede3a92
Greg Watson <g.watson@computer.org>
2009-04-02 15:07:47 +0000

    Externalize strings.

---
+++ tools/remotetools/org.eclipse.ptp.remotetools.environment.core/src/org/eclipse/ptp/remotetools/environment/core/messages/Messages.java
@@ -0,0 +1,19 @@
+package org.eclipse.ptp.remotetools.environment.core.messages;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.ptp.remotetools.environment.core.messages.messages"; //$NON-NLS-1$
+	public static String SSHTargetControl_0;
+	public static String SSHTargetControl_1;
+	public static String SSHTargetControl_2;
+	public static String SSHTargetControl_3;
+	public static String SSHTargetControl_4;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
