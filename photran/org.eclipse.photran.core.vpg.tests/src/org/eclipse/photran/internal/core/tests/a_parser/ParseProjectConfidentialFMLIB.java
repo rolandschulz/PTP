bdaf5d8de45fec9691a86c1ae7d56dffde44a055
joverbey <joverbey>
2009-05-27 19:27:51 +0000

    Began integrating acceptance testing projects into test suite

--- photran/org.eclipse.photran.core.vpg.tests/src/org/eclipse/photran/internal/core/tests/a_parser/ParseProjectConfidentialFMLIB.java
+++ photran/org.eclipse.photran.core.vpg.tests/src/org/eclipse/photran/internal/core/tests/a_parser/ParseProjectConfidentialFMLIB.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 University of Illinois at Urbana-Champaign and others.
+ * Copyright (c) 2009 University of Illinois at Urbana-Champaign and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -17,10 +17,10 @@ import junit.framework.Test;
 
 import org.eclipse.photran.internal.core.tests.ParserTestSuite;
 
-public class ParseIBEAM
+public class ParseProjectConfidentialFMLIB
 {
     public static Test suite() throws FileNotFoundException, IOException
     {
-        return new ParserTestSuite("confidential-ibeam-object_cpp_mod", false, false) {};
+        return new ParserTestSuite("../../org.eclipse.photran-projects.confidential.fmlib", false, false) {};
     }
 }
---
+++ photran/org.eclipse.photran.core.vpg.tests/src/org/eclipse/photran/internal/core/tests/a_parser/ParseProjectConfidentialIBEAMCppMod.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2009 University of Illinois at Urbana-Champaign and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     UIUC - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.photran.internal.core.tests.a_parser;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import junit.framework.Test;
+
+import org.eclipse.photran.internal.core.tests.ParserTestSuite;
+
+public class ParseProjectConfidentialIBEAMCppMod
+{
+    public static Test suite() throws FileNotFoundException, IOException
+    {
+        return new ParserTestSuite("../../org.eclipse.photran-projects.confidential.ibeam-cpp-mod", false, false) {};
+    }
+}
