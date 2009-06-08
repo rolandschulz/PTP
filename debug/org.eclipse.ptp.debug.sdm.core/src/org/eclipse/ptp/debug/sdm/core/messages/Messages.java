5254d015624bb6eeb4f7ed5d9539e51f1ca40386
gwatson <gwatson>
2009-06-08 18:25:03 +0000

    Fix launch dependency.

---
+++ debug/org.eclipse.ptp.debug.sdm.core/src/org/eclipse/ptp/debug/sdm/core/messages/Messages.java
@@ -0,0 +1,34 @@
+/******************************************************************************
+ * Copyright (c) 2006 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial Implementation
+ *
+ *****************************************************************************/
+package org.eclipse.ptp.debug.sdm.core.messages;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.ptp.debug.sdm.core.messages.messages"; //$NON-NLS-1$
+	
+	public static String SDMDebugger_0;
+	public static String SDMDebugger_1;
+	public static String SDMDebugger_2;
+	public static String SDMDebugger_3;
+	public static String SDMDebugger_4;
+	public static String SDMDebugger_5;
+	public static String SDMDebugger_6;
+	
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
