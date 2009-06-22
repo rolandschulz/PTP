cc385646f90b109a7b13b699f4f2c5dadccbfd94
joverbey <joverbey>
2009-06-22 19:03:29 +0000

    Fixed NPE in ReferenceCollector#visitASTWaitStmtNode

--- photran/org.eclipse.photran.core.vpg/src/org/eclipse/photran/internal/core/analysis/binding/ReferenceCollector.java
+++ photran/org.eclipse.photran.core.vpg/src/org/eclipse/photran/internal/core/analysis/binding/ReferenceCollector.java
@@ -972,7 +972,7 @@ class ReferenceCollector extends BindingCollector
         for (ASTWaitSpecNode waitSpec : node.getWaitSpecList())
         {
             String keyword = waitSpec.getKeyword().getText().toLowerCase();
-            if (keyword.equals("id") || keyword.equals("iomsg") || keyword.equals("iostat"))
+            if (keyword != null && (keyword.equals("id") || keyword.equals("iomsg") || keyword.equals("iostat")))
             {
                 Token variable = waitSpec.getExpr().findFirstToken();
                 if (variable != null && variable.getTerminal() == Terminal.T_IDENT)
