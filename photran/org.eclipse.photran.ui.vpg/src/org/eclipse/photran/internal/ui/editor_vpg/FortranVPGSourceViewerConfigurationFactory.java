8b8c82b1bd3ccd714ee3a5ab88a7b2a4c4bafedf
joverbey <joverbey>
2009-06-30 21:56:14 +0000

    Added copyright and JavaDoc

--- photran/org.eclipse.photran.ui.vpg/src/org/eclipse/photran/internal/ui/editor_vpg/FortranVPGSourceViewerConfigurationFactory.java
+++ photran/org.eclipse.photran.ui.vpg/src/org/eclipse/photran/internal/ui/editor_vpg/FortranVPGSourceViewerConfigurationFactory.java
@@ -1,12 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2008 University of Illinois at Urbana-Champaign and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     UIUC - Initial API and implementation
+ *******************************************************************************/
 package org.eclipse.photran.internal.ui.editor_vpg;
 
 import org.eclipse.cdt.internal.ui.text.CCompositeReconcilingStrategy;
-import org.eclipse.cdt.internal.ui.text.CReconciler;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.text.ITextHover;
 import org.eclipse.jface.text.contentassist.IContentAssistant;
-import org.eclipse.jface.text.reconciler.IReconciler;
-import org.eclipse.jface.text.reconciler.MonoReconciler;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.SourceViewerConfiguration;
 import org.eclipse.photran.internal.ui.editor.AbstractFortranEditor;
@@ -15,6 +21,12 @@ import org.eclipse.photran.internal.ui.editor.AbstractFortranEditor.FortranSourc
 import org.eclipse.photran.internal.ui.editor_vpg.contentassist.FortranCompletionProcessor;
 import org.eclipse.photran.internal.ui.editor_vpg.hover.FortranDeclarationHover;
 
+/**
+ * Factory providing a <code>SourceViewerConfiguration</code> for the Fortran editors which supports
+ * content assist and hover tips.
+ * 
+ * @author Jeff Overbey
+ */
 @SuppressWarnings("restriction")
 public class FortranVPGSourceViewerConfigurationFactory implements IFortranSourceViewerConfigurationFactory
 {
@@ -22,8 +34,7 @@ public class FortranVPGSourceViewerConfigurationFactory implements IFortranSourc
     {
         return new FortranSourceViewerConfiguration(editor)
         {
-            private final FortranCompletionProcessor fortranCompletionProcessor =
-                new FortranCompletionProcessor();
+            private final FortranCompletionProcessor fortranCompletionProcessor = new FortranCompletionProcessor();
             
             @Override protected CCompositeReconcilingStrategy createReconcilingStrategy(ISourceViewer sourceViewer)
             {
