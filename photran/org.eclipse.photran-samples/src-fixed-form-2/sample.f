21fa0868e7d39881ba8cb4b06149799ac9ca32d7
joverbey <joverbey>
2009-07-24 20:08:59 +0000

    Added sample file from Bug 284597

---
+++ photran/org.eclipse.photran-samples/src-fixed-form-2/sample.f
@@ -0,0 +1,14 @@
+      program blah
+        integer :: xyz,y
+        xy                                                              sdfgsdfgsd
+     $  z = 3
+        y = 5
+        if(sine(y)
+     $  .lt.x)then                                                            Variable cr
+         print*,'Hello, old-school Fortran!'
+        endif
+      end pro
+     $      gram
+
+
+
