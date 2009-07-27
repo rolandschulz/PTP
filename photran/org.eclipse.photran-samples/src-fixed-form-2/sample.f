394caf6391a425d83f955ad48bb7adc0dee777d0
joverbey <joverbey>
2009-07-27 17:07:39 +0000

    Updated makefile due to fixed form bug in gfortran

--- photran/org.eclipse.photran-samples/src-fixed-form-2/sample.f
+++ photran/org.eclipse.photran-samples/src-fixed-form-2/sample.f
@@ -3,7 +3,7 @@
         xy                                                              sdfgsdfgsd
      $  z = 3
         y = 5
-        if(sine(y)
+        if(sin(y+0.0)
      $  .lt.x)then                                                            Variable cr
          print*,'Hello, old-school Fortran!'
         endif
