e72fc26695a919489bf1a67eb50d591678d160d8
Greg Watson <g.watson@computer.org>
2009-08-12 21:44:28 +0000

    Added context menu actions.

--- core/org.eclipse.ptp.services.ui/src/org/eclipse/ptp/services/ui/views/ServiceConfigurationView.java
+++ core/org.eclipse.ptp.services.ui/src/org/eclipse/ptp/services/ui/views/ServiceConfigurationView.java
@@ -11,36 +11,36 @@
 package org.eclipse.ptp.services.ui.views;
 
 import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.jface.action.IMenuListener;
-import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
-import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.resource.FontDescriptor;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.resource.LocalResourceManager;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.ITreeSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.ptp.services.core.IServiceConfiguration;
+import org.eclipse.ptp.services.core.IServiceModelEvent;
+import org.eclipse.ptp.services.core.IServiceModelEventListener;
+import org.eclipse.ptp.services.core.IServiceModelManager;
 import org.eclipse.ptp.services.core.ServiceModelManager;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.MouseAdapter;
-import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.FontData;
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.TreeItem;
-import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.model.WorkbenchContentProvider;
 import org.eclipse.ui.model.WorkbenchLabelProvider;
 import org.eclipse.ui.part.ViewPart;
 
 public class ServiceConfigurationView extends ViewPart {
 
+	/**
+	 * Listener for service configuration selection events.
+	 */
+	private class ServiceModelEventListener implements IServiceModelEventListener {
+		public void handleEvent(IServiceModelEvent event) {
+			refreshViewer();
+		}
+	}
+	
 	private class ServiceConfigurationLabelProvider extends WorkbenchLabelProvider {
 		
 		private Font selectedFont;
@@ -53,6 +53,18 @@ public class ServiceConfigurationView extends ViewPart {
 			selectedFont = (Font)new LocalResourceManager(JFaceResources.getResources()).get(FontDescriptor.createFrom(selectedFontData));
 		}
 
+		/* (non-Javadoc)
+		 * @see org.eclipse.ui.model.WorkbenchLabelProvider#getFont(java.lang.Object)
+		 */
+		@Override
+		public Font getFont(Object element) {
+			IServiceConfiguration conf = getServiceConfiguration(element);
+			if (conf != null && conf == fManager.getActiveConfiguration()) {
+				return selectedFont;
+			}
+			return unSelectedFont;
+		}
+		
 		private IServiceConfiguration getServiceConfiguration(Object parentElement) {
 			IServiceConfiguration conf = null;
 			if (parentElement instanceof IAdaptable) {
@@ -60,43 +72,28 @@ public class ServiceConfigurationView extends ViewPart {
 			}
 			return conf;
 		}
-		
-		/* (non-Javadoc)
-		 * @see org.eclipse.ui.model.WorkbenchLabelProvider#getFont(java.lang.Object)
-		 */
-		@Override
-		public Font getFont(Object element) {
-			IServiceConfiguration conf = getServiceConfiguration(element);
-			if (conf != null) {// && conf == ServiceModelManager.getInstance().getSelected()) {
-				return selectedFont;
-			}
-			return unSelectedFont;
-		}
 	}
 
-	private TreeViewer viewer;
-
-//	private RemoveResourceManagersAction removeResourceManagerAction;
-//	private AddResourceManagerAction addResourceManagerAction;
-//	private EditResourceManagerAction editResourceManagerAction;
-//	private SelectDefaultResourceManagerAction selectResourceManagerAction;
+	private TreeViewer fViewer;
+	private IServiceModelManager fManager = ServiceModelManager.getInstance();
 
 	public ServiceConfigurationView() {
+		fManager.addEventListener(new ServiceModelEventListener(), 
+				IServiceModelEvent.SERVICE_CONFIGURATION_SELECTED |
+				IServiceModelEvent.SERVICE_CONFIGURATION_ADDED |
+				IServiceModelEvent.SERVICE_CONFIGURATION_REMOVED);
 	}
 	
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
 	 */
 	public void createPartControl(Composite parent) {
-		viewer = new TreeViewer(parent, SWT.MULTI);
-		viewer.setContentProvider(new WorkbenchContentProvider());
-		viewer.setLabelProvider(new ServiceConfigurationLabelProvider(viewer.getTree().getFont()));
+		fViewer = new TreeViewer(parent, SWT.MULTI);
+		fViewer.setContentProvider(new WorkbenchContentProvider());
+		fViewer.setLabelProvider(new ServiceConfigurationLabelProvider(fViewer.getTree().getFont()));
 
-		viewer.setInput(ServiceModelManager.getInstance());
+		fViewer.setInput(ServiceModelManager.getInstance());
 
-		// -----------------------------
-		// Enable right-click popup menu
-		// -----------------------------
 		createContextMenu();
 
 		// ----------------------------------------------------------------------
@@ -105,136 +102,49 @@ public class ServiceConfigurationView extends ViewPart {
 		// IPropertySource, or support IPropertySource.class as an adapter type
 		// in its AdapterFactory.
 		// ----------------------------------------------------------------------
-		getSite().setSelectionProvider(viewer);
-
-		viewer.getTree().addMouseListener(new MouseAdapter() {
-			/* (non-Javadoc)
-			 * @see org.eclipse.swt.events.MouseAdapter#mouseDoubleClick(org.eclipse.swt.events.MouseEvent)
-			 */
-			@Override
-			public void mouseDoubleClick(MouseEvent e) {
-				ITreeSelection selection = (ITreeSelection)viewer.getSelection();
-				if (!selection.isEmpty()) {
-					IServiceConfiguration rm = (IServiceConfiguration)selection.getFirstElement();
-					//editResourceManagerAction.setResourceManager(rm);
-					//editResourceManagerAction.run();
-				}
-			}
-
-			/* (non-Javadoc)
-			 * @see org.eclipse.swt.events.MouseAdapter#mouseDown(org.eclipse.swt.events.MouseEvent)
-			 */
-			public void mouseDown(MouseEvent e) {
-				ISelection selection = viewer.getSelection();
-				TreeItem item = viewer.getTree().getItem(new Point(e.x, e.y));
-				if (item == null && !selection.isEmpty()) {
-					viewer.getTree().deselectAll();
-				}
-				else if (item != null) {
-				}
-			}
-		});
+		getSite().setSelectionProvider(fViewer);
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.part.WorkbenchPart#dispose()
+	 */
 	public synchronized void dispose() {
 		super.dispose();
 	}
 
 	public Font getFont() {
-		if (viewer == null) {
+		if (fViewer == null) {
 			return null;
 		}
-		return viewer.getTree().getFont();
+		return fViewer.getTree().getFont();
 	}
 
+	/**
+	 * Refresh the tree viewer when the model changes
+	 */
 	public void refreshViewer() {
-		viewer.getControl().getDisplay().asyncExec(new Runnable(){
+		fViewer.getControl().getDisplay().asyncExec(new Runnable(){
 			public void run() {
-				viewer.refresh();
+				fViewer.refresh();
 			}
 		});
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
+	 */
 	public void setFocus() {
-		viewer.getControl().setFocus();
+		fViewer.getControl().setFocus();
 	}
 	
+	/**
+	 * Create the popup menu
+	 */
 	private void createContextMenu() {
-		final Shell shell = getSite().getShell();
-//		addResourceManagerAction = new AddResourceManagerAction(shell);
-//		removeResourceManagerAction = new RemoveResourceManagersAction(shell);
-//		editResourceManagerAction = new EditResourceManagerAction(shell);
-//		selectResourceManagerAction = new SelectDefaultResourceManagerAction(this);
-
-		MenuManager menuManager = new MenuManager("#PopupMenu"); //$NON-NLS-1$
+		MenuManager menuManager = new MenuManager("#PopupMenu", "org.eclipse.ptp.services.ui.views.serviceConfigurationView.contextMenu"); //$NON-NLS-1$
 		menuManager.setRemoveAllWhenShown(true);
-		menuManager.addMenuListener(new IMenuListener() {
-			public void menuAboutToShow(IMenuManager manager) {
-				fillContextMenu(manager);
-			}
-		});
-
-		Menu menu = menuManager.createContextMenu(viewer.getControl());
-		viewer.getControl().setMenu(menu);
-		getSite().registerContextMenu(menuManager, viewer);
+		Menu menu = menuManager.createContextMenu(fViewer.getControl());
+		fViewer.getControl().setMenu(menu);
+		getSite().registerContextMenu(menuManager, fViewer);
 	}
-
-	private void fillContextMenu(IMenuManager manager) {
-		final IStructuredSelection selection = (IStructuredSelection) viewer.getSelection();
-//		manager.add(addResourceManagerAction);
-		Object[] selectedObjects = selection.toArray();
-		boolean inContextForRM = selection.size() > 0;
-		boolean inContextForEditRM = inContextForRM;
-		boolean inContextForRemoveRM = inContextForRM;
-		boolean inContextForSelectRM = inContextForRM;
-//		for (int i = 0; i < selectedObjects.length; ++i) {
-//			if (!(selectedObjects[i] instanceof IResourceManagerMenuContribution)) {
-//				// Not all of the selected are RMs
-//				inContextForRM = false;
-//				inContextForEditRM = false;
-//				inContextForRemoveRM = false;
-//				inContextForSelectRM = false;
-//				break;
-//			}
-//			else {
-//                final IResourceManagerMenuContribution menuContrib = (IResourceManagerMenuContribution) selectedObjects[i];
-//			    IResourceManagerControl rm = (IResourceManagerControl) menuContrib.getAdapter(IResourceManagerControl.class);
-//			    if (rm.getState() != ResourceManagerAttributes.State.STOPPED) {
-//			    	inContextForEditRM = false;
-//			        inContextForRemoveRM = false;
-//			    } else {
-//			        inContextForSelectRM = false;
-//			    }
-//			}
-//		}
-//		manager.add(removeResourceManagerAction);
-//		removeResourceManagerAction.setEnabled(inContextForRemoveRM);
-//		if (inContextForRemoveRM) {
-//			IResourceManagerControl[] rmManagers = new IResourceManagerControl[selection.size()];
-//			for (int i = 0; i < rmManagers.length; ++i) {
-//				final IResourceManagerMenuContribution menuContrib = (IResourceManagerMenuContribution) selectedObjects[i];
-//				rmManagers[i] = (IResourceManagerControl) menuContrib.getAdapter(IResourceManagerControl.class);
-//			}
-//			removeResourceManagerAction.setResourceManager(rmManagers);
-//		}
-//		manager.add(editResourceManagerAction);
-//		editResourceManagerAction.setEnabled(inContextForEditRM);
-//		if (inContextForEditRM) {
-//			final IResourceManagerMenuContribution menuContrib = (IResourceManagerMenuContribution) selectedObjects[0];
-//			IResourceManagerControl rmManager = (IResourceManagerControl) menuContrib.getAdapter(IResourceManagerControl.class);
-//			editResourceManagerAction.setResourceManager(rmManager);
-//		}
-//		manager.add(new Separator());
-//		manager.add(selectResourceManagerAction);
-//		selectResourceManagerAction.setEnabled(inContextForSelectRM);
-//		if (inContextForSelectRM) {
-//			final IResourceManagerMenuContribution menuContrib = (IResourceManagerMenuContribution) selectedObjects[0];
-//			IResourceManagerControl rmManager = (IResourceManagerControl) menuContrib.getAdapter(IResourceManagerControl.class);
-//			selectResourceManagerAction.setResourceManager(rmManager);
-//		}
-		manager.add(new Separator());
-		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
-		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS + "-end")); //$NON-NLS-1$
-	}
-
 }
