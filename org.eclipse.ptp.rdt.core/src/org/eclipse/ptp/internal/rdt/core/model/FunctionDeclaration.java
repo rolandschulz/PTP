21be8263b86b2508ef82e658c3ae348b81feb6b7
vkong <vkong>
2009-08-13 18:53:25 +0000

    Bug 286563 - CElement.equals() is incomplete

--- org.eclipse.ptp.rdt.core/src/org/eclipse/ptp/internal/rdt/core/model/FunctionDeclaration.java
+++ org.eclipse.ptp.rdt.core/src/org/eclipse/ptp/internal/rdt/core/model/FunctionDeclaration.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 IBM Corporation and others.
+ * Copyright (c) 2008, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -132,7 +132,7 @@ public class FunctionDeclaration extends SourceManipulation implements IFunction
 			return false;
 		}
 		IFunctionDeclaration other = (IFunctionDeclaration) o;
-		if (!Arrays.equals(fParameterTypes, getParameterTypes())) {
+		if (!Arrays.equals(fParameterTypes, other.getParameterTypes())) {
 			return false;
 		}
 		String returnType = other.getReturnType();
