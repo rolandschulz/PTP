22c77c52b632172ce627c254997051dea40dcf2e
gwatson <gwatson>
2009-08-21 20:01:08 +0000

    Added copyright.

--- core/org.eclipse.ptp.services.core/src/org/eclipse/ptp/services/core/Activator.java
+++ core/org.eclipse.ptp.services.core/src/org/eclipse/ptp/services/core/Activator.java
@@ -1,9 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - Initial API and implementation
+ *******************************************************************************/
 package org.eclipse.ptp.services.core;
-
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Plugin;
 import org.eclipse.core.runtime.Status;
 import org.osgi.framework.BundleContext;
+
 
 /**
  * <strong>EXPERIMENTAL</strong>. This class or interface has been added as
