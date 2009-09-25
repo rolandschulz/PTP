b9604dc02d5866e340940ed8371c2c87d864d2f7
Greg Watson <g.watson@computer.org>
2009-09-25 13:42:37 +0000

    Rename Activator to ServicesUIPlugin

--- core/org.eclipse.ptp.services.ui/src/org/eclipse/ptp/services/ui/ServicesUIPlugin.java
+++ core/org.eclipse.ptp.services.ui/src/org/eclipse/ptp/services/ui/ServicesUIPlugin.java
@@ -14,18 +14,18 @@ import org.osgi.framework.BundleContext;
  * 
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class ServicesUIPlugin extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.eclipse.ptp.services.ui"; //$NON-NLS-1$
 
 	// The shared instance
-	private static Activator plugin;
+	private static ServicesUIPlugin plugin;
 
 	/**
 	 * The constructor
 	 */
-	public Activator() {
+	public ServicesUIPlugin() {
 	}
 
 	@Override
@@ -45,7 +45,7 @@ public class Activator extends AbstractUIPlugin {
 	 *
 	 * @return the shared instance
 	 */
-	public static Activator getDefault() {
+	public static ServicesUIPlugin getDefault() {
 		return plugin;
 	}
 
