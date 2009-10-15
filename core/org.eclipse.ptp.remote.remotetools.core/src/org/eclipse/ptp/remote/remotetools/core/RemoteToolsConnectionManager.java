5acd5bb32beca8172dc3c9e0dcbd2b2aa4c97d8d
gwatson <gwatson>
2009-10-15 22:47:26 +0000

    Use config constants when creating a new connection. Also, avoid NPE if remoteHost is null (shouldn't happen)

--- core/org.eclipse.ptp.remote.remotetools.core/src/org/eclipse/ptp/remote/remotetools/core/RemoteToolsConnectionManager.java
+++ core/org.eclipse.ptp.remote.remotetools.core/src/org/eclipse/ptp/remote/remotetools/core/RemoteToolsConnectionManager.java
@@ -16,9 +16,11 @@ import java.util.Map;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.ptp.remote.core.IRemoteConnection;
 import org.eclipse.ptp.remote.core.IRemoteConnectionManager;
+import org.eclipse.ptp.remote.core.exception.RemoteConnectionException;
+import org.eclipse.ptp.remote.remotetools.core.environment.ConfigFactory;
 import org.eclipse.ptp.remote.remotetools.core.environment.PTPTargetControl;
+import org.eclipse.ptp.remote.remotetools.core.messages.Messages;
 import org.eclipse.ptp.remotetools.environment.EnvironmentPlugin;
-import org.eclipse.ptp.remotetools.environment.control.ITargetControl;
 import org.eclipse.ptp.remotetools.environment.core.ITargetElement;
 import org.eclipse.ptp.remotetools.environment.core.TargetEnvironmentManager;
 import org.eclipse.ptp.remotetools.environment.core.TargetTypeElement;
@@ -68,26 +70,46 @@ public class RemoteToolsConnectionManager implements IRemoteConnectionManager {
 	 * by another entity.
 	 */
 	private void refreshConnections() {
-		Map<String, IRemoteConnection> newConns = new HashMap<String, IRemoteConnection>();
-		for (Object obj : remoteHost.getElements()) {
-			ITargetElement element = (ITargetElement)obj;
-			IRemoteConnection conn = connections.get(element.getName());
-			if (conn == null) {
-				ITargetControl control;
-				try {
-					control = element.getControl();
-					/*
-					 * FIXME: need to work out how to get this information correctly!!!!
-					 */
-					String address = (String) element.getAttributes().get("ptp.connection-address"); //$NON-NLS-1$
-					String user = (String) element.getAttributes().get("ptp.login-username"); //$NON-NLS-1$
-					conn = new RemoteToolsConnection(element.getName(), address, user, (PTPTargetControl)control);
-					((PTPTargetControl)control).setConnection((RemoteToolsConnection)conn);
-				} catch (CoreException e) {
+		if (remoteHost != null) {
+			Map<String, IRemoteConnection> newConns = new HashMap<String, IRemoteConnection>();
+			for (Object obj : remoteHost.getElements()) {
+				ITargetElement element = (ITargetElement)obj;
+				IRemoteConnection conn = connections.get(element.getName());
+				if (conn == null) {
+					try {
+						conn = createConnection(element);
+					} catch (RemoteConnectionException e) {
+					}
 				}
+				newConns.put(element.getName(), conn);
 			}
-			newConns.put(element.getName(), conn);
+			connections.clear();
+			connections.putAll(newConns);
 		}
-		connections = newConns;
+	}
+	
+	/**
+	 * Create a connection using information from the target element.
+	 * 
+	 * @param element target element
+	 * @return new remote tools connection
+	 * @throws RemoteConnectionException
+	 */
+	private IRemoteConnection createConnection(ITargetElement element) throws RemoteConnectionException {
+		String address = (String) element.getAttributes().get(ConfigFactory.ATTR_CONNECTION_ADDRESS);
+		String user = (String) element.getAttributes().get(ConfigFactory.ATTR_LOGIN_USERNAME);
+		if (address == null) {
+			throw new RemoteConnectionException(Messages.RemoteToolsConnectionManager_1);
+		}
+		if (user == null) {
+			throw new RemoteConnectionException(Messages.RemoteToolsConnectionManager_2);
+		}
+		try {
+			RemoteToolsConnection conn = new RemoteToolsConnection(element.getName(), address, user, (PTPTargetControl)element.getControl());
+			((PTPTargetControl)element.getControl()).setConnection(conn);
+			return conn;
+		} catch (CoreException e) {
+			throw new RemoteConnectionException(e.getMessage());
+		}
 	}
 }
