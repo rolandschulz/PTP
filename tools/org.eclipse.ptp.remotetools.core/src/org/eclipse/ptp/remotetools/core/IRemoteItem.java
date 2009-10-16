48582195505948e14af64ce3f95fe7cc747ff5f6
gwatson <gwatson>
2009-10-16 20:38:59 +0000

    Added isDirectory()

--- tools/org.eclipse.ptp.remotetools.core/src/org/eclipse/ptp/remotetools/core/IRemoteItem.java
+++ tools/org.eclipse.ptp.remotetools.core/src/org/eclipse/ptp/remotetools/core/IRemoteItem.java
@@ -13,7 +13,6 @@ package org.eclipse.ptp.remotetools.core;
 
 import org.eclipse.ptp.remotetools.exception.CancelException;
 import org.eclipse.ptp.remotetools.exception.RemoteConnectionException;
-import org.eclipse.ptp.remotetools.exception.RemoteExecutionException;
 import org.eclipse.ptp.remotetools.exception.RemoteOperationException;
 
 /**
@@ -22,35 +21,74 @@ import org.eclipse.ptp.remotetools.exception.RemoteOperationException;
  */
 public interface IRemoteItem {
 
+	/**
+	 * Get the path represented by this item.
+	 * 
+	 * @return path
+	 */
 	public String getPath();
+	
+	/**
+	 * Test if this remote item actually exists on the remote system.
+	 * 
+	 * @return true if the remote item exists
+	 */
 	public boolean exists();
 	
 	/**
 	 * Returns true if the remote item has a permission that allows to read this
 	 * item. Several permissions are considered (user, group, others) to
-	 * calculate de effective permission.
+	 * calculate the effective permission.
 	 * 
 	 * @return true/false
 	 */
 	public boolean isReadable();
+	
+	/**
+	 * Set the permission on the remote item enable/disable read access.
+	 * 
+	 * @param flag
+	 */
 	public void setReadable(boolean flag);
 	
 	/**
 	 * Returns true if the remote item has a permission that allows to write
 	 * this item. Several permissions are considered (user, group, others) to
-	 * calculate de effective permission.
+	 * calculate the effective permission.
 	 * 
 	 * @return false
 	 */
 	public boolean isWritable();
+	
+	/**
+	 * Set the permission on the remote item to enable/disable write access.
+	 * 
+	 * @param flag
+	 */
 	public void setWriteable(boolean flag);
 	
 //	public long getCreationTime();
 //	public void setCreationTime(long time);
 
+	/**
+	 * Get the modification time of the item.
+	 * 
+	 * @return last modified time
+	 */
 	public long getModificationTime();
+	
+	/**
+	 * Set the modification time of the item
+	 * 
+	 * @param time modification time
+	 */
 	public void setModificationTime(long time);
 
+	/**
+	 * Get the last accessed time of the item
+	 * 
+	 * @return last access time
+	 */
 	public long getAccessTime();
 //	public void setAccessTime(long time);
 
@@ -59,6 +97,8 @@ public interface IRemoteItem {
 //	
 //	public String getGroupId();
 //	public String setGroupId();
+	
+	public boolean isDirectory();
 	
 	/**
 	 * Fetch all the attributes from the item on remote host.
