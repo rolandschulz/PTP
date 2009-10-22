9d3703a953c472e69ec34bc94afae4d2dec0bca0
gwatson <gwatson>
2009-10-22 20:25:28 +0000

    Externalized string.

--- core/org.eclipse.ptp.remote.remotetools.ui/src/org/eclipse/ptp/remote/remotetools/ui/messages/Messages.java
+++ core/org.eclipse.ptp.remote.remotetools.ui/src/org/eclipse/ptp/remote/remotetools/ui/messages/Messages.java
@@ -5,6 +5,7 @@ import org.eclipse.osgi.util.NLS;
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.ptp.remote.remotetools.ui.messages.messages"; //$NON-NLS-1$
 	public static String RemoteToolsFileSystemContributor_0;
+	public static String RemoteToolsUIConnectionManager_0;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
