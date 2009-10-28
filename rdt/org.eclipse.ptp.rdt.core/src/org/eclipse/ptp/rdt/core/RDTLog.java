234c3da8d427357191232a0f6ef4508c966c0418
Vivian Kong <vivkong@ca.ibm.com>
2009-10-28 16:55:20 +0000

    Bug 293382 -  Provide feedback on problems with the indexer - display message in a dialog

--- rdt/org.eclipse.ptp.rdt.core/src/org/eclipse/ptp/rdt/core/RDTLog.java
+++ rdt/org.eclipse.ptp.rdt.core/src/org/eclipse/ptp/rdt/core/RDTLog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 IBM Corporation and others.
+ * Copyright (c) 2008, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -48,6 +48,10 @@ public class RDTLog {
 	
 	public static void logInfo(String message) {
 		log(new Status(IStatus.INFO, Activator.PLUGIN_ID, message));
+	}
+	
+	public static void logWarning(String message) {
+		log(new Status(IStatus.WARNING, Activator.PLUGIN_ID, message));
 	}
 	
 }
