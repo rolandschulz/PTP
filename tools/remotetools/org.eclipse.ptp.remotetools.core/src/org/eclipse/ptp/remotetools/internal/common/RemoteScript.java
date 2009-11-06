26ba4fbfbcbc1fe36f3ef38b10de258d6a5d865a
Greg Watson <g.watson@computer.org>
2009-11-06 21:24:30 +0000

    Update formatting, fix warnings.

--- tools/remotetools/org.eclipse.ptp.remotetools.core/src/org/eclipse/ptp/remotetools/internal/common/RemoteScript.java
+++ tools/remotetools/org.eclipse.ptp.remotetools.core/src/org/eclipse/ptp/remotetools/internal/common/RemoteScript.java
@@ -13,157 +13,111 @@ package org.eclipse.ptp.remotetools.internal.common;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.eclipse.ptp.remotetools.core.IRemoteScript;
 
-
-
 /**
  * Default implementation of <code>IRemoteScript</code>.
- * @author Richard Maciel
- * <b>Review OK</b>
+ * 
+ * @author Richard Maciel <b>Review OK</b>
  */
-public class RemoteScript implements IRemoteScript
-{
-	private List environment;
-	
+public class RemoteScript implements IRemoteScript {
+	private List<String> environment = new ArrayList<String>();
+
 	private boolean willForwardX11;
-	
+
 	private InputStream inputStream = null;
 	private OutputStream outputStream = null;
 	private OutputStream errorStream = null;
-	
-	String [] script;
+
+	private String[] script;
 
 	private boolean fetchProcessErrorStream = false;
 	private boolean fetchProcessInputStream = false;
 	private boolean fetchProcessOutputStream = false;
-	
+
 	private boolean allocateTerminal = false;
-	
-	public RemoteScript()
-	{
-		environment = new LinkedList();
-		inputStream = null;
-		outputStream = null;
-		willForwardX11 = false;
-	}
-	
+
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ptp.remotetools.IRemoteScript#addEnvironment(java.lang.String)
+	 * 
+	 * @see
+	 * org.eclipse.ptp.remotetools.IRemoteScript#addEnvironment(java.lang.String
+	 * )
 	 */
-	public void addEnvironment(String variable)
-	{
+	public void addEnvironment(String variable) {
 		environment.add(variable);
 	}
-	
-	public void addEnvironment(String[] environment) {
-		this.environment.addAll(Arrays.asList(environment));
-	}
-
-	/* (non-Javadoc)
-	 * @see org.eclipse.ptp.remotetools.IRemoteScript#setProcessInputStream(java.io.InputStream)
-	 */
-	public void setProcessInputStream(InputStream input)
-	{
-		inputStream = input;
-		fetchProcessInputStream = false;
-	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ptp.remotetools.IRemoteScript#setProcessOutputStream(java.io.OutputStream)
+	 * 
+	 * @see
+	 * org.eclipse.ptp.remotetools.core.IRemoteScript#addEnvironment(java.lang
+	 * .String[])
 	 */
-	public void setProcessOutputStream(OutputStream output)
-	{
-		outputStream = output;
-		fetchProcessOutputStream = false;
+	public void addEnvironment(String[] variables) {
+		environment.addAll(Arrays.asList(variables));
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ptp.remotetools.core.IRemoteScript#setProcessErrorStream(java.io.OutputStream)
-	 */
-	public void setProcessErrorStream(OutputStream output) {
-		errorStream = output;
-		fetchProcessErrorStream = false;
-	}
-	
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ptp.remotetools.IRemoteScript#setForwardX11(boolean)
-	 */
-	public void setForwardX11(boolean willForward)
-	{
-		this.willForwardX11 = willForward;
+	public boolean getAllocateTerminal() {
+		return allocateTerminal;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ptp.remotetools.IRemoteScript#setScript(java.lang.String)
-	 */
-	public void setScript(String script)
-	{
-		this.script = new String[1];
-		this.script[0] = script;
+	public OutputStream getErrorStream() {
+		return errorStream;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ptp.remotetools.IRemoteScript#setScript(java.lang.String[])
-	 */
-	public void setScript(String[] script)
-	{
-		this.script = script;
+	public boolean getFetchProcessErrorStream() {
+		return fetchProcessErrorStream;
 	}
 
-	public String getScriptString()
-	{
-		if(script == null)
-		{
+	public boolean getFetchProcessInputStream() {
+		return fetchProcessInputStream;
+	}
+
+	public boolean getFetchProcessOutputStream() {
+		return fetchProcessOutputStream;
+	}
+
+	public InputStream getInputStream() {
+		return inputStream;
+	}
+
+	public OutputStream getOutputStream() {
+		return outputStream;
+	}
+
+	public String getScriptString() {
+		if (script == null) {
 			return null;
 		}
 
 		StringBuffer sb = new StringBuffer();
-				
-		Iterator it = environment.iterator();
-		if(environment.size() > 0)
+
+		if (environment.size() > 0) {
 			sb.append("declare -x "); //$NON-NLS-1$
-		while (it.hasNext()) { 
-			sb.append("\"" + (String)it.next() + "\" "); //$NON-NLS-1$ //$NON-NLS-2$
 		}
-		if(environment.size() > 0)
+
+		for (String env : environment) {
+			sb.append("\"" + env + "\" "); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+
+		if (environment.size() > 0) {
 			sb.append("; "); //$NON-NLS-1$
-		
-		for(int i = 0; i < script.length; i++)
+		}
+
+		for (int i = 0; i < script.length; i++) {
 			sb.append(script[i] + "; "); //$NON-NLS-1$
-		
+		}
 		return sb.toString();
 	}
 
-	public InputStream getInputStream()
-	{
-		return inputStream;
-	}
-
-	public OutputStream getOutputStream()
-	{
-		return outputStream;
-	}
-
-	public OutputStream getErrorStream()
-	{
-		return errorStream;
-	}
-
-	public boolean willForwardX11()
-	{
-		return willForwardX11;
+	public void setAllocateTerminal(boolean flag) {
+		this.allocateTerminal = flag;
 	}
 
 	public void setFetchProcessErrorStream(boolean flag) {
@@ -180,24 +134,74 @@ public class RemoteScript implements IRemoteScript
 		this.fetchProcessOutputStream = flag;
 		this.outputStream = null;
 	}
-	
-	public boolean getFetchProcessErrorStream() {
-		return fetchProcessErrorStream;
-	}
-	
-	public boolean getFetchProcessOutputStream() {
-		return fetchProcessOutputStream;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ptp.remotetools.IRemoteScript#setForwardX11(boolean)
+	 */
+	public void setForwardX11(boolean willForward) {
+		this.willForwardX11 = willForward;
 	}
 
-	public boolean getFetchProcessInputStream() {
-		return fetchProcessInputStream;
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ptp.remotetools.core.IRemoteScript#setProcessErrorStream(
+	 * java.io.OutputStream)
+	 */
+	public void setProcessErrorStream(OutputStream output) {
+		errorStream = output;
+		fetchProcessErrorStream = false;
 	}
 
-	public void setAllocateTerminal(boolean flag) {
-		this.allocateTerminal = flag;	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ptp.remotetools.IRemoteScript#setProcessInputStream(java.
+	 * io.InputStream)
+	 */
+	public void setProcessInputStream(InputStream input) {
+		inputStream = input;
+		fetchProcessInputStream = false;
 	}
-	
-	public boolean getAllocateTerminal() {
-		return allocateTerminal;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ptp.remotetools.IRemoteScript#setProcessOutputStream(java
+	 * .io.OutputStream)
+	 */
+	public void setProcessOutputStream(OutputStream output) {
+		outputStream = output;
+		fetchProcessOutputStream = false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ptp.remotetools.IRemoteScript#setScript(java.lang.String)
+	 */
+	public void setScript(String script) {
+		this.script = new String[1];
+		this.script[0] = script;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ptp.remotetools.IRemoteScript#setScript(java.lang.String[])
+	 */
+	public void setScript(String[] script) {
+		this.script = script;
+	}
+
+	public boolean willForwardX11() {
+		return willForwardX11;
 	}
 }
