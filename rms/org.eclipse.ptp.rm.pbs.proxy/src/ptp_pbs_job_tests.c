5bc43adf04b68cdf851d1e6ce7310eea9107a627
Greg Watson <g.watson@computer.org>
2009-11-18 22:38:22 +0000

    Copyright.

--- rms/org.eclipse.ptp.rm.pbs.proxy/src/ptp_pbs_job_tests.c
+++ rms/org.eclipse.ptp.rm.pbs.proxy/src/ptp_pbs_job_tests.c
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2009 National Center for Supercomputing Applications
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
