f674543aee021470fb2fd27e4551658d6be49c75
btibbitts <btibbitts>
2009-11-30 15:25:16 +0000

    cleanup

--- tools/org.eclipse.ptp.pldt.mpi.analysis/src/org/eclipse/ptp/pldt/mpi/analysis/view/MPIErrorView.java
+++ tools/org.eclipse.ptp.pldt.mpi.analysis/src/org/eclipse/ptp/pldt/mpi/analysis/view/MPIErrorView.java
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (c) 2007 IBM Corporation.
+ * Copyright (c) 2007,2009 IBM Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,15 +11,12 @@
 
 package org.eclipse.ptp.pldt.mpi.analysis.view;
 
-//import org.eclipse.ptp.pldt.common.views.SimpleTreeTableMarkerView;
 import org.eclipse.ptp.pldt.mpi.analysis.Activator;
 import org.eclipse.ptp.pldt.mpi.analysis.IDs;
 
-//public class MPIErrorView extends SimpleTreeTableErrorView {
 /**
  * View to show list of barrier errors [new view architecture]
  * 
- * TODO: try to replace with view class from 'common' package
  */
 public class MPIErrorView extends SimpleTreeTableMarkerView {
 	public MPIErrorView() {
