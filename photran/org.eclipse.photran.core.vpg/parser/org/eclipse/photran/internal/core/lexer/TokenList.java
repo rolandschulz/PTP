581fb7df875e7ca9a6669324119050bb116dfad6
joverbey <joverbey>
2009-12-21 22:16:48 +0000

    Added TokenList to Rephraser core

--- photran/org.eclipse.photran.core.vpg/parser/org/eclipse/photran/internal/core/lexer/TokenList.java
+++ photran/org.eclipse.photran.core.vpg/parser/org/eclipse/photran/internal/core/lexer/TokenList.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 University of Illinois at Urbana-Champaign and others.
+ * Copyright (c) 2007-2009 University of Illinois at Urbana-Champaign and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,8 +10,6 @@
  *******************************************************************************/
 package org.eclipse.photran.internal.core.lexer;
 
-import java.util.Iterator;
-
 import org.eclipse.photran.internal.core.analysis.loops.ASTVisitorWithLoops;
 import org.eclipse.photran.internal.core.parser.ASTExecutableProgramNode;
 
@@ -21,15 +19,11 @@ import org.eclipse.photran.internal.core.parser.ASTExecutableProgramNode;
  * 
  * @author Jeff Overbey
  */
-public class TokenList
+public final class TokenList extends org.eclipse.rephraserengine.core.util.TokenList<Token>
 {
-    private Token[] array;
-    private int size;
-    
     public TokenList()
     {
-    	this.array = new Token[4096]; // Heuristic
-    	this.size = 0;
+    	super();
     }
     
     public TokenList(ASTExecutableProgramNode ast)
@@ -44,141 +38,27 @@ public class TokenList
     	});
     }
 
-    public void add(Token token)
+    @Override
+    protected Token[] createTokenArray(int size)
     {
-        ensureCapacity();
-        array[size++] = token;
+        return new Token[size];
     }
 
-    private void ensureCapacity()
+    @Override
+    protected int getStreamOffset(Token token)
     {
-        if (size == array.length) expandArray();
+        return token.getStreamOffset();
     }
 
-    private void expandArray()
+    @Override
+    protected int getLength(Token token)
     {
-        Token[] newTokenArray = new Token[array.length*2];
-        System.arraycopy(array, 0, newTokenArray, 0, array.length);
-        array = newTokenArray;
-    }
-    
-    public void add(int index, Token token)
-    {
-        if (index < 0 || index > size) throw new IllegalArgumentException("Invalid index " + index);
-        
-        ensureCapacity();
-        for (int i = size; i >= index; i--)
-            array[i+1] = array[i];
-        array[index] = token;
-        size++;
+        return token.getLength();
     }
 
-    public boolean remove(Token tokenToRemove)
+    @Override
+    protected int getLine(Token token)
     {
-        int index = find(tokenToRemove);
-        return index < 0 ? false : remove(index);
-    }
-    
-    public boolean remove(int index)
-    {
-        if (index < 0 || index >= size) throw new IllegalArgumentException("Invalid index " + index);
-        
-        for (int i = index + 1; i < size; i++)
-            array[i-1] = array[i];
-        size--;
-        return true;
-    }
-    
-    public Token get(int index)
-    {
-        if (index < 0 || index >= size) throw new IllegalArgumentException("Invalid index " + index);
-
-        return array[index];
-    }
-    
-    public int size()
-    {
-    	return size;
-    }
-    
-    public int find(Token token)
-    {
-        for (int i = 0; i < size; i++)
-            if (array[i].equals(token))
-                return i;
-        return -1;
-    }
-    
-    public Token findStreamOffsetLength(int offset, int length)
-    {
-        int low = 0, high = size - 1;
-        for (int mid = (high+low)/2; low <= high; mid = (high+low)/2)
-        {
-            int value = array[mid].getStreamOffset();
-            if (offset > value)
-                low = mid + 1;
-            else if (offset < value)
-                high = mid - 1;
-            else // (value == offset)
-                return array[mid].getLength() == length ? array[mid] : null;
-        }
-        return null;
-    }
-    
-    public Token findFirstTokenOnLine(int line)
-    {
-        int index = findIndexOfAnyTokenOnLine(line);
-        if (index < 0) return null;
-        while (index > 0 && array[index-1].getLine() == line)
-            index--;
-        return array[index];
-    }
-    
-    private int findIndexOfAnyTokenOnLine(int line)
-    {
-        int low = 0, high = size - 1;
-        for (int mid = (high+low)/2; low <= high; mid = (high+low)/2)
-        {
-            int value = array[mid].getLine();
-            if (line > value)
-                low = mid + 1;
-            else if (line < value)
-                high = mid - 1;
-            else // (value == offset)
-                return mid;
-        }
-        return -1;
-    }
-    
-    public Token findLastTokenOnLine(int line)
-    {
-        int index = findIndexOfAnyTokenOnLine(line);
-        if (index < 0) return null;
-        while (index+1 < array.length && array[index+1].getLine() == line)
-            index++;
-        return array[index];
-    }
-
-    public Iterator<Token> iterator()
-    {
-        return new Iterator<Token>()
-        {
-            int index = 0;
-            
-            public boolean hasNext()
-            {
-                return index < size;
-            }
-
-            public Token next()
-            {
-                return index < size ? array[index++] : null;
-            }
-
-            public void remove()
-            {
-                throw new UnsupportedOperationException();
-            }
-        };
+        return token.getLine();
     }
 }
---
+++ photran/org.eclipse.rephraserengine.core/src/org/eclipse/rephraserengine/core/util/TokenList.java
@@ -0,0 +1,190 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2009 University of Illinois at Urbana-Champaign and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     UIUC - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.rephraserengine.core.util;
+
+import java.util.Iterator;
+
+/**
+ * Maintains a list of all of the tokens in a program, allowing them to be
+ * located quickly by offset.
+ * 
+ * @author Jeff Overbey
+ */
+public abstract class TokenList<T> implements Iterable<T>
+{
+    private T[] array;
+    private int size;
+    
+    public TokenList()
+    {
+    	this.array = internalCreateTokenArray(4096); // Heuristic
+    	this.size = 0;
+    }
+
+    protected abstract T[] createTokenArray(int size);
+    
+    protected abstract int getStreamOffset(T token);
+    
+    protected abstract int getLength(T token);
+    
+    protected abstract int getLine(T token);
+
+    private T[] internalCreateTokenArray(int size)
+    {
+        T[] result = createTokenArray(size);
+        
+        if (result.length != size)
+            throw new IllegalStateException(
+                "TokenList#createTokenArray is not implemented correctly.  It was " +
+                "asked to create an array of length " + size + ", but it created " +
+                "an array of length " + result.length + " instead.");
+        
+        return result;
+    }
+
+    public void add(T token)
+    {
+        ensureCapacity();
+        array[size++] = token;
+    }
+
+    private void ensureCapacity()
+    {
+        if (size == array.length) expandArray();
+    }
+
+    private void expandArray()
+    {
+        T[] newTokenArray = internalCreateTokenArray(array.length*2);
+        System.arraycopy(array, 0, newTokenArray, 0, array.length);
+        array = newTokenArray;
+    }
+    
+    public void add(int index, T token)
+    {
+        if (index < 0 || index > size) throw new IllegalArgumentException("Invalid index " + index);
+        
+        ensureCapacity();
+        for (int i = size; i >= index; i--)
+            array[i+1] = array[i];
+        array[index] = token;
+        size++;
+    }
+
+    public boolean remove(T tokenToRemove)
+    {
+        int index = find(tokenToRemove);
+        return index < 0 ? false : remove(index);
+    }
+    
+    public boolean remove(int index)
+    {
+        if (index < 0 || index >= size) throw new IllegalArgumentException("Invalid index " + index);
+        
+        for (int i = index + 1; i < size; i++)
+            array[i-1] = array[i];
+        size--;
+        return true;
+    }
+    
+    public T get(int index)
+    {
+        if (index < 0 || index >= size) throw new IllegalArgumentException("Invalid index " + index);
+
+        return array[index];
+    }
+    
+    public int size()
+    {
+    	return size;
+    }
+    
+    public int find(T token)
+    {
+        for (int i = 0; i < size; i++)
+            if (array[i].equals(token))
+                return i;
+        return -1;
+    }
+    
+    public T findStreamOffsetLength(int offset, int length)
+    {
+        int low = 0, high = size - 1;
+        for (int mid = (high+low)/2; low <= high; mid = (high+low)/2)
+        {
+            int value = getStreamOffset(array[mid]);
+            if (offset > value)
+                low = mid + 1;
+            else if (offset < value)
+                high = mid - 1;
+            else // (value == offset)
+                return getLength(array[mid]) == length ? array[mid] : null;
+        }
+        return null;
+    }
+    
+    public T findFirstTokenOnLine(int line)
+    {
+        int index = findIndexOfAnyTokenOnLine(line);
+        if (index < 0) return null;
+        while (index > 0 && getLine(array[index-1]) == line)
+            index--;
+        return array[index];
+    }
+    
+    private int findIndexOfAnyTokenOnLine(int line)
+    {
+        int low = 0, high = size - 1;
+        for (int mid = (high+low)/2; low <= high; mid = (high+low)/2)
+        {
+            int value = getLine(array[mid]);
+            if (line > value)
+                low = mid + 1;
+            else if (line < value)
+                high = mid - 1;
+            else // (value == offset)
+                return mid;
+        }
+        return -1;
+    }
+    
+    public T findLastTokenOnLine(int line)
+    {
+        int index = findIndexOfAnyTokenOnLine(line);
+        if (index < 0) return null;
+        while (index+1 < array.length && getLine(array[index+1]) == line)
+            index++;
+        return array[index];
+    }
+
+    public Iterator<T> iterator()
+    {
+        return new Iterator<T>()
+        {
+            int index = 0;
+            
+            public boolean hasNext()
+            {
+                return index < size;
+            }
+
+            public T next()
+            {
+                return index < size ? array[index++] : null;
+            }
+
+            public void remove()
+            {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+}
