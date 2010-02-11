74ce0ae121e79483c0f7f73d799917f9dda323d9
joverbey <joverbey>
2010-02-11 16:54:51 +0000

    Added TwoKeyHashMap and SymbolTable

---
+++ photran/org.eclipse.rephraserengine.core/src/org/eclipse/rephraserengine/core/util/TwoKeyHashMap.java
@@ -0,0 +1,241 @@
+package org.eclipse.rephraserengine.core.util;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+/**
+ * This is essentially a table -- a big block of cells indexed by row and column.
+ * 
+ * @author Jeff Overbey
+ * 
+ * @param <RowType>
+ * @param <ColType>
+ * @param <CellType>
+ */
+public final class TwoKeyHashMap<RowType, ColType, CellType> implements Iterable<CellType>
+{
+    protected HashMap<RowType, HashMap<ColType, CellType>> table = new HashMap<RowType, HashMap<ColType, CellType>>();
+
+    /**
+     * Add an entry, or if one already exists, replace it.
+     * @param k1 - First key
+     * @param k2
+     * @param v
+     */
+    public CellType put(RowType k1, ColType k2, CellType v)
+    {
+        // Map the first key...
+        if (!table.containsKey(k1)) table.put(k1, new HashMap<ColType, CellType>());
+        HashMap<ColType, CellType> thisRow = table.get(k1);
+
+        // ...and the second one to the table entry
+        thisRow.put(k2, v);
+        
+        return v;
+    }
+
+    /**
+     * Retrieve the entry keyed by the given symbols.
+     * @param k1
+     * @param k2
+     * @return value
+     */
+    public CellType getEntry(RowType k1, ColType k2)
+    {
+        // Map the first key (row ID)...
+        if (!table.containsKey(k1)) return null;
+        HashMap<ColType, CellType> thisRow = table.get(k1);
+
+        // ...and the second one (column ID)...
+        if (!thisRow.containsKey(k2)) return null;
+
+        // ...to the table entry
+        return thisRow.get(k2);
+    }
+
+    /**
+     * Retrieve all entries keyed by the given symbol.
+     * @param k1
+     * @return value
+     */
+    public HashMap<ColType, CellType> getAllEntriesFor(RowType k1)
+    {
+        // Map the first key (row ID)...
+        if (!table.containsKey(k1)) return null;
+        return table.get(k1);
+    }
+
+    /**
+     * @return a <code>Set</code> of all of the (first) keys. Use <code>getAllEntriesFor</code> to get the corresponding entries.
+     */
+    public Set<RowType> keySet()
+    {
+        return table.keySet();
+    }
+
+    /**
+     * Returns true iff the entry keyed by the given symbols is not null (i.e., returns true if there is no entry keyed by the given symbols).
+     * @param k1
+     * @param k2
+     * @return boolean
+     */
+    public boolean containsEntry(RowType k1, ColType k2)
+    {
+        return getEntry(k1, k2) != null;
+    }
+
+    /**
+     * Removes all entries.
+     */
+    public void clear()
+    {
+        table.clear();
+    }
+
+    /**
+     * Removes all entries keyed by the given value.
+     * @param key
+     */
+    public void remove(RowType key)
+    {
+        table.remove(key);
+    }
+
+    /**
+     * Removes all entries keyed by the given values.
+     * @param k1
+     * @param k2
+     */
+    public void remove(RowType k1, ColType k2)
+    {
+        if (table.containsKey(k1)) table.get(k1).remove(k2);
+    }
+
+    @Override public String toString()
+    {
+        StringBuffer sb = new StringBuffer();
+        sb.append("{");
+        for (RowType r : table.keySet())
+        {
+            HashMap<ColType, CellType> thisRow = table.get(r);
+            if (thisRow != null)
+            {
+                for (ColType c : thisRow.keySet())
+                {
+                    sb.append("   ");
+                    sb.append("(");
+                    sb.append(r);
+                    sb.append(", ");
+                    sb.append(c);
+                    sb.append(") ==> ");
+                    sb.append(thisRow.get(c));
+                }
+            }
+        }
+        sb.append("   }");
+        return sb.toString();
+    }
+
+    /**
+     * An <code>Iterator</code> that iterates over all of the values (cells) in this <code>TwoKeyHashMap</code>.
+     */
+    protected final class ValueIterator implements Iterator<CellType>
+    {
+        protected State currentState;
+
+        protected Iterator<HashMap<ColType, CellType>> rowIterator;
+
+        protected Iterator<CellType> colIterator;
+
+        protected abstract class State
+        {
+            protected State() {}
+            abstract CellType getNextValue();
+        }
+
+        protected final State INITIAL = new State()
+        {
+            @Override CellType getNextValue()
+            {
+                rowIterator = table.values().iterator();
+
+                currentState = STARTING_NEW_ROW;
+                return currentState.getNextValue();
+            }
+        };
+
+        protected final State STARTING_NEW_ROW = new State()
+        {
+            @Override CellType getNextValue()
+            {
+                if (rowIterator.hasNext())
+                {
+                    colIterator = rowIterator.next().values().iterator();
+                    currentState = WORKING_THROUGH_ROW;
+                }
+                else
+                {
+                    currentState = DONE;
+                }
+                return currentState.getNextValue();
+            }
+        };
+
+        protected final State WORKING_THROUGH_ROW = new State()
+        {
+            @Override CellType getNextValue()
+            {
+                if (colIterator.hasNext())
+                    return colIterator.next();
+                else
+                {
+                    currentState = STARTING_NEW_ROW;
+                    return currentState.getNextValue();
+                }
+            }
+        };
+
+        protected final State DONE = new State()
+        {
+            @Override CellType getNextValue()
+            {
+                return null;
+            }
+        };
+
+        // /////////////////////////////////////////////////////////////////////////////////////////
+
+        protected CellType nextValue;
+
+        ValueIterator()
+        {
+            currentState = INITIAL;
+            nextValue = currentState.getNextValue();
+        }
+
+        public boolean hasNext()
+        {
+            return nextValue != null;
+        }
+
+        public CellType next()
+        {
+            CellType currentValue = nextValue;
+            nextValue = currentState.getNextValue();
+            if (currentValue == null && nextValue == null) throw new NoSuchElementException();
+            return currentValue;
+        }
+
+        public void remove()
+        {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public Iterator<CellType> iterator()
+    {
+        return new ValueIterator();
+    }
+}
---
+++ photran/org.eclipse.rephraserengine.core/src/org/eclipse/rephraserengine/core/util/symtab/SymbolTable.java
@@ -0,0 +1,290 @@
+/*******************************************************************************
+ * Copyright (c) 2010 University of Illinois at Urbana-Champaign and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    UIUC - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.rephraserengine.core.util.symtab;
+
+import org.eclipse.rephraserengine.core.util.TwoKeyHashMap;
+
+
+
+/**
+ * A simple, generic symbol table with nested scopes.
+ * 
+ * @author Jeff Overbey
+ * 
+ * @param <N> namespace type
+ * @param <S> symbol type
+ */
+public class SymbolTable<N, S>
+{
+    /**
+     * Settings that determine how this symbol table and its children will operate.
+     */
+    public static class Settings<S>
+    {
+        /** Constructor.  See {@link #errorEntry}. */
+        public Settings() { this.errorEntry = null; }
+        
+        /** Constructor.  See {@link #errorEntry}. */
+        public Settings(S errorEntry) { this.errorEntry = errorEntry; }
+        
+        /**
+         * A (unique) error symbol which will be returned whenever lookup fails.
+         * May be <code>null</code>.
+         * <p>
+         * Failure will always be determined by comparing symbols <i>pointerwise</i>
+         * with this value.
+         * <p>
+         * The value of this field is set via the {@link Settings#Settings(Object)}
+         * constructor.
+         */
+        public final S errorEntry;
+        
+        /**
+         * If a lookup fails in this scope, this setting determines whether the
+         * {@link SymbolTable#lookup(Object, String)} method should fail immediately
+         * (<code>false</code>) or if it should attempt to look up the name in an
+         * outer scope (<code>true</code>).
+         */
+        public boolean resolveInOuterScopeIfLookupFails = true;
+        
+        /**
+         * This determines whether {@link SymbolTable#enter(Object, String, Object)}
+         * should allow a symbol to be added if it already exists in an outer scope.
+         * <p>
+         * If {@link #resolveInOuterScopeIfLookupFails} is <code>false</code>, this
+         * setting has no effect, since a lookup will never search an outer scope.
+         */
+        public boolean allowShadowing = true;
+        
+        /**
+         * This determines whether symbol names are case-sensitive.  If
+         * <code>false</code>, all symbol names will be converted to lower case.
+         */
+        public boolean caseSensitive = true;
+    }
+    
+    /**
+     * Settings for this symbol table hierarchy, including what symbol will be
+     * returned when a lookup fails and what scoping rules should apply.
+     */
+    protected final Settings<S> settings;
+    
+    /**
+     * Symbol table representing the outer scope, or <code>null</code> if this is the
+     * outermost (global) scope.
+     */
+    protected final SymbolTable<N, S> parent;
+    
+    /**
+     * Entries in the scope represented by this symbol table.  Maps a namespace and
+     * a name to a symbol.
+     */
+    protected final TwoKeyHashMap<N, String, S> entries;
+    
+    public SymbolTable()
+    {
+        this.settings = new Settings<S>();
+        this.entries = new TwoKeyHashMap<N, String, S>();
+        this.parent = null;
+    }
+    
+    public SymbolTable(Settings<S> settings)
+    {
+        assert settings != null;
+        
+        this.settings = settings;
+        this.entries = new TwoKeyHashMap<N, String, S>();
+        this.parent = null;
+    }
+    
+    public SymbolTable(SymbolTable<N, S> parent)
+    {
+        assert parent != null;
+        
+        this.settings = parent.settings;
+        this.entries = new TwoKeyHashMap<N, String, S>();
+        this.parent = parent;
+    }
+    
+    protected String canonicalize(String name)
+    {
+        assert name != null;
+        
+        if (settings.caseSensitive)
+            return name;
+        else
+            return name.toLowerCase();
+    }
+    
+    /**
+     * Looks up the given name in the given namespace.
+     * 
+     * @param namespace
+     * @param name
+     * @return the corresponding symbol, or {@link Settings#errorEntry} if the symbol
+     *         cannot be found
+     */
+    public S lookup(N namespace, String name)
+    {
+        assert namespace != null && name != null;
+        
+        name = canonicalize(name);
+        
+        if (entries.containsEntry(namespace, name))
+            return entries.getEntry(namespace, name);
+        else if (parent != null && settings.resolveInOuterScopeIfLookupFails)
+            return parent.lookup(namespace, name);
+        else
+            return settings.errorEntry;
+    }
+
+    /**
+     * @return <code>true</code> iff {@link #lookup(Object, String)} will succeed
+     *         for the given namespace and name
+     */
+    public boolean contains(N namespace, String name)
+    {
+        assert namespace != null && name != null;
+        
+        return lookup(namespace, name) != settings.errorEntry;
+    }
+    
+    /**
+     * Adds an entry to the symbol type for the given name in the given namespace.
+     * 
+     * @param namespace
+     * @param name
+     * @param symbol
+     * @return the symbol iff entry succeeds, or <code>null</code> if the symbol
+     *         cannot be added because it will conflict with an existing symbol
+     */
+    public S enter(N namespace, String name, S symbol)
+    {
+        assert namespace != null && name != null && symbol != null;
+        
+        name = canonicalize(name);
+        
+        if (!canEnter(namespace, name))
+            return null;
+        else
+            return entries.put(namespace, name, symbol);
+    }
+    
+    /**
+     * Adds an entry to the symbol type if there is not already a symbol for the
+     * given name in the given namespace; otherwise, returns the existing symbol.
+     * 
+     * @param namespace
+     * @param name
+     * @param symbol
+     * @return the symbol corresponding to the given name in the given namespace
+     */
+    public S ensure(N namespace, String name, S symbol)
+    {
+        assert namespace != null && name != null && symbol != null;
+        
+        name = canonicalize(name);
+        
+        if (!canEnter(namespace, name))
+            return lookup(namespace, name);
+        else
+            return entries.put(namespace, name, symbol);
+    }
+    
+    /**
+     * @return <code>true</code> iff {@link #enter(Object, String, Object)} will
+     *         succeed for the given namespace and name
+     */
+    public boolean canEnter(N namespace, String name)
+    {
+        assert namespace != null && name != null;
+        
+        name = canonicalize(name);
+        
+        if (entries.containsEntry(namespace, name))
+            return false;
+        else if (parent != null && settings.resolveInOuterScopeIfLookupFails)
+            return settings.allowShadowing || parent.lookup(namespace, name) == settings.errorEntry;
+        else
+            return true;
+    }
+    
+    /**
+     * Returns a new symbol table for a child scope of this scope (i.e., a nested
+     * scope).
+     * <p>
+     * Note that this does not modify this {@link SymbolTable} object; the correct
+     * usage is <code>symtab = symtab.enterScope()</code>.
+     * 
+     * @return a new symbol table for a child scope of this scope
+     */
+    public SymbolTable<N, S> enterScope()
+    {
+        return new SymbolTable<N, S>(this);
+    }
+    
+    /**
+     * Returns the symbol table for the parent scope of this scope (i.e., the next
+     * outermost scope).
+     * <p>
+     * Note that this does not modify this {@link SymbolTable} object; the correct
+     * usage is <code>symtab = symtab.exitScope()</code>.
+     * 
+     * @return the symbol table for the parent scope of this scope, or
+     *         <code>null</code> iff this symbol table has no parent (i.e., it
+     *         represents the outermost scope)
+     */
+    public SymbolTable<N, S> exitScope()
+    {
+        return parent;
+    }
+
+    /**
+     * Returns the symbol table for the outermost scope containing this scope (i.e.,
+     * the ancester symbol table with a <code>null</code> parent symbol table).
+     * <p>
+     * Note that this does not modify this {@link SymbolTable} object; the most
+     * common usage is <code>symtab.outermostScope().enter(...)</code>.
+     * 
+     * @return the symbol table for the parent scope of this scope
+     */
+    public SymbolTable<N, S> outermostScope()
+    {
+        if (parent == null)
+            return this;
+        else
+            return parent.outermostScope();
+    }
+    
+    @Override public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("\n\n================= SYMBOL TABLE =================\n");
+        for (N namespace : entries.keySet())
+        {
+            sb.append(namespace.toString().toUpperCase() + " NAMESPACE:\n");
+            
+            for (String name : entries.getAllEntriesFor(namespace).keySet())
+                sb.append(name + " -> " + entries.getEntry(namespace, name) + "\n");
+            
+            sb.append("\n");
+        }
+        
+        if (parent != null)
+        {
+            sb.append("\n\n==================== PARENT ====================\n");
+            sb.append(parent.toString());
+        }
+        
+        return sb.toString();
+    }
+}
