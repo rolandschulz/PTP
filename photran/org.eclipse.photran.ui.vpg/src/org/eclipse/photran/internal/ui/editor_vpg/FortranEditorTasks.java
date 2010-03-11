909f712a5afa1b466faf002cb003729e5809f6e9
joverbey <joverbey>
2010-03-11 20:19:30 +0000

    Refactored and fixed Bug 305388

--- photran/org.eclipse.photran.ui.vpg/src/org/eclipse/photran/internal/ui/editor_vpg/FortranEditorTasks.java
+++ photran/org.eclipse.photran.ui.vpg/src/org/eclipse/photran/internal/ui/editor_vpg/FortranEditorTasks.java
@@ -23,6 +23,7 @@ import org.eclipse.photran.internal.core.properties.SearchPathProperties;
 import org.eclipse.photran.internal.core.vpg.PhotranVPG;
 import org.eclipse.photran.internal.ui.FortranUIPlugin;
 import org.eclipse.photran.internal.ui.editor.AbstractFortranEditor;
+import org.eclipse.photran.internal.ui.editor.FixedFormFortranEditor;
 import org.eclipse.rephraserengine.core.vpg.eclipse.VPGJob;
 
 public class FortranEditorTasks
@@ -46,6 +47,10 @@ public class FortranEditorTasks
 
         this.runner = new Runner();
     }
+    
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    private Parser parser = new Parser();
 
     private AbstractFortranEditor editor;
 
@@ -98,8 +103,6 @@ public class FortranEditorTasks
         protected Job dispatchVPGTasksJob = null;
         protected IFortranAST vpgAST = null;
 
-        private Parser parser = new Parser();
-
         public void runTasks()
         {
             runTasks(true);
@@ -122,53 +125,7 @@ public class FortranEditorTasks
         {
             if (dispatchASTTasksJob != null) return; // Already running an update
 
-            dispatchASTTasksJob = new Job("Updating Fortran editor with new parse information")
-            {
-                @Override
-                protected IStatus run(IProgressMonitor monitor)
-                {
-                    ASTExecutableProgramNode astRootNode = null;
-                    try
-                    {
-                        if (editor.getDocumentProvider() != null)
-                        {
-                            String editorContents = editor.getDocumentProvider().getDocument(editor.getEditorInput()).get();
-
-                            SourceForm sourceForm =
-                                editor.getIFile() == null || editor.getIFile().getProject() == null
-                                ? SourceForm.UNPREPROCESSED_FREE_FORM
-                                : SourceForm.preprocessedFreeForm(new IncludeLoaderCallback(editor.getIFile().getProject()));
-                            IAccumulatingLexer lexer = LexerFactory.createLexer(new ByteArrayInputStream(editorContents.getBytes()),
-                                                                                editor.getIFile(),
-                                                                                editor.getIFile().getName(),
-                                                                                sourceForm,
-                                                                                true /*false*/);
-                            astRootNode = parser.parse(lexer);
-                            if (astRootNode == null) return Status.OK_STATUS;
-
-                            HashSet<IFortranEditorASTTask> tasksToRemove = new HashSet<IFortranEditorASTTask>();
-                            synchronized (FortranEditorTasks.instance(editor))
-                            {
-                                for (IFortranEditorASTTask task : FortranEditorTasks.instance(editor).astTasks)
-                                    if (!task.handle(astRootNode, lexer.getTokenList(), defMap))
-                                        tasksToRemove.add(task);
-                            }
-                            FortranEditorTasks.instance(editor).astTasks.removeAll(tasksToRemove);
-                        }
-                    }
-                    catch (SyntaxException e)
-                    {
-                        // Ignore syntax errors
-                    }
-                    catch (Throwable e)
-                    {
-                        FortranUIPlugin.log("Error running AST tasks", e);
-                    }
-
-                    dispatchASTTasksJob = null;
-                    return Status.OK_STATUS;
-                }
-            };
+            dispatchASTTasksJob = new DispatchASTTasksJob();
             dispatchASTTasksJob.setPriority(Job.DECORATE);
             dispatchASTTasksJob.schedule();
         }
@@ -179,66 +136,106 @@ public class FortranEditorTasks
 
             //if (vpgAST == null || vpg.db.isOutOfDate(PhotranVPG.getFilenameForIFile(editor.getIFile())))
             {
-                vpg.queueJobToEnsureVPGIsUpToDate();
+                //vpg.queueJobToEnsureVPGIsUpToDate();
 
                 vpgAST = null;
-                updateVPGJob = new VPGJob<IFortranAST, Token>("Updating Fortran editor with new analysis information")
-                {
-                    @Override
-                    public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException
-                    {
-                        vpgAST = vpg.acquireTransientAST(PhotranVPG.getFilenameForIFile(editor.getIFile()));
-                        updateVPGJob = null;
-                        //scheduleVPGTaskDispatchJob();
-
-                        if (vpgAST != null)
-                        {
-                            defMap = createDefMap();
-                            for (IFortranEditorVPGTask task : FortranEditorTasks.instance(editor).vpgTasks)
-                                task.handle(editor.getIFile(), vpgAST, defMap);
-                        }
-
-                        return Status.OK_STATUS;
-                    }
-                };
+                updateVPGJob = new UpdateVPGJob();
                 updateVPGJob.setPriority(Job.DECORATE);
                 updateVPGJob.schedule();
             }
         }
 
-//        private void scheduleVPGTaskDispatchJob()
-//        {
-//            dispatchVPGTasksJob = new Job("Updating Fortran editor (3/3)")
-//            {
-//                @Override
-//                protected IStatus run(IProgressMonitor monitor)
-//                {
-//                    if (vpgAST != null) // Parse might have failed
-//                    {
-//                        synchronized (FortranEditorTasks.instance(editor))
-//                        {
-//                            defMap = createDefMap();
-//                            for (IFortranEditorVPGTask task : FortranEditorTasks.instance(editor).vpgTasks)
-//                                task.handle(editor.getIFile(), vpgAST, defMap);
-//                        }
-//                    }
-//                    dispatchVPGTasksJob = null;
-//                    return Status.OK_STATUS;
-//                }
+        private final class DispatchASTTasksJob extends Job
+        {
+            private DispatchASTTasksJob()
+            {
+                super("Updating Fortran editor with new parse information");
+            }
 
-                private DefinitionMap<Definition> createDefMap()
+            private SourceForm determineSourceForm()
+            {
+                if (editor instanceof FixedFormFortranEditor)
+                    return SourceForm.FIXED_FORM;
+                else if (editor.getIFile() == null || editor.getIFile().getProject() == null)
+                    return SourceForm.UNPREPROCESSED_FREE_FORM;
+                else
+                    return SourceForm.preprocessedFreeForm(new IncludeLoaderCallback(editor.getIFile().getProject()));
+            }
+
+            @Override
+            protected IStatus run(IProgressMonitor monitor)
+            {
+                parseThenRunASTTasks();
+                dispatchASTTasksJob = null;
+                return Status.OK_STATUS;
+            }
+
+            private void parseThenRunASTTasks()
+            {
+                try
                 {
-                    return new DefinitionMap<Definition>(vpgAST)
+                    if (editor.getDocumentProvider() != null)
                     {
-                        @Override protected Definition map(String qualifiedName, Definition def)
+                        String editorContents = editor.getDocumentProvider().getDocument(editor.getEditorInput()).get();
+
+                        SourceForm sourceForm = determineSourceForm();
+                        IAccumulatingLexer lexer = LexerFactory.createLexer(new ByteArrayInputStream(editorContents.getBytes()),
+                                                                            editor.getIFile(),
+                                                                            editor.getIFile().getName(),
+                                                                            sourceForm,
+                                                                            true /*false*/);
+                        ASTExecutableProgramNode astRootNode = parser.parse(lexer);
+                        if (astRootNode == null) return;
+
+                        HashSet<IFortranEditorASTTask> tasksToRemove = new HashSet<IFortranEditorASTTask>();
+                        synchronized (FortranEditorTasks.instance(editor))
                         {
-                            return def;
+                            for (IFortranEditorASTTask task : FortranEditorTasks.instance(editor).astTasks)
+                                if (!task.handle(astRootNode, lexer.getTokenList(), defMap))
+                                    tasksToRemove.add(task);
                         }
-                    };
+                        FortranEditorTasks.instance(editor).astTasks.removeAll(tasksToRemove);
+                    }
                 }
-//            };
-//            dispatchVPGTasksJob.setPriority(Job.DECORATE);
-//            dispatchVPGTasksJob.schedule();
-//        }
+                catch (SyntaxException e) { /* Ignore syntax errors */ }
+                catch (Throwable e) { FortranUIPlugin.log("Error running AST tasks", e); }
+            }
+        }
+
+        private final class UpdateVPGJob extends VPGJob<IFortranAST, Token>
+        {
+            private UpdateVPGJob()
+            {
+                super("Updating Fortran editor with new analysis information");
+            }
+
+            @Override
+            public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException
+            {
+                vpgAST = vpg.acquireTransientAST(PhotranVPG.getFilenameForIFile(editor.getIFile()));
+                updateVPGJob = null;
+                //scheduleVPGTaskDispatchJob();
+
+                if (vpgAST != null)
+                {
+                    defMap = createDefMap();
+                    for (IFortranEditorVPGTask task : FortranEditorTasks.instance(editor).vpgTasks)
+                        task.handle(editor.getIFile(), vpgAST, defMap);
+                }
+
+                return Status.OK_STATUS;
+            }
+            
+            private DefinitionMap<Definition> createDefMap()
+            {
+                return new DefinitionMap<Definition>(vpgAST)
+                {
+                    @Override protected Definition map(String qualifiedName, Definition def)
+                    {
+                        return def;
+                    }
+                };
+            }
+        }
     }
 }
