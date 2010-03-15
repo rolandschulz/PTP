bd6da737c70a048cc22ffa31139fa532ddf21840
gwatson <gwatson>
2010-03-15 14:31:35 +0000

    Removed deprecated classes

--- org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/rdt/ui/properties/RemoteEnvironmentPropertyPage.java
+++ org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/rdt/ui/properties/RemoteEnvironmentPropertyPage.java
@@ -1,3 +1,4 @@
+package org.eclipse.ptp.rdt.ui.properties;
 /*******************************************************************************
  * Copyright (c) 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
