fd640de8168fa98fad44b1ffdb71fa0b750dc9b5
Mike Kucera <mkucera@ca.ibm.com>
2010-03-16 21:28:57 +0000

    Bug 305390 -  encoding change event is not handled

--- rdt/org.eclipse.ptp.rdt.core.tests/miner/org/eclipse/ptp/rdt/core/tests/miner/StandaloneIndexerTest.java
+++ rdt/org.eclipse.ptp.rdt.core.tests/miner/org/eclipse/ptp/rdt/core/tests/miner/StandaloneIndexerTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -106,7 +106,7 @@ public class StandaloneIndexerTest extends TestCase {
 		Map<String,String> macroDefinitions = Collections.emptyMap();
 		IScannerInfo scannerInfo = new ScannerInfo(macroDefinitions, includePaths);
 		
-		StandaloneFastIndexer indexer = new StandaloneFastIndexer(indexFile, locationConverter, linkageFactoryMap, null, LOG);
+		StandaloneFastIndexer indexer = new StandaloneFastIndexer(indexFile, locationConverter, linkageFactoryMap, null, null, LOG);
 		indexer.setScannerInfoProvider(new DirtSimpleScannerInfoProvider(scannerInfo));
 		indexer.setLanguageMapper(mapper);
 		indexer.setTraceStatistics(true);
