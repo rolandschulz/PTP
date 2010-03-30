8de7081161da8314c5830a3a7964d1a34e4f5ff2
Beth Tibbitts <tibbitts@us.ibm.com>
2010-03-30 20:30:30 +0000

    getDocument(file) now no longer requires file to be open in editor and thus need textbuffer

--- tools/pldt/org.eclipse.ptp.pldt.common/src/org/eclipse/ptp/pldt/common/util/Utility.java
+++ tools/pldt/org.eclipse.ptp.pldt.common/src/org/eclipse/ptp/pldt/common/util/Utility.java
@@ -15,17 +15,17 @@ import org.eclipse.cdt.core.dom.ast.IASTNode;
 import org.eclipse.cdt.core.dom.ast.IASTNodeLocation;
 import org.eclipse.cdt.core.parser.ParserUtil;
 import org.eclipse.cdt.internal.core.dom.parser.ASTNode;
-import org.eclipse.core.filebuffers.FileBuffers;
-import org.eclipse.core.filebuffers.ITextFileBuffer;
-import org.eclipse.core.filebuffers.ITextFileBufferManager;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IPathEditorInput;
 import org.eclipse.ui.editors.text.ILocationProvider;
+import org.eclipse.ui.editors.text.TextFileDocumentProvider;
 import org.eclipse.ui.ide.ResourceUtil;
+import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.eclipse.ui.texteditor.ITextEditor;
 
 /**
@@ -102,20 +102,25 @@ public class Utility
     }
     
     /**
-     * getDocument - get document using IFile
+     * getDocument - get document using IFile - now no longer depends on text buffer; can return IDocument
+     * even for a file that is not open in editor (and thus not in textbuffer)
      * @param file - IFile
      * @return IDocument
      */
-    public static IDocument getDocument(IFile file)
-    {
-        ITextFileBufferManager bufferManager= FileBuffers.getTextFileBufferManager();
-        ITextFileBuffer textFileBuffer= bufferManager.getTextFileBuffer(file.getFullPath());
-        IDocument document = null;
-        if (textFileBuffer != null) {
-            document= textFileBuffer.getDocument();
-        }
-        return document;
-    }
+	public static IDocument getDocument(IFile file) {
+		IDocument document = null;
+
+		IDocumentProvider provider = new TextFileDocumentProvider();
+		try {
+			provider.connect(file);
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		document = provider.getDocument(file);
+
+		return document;
+	}
 	/**
 	 * From an editor, determine the file (absolute path name) open in the editor.
 	 * @param editor
