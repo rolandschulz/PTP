e25b9d6f3c85cb0b668c1979400187a9b612cc57
gwatson <gwatson>
2010-05-14 23:48:36 +0000

    Exception if parsing fails

---
+++ debug/org.eclipse.ptp.debug.core/pdi/org/eclipse/ptp/debug/core/pdi/model/aif/AIFFormatException.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation and others. 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ptp.debug.core.pdi.model.aif;
+
+public class AIFFormatException extends AIFException {
+
+	private static final long serialVersionUID = 6576643238109867525L;
+
+	public AIFFormatException(String msg) {
+		super(msg);
+	}
+}
