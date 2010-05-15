3c4c9a958d8e9b0454121183edc31c210c57c3d1
Greg Watson <g.watson@computer.org>
2010-05-15 00:30:37 +0000

    Cleanup

--- debug/org.eclipse.ptp.debug.core/pdi/org/eclipse/ptp/debug/core/pdi/IPDIVariableManagement.java
+++ debug/org.eclipse.ptp.debug.core/pdi/org/eclipse/ptp/debug/core/pdi/IPDIVariableManagement.java
@@ -22,65 +22,97 @@ import org.eclipse.ptp.debug.core.TaskSet;
 
 /**
  * Provides the ability to evaluated variable
+ * 
  * @author clement
- *
+ * 
  */
 public interface IPDIVariableManagement {
 	/**
 	 * Requests to delete named partial expression
-	 * @param tasks target process
-	 * @param exprId ID of partial expression to delete
-	 * @throws PDIException on failure
+	 * 
+	 * @param tasks
+	 *            target process
+	 * @param exprId
+	 *            ID of partial expression to delete
+	 * @throws PDIException
+	 *             on failure
 	 */
-	void deletePartialExpression(TaskSet tasks, String exprId) throws PDIException;
+	public void deletePartialExpression(TaskSet tasks, String exprId) throws PDIException;
 
 	/**
 	 * Requests String value of given expression
-	 * @param tasks target process
-	 * @param expr expression to evaluate
-	 * @throws PDIException on failure
+	 * 
+	 * @param tasks
+	 *            target process
+	 * @param expr
+	 *            expression to evaluate
+	 * @throws PDIException
+	 *             on failure
 	 */
-	void evaluateExpression(TaskSet tasks, String expr) throws PDIException;
-		
+	public void evaluateExpression(TaskSet tasks, String expr) throws PDIException;
+
 	/**
 	 * Requests partial IAIF value of given expression
-	 * @param tasks target process
-	 * @param expr expression to evaluate
-	 * @param exprId ID to refer to a pre-evaluated expression
-	 * @param listChildren is list a children?
-	 * @param express is expression?
-	 * @throws PDIException on failure
+	 * 
+	 * @param tasks
+	 *            target process
+	 * @param expr
+	 *            expression to evaluate
+	 * @param exprId
+	 *            ID to refer to a pre-evaluated expression
+	 * @param listChildren
+	 *            is list a children?
+	 * @param express
+	 *            is expression?
+	 * @throws PDIException
+	 *             on failure
 	 */
-	void evaluatePartialExpression(TaskSet tasks, String expr, String exprId, boolean listChildren, boolean express) throws PDIException;
-	
+	public void evaluatePartialExpression(TaskSet tasks, String expr, String exprId, boolean listChildren, boolean express)
+			throws PDIException;
+
 	/**
 	 * Request a list of argument for a range of level
-	 * @param tasks target process
-	 * @param low minimum level
-	 * @param high maximum level
-	 * @throws PDIException failure
+	 * 
+	 * @param tasks
+	 *            target process
+	 * @param low
+	 *            minimum level
+	 * @param high
+	 *            maximum level
+	 * @throws PDIException
+	 *             failure
 	 */
-	void listArguments(TaskSet tasks, int low, int high) throws PDIException;
-	
+	public void listArguments(TaskSet tasks, int low, int high) throws PDIException;
+
 	/**
 	 * Requests a list of global variables
-	 * @param tasks target process 
-	 * @throws PDIException on failure
+	 * 
+	 * @param tasks
+	 *            target process
+	 * @throws PDIException
+	 *             on failure
 	 */
-	void listGlobalVariables(TaskSet tasks) throws PDIException;
-	
+	public void listGlobalVariables(TaskSet tasks) throws PDIException;
+
 	/**
-	 * Requests a list of local variables 
-	 * @param tasks target process
-	 * @throws PDIException on failure 
+	 * Requests a list of local variables
+	 * 
+	 * @param tasks
+	 *            target process
+	 * @throws PDIException
+	 *             on failure
 	 */
-	void listLocalVariables(TaskSet tasks) throws PDIException;
+	public void listLocalVariables(TaskSet tasks) throws PDIException;
 
 	/**
 	 * Requests IAIFType of given variable name
-	 * @param tasks target process
-	 * @param var variable name
-	 * @throws PDIException on failure
+	 * 
+	 * @param tasks
+	 *            target process
+	 * @param var
+	 *            variable name
+	 * @throws PDIException
+	 *             on failure
 	 */
-	void retrieveVariableType(TaskSet tasks, String var) throws PDIException;
+	public void retrieveVariableType(TaskSet tasks, String var) throws PDIException;
 }
