1fc3111dcbbc96b9426b8ebf097b7a736045eab0
vkong <vkong>
2010-05-18 17:27:46 +0000

    merge changes from CDT to RDT

--- org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/internal/rdt/ui/contentassist/AbstractContentAssistService.java
+++ org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/internal/rdt/ui/contentassist/AbstractContentAssistService.java
@@ -12,7 +12,7 @@
 /* -- ST-Origin --
  * Source folder: org.eclipse.cdt.ui/src
  * Class: org.eclipse.cdt.internal.ui.text.contentassist.DOMCompletionProposalComputer
- * Version: 1.18
+ * Version: 1.27
  */
 
 package org.eclipse.ptp.internal.rdt.ui.contentassist;
