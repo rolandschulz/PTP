ad39c812f104159d43578c5aebb88c4931cd8077
Beth Tibbitts <tibbitts@us.ibm.com>
2010-05-19 01:21:03 +0000

    externalize strings and update copyrights

---
+++ tools/pldt/org.eclipse.ptp.pldt.openmp.ui.pv/src/org/eclipse/ptp/pldt/openmp/ui/pv/views/Messages.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corp. 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corp. - initial implementation
+ *******************************************************************************/
+package org.eclipse.ptp.pldt.openmp.ui.pv.views;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.ptp.pldt.openmp.ui.pv.views.messages"; //$NON-NLS-1$
+	public static String OpenMPProblemsView_action1;
+	public static String OpenMPProblemsView_action1executed;
+	public static String OpenMPProblemsView_action1tooltip;
+	public static String OpenMPProblemsView_Description;
+	public static String OpenMPProblemsView_InFolder;
+	public static String OpenMPProblemsView_Location;
+	public static String OpenMPProblemsView_removeAllMarkers;
+	public static String OpenMPProblemsView_removeMarkers;
+	public static String OpenMPProblemsView_Resource;
+	public static String OpenMPProblemsView_SampleView;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
--- tools/pldt/org.eclipse.ptp.pldt.openmp.ui.pv/src/org/eclipse/ptp/pldt/openmp/ui/pv/views/ProblemMarkerAttrIds.java
+++ tools/pldt/org.eclipse.ptp.pldt.openmp.ui.pv/src/org/eclipse/ptp/pldt/openmp/ui/pv/views/ProblemMarkerAttrIds.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006,2007 IBM Corp. 
+ * Copyright (c) 2006,2010 IBM Corp. 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,18 +18,18 @@ public class ProblemMarkerAttrIds
 {
     
     /** identifies the OpenMP Error marker */
-    public static final String MARKER_ERROR_ID       = "org.eclipse.ptp.pldt.openmp.ui.pv.openMPProblemMarker";
+    public static final String MARKER_ERROR_ID       = "org.eclipse.ptp.pldt.openmp.ui.pv.openMPProblemMarker"; //$NON-NLS-1$
  
     
     /** Marker id for storage of additional information about the artifact */
-    public static final String   DESCRIPTION        = "description";
+    public static final String   DESCRIPTION        = "description"; //$NON-NLS-1$
     /** Marker ID for storage of the filename in which the artifact is found */
-    public static final String   RESOURCE           = "resource";
+    public static final String   RESOURCE           = "resource"; //$NON-NLS-1$
     /** Marker ID for folder in which artifact is found */
-    public static final String   INFOLDER           = "infoldername";
+    public static final String   INFOLDER           = "infoldername"; //$NON-NLS-1$
     /** Marker ID for location (line number) in which the artifact is found */
-    public static final String   LOCATION           = "location";
+    public static final String   LOCATION           = "location"; //$NON-NLS-1$
     /** Marker ID that holds the problem object                           */
-    public static final String   PROBLEMOBJECT      = "problem";
+    public static final String   PROBLEMOBJECT      = "problem"; //$NON-NLS-1$
 
 }
