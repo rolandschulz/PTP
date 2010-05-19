c9eb86cad8dd77bd6ecc72ca31f836897304882c
vkong <vkong>
2010-05-19 19:12:04 +0000

    merge changes from CDT to RDT

--- org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/internal/rdt/ui/callhierarchy/CHHistoryDropDownAction.java
+++ org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/internal/rdt/ui/callhierarchy/CHHistoryDropDownAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,7 +13,7 @@
 /* -- ST-Origin --
  * Source folder: org.eclipse.cdt.ui/src
  * Class: org.eclipse.cdt.internal.ui.callhierarchy.CHHistoryDropDownAction
- * Version: 1.1
+ * Version: 1.3
  */
 
 package org.eclipse.ptp.internal.rdt.ui.callhierarchy;
@@ -39,7 +39,8 @@ public class CHHistoryDropDownAction extends Action implements IMenuCreator {
 			super(CHMessages.CHHistoryDropDownAction_ClearHistory_label);
 			fView= view;
 		}
-			
+		
+		@Override
 		public void run() {
 			fView.setHistoryEntries(new ICElement[0]);
 			fView.setInput(null);
@@ -104,7 +105,8 @@ public class CHHistoryDropDownAction extends Action implements IMenuCreator {
 		ActionContributionItem item= new ActionContributionItem(action);
 		item.fill(parent, -1);
 	}
-
+	
+	@Override
 	public void run() {
 		(new CHHistoryListAction(fHierarchyView)).run();
 	}
