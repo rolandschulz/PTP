9921ff769b5b6ea978c125a2f6bc7caad33991f4
gwatson <gwatson>
2010-05-20 01:03:25 +0000

    Document API changes.

--- core/org.eclipse.ptp.ui/src/org/eclipse/ptp/ui/IJobManager.java
+++ core/org.eclipse.ptp.ui/src/org/eclipse/ptp/ui/IJobManager.java
@@ -17,108 +17,114 @@ import org.eclipse.ptp.ui.listeners.IJobChangedListener;
 import org.eclipse.ptp.ui.model.IElementHandler;
 
 public interface IJobManager extends IElementManager {
-	/** 
+	/**
 	 * Add job listener
 	 * 
 	 * @param jobListener
 	 */
 	public void addJobChangedListener(IJobChangedListener jobListener);
-	
+
 	/**
 	 * Add a process to the view.
 	 * 
 	 * @param job
 	 * @param procJobRank
+	 * @since 4.0
 	 */
 	public void addProcess(IPJob job, int procJobRank);
-	
+
 	/**
 	 * Create an element handler for the job
 	 * 
-	 * @param job job
+	 * @param job
+	 *            job
 	 * @return element handler for the job
 	 */
 	public IElementHandler createElementHandler(IPJob job);
-	
+
 	/**
 	 * Find a job give its ID.
 	 * 
-	 * @param jobId ID of job to find
+	 * @param jobId
+	 *            ID of job to find
 	 * @return job with corresponding ID
 	 */
 	public IPJob findJobById(String jobId);
-	
-	/** 
+
+	/**
 	 * Fire job event when job is changed
 	 * 
-	 * @param type job change type or remove type
+	 * @param type
+	 *            job change type or remove type
 	 * @param cur_jid
 	 * @param pre_jid
 	 */
 	public void fireJobChangedEvent(int type, String cur_jid, String pre_jid);
-	
+
 	/**
 	 * Get the currently selected job.
 	 * 
 	 * @return currently selected job
 	 */
 	public IPJob getJob();
-	
-	/** 
+
+	/**
 	 * Get Jobs that we know about.
 	 * 
 	 * @return jobs
 	 */
 	public IPJob[] getJobs();
-	
+
 	/**
 	 * Get the currently selected queue.
 	 * 
 	 * @return currently selected queue
 	 */
 	public IPQueue getQueue();
-	
+
 	/**
 	 * Test if there is at least one completed job.
 	 * 
 	 * @return true if there is a completed job
 	 */
 	public boolean hasStoppedJob();
-	
+
 	/**
 	 * Remove all jobs that have completed from the view
 	 */
 	public void removeAllStoppedJobs();
-	
+
 	/**
 	 * Remove job from view.
 	 * 
 	 * @param job
 	 */
 	public void removeJob(IPJob job);
-	
-	/** 
+
+	/**
 	 * Remove job listener
 	 * 
 	 * @param jobListener
 	 */
 	public void removeJobChangedListener(IJobChangedListener jobListener);
-	
+
 	/**
 	 * Remove a process from the view.
 	 * 
 	 * @param job
 	 * @param procJobRank
+	 * @since 4.0
 	 */
 	public void removeProcess(IPJob job, int procJobRank);
-	
-	/** 
+
+	/**
 	 * Set the current job
 	 * 
-	 * @param job the current job to set
+	 * @param job
+	 *            the current job to set
 	 */
 	public void setJob(IPJob job);
-	
+
 	/**
 	 * Terminate the currently selected job.
 	 * 
--- core/org.eclipse.ptp.ui/src/org/eclipse/ptp/ui/views/ProcessEditorInput.java
+++ core/org.eclipse.ptp.ui/src/org/eclipse/ptp/ui/views/ProcessEditorInput.java
@@ -23,28 +23,30 @@ import org.eclipse.ptp.internal.ui.model.PProcessUI;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IPersistableElement;
 
-/**
- *
- */
-
 public class ProcessEditorInput implements IEditorInput {
 	// FIXME PProcessUI goes away when we address UI scalability. See Bug 311057
-    private PProcessUI process = null;
-    
-    /** Constructor
-     * @param process
-     */
+	private PProcessUI process = null;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param process
+	 * @since 4.0
+	 */
 	// FIXME PProcessUI goes away when we address UI scalability. See Bug 311057
-    public ProcessEditorInput(PProcessUI process) {
+	public ProcessEditorInput(PProcessUI process) {
 		if (process == null) {
 			throw new IllegalArgumentException();
 		}
-        this.process = process;
-    }
-    
-	/* (non-Javadoc)
+		this.process = process;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
+	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
@@ -53,68 +55,81 @@ public class ProcessEditorInput implements IEditorInput {
 		ProcessEditorInput other = (ProcessEditorInput) obj;
 		return process.equals(other.process);
 	}
-	
+
 	/**
-	 * From the implementing class IEditorInput.  This method only has to do
-	 * with whether "an editor input should appear in the "File Most Recently 
-	 * Used" menu.  In our case, just return true.
+	 * From the implementing class IEditorInput. This method only has to do with
+	 * whether "an editor input should appear in the "File Most Recently Used"
+	 * menu. In our case, just return true.
 	 * 
-	 * @see	IEditorInput
+	 * @see IEditorInput
 	 */
 	public boolean exists() {
 		/* return process.exists(); */
 		return true;
 	}
+
 	/*
-	public boolean exists() {
-		return process.exists();
-	}
-	*/
-	
-	/** Get process
+	 * public boolean exists() { return process.exists(); }
+	 */
+
+	/**
+	 * Get process
+	 * 
 	 * @return
+	 * @since 4.0
 	 */
 	// FIXME PProcessUI goes away when we address UI scalability. See Bug 311057
 	public PProcessUI getProcess() {
 		return process;
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.ui.IEditorInput#getImageDescriptor()
 	 */
 	public ImageDescriptor getImageDescriptor() {
 		return null;
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.ui.IEditorInput#getName()
 	 */
 	public String getName() {
 		return process.getJob().getName() + ":" + process.getName(); //$NON-NLS-1$
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.ui.IEditorInput#getPersistable()
 	 */
 	public IPersistableElement getPersistable() {
 		return null;
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.ui.IEditorInput#getToolTipText()
 	 */
 	public String getToolTipText() {
 		return getName();
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.core.runtime.IAdaptable#getAdapter(java.lang.Class)
 	 */
 	@SuppressWarnings("rawtypes")
 	public Object getAdapter(Class adapter) {
-		// FIXME PProcessUI goes away when we address UI scalability. See Bug 311057
+		// FIXME PProcessUI goes away when we address UI scalability. See Bug
+		// 311057
 		if (adapter == PProcessUI.class)
 			return process;
 		return null;
-	}	
+	}
 }
