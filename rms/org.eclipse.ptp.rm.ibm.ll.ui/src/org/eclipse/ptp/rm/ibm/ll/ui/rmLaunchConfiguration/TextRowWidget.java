572332ec71561f2fdba0d8c99e789f157459eb41
Greg Watson <g.watson@computer.org>
2010-05-20 17:12:07 +0000

    Removed unused imports.

--- rms/org.eclipse.ptp.rm.ibm.ll.ui/src/org/eclipse/ptp/rm/ibm/ll/ui/rmLaunchConfiguration/TextRowWidget.java
+++ rms/org.eclipse.ptp.rm.ibm.ll.ui/src/org/eclipse/ptp/rm/ibm/ll/ui/rmLaunchConfiguration/TextRowWidget.java
@@ -9,7 +9,6 @@ package org.eclipse.ptp.rm.ibm.ll.ui.rmLaunchConfiguration;
 
 import org.eclipse.ptp.core.attributes.IAttributeDefinition;
 import org.eclipse.ptp.core.attributes.IllegalValueException;
-import org.eclipse.ptp.rm.ibm.ll.ui.*;
 import org.eclipse.ptp.rm.ibm.ll.ui.messages.Messages;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
@@ -20,169 +19,173 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Widget;
 
-public class TextRowWidget implements ValidationState
-{
-    private Label label;
-    private Label nonDefaultMarker;
-    private GridData gridData;
-    private IAttributeDefinition<?, ?, ?> attrDef;
-    private Text widget;
-    private String defaultValue;
-    private EventMonitor eventMonitor;
-    private int validationState;
+public class TextRowWidget implements ValidationState {
+	private final Label label;
+	private final Label nonDefaultMarker;
+	private GridData gridData;
+	private final IAttributeDefinition<?, ?, ?> attrDef;
+	private final Text widget;
+	private String defaultValue;
+	private final EventMonitor eventMonitor;
+	private int validationState;
 
-    /**
-     * Internal class which listens for events indicating the data value for this object has changed
-     *
-     */
-    private class EventMonitor implements ModifyListener
-    {
-	public EventMonitor()
-	{
+	/**
+	 * Internal class which listens for events indicating the data value for
+	 * this object has changed
+	 * 
+	 */
+	private class EventMonitor implements ModifyListener {
+		public EventMonitor() {
+		}
+
+		/**
+		 * Change the non-default value indicator for this object to indicate
+		 * whether the data value matches the default value from the attribute
+		 * definition
+		 * 
+		 * @param e
+		 *            The event indicating data value has changed
+		 */
+		public void modifyText(ModifyEvent e) {
+			String value;
+
+			value = widget.getText().trim();
+			if ((value.length() == 0) || (value.equals(defaultValue))) {
+				nonDefaultMarker.setText(" ");
+			} else {
+				nonDefaultMarker.setText("*");
+			}
+		}
 	}
 
 	/**
-	 * Change the non-default value indicator for this object to indicate whether the data value matches
-	 * the default value from the attribute definition
-	 * @param e The event indicating data value has changed 
+	 * Create a widget which is used as a text entry field in the launch
+	 * configuration. This class assumes that the layout manager set for the
+	 * parent Composite object is a GridLayout widget with 4 columns
+	 * 
+	 * @param parent
+	 *            The parent Composite object
+	 * @param id
+	 *            The name of the attribute for this widget
+	 * @param def
+	 *            The attribute definition for this widget
 	 */
-	public void modifyText(ModifyEvent e)
-	{
-	    String value;
-	    
-	    value = widget.getText().trim();
-	    if ((value.length () == 0) || (value.equals(defaultValue))) {
-		nonDefaultMarker.setText(" ");
-	    } else {
+	public TextRowWidget(Composite parent, String id, IAttributeDefinition<?, ?, ?> def) {
+		attrDef = def;
+		gridData = new GridData(GridData.FILL_HORIZONTAL);
+		gridData.grabExcessHorizontalSpace = false;
+		label = new Label(parent, SWT.NONE);
+		label.setText(Messages.getString(attrDef.getName()));
+		label.setLayoutData(gridData);
+		nonDefaultMarker = new Label(parent, SWT.NONE);
 		nonDefaultMarker.setText("*");
-	    }
+		widget = new Text(parent, SWT.SINGLE | SWT.BORDER);
+		widget.setToolTipText(Messages.getString(attrDef.getDescription()));
+		gridData = new GridData(GridData.FILL_HORIZONTAL);
+		gridData.horizontalSpan = 2;
+		gridData.grabExcessHorizontalSpace = true;
+		widget.setLayoutData(gridData);
+		widget.setData(WidgetAttributes.ATTR_NAME, id);
+		try {
+			defaultValue = attrDef.create().getValueAsString();
+		} catch (IllegalValueException e) {
+			defaultValue = "";
+		}
+		eventMonitor = new EventMonitor();
+		widget.addModifyListener(eventMonitor);
 	}
-    }
 
-    /**
-     * Create a widget which is used as a text entry field in the launch configuration. This class assumes that
-     * the layout manager set for the parent Composite object is a GridLayout widget with 4 columns
-     * @param parent The parent Composite object
-     * @param id The name of the attribute for this widget
-     * @param def The attribute definition for this widget
-     */
-    public TextRowWidget(Composite parent, String id, IAttributeDefinition<?, ?, ?> def)
-    {
-	attrDef = def;
-	gridData = new GridData(GridData.FILL_HORIZONTAL);
-	gridData.grabExcessHorizontalSpace = false;
-	label = new Label(parent, SWT.NONE);
-	label.setText(Messages.getString(attrDef.getName()));
-	label.setLayoutData(gridData);
-	nonDefaultMarker = new Label(parent, SWT.NONE);
-	nonDefaultMarker.setText("*");
-	widget = new Text(parent, SWT.SINGLE | SWT.BORDER);
-	widget.setToolTipText(Messages.getString(attrDef.getDescription()));
-	gridData = new GridData(GridData.FILL_HORIZONTAL);
-	gridData.horizontalSpan = 2;
-	gridData.grabExcessHorizontalSpace = true;
-	widget.setLayoutData(gridData);
-	widget.setData(WidgetAttributes.ATTR_NAME, id);
-	try {
-	    defaultValue = attrDef.create().getValueAsString();
+	/**
+	 * Add a ModifyListener to this object
+	 * 
+	 * @param monitor
+	 *            the listening object
+	 */
+	public void addModifyListener(ModifyListener monitor) {
+		widget.addModifyListener(monitor);
 	}
-	catch (IllegalValueException e) {
-	    defaultValue = "";
+
+	/**
+	 * Get the data value for this object
+	 * 
+	 * @return The data value
+	 */
+	public String getValue() {
+		return widget.getText().trim();
 	}
-	eventMonitor = new EventMonitor();
-	widget.addModifyListener(eventMonitor);
-    }
 
-    /**
-     * Add a ModifyListener to this object
-     * @param monitor the listening object
-     */
-    public void addModifyListener(ModifyListener monitor)
-    {
-	widget.addModifyListener(monitor);
-    }
+	/**
+	 * Set the data value for this object
+	 * 
+	 * @param value
+	 *            The data value
+	 */
+	public void setValue(String value) {
+		widget.setText(value);
+	}
 
-    /**
-     * Get the data value for this object
-     * @return The data value
-     */
-    public String getValue()
-    {
-	return widget.getText().trim();
-    }
+	/**
+	 * Get the data object (attribute name) stored in this widget
+	 * 
+	 * @param key
+	 *            The key to use in retrieving the data object
+	 * @return the data object
+	 */
+	public String getData(String key) {
+		return (String) widget.getData(key);
+	}
 
-    /**
-     * Set the data value for this object
-     * @param value The data value
-     */
-    public void setValue(String value)
-    {
-	widget.setText(value);
-    }
+	/**
+	 * Set this object's validation state to indicate no validation is required
+	 */
+	public void resetValidationState() {
+		validationState = ValidationState.UNCHANGED;
+	}
 
-    /**
-     * Get the data object (attribute name) stored in this widget
-     * @param key The key to use in retrieving the data object
-     * @return the data object
-     */
-    public String getData(String key)
-    {
-	return (String) widget.getData(key);
-    }
+	/**
+	 * Set this object's validation state to indicate that the value is in error
+	 * and must be validated again
+	 */
+	public void setFieldInError() {
+		validationState = ValidationState.IN_ERROR;
+	}
 
-    /**
-     * Set this object's validation state to indicate no validation is required
-     */
-    public void resetValidationState()
-    {
-	validationState = ValidationState.UNCHANGED;
-    }
+	/**
+	 * Set this object's validation state to indicate the value has changed and
+	 * validation is required
+	 */
+	public void setValidationRequired() {
+		validationState = ValidationState.CHANGED;
+	}
 
-    /**
-     * Set this object's validation state to indicate that the value is in error and must be validated again
-     */
-    public void setFieldInError()
-    {
-	validationState = ValidationState.IN_ERROR;
-    }
+	/**
+	 * Return status indicating if this object needs to be validated
+	 * 
+	 * @return - validation status
+	 */
+	public boolean isValidationRequired() {
+		return ((validationState == ValidationState.CHANGED) || (validationState == ValidationState.IN_ERROR));
+	}
 
-    /**
-     * Set this object's validation state to indicate the value has changed and validation is required
-     */
-    public void setValidationRequired()
-    {
-	validationState = ValidationState.CHANGED;
-    }
+	/**
+	 * Return status indicating that the actual widget contained in this object
+	 * is the widget that is being searched for
+	 * 
+	 * @param source
+	 *            - The widget that is being searched for
+	 * @return - status indicating this object contains the matching widget.
+	 */
+	public boolean isMatchingWidget(Widget source) {
+		return widget == source;
+	}
 
-    /**
-     * Return status indicating if this object needs to be validated
-     * @return - validation status
-     */
-    public boolean isValidationRequired()
-    {
-	return ((validationState == ValidationState.CHANGED) || (validationState == ValidationState.IN_ERROR));
-    }
-    
-    /** 
-     * Return status indicating that the actual widget contained in this object is the widget that is being searched
-     * for
-     * @param source - The widget that is being searched for
-     * @return - status indicating this object contains the matching widget.
-     */
-    public boolean isMatchingWidget(Widget source)
-    {
-	return widget == source;
-    }
-    
-    public void setEnabled(boolean state)
-    {
-	widget.setEnabled(state);
-    }
-    
-    public boolean isEnabled()
-    {
-	return widget.isEnabled();
-    }
+	public void setEnabled(boolean state) {
+		widget.setEnabled(state);
+	}
+
+	public boolean isEnabled() {
+		return widget.isEnabled();
+	}
 
 	public String getDefaultValue() {
 		return defaultValue;
