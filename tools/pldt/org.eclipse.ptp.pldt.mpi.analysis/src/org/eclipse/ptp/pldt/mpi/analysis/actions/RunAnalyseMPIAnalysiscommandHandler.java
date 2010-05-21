cd20138a6b1dc3ccb69099cd70f5cde652345736
Beth Tibbitts <tibbitts@us.ibm.com>
2010-05-21 20:23:52 +0000

    ViewActivater->ViewActivator

--- tools/pldt/org.eclipse.ptp.pldt.mpi.analysis/src/org/eclipse/ptp/pldt/mpi/analysis/actions/RunAnalyseMPIAnalysiscommandHandler.java
+++ tools/pldt/org.eclipse.ptp.pldt.mpi.analysis/src/org/eclipse/ptp/pldt/mpi/analysis/actions/RunAnalyseMPIAnalysiscommandHandler.java
@@ -26,45 +26,46 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ptp.pldt.common.actions.AnalysisDropdownHandler;
 import org.eclipse.ptp.pldt.common.actions.RunAnalyseHandler;
-import org.eclipse.ptp.pldt.common.util.ViewActivater;
+import org.eclipse.ptp.pldt.common.util.ViewActivator;
 import org.eclipse.ptp.pldt.mpi.analysis.IDs;
 import org.eclipse.ptp.pldt.mpi.analysis.analysis.MPICallGraph;
 import org.eclipse.ptp.pldt.mpi.analysis.analysis.MPIResourceCollector;
+
 
 /**
  * Do MPI barrier analysis from dropdown toolbar menu
  * 
  * @author Beth Tibbitts
- * 
+ *
  */
-public class RunAnalyseMPIAnalysiscommandHandler extends RunAnalyseHandler {
+public class RunAnalyseMPIAnalysiscommandHandler extends RunAnalyseHandler  {
 	protected MPICallGraph callGraph_;
-	boolean traceOn = false;
-
-	public RunAnalyseMPIAnalysiscommandHandler() {
+	boolean traceOn=false;
+	
+	public RunAnalyseMPIAnalysiscommandHandler(){ 
 		callGraph_ = null;
 	}
 
-	/**
+	/** 
 	 * Execute the action for MPI barrier analysis
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		boolean foundError = false;
+		boolean foundError=false;
 		getSelection(event);
-		AnalysisDropdownHandler.setLastHandledAnalysis(this, selection);
+		AnalysisDropdownHandler.setLastHandledAnalysis(this,selection);
 		if ((selection == null) || selection.isEmpty()) {
 			MessageDialog.openWarning(null, "No files selected for analysis.",
-					"Please select a source file  or container (folder or project) to analyze.");
+							"Please select a source file  or container (folder or project) to analyze.");
 			return null;
 		} else {
-			final boolean reportErrors = true;
-			foundError = analyseBarriers(selection, reportErrors);
+			final boolean reportErrors=true;
+			foundError = analyseBarriers(selection,reportErrors);
 		}
-		ViewActivater.activateView(IDs.matchingSetViewID);
-		ViewActivater.activateView(IDs.barrierViewID);
+		ViewActivator.activateView(IDs.matchingSetViewID);
+		ViewActivator.activateView(IDs.barrierViewID);
 		// if error found, assure its view has focus
-		if (foundError) {
-			ViewActivater.activateView(IDs.errorViewID);
+		if(foundError) {
+			ViewActivator.activateView(IDs.errorViewID);
 		}
 		return null;
 	}
@@ -80,11 +81,11 @@ public class RunAnalyseMPIAnalysiscommandHandler extends RunAnalyseHandler {
 	public boolean analyseBarriers(IStructuredSelection selection, boolean reportErrors) {
 		boolean foundError;
 		callGraph_ = new MPICallGraph();
-		// int numFiles=this.countFilesSelected();
+		//int numFiles=this.countFilesSelected();
 		// BRT use numfiles for a progress monitor?
 
-		for (Iterator iter = selection.iterator(); iter.hasNext();) {
-			Object obj = iter.next();
+		for(Iterator iter = selection.iterator(); iter.hasNext();){
+			Object obj =  iter.next();
 			// It can be a Project, Folder, File, etc...
 			if (obj instanceof IAdaptable) {
 				final IResource res = (IResource) ((IAdaptable) obj).getAdapter(IResource.class);
@@ -97,10 +98,10 @@ public class RunAnalyseMPIAnalysiscommandHandler extends RunAnalyseHandler {
 			}
 		} // end for
 		MPIAnalysisManager manager = new MPIAnalysisManager(callGraph_);
-		foundError = manager.run(reportErrors);
+		foundError=manager.run(reportErrors);
 		return foundError;
 	}
-
+	
 	/**
 	 * Run analysis (collect resource info in the call graph) on a resource
 	 * (e.g. File or Folder) <br>
@@ -116,14 +117,14 @@ public class RunAnalyseMPIAnalysiscommandHandler extends RunAnalyseHandler {
 		boolean foundError = false;
 
 		if (resource instanceof IFile) {
-			try {
+			try{
 				resource.deleteMarkers(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE);
-			} catch (CoreException e) {
-				// System.out.println("Exception deleting markers.");
+			} catch(CoreException e){
+				//System.out.println("Exception deleting markers.");
 			}
 			IFile file = (IFile) resource;
 			String filename = file.getName();
-			if (filename.endsWith(".c")) {
+			if(filename.endsWith(".c")){
 				if (traceOn)
 					System.out.println("resourceCollector on c file: " + file.getName());
 				MPIResourceCollector rc = new MPIResourceCollector(callGraph_, file); // BRT
