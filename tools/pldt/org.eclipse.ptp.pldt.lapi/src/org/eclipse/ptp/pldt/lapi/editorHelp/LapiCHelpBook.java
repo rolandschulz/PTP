79518f69ae2bff0f3349308d4d38b08b1b356fa8
Beth Tibbitts <tibbitts@us.ibm.com>
2010-05-21 19:10:33 +0000

    consolidate messages

--- tools/pldt/org.eclipse.ptp.pldt.lapi/src/org/eclipse/ptp/pldt/lapi/editorHelp/LapiCHelpBook.java
+++ tools/pldt/org.eclipse.ptp.pldt.lapi/src/org/eclipse/ptp/pldt/lapi/editorHelp/LapiCHelpBook.java
@@ -14,6 +14,7 @@ import org.eclipse.ptp.pldt.common.editorHelp.CHelpBookImpl;
 import org.eclipse.ptp.pldt.common.editorHelp.FunctionPrototypeSummaryImpl;
 import org.eclipse.ptp.pldt.common.editorHelp.FunctionSummaryImpl;
 import org.eclipse.ptp.pldt.lapi.LapiPlugin;
+import org.eclipse.ptp.pldt.lapi.messages.Messages;
 
 
 public class LapiCHelpBook extends CHelpBookImpl {
