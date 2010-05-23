e44c1bf8dc3e74b7d08e59b376dc2bf7d503c2cf
joverbey <joverbey>
2010-05-23 21:36:06 +0000

    Modified to handle two-part Dev Guide

--- photran/org.eclipse.photran-dev-docs/dev-guide/build.sh
+++ photran/org.eclipse.photran-dev-docs/dev-guide/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Use this if you have problems running the Ant build script (build.xml)
-pdflatex dev-guide.ltx
-pdflatex dev-guide.ltx
+pdflatex dev-guide-general.ltx
+pdflatex dev-guide-specialized.ltx
 pdflatex cvs-instructions.ltx
 rm -rf *.aux *.log *.toc *.dvi *.ps *.out
