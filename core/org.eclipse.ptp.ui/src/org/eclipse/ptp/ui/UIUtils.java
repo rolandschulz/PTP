e2080120ff397ed769863773840525149ccdea06
Greg Watson <g.watson@computer.org>
2010-06-21 14:30:19 +0000

    Missing @since tags

--- core/org.eclipse.ptp.ui/src/org/eclipse/ptp/ui/UIUtils.java
+++ core/org.eclipse.ptp.ui/src/org/eclipse/ptp/ui/UIUtils.java
@@ -40,138 +40,136 @@ import org.eclipse.ui.WorkbenchException;
  *
  */
 public class UIUtils {
-    public static final int NORMAL = 0;
-    public static final int ASYNC = 1;
+	public static final int NORMAL = 0;
+	public static final int ASYNC = 1;
 
-    protected static void showDialog(final Shell shell, final String title, final String message, final int style) {
-        MessageBox dialog = new MessageBox(shell, style);
-        dialog.setMessage(message);
-        dialog.setText(title);
-        dialog.open();
-    }
+	protected static void showDialog(final Shell shell, final String title, final String message, final int style) {
+		MessageBox dialog = new MessageBox(shell, style);
+		dialog.setMessage(message);
+		dialog.setText(title);
+		dialog.open();
+	}
 
-    protected static void showDialogAsync(final Shell shell, final String title, final String message, final int style) {
-        shell.getDisplay().asyncExec(new Runnable() {
-            public void run() {
-                showDialog(shell, title, message, style);
-            }
-        });
-    }
+	protected static void showDialogAsync(final Shell shell, final String title, final String message, final int style) {
+		shell.getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				showDialog(shell, title, message, style);
+			}
+		});
+	}
 
-    public static void showWarningDialog(final Shell shell, final String title, final String message, int flag) {
-        if (flag == NORMAL)
-            showDialog(shell, title, message, SWT.ICON_WARNING | SWT.OK);
-        else
-            showDialogAsync(shell, title, message, SWT.ICON_WARNING | SWT.OK);
-    }
+	public static void showWarningDialog(final Shell shell, final String title, final String message, int flag) {
+		if (flag == NORMAL)
+			showDialog(shell, title, message, SWT.ICON_WARNING | SWT.OK);
+		else
+			showDialogAsync(shell, title, message, SWT.ICON_WARNING | SWT.OK);
+	}
 
-    public static void showInformationDialog(final Shell shell, final String title, final String message, int flag) {
-        if (flag == NORMAL)
-            showDialog(shell, title, message, SWT.ICON_INFORMATION | SWT.OK);
-        else
-            showDialogAsync(shell, title, message, SWT.ICON_INFORMATION | SWT.OK);
-    }
+	public static void showInformationDialog(final Shell shell, final String title, final String message, int flag) {
+		if (flag == NORMAL)
+			showDialog(shell, title, message, SWT.ICON_INFORMATION | SWT.OK);
+		else
+			showDialogAsync(shell, title, message, SWT.ICON_INFORMATION | SWT.OK);
+	}
 
-    public static void showErrorDialog(final Shell shell, final String title, final String message, int flag) {
-        if (flag == NORMAL)
-            showDialog(shell, title, message, SWT.ICON_ERROR | SWT.OK);
-        else
-            showDialogAsync(shell, title, message, SWT.ICON_ERROR | SWT.OK);
-    }
+	public static void showErrorDialog(final Shell shell, final String title, final String message, int flag) {
+		if (flag == NORMAL)
+			showDialog(shell, title, message, SWT.ICON_ERROR | SWT.OK);
+		else
+			showDialogAsync(shell, title, message, SWT.ICON_ERROR | SWT.OK);
+	}
 
-    // IStatus can be retrieved from CoreException e.getStatus()
-    public static void showDetailErrorDialog(final Shell shell, final String title, final String message, final IStatus status) {
-        shell.getDisplay().asyncExec(new Runnable() {
-            public void run() {
-                ErrorDialog.openError(shell, title, message, status);
-            }
-        });
-    }
+	// IStatus can be retrieved from CoreException e.getStatus()
+	public static void showDetailErrorDialog(final Shell shell, final String title, final String message, final IStatus status) {
+		shell.getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				ErrorDialog.openError(shell, title, message, status);
+			}
+		});
+	}
 
-    public static void showErrorDialog(final String title, final String message, final IStatus status) {
-        Display.getDefault().asyncExec(new Runnable() {
-            public void run() {
-                final Shell shell = Display.getDefault().getActiveShell();
-                if (status == null)
-                    MessageDialog.openError(shell, title, message);
-                else
-                    ErrorDialog.openError(shell, title, message, status);
-            }
-        });
-    }
+	public static void showErrorDialog(final String title, final String message, final IStatus status) {
+		Display.getDefault().asyncExec(new Runnable() {
+			public void run() {
+				final Shell shell = Display.getDefault().getActiveShell();
+				if (status == null)
+					MessageDialog.openError(shell, title, message);
+				else
+					ErrorDialog.openError(shell, title, message, status);
+			}
+		});
+	}
 
-    public static boolean showQuestionDialog(final String title, final String message) {
-        final Shell shell = Display.getDefault().getActiveShell();
-        return MessageDialog.openQuestion(shell, title, message);
-    }
+	public static boolean showQuestionDialog(final String title, final String message) {
+		final Shell shell = Display.getDefault().getActiveShell();
+		return MessageDialog.openQuestion(shell, title, message);
+	}
 
-    public static void showView(final String viewID) {
-        BusyIndicator.showWhile(null, new Runnable() {
-            public void run() {
-                Display.getDefault().syncExec(new Runnable() {
-                    public void run() {
-                        try {
-                            displayView(viewID);
-                        } catch (NullPointerException e) {
-                            System.out.println("Show View err: " + e.getMessage()); //$NON-NLS-1$
-                        }
-                    }
-                });
-            }
-        });
-    }
+	public static void showView(final String viewID) {
+		BusyIndicator.showWhile(null, new Runnable() {
+			public void run() {
+				Display.getDefault().syncExec(new Runnable() {
+					public void run() {
+						try {
+							displayView(viewID);
+						} catch (NullPointerException e) {
+							System.out.println("Show View err: " + e.getMessage()); //$NON-NLS-1$
+						}
+					}
+				});
+			}
+		});
+	}
 
-    public static void switchPerspectiveTo(final String perspectiveID) {
-        Display.getDefault().syncExec(new Runnable() {
-            public void run() {
-                IWorkbenchPage page = getActivePage();
-                if (page.getPerspective().getId().equals(perspectiveID))
-                    return;
-                IWorkbench bench = PTPUIPlugin.getDefault().getWorkbench();
-                try {
-                    bench.showPerspective(perspectiveID, PTPUIPlugin.getActiveWorkbenchWindow());
-                } catch (WorkbenchException e) {
-                    showErrorDialog(Messages.UIUtils_1, Messages.UIUtils_2 + perspectiveID, e.getStatus());
-                }
-            }
-        });
-    }
+	public static void switchPerspectiveTo(final String perspectiveID) {
+		Display.getDefault().syncExec(new Runnable() {
+			public void run() {
+				IWorkbenchPage page = getActivePage();
+				if (page.getPerspective().getId().equals(perspectiveID))
+					return;
+				IWorkbench bench = PTPUIPlugin.getDefault().getWorkbench();
+				try {
+					bench.showPerspective(perspectiveID, PTPUIPlugin.getActiveWorkbenchWindow());
+				} catch (WorkbenchException e) {
+					showErrorDialog(Messages.UIUtils_1, Messages.UIUtils_2 + perspectiveID, e.getStatus());
+				}
+			}
+		});
+	}
 
-    public static IWorkbenchPage getActivePage() {
-        return PTPUIPlugin.getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage();
-    }
+	public static IWorkbenchPage getActivePage() {
+		return PTPUIPlugin.getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage();
+	}
 
-    public static void displayView(String viewID) throws NullPointerException {
-        IWorkbenchPage page = getActivePage();
-        try {
-            page.showView(viewID);
-        } catch (PartInitException e) {
-            showErrorDialog(Messages.UIUtils_3, Messages.UIUtils_4 + viewID, e.getStatus());
-        }
-    }
+	public static void displayView(String viewID) throws NullPointerException {
+		IWorkbenchPage page = getActivePage();
+		try {
+			page.showView(viewID);
+		} catch (PartInitException e) {
+			showErrorDialog(Messages.UIUtils_3, Messages.UIUtils_4 + viewID, e.getStatus());
+		}
+	}
 
-    public static IViewPart findView(String viewID) {
-        IWorkbenchPage page = getActivePage();
-        return page.findView(viewID);
-    }
+	public static IViewPart findView(String viewID) {
+		IWorkbenchPage page = getActivePage();
+		return page.findView(viewID);
+	}
 
-    public static void closeView(String viewID) {
-        IViewPart viewPart = findView(viewID);
-        if (viewPart == null)
-            return;
-        viewPart.dispose();
-    }
-    
+	public static void closeView(String viewID) {
+		IViewPart viewPart = findView(viewID);
+		if (viewPart == null)
+			return;
+		viewPart.dispose();
+	}
 
-    public static void safeRunAsyncInUIThread(final ISafeRunnable safeRunnable) {
+	public static void safeRunAsyncInUIThread(final ISafeRunnable safeRunnable) {
 		if (PTPUIPlugin.getDisplay().isDisposed()) {
 			try {
 				safeRunnable.run();
 			} catch (Exception e) {
 				PTPCorePlugin.log(e);
 			}
-		}
-		else {
+		} else {
 			PTPUIPlugin.getDisplay().asyncExec(new Runnable() {
 				public void run() {
 					SafeRunnable.run(safeRunnable);
@@ -179,15 +177,18 @@ public class UIUtils {
 			});
 		}
 	}
-    public static void safeRunSyncInUIThread(final ISafeRunnable safeRunnable) {
+
+	/**
+	 * @since 4.0
+	 */
+	public static void safeRunSyncInUIThread(final ISafeRunnable safeRunnable) {
 		if (PTPUIPlugin.getDisplay().isDisposed()) {
 			try {
 				safeRunnable.run();
 			} catch (Exception e) {
 				PTPCorePlugin.log(e);
 			}
-		}
-		else {
+		} else {
 			PTPUIPlugin.getDisplay().syncExec(new Runnable() {
 				public void run() {
 					SafeRunnable.run(safeRunnable);
