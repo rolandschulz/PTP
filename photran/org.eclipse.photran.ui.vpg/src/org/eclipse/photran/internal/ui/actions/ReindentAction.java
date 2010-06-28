bfd056eeb71f6b8aa55f4c17aeab42c6c843bd52
joverbey <joverbey>
2010-06-28 22:49:54 +0000

    Added Reindent command (Bug 318258)

---
+++ photran/org.eclipse.photran.ui.vpg/src/org/eclipse/photran/internal/ui/actions/ReindentAction.java
@@ -0,0 +1,109 @@
+/*******************************************************************************
+ * Copyright (c) 2010 University of Illinois at Urbana-Champaign and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     UIUC - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.photran.internal.ui.actions;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.photran.core.IFortranAST;
+import org.eclipse.photran.internal.core.FortranAST;
+import org.eclipse.photran.internal.core.SyntaxException;
+import org.eclipse.photran.internal.core.lexer.ASTLexerFactory;
+import org.eclipse.photran.internal.core.lexer.IAccumulatingLexer;
+import org.eclipse.photran.internal.core.lexer.LexerException;
+import org.eclipse.photran.internal.core.lexer.TokenList;
+import org.eclipse.photran.internal.core.parser.ASTExecutableProgramNode;
+import org.eclipse.photran.internal.core.parser.Parser;
+import org.eclipse.photran.internal.core.refactoring.infrastructure.SourcePrinter;
+import org.eclipse.photran.internal.core.reindenter.Reindenter;
+import org.eclipse.photran.internal.core.reindenter.Reindenter.Strategy;
+import org.eclipse.photran.internal.core.sourceform.SourceForm;
+
+/**
+ * The Reindent action in the Fortran editor's context menu.
+ * 
+ * @author Esfar Huq
+ * @author Rui Wang
+ * @author Jeff Overbey
+ */
+public class ReindentAction extends FortranEditorASTActionDelegate
+{
+    public void run(IProgressMonitor progressMonitor) throws InvocationTargetException, InterruptedException
+    {
+        try
+        {
+            ITextSelection selection = getFortranEditor().getSelection();
+
+            String sourceCode = reindent(selection);
+
+            setTextInEditor(sourceCode);
+            
+            setEditorSelectionTo(selection, sourceCode);
+        }
+        catch (Exception e)
+        {
+            String message = e.getMessage();
+            if (message == null) message = e.getClass().getName();
+            MessageDialog.openError(getFortranEditor().getShell(), "Error", message); //$NON-NLS-1$
+        }
+    }
+
+    private String reindent(ITextSelection selection) throws IOException, LexerException, SyntaxException
+    {
+        IFortranAST ast = parseTextInEditor();
+
+        if (selection.getLength() == 0)
+            Reindenter.reindent(ast.getRoot(), ast, Strategy.REINDENT_EACH_LINE);
+        else
+            Reindenter.reindent(selection.getStartLine()+1, selection.getEndLine()+1, ast, Strategy.REINDENT_EACH_LINE);
+
+        return SourcePrinter.getSourceCodeFromAST(ast);
+    }
+
+    private IFortranAST parseTextInEditor() throws IOException, LexerException, SyntaxException
+    {
+        IFile ifile = getFortranEditor().getIFile();
+        String filename = ifile != null ? ifile.getName() : "new_file.f90"; //$NON-NLS-1$
+        IAccumulatingLexer lexer = new ASTLexerFactory().createLexer(
+            new StringReader(getDocument().get()),
+            ifile,
+            filename,
+            SourceForm.of(ifile));
+        ASTExecutableProgramNode astRoot = new Parser().parse(lexer);
+        return new FortranAST(ifile, astRoot, new TokenList(astRoot));
+    }
+
+    private IDocument getDocument()
+    {
+        return getFortranEditor().getDocumentProvider().getDocument(getFortranEditor().getEditorInput());
+    }
+
+    private void setTextInEditor(String sourceCode)
+    {
+        getDocument().set(sourceCode);
+    }
+
+    private void setEditorSelectionTo(ITextSelection selection, String sourceCode)
+    {
+//        int newStartOffset = Math.min(selection.getOffset(), sourceCode.length()-1);
+//        int newEndOffset = Math.min(newStartOffset+selection.getLength(), sourceCode.length()-1);
+//        getFortranEditor().selectAndReveal(newStartOffset, newEndOffset-newStartOffset);
+
+        int newOffset = Math.min(selection.getOffset(), sourceCode.length()-1);
+        getFortranEditor().selectAndReveal(newOffset, 0);
+    }
+}
