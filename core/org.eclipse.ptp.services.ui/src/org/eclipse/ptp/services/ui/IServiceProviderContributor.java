d8df2ad590464106a195a6f87350dc4007732f96
gwatson <gwatson>
2010-06-30 15:26:44 +0000

    Missing @since tag.

--- core/org.eclipse.ptp.services.ui/src/org/eclipse/ptp/services/ui/IServiceProviderContributor.java
+++ core/org.eclipse.ptp.services.ui/src/org/eclipse/ptp/services/ui/IServiceProviderContributor.java
@@ -18,41 +18,49 @@ import org.eclipse.ptp.services.core.IServiceProviderWorkingCopy;
 import org.eclipse.swt.widgets.Composite;
 
 /**
- * Interface that supplies a UI which can be launched to configure a service provider.
- *
- * <strong>EXPERIMENTAL</strong>. This class or interface has been added as
- * part of a work in progress. There is no guarantee that this API will work or
- * that it will remain the same. Please do not use this API without consulting
- * with the RDT team.
+ * Interface that supplies a UI which can be launched to configure a service
+ * provider.
+ * 
+ * <strong>EXPERIMENTAL</strong>. This class or interface has been added as part
+ * of a work in progress. There is no guarantee that this API will work or that
+ * it will remain the same. Please do not use this API without consulting with
+ * the RDT team.
  * 
  * @see IServiceProvider
  */
 public interface IServiceProviderContributor {
 
-
 	/**
-	 * Creates a section on the service model properties page to configure the service provider working copy.
-	 * The composite does not have a layout manager attached.
+	 * Creates a section on the service model properties page to configure the
+	 * service provider working copy. The composite does not have a layout
+	 * manager attached.
 	 * 
-	 * @param provider working copy
+	 * @param provider
+	 *            working copy
 	 * @param composite
+	 * @since 2.0
 	 */
 	public void configureServiceProvider(IServiceProviderWorkingCopy provider, Composite composite);
-	
+
 	/**
 	 * Retrieve the wizard pages for a service provider
 	 * 
-	 * @param wizard the wizard that will display the pages
-	 * @param provider the provider being configured
+	 * @param wizard
+	 *            the wizard that will display the pages
+	 * @param provider
+	 *            the provider being configured
 	 * @return wizard pages for the provider
 	 */
 	public WizardPage[] getWizardPages(IWizard wizard, IServiceProvider provider);
-	
+
 	/**
 	 * Retrieve the wizard for a service provider
 	 * 
-	 * @param provider the provider being configured
-	 * @param page next ServiceConfigurationWizardPage if we are being called by a ServiceConfigurationWizard, or null otherwise
+	 * @param provider
+	 *            the provider being configured
+	 * @param page
+	 *            next ServiceConfigurationWizardPage if we are being called by
+	 *            a ServiceConfigurationWizard, or null otherwise
 	 * @return the wizard to configure this provider
 	 */
 	public IWizard getWizard(IServiceProvider provider, IWizardPage page);
