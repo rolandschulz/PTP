a71609f3aed45bb63376c00d832b3bafa917de48
gwatson <gwatson>
2010-06-30 17:01:01 +0000

    Cancel ExecutionObserver if connection terminated. See bug 316388.

--- tools/org.eclipse.ptp.remotetools.core/src/org/eclipse/ptp/remotetools/internal/ssh/ExecutionObserver.java
+++ tools/org.eclipse.ptp.remotetools.core/src/org/eclipse/ptp/remotetools/internal/ssh/ExecutionObserver.java
@@ -11,8 +11,8 @@
  *****************************************************************************/
 package org.eclipse.ptp.remotetools.internal.ssh;
 
-import java.util.Iterator;
-import java.util.Map.Entry;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.ptp.remotetools.core.messages.Messages;
 
@@ -36,6 +36,30 @@ public class ExecutionObserver extends Thread {
 
 	public void cancel() {
 		fCanceled = true;
+		synchronized (this) {
+			notify();
+		}
+	}
+
+	/**
+	 * Check for executions and notify appropriately.
+	 * 
+	 * @param cancel
+	 *            if true, cancel all running executions, otherwise notify any
+	 *            finished executions
+	 */
+	private void checkAndNotify(boolean cancel) {
+		List<KillableExecution> execs;
+		synchronized (fConnection.getActiveProcessTable()) {
+			execs = new ArrayList<KillableExecution>(fConnection.getActiveProcessTable().values());
+		}
+		for (KillableExecution exec : execs) {
+			if (cancel && exec.isRunning()) {
+				exec.notifyCancel();
+			} else if (!cancel && !exec.isRunning()) {
+				exec.notifyFinish();
+			}
+		}
 	}
 
 	/**
@@ -44,38 +68,16 @@ public class ExecutionObserver extends Thread {
 	 */
 	@Override
 	public void run() {
-		while (!fCanceled) {
-			KillableExecution finished = null;
-			if (fConnection.isConnected() && fConnection.getActiveProcessTable() != null) {
-				synchronized (fConnection) {
-					Iterator<Entry<Integer, KillableExecution>> iterator = fConnection.getActiveProcessTable().entrySet()
-							.iterator();
-
-					while (iterator.hasNext()) {
-						Entry<Integer, KillableExecution> entry = iterator.next();
-
-						if (!entry.getValue().isRunning()) {
-							finished = entry.getValue();
-							break;
-						}
-					}
+		while (!fCanceled && fConnection.isConnected()) {
+			checkAndNotify(false);
+			synchronized (this) {
+				try {
+					wait(100);
+				} catch (InterruptedException e) {
+					// Safe to ignore
 				}
 			}
-			if (finished != null) {
-				// Notify threads that were
-				finished.notifyFinish();
-				finished = null;
-			} else {
-				synchronized (fConnection) {
-					try {
-						fConnection.wait(100);
-					} catch (InterruptedException e) {
-						// Safe to ignore
-					}
-				}
-			}
-
 		}
+		checkAndNotify(true);
 	}
-
 }
