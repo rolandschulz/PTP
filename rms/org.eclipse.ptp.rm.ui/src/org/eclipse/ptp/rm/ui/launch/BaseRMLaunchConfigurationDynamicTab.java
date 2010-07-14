46d291c0faf5b9d7d86b36d11dac779bf9cc8d6a
arossi <arossi>
2010-07-14 17:58:57 +0000

    Bug fix 319750.  BaseRMLaunchConfigurationDynamicTab now extends AbstractRMLaunchConfigurationDynamicTab.
    NOTE: it was not necessary to change the API for this refactoring.

--- rms/org.eclipse.ptp.rm.ui/src/org/eclipse/ptp/rm/ui/launch/BaseRMLaunchConfigurationDynamicTab.java
+++ rms/org.eclipse.ptp.rm.ui/src/org/eclipse/ptp/rm/ui/launch/BaseRMLaunchConfigurationDynamicTab.java
@@ -7,90 +7,34 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Albert L. Rossi - made this class extend AbstractRMLaunchConfigurationDynamicTab
  *******************************************************************************/
 package org.eclipse.ptp.rm.ui.launch;
 
-import org.eclipse.core.runtime.ListenerList;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.ptp.core.elements.IPQueue;
 import org.eclipse.ptp.core.elements.IResourceManager;
-import org.eclipse.ptp.launch.ui.extensions.IRMLaunchConfigurationContentsChangedListener;
-import org.eclipse.ptp.launch.ui.extensions.IRMLaunchConfigurationDynamicTab;
+import org.eclipse.ptp.launch.ui.extensions.AbstractRMLaunchConfigurationDynamicTab;
 import org.eclipse.ptp.launch.ui.extensions.RMLaunchValidation;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Control;
 
-public abstract class BaseRMLaunchConfigurationDynamicTab implements
-IRMLaunchConfigurationDynamicTab {
-
-	private final ListenerList listenerList = new ListenerList();
-
+public abstract class BaseRMLaunchConfigurationDynamicTab extends AbstractRMLaunchConfigurationDynamicTab {
 	private final RMLaunchConfigurationDynamicTabDataSource dataSource = createDataSource();
 	private final RMLaunchConfigurationDynamicTabWidgetListener widgetListener = createListener();
 
-	/**
-	 * Create listener for the tab. The listener must extend {@link RMLaunchConfigurationDynamicTabWidgetListener} and add
-	 * specific behavior for widgets of the the tab.
-	 * @return the listener
-	 */
-	protected abstract RMLaunchConfigurationDynamicTabWidgetListener createListener();
-
-	/**
-	 * Create data source to handle tab content. The listener must extend {@link RMLaunchConfigurationDynamicTabDataSource} and add
-	 * specific behavior for widgets of the the tab.
-	 * @return the listener
-	 */
-	protected abstract RMLaunchConfigurationDynamicTabDataSource createDataSource();
-
-	protected RMLaunchConfigurationDynamicTabWidgetListener getListener() {
-		return widgetListener;
-	}
-
-	protected RMLaunchConfigurationDynamicTabDataSource getDataSource() {
-		return dataSource;
-	}
-
-	public void addContentsChangedListener(IRMLaunchConfigurationContentsChangedListener listener) {
-		listenerList.add(listener);
-	}
-
-	public void removeContentsChangedListener(IRMLaunchConfigurationContentsChangedListener listener) {
-		listenerList.remove(listener);
-	}
-
-	/**
-	 * This should be called when GUI elements are modified by the user,
-	 * e.g. a Text widget should have its ModifyListener's
-	 * modifyText method set up to notify all of the contents
-	 * changed listeners.
-	 */
-	protected void fireContentsChanged() {
-		Object[] listeners = listenerList.getListeners();
-		for (Object listener : listeners) {
-			((IRMLaunchConfigurationContentsChangedListener) listener).handleContentsChanged(this);
-		}
-	}
-
-	public RMLaunchValidation canSave(Control control,
-			IResourceManager rm, IPQueue queue) {
+	public RMLaunchValidation canSave(Control control, IResourceManager rm, IPQueue queue) {
 		if (dataSource.canSave())
 			return new RMLaunchValidation(true, null);
-		else
-			return new RMLaunchValidation(false, dataSource.getErrorMessage());
+		return new RMLaunchValidation(false, dataSource.getErrorMessage());
 	}
 
-	public RMLaunchValidation isValid(ILaunchConfiguration launchConfig,
-			IResourceManager rm, IPQueue queue) {
-		if (dataSource.canAccept())
-			return new RMLaunchValidation(true, null);
-		else
-			return new RMLaunchValidation(false, dataSource.getErrorMessage());
-	}
+	public abstract Image getImage();
 
-	public RMLaunchValidation initializeFrom(Control control,
-			IResourceManager rm, IPQueue queue,
-			ILaunchConfiguration configuration) {
+	public abstract String getText();
+
+	public RMLaunchValidation initializeFrom(Control control, IResourceManager rm, IPQueue queue, ILaunchConfiguration configuration) {
 		dataSource.setResourceManager(rm);
 		dataSource.setQueue(queue);
 		dataSource.setConfiguration(configuration);
@@ -99,40 +43,62 @@ IRMLaunchConfigurationDynamicTab {
 		widgetListener.enable();
 		if (dataSource.getErrorMessage() == null)
 			return new RMLaunchValidation(true, null);
-		else
-			return new RMLaunchValidation(false, dataSource.getErrorMessage());
+		return new RMLaunchValidation(false, dataSource.getErrorMessage());
 	}
 
-	public RMLaunchValidation performApply(
-			ILaunchConfigurationWorkingCopy configuration, IResourceManager rm,
-			IPQueue queue) {
+	public RMLaunchValidation isValid(ILaunchConfiguration launchConfig, IResourceManager rm, IPQueue queue) {
+		if (dataSource.canAccept())
+			return new RMLaunchValidation(true, null);
+		return new RMLaunchValidation(false, dataSource.getErrorMessage());
+	}
+
+	public RMLaunchValidation performApply(ILaunchConfigurationWorkingCopy configuration, IResourceManager rm, IPQueue queue) {
 		dataSource.setResourceManager(rm);
 		dataSource.setQueue(queue);
 		dataSource.setConfigurationWorkingCopy(configuration);
 		dataSource.storeAndValidate();
 		if (dataSource.getErrorMessage() == null)
 			return new RMLaunchValidation(true, null);
-		else
-			return new RMLaunchValidation(false, dataSource.getErrorMessage());
+		return new RMLaunchValidation(false, dataSource.getErrorMessage());
 	}
 
-	//	public RMLaunchValidation setDefaults(
-	//			ILaunchConfigurationWorkingCopy configuration, IResourceManager rm,
-	//			IPQueue queue) {
-	//		dataSource.setResourceManager(rm);
-	//		dataSource.setQueue(queue);
-	//		dataSource.setConfigurationWorkingCopy(configuration);
-	//		dataSource.storeDefaults();
-	//		if (dataSource.getErrorMessage() == null) {
-	//			return new RMLaunchValidation(true, null);
-	//		} else {
-	//			return new RMLaunchValidation(false, dataSource.getErrorMessage());
-	//		}
-	//	}
-
-	public abstract Image getImage();
-
-	public abstract String getText();
-
 	abstract public void updateControls();
+
+	/**
+	 * Create data source to handle tab content. The listener must extend
+	 * {@link RMLaunchConfigurationDynamicTabDataSource} and add specific
+	 * behavior for widgets of the the tab.
+	 * 
+	 * @return the listener
+	 */
+	protected abstract RMLaunchConfigurationDynamicTabDataSource createDataSource();
+
+	/**
+	 * Create listener for the tab. The listener must extend
+	 * {@link RMLaunchConfigurationDynamicTabWidgetListener} and add specific
+	 * behavior for widgets of the the tab.
+	 * 
+	 * @return the listener
+	 */
+	protected abstract RMLaunchConfigurationDynamicTabWidgetListener createListener();
+
+	/*
+	 * (non-Javadoc) Avoid API change.
+	 * 
+	 * @see
+	 * org.eclipse.ptp.launch.ui.extensions.AbstractRMLaunchConfigurationDynamicTab
+	 * #fireContentsChanged()
+	 */
+	@Override
+	protected void fireContentsChanged() {
+		super.fireContentsChanged();
+	}
+
+	protected RMLaunchConfigurationDynamicTabDataSource getDataSource() {
+		return dataSource;
+	}
+
+	protected RMLaunchConfigurationDynamicTabWidgetListener getListener() {
+		return widgetListener;
+	}
 }
