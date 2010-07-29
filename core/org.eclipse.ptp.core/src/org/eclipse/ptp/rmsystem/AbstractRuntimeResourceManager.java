9484771043a66520642600a102ca16862525bb82
Roland Schulz <roland@utk.edu>
2010-07-29 23:56:16 +0000

    fixed (uncritical) warning

--- core/org.eclipse.ptp.core/src/org/eclipse/ptp/rmsystem/AbstractRuntimeResourceManager.java
+++ core/org.eclipse.ptp.core/src/org/eclipse/ptp/rmsystem/AbstractRuntimeResourceManager.java
@@ -1,15 +1,15 @@
 /*******************************************************************************
- * Copyright (c) 2006 The Regents of the University of California. 
- * This material was produced under U.S. Government contract W-7405-ENG-36 
- * for Los Alamos National Laboratory, which is operated by the University 
- * of California for the U.S. Department of Energy. The U.S. Government has 
- * rights to use, reproduce, and distribute this software. NEITHER THE 
- * GOVERNMENT NOR THE UNIVERSITY MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR 
- * ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE. If software is modified 
- * to produce derivative works, such modified software should be clearly marked, 
+ * Copyright (c) 2006 The Regents of the University of California.
+ * This material was produced under U.S. Government contract W-7405-ENG-36
+ * for Los Alamos National Laboratory, which is operated by the University
+ * of California for the U.S. Department of Energy. The U.S. Government has
+ * rights to use, reproduce, and distribute this software. NEITHER THE
+ * GOVERNMENT NOR THE UNIVERSITY MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR
+ * ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE. If software is modified
+ * to produce derivative works, such modified software should be clearly marked,
  * so as not to confuse it with the version available from LANL.
  * 
- * Additionally, this program and the accompanying materials 
+ * Additionally, this program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
@@ -397,8 +397,7 @@ public abstract class AbstractRuntimeResourceManager extends AbstractResourceMan
 	 * org.eclipse.core.runtime.IProgressMonitor)
 	 */
 	@Override
-	protected IPJob doSubmitJob(String subId, ILaunchConfiguration configuration, AttributeManager attrMgr, IProgressMonitor monitor)
-			throws CoreException {
+	protected IPJob doSubmitJob(String subId, ILaunchConfiguration configuration, AttributeManager attrMgr, IProgressMonitor monitor) throws CoreException {
 		if (monitor == null) {
 			monitor = new NullProgressMonitor();
 		}
@@ -694,6 +693,8 @@ public abstract class AbstractRuntimeResourceManager extends AbstractResourceMan
 			severity = IStatus.INFO;
 			break;
 		case FATAL: /* should this map to cancel instead? */
+		case ERROR:
+		case UNDEFINED:
 			severity = IStatus.ERROR;
 			break;
 		case WARNING:
