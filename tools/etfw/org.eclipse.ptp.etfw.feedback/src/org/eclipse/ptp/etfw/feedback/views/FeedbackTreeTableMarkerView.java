8bc3f30ca00649f9253a58aec06c0bcfdfdb5666
Beth Tibbitts <tibbitts@us.ibm.com>
2010-08-04 17:18:53 +0000

    comments only

--- tools/etfw/org.eclipse.ptp.etfw.feedback/src/org/eclipse/ptp/etfw/feedback/views/FeedbackTreeTableMarkerView.java
+++ tools/etfw/org.eclipse.ptp.etfw.feedback/src/org/eclipse/ptp/etfw/feedback/views/FeedbackTreeTableMarkerView.java
@@ -34,6 +34,11 @@ public class FeedbackTreeTableMarkerView extends SimpleTreeTableMarkerView {
 	 */
 	private static boolean CREATE_PARENT_NODES=false;// Hack for hpcst 10/30/09
  
+	/**
+	 * This list tells the view which marker attributes map to each column in the view.
+	 * These are the default values if we don't get one in the constructor, but I'm not sure
+	 * this works to use these. 
+	 */
 	private static String[] attrNames=new String[] {
 		FeedbackIDs.FEEDBACK_ATTR_NAME,
 		FeedbackIDs.FEEDBACK_ATTR_FILENAME,
