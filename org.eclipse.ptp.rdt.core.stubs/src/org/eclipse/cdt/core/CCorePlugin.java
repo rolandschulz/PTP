d07bd677627f75043c77950e484274ac545f8536
vkong <vkong>
2010-08-11 16:41:38 +0000

    Bug 322264 -  stub classes need updates for RDT server for John Liu

--- org.eclipse.ptp.rdt.core.stubs/src/org/eclipse/cdt/core/CCorePlugin.java
+++ org.eclipse.ptp.rdt.core.stubs/src/org/eclipse/cdt/core/CCorePlugin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -17,6 +17,15 @@ import org.eclipse.core.runtime.Status;
 
 
 public class CCorePlugin {
+	
+	public static final int STATUS_CDTPROJECT_EXISTS = 1;
+	public static final int STATUS_CDTPROJECT_MISMATCH = 2;
+	public static final int CDT_PROJECT_NATURE_ID_MISMATCH = 3;
+	/**
+	 * Status code for core exception that is thrown if a pdom grew larger than the supported limit.
+	 * @since 5.2
+	 */
+	public static final int STATUS_PDOM_TOO_LARGE = 4;
 
 	public static final String PLUGIN_ID = "org.eclipse.cdt.core"; //$NON-NLS-1$
 	
@@ -38,7 +47,7 @@ public class CCorePlugin {
 	}
 	
 	public static IStatus createStatus(String msg, Throwable e) {
-		return new Status(IStatus.ERROR, PLUGIN_ID, IStatus.ERROR, msg, e);
+		return new Status(IStatus.ERROR, PLUGIN_ID, msg, e);
 	}
 	
 	public static String getResourceString(String key) {
