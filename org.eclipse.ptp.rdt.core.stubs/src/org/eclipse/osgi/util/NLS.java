113d3d093aedddd3ecd04d0cf4a90228a6db6160
vkong <vkong>
2010-08-11 16:41:38 +0000

    Bug 322264 -  stub classes need updates for RDT server for John Liu

--- org.eclipse.ptp.rdt.core.stubs/src/org/eclipse/osgi/util/NLS.java
+++ org.eclipse.ptp.rdt.core.stubs/src/org/eclipse/osgi/util/NLS.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 IBM Corporation and others.
+ * Copyright (c) 2008, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,15 +14,15 @@ package org.eclipse.osgi.util;
 public class NLS {
 
 	public static String bind(String message, Object binding) {
-		return null;
+		return "";
 	}
 	
 	public static String bind(String message, Object binding1, Object binding2) {
-		return null;
+		return "";
 	}
 	
 	public static String bind(String message, Object[] bindings) {
-		return null;
+		return "";
 	}
 	
 	public static void initializeMessages(final String bundleName, final Class clazz) {
