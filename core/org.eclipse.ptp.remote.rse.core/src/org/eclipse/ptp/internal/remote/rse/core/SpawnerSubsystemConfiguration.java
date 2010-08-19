c30c4a1b4eb6178f287a2f0b34a966489e8dd2af
gwatson <gwatson>
2010-08-19 18:48:38 +0000

    Remove illegal implementations

--- core/org.eclipse.ptp.remote.rse.core/src/org/eclipse/ptp/internal/remote/rse/core/SpawnerSubsystemConfiguration.java
+++ core/org.eclipse.ptp.remote.rse.core/src/org/eclipse/ptp/internal/remote/rse/core/SpawnerSubsystemConfiguration.java
@@ -14,29 +14,30 @@ import org.eclipse.rse.connectorservice.dstore.DStoreConnectorServiceManager;
 import org.eclipse.rse.core.model.IHost;
 import org.eclipse.rse.core.subsystems.IConnectorService;
 import org.eclipse.rse.core.subsystems.ISubSystem;
-import org.eclipse.rse.core.subsystems.ISubSystemConfiguration;
 import org.eclipse.rse.core.subsystems.SubSystemConfiguration;
 import org.eclipse.rse.services.dstore.IDStoreService;
 
 /**
  * @author crecoskie
- *
+ * 
  */
-public class SpawnerSubsystemConfiguration extends SubSystemConfiguration
-		implements ISubSystemConfiguration {
+public class SpawnerSubsystemConfiguration extends SubSystemConfiguration {
 
 	public SpawnerSubsystemConfiguration() {
 		// TODO Auto-generated constructor stub
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.rse.core.subsystems.SubSystemConfiguration#createSubSystemInternal(org.eclipse.rse.core.model.IHost)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.rse.core.subsystems.SubSystemConfiguration#
+	 * createSubSystemInternal(org.eclipse.rse.core.model.IHost)
 	 */
 	@Override
 	public ISubSystem createSubSystemInternal(IHost conn) {
 		return new SpawnerSubsystem(conn, getConnectorService(conn));
 	}
-	
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -49,19 +50,24 @@ public class SpawnerSubsystemConfiguration extends SubSystemConfiguration
 		return DStoreConnectorServiceManager.getInstance().getConnectorService(host, getServiceImplType());
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.rse.core.subsystems.SubSystemConfiguration#setConnectorService(org.eclipse.rse.core.model.IHost, org.eclipse.rse.core.subsystems.IConnectorService)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.rse.core.subsystems.SubSystemConfiguration#setConnectorService
+	 * (org.eclipse.rse.core.model.IHost,
+	 * org.eclipse.rse.core.subsystems.IConnectorService)
 	 */
 	@Override
 	public void setConnectorService(IHost host, IConnectorService connectorService) {
 		DStoreConnectorServiceManager.getInstance().setConnectorService(host, getServiceImplType(), connectorService);
-        ISubSystem[] sses = getSubSystems(host, false);
-        if (sses != null && sses.length > 0){
-            for (int i = 0; i < sses.length; i++){
-                ISubSystem ss = sses[i];
-                ss.setConnectorService(connectorService);
-            }
-        }
+		ISubSystem[] sses = getSubSystems(host, false);
+		if (sses != null && sses.length > 0) {
+			for (int i = 0; i < sses.length; i++) {
+				ISubSystem ss = sses[i];
+				ss.setConnectorService(connectorService);
+			}
+		}
 	}
 
 	/*
