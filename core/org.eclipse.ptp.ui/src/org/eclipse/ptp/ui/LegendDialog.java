4111100cae7a5b243fbcb9c9734cb57c2b81d85d
Beth Tibbitts <tibbitts@us.ibm.com>
2010-09-20 17:37:25 +0000

    fix incorrect message, rename others so can tell what's what, remove unused messages. Fixes Bug 325767

--- core/org.eclipse.ptp.ui/src/org/eclipse/ptp/ui/LegendDialog.java
+++ core/org.eclipse.ptp.ui/src/org/eclipse/ptp/ui/LegendDialog.java
@@ -39,43 +39,43 @@ import org.eclipse.swt.widgets.Shell;
 public class LegendDialog extends Dialog 
 {
 	private String rmStateText[] = {
-		Messages.LegendDialog_0,
-		Messages.LegendDialog_1,
-		Messages.LegendDialog_2,
-		Messages.LegendDialog_3,
-		Messages.LegendDialog_4,
-		Messages.LegendDialog_5
+		Messages.LegendDialog_Starting,
+		Messages.LegendDialog_Started,
+		Messages.LegendDialog_Stopping,
+		Messages.LegendDialog_Stopped,
+		Messages.LegendDialog_Suspended,
+		Messages.LegendDialog_ErrorRM
 	};
 	
 	private String machineStateText[] = {
-		Messages.LegendDialog_6,
-		Messages.LegendDialog_7,
-		Messages.LegendDialog_8,
-		Messages.LegendDialog_9,
-		Messages.LegendDialog_10
+		Messages.LegendDialog_UpM,
+		Messages.LegendDialog_DownM,
+		Messages.LegendDialog_AlertM,
+		Messages.LegendDialog_ErrorM,
+		Messages.LegendDialog_UnknownM
 	};
 
 	private String jobStateText[] = {
-		Messages.LegendDialog_11,
-		Messages.LegendDialog_12,
-		Messages.LegendDialog_13,
-		Messages.LegendDialog_14,
+		Messages.LegendDialog_StartedJ,
+		Messages.LegendDialog_RunningJ,
+		Messages.LegendDialog_SuspendedJ,
+		Messages.LegendDialog_CompletedJ,
 	};
 	
 	private String nodeStateText[] = {
-		Messages.LegendDialog_15,
-		Messages.LegendDialog_16,
-		Messages.LegendDialog_17,
-		Messages.LegendDialog_18,
-		Messages.LegendDialog_19,
-		Messages.LegendDialog_20,
+		Messages.LegendDialog_Up,
+		Messages.LegendDialog_Down,
+		Messages.LegendDialog_Error,
+		Messages.LegendDialog_Unknown,
+		Messages.LegendDialog_ProcessRunning,
+		Messages.LegendDialog_ProcessTerminated,
 	};
 	
 	private String processStateText[] = {
-		Messages.LegendDialog_21,
-		Messages.LegendDialog_22,
-		Messages.LegendDialog_23,
-		Messages.LegendDialog_24,
+		Messages.LegendDialog_StartedP,
+		Messages.LegendDialog_RunningP,
+		Messages.LegendDialog_SuspendedP,
+		Messages.LegendDialog_CompletedP,
 	};
 	
 	/**
@@ -88,7 +88,7 @@ public class LegendDialog extends Dialog
 	
 	public LegendDialog(Shell parent, int style) {
 		super(parent, style);
-		setText(Messages.LegendDialog_25);
+		setText(Messages.LegendDialog_LegendTitle);
 	}
 	
 	public String open() {
@@ -113,7 +113,7 @@ public class LegendDialog extends Dialog
 		
 		/* the RM box */
 		Group box = new Group(shell, SWT.BORDER);
-		box.setText(Messages.LegendDialog_26);
+		box.setText(Messages.LegendDialog_ResourceManagerIcons);
 		FillLayout fill = new FillLayout(SWT.VERTICAL);
 		fill.marginHeight = 5;
 		fill.marginWidth = 5;
@@ -144,7 +144,7 @@ public class LegendDialog extends Dialog
 		
 		/* the machines box */
 		box = new Group(shell, SWT.BORDER);
-		box.setText(Messages.LegendDialog_27);
+		box.setText(Messages.LegendDialog_MachineIcons);
 		fill = new FillLayout(SWT.VERTICAL);
 		fill.marginHeight = 5;
 		fill.marginWidth = 5;
@@ -173,7 +173,7 @@ public class LegendDialog extends Dialog
 		
 		/* the node box */
 		box = new Group(shell, SWT.BORDER);
-		box.setText(Messages.LegendDialog_28);
+		box.setText(Messages.LegendDialog_NodeIcons);
 		GridLayout g = new GridLayout(2, true);
 		box.setLayout(g);
 		data = new GridData(SWT.FILL, SWT.FILL, true, true);
@@ -202,7 +202,7 @@ public class LegendDialog extends Dialog
 		
 		/* the job box */
 		box = new Group(shell, SWT.BORDER);
-		box.setText(Messages.LegendDialog_29);
+		box.setText(Messages.LegendDialog_JobIcons);
 		fill = new FillLayout(SWT.VERTICAL);
 		fill.marginHeight = 5;
 		fill.marginWidth = 5;
@@ -231,7 +231,7 @@ public class LegendDialog extends Dialog
 				
 		/* the process box */
 		box = new Group(shell, SWT.BORDER);
-		box.setText(Messages.LegendDialog_30);
+		box.setText(Messages.LegendDialog_ProcessIcons);
 		fill = new FillLayout(SWT.VERTICAL);
 		fill.marginHeight = 5;
 		fill.marginWidth = 5;
@@ -259,7 +259,7 @@ public class LegendDialog extends Dialog
 		}
 		
         Button close = new Button(shell, SWT.PUSH);
-        close.setText(Messages.LegendDialog_31);
+        close.setText(Messages.LegendDialog_Close);
         data = new GridData(GridData.HORIZONTAL_ALIGN_CENTER);
         data.horizontalSpan = 2;
 		close.setLayoutData(data);
