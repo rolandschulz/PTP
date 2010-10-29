e194bbc452694dbb0da5a290d19449fd3d709085
gwatson <gwatson>
2010-10-29 20:47:56 +0000

    Added parallel debug locations view. See bug 323295.

---
+++ debug/org.eclipse.ptp.debug.core/src/org/eclipse/ptp/debug/core/IPLocationSetManager.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ptp.debug.core;
+
+import org.eclipse.ptp.debug.core.model.IPLocationSet;
+
+/**
+ * @since 5.0
+ */
+public interface IPLocationSetManager {
+
+	/**
+	 * @return
+	 */
+	public IPLocationSet[] getLocationSets();
+
+}
