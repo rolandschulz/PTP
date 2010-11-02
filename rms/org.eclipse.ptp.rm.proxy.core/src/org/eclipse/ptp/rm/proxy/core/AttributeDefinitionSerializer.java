d96426f022c27a5bbd6bd22fff6393b5c8a95df5
Greg Watson <g.watson@computer.org>
2010-11-02 15:36:27 +0000

    Fix imports and externalize strings. Still needs to have potential null pointer access fixed.

--- rms/org.eclipse.ptp.rm.proxy.core/src/org/eclipse/ptp/rm/proxy/core/AttributeDefinitionSerializer.java
+++ rms/org.eclipse.ptp.rm.proxy.core/src/org/eclipse/ptp/rm/proxy/core/AttributeDefinitionSerializer.java
@@ -1,21 +1,32 @@
- /*******************************************************************************
-  * Copyright (c) 2010 The University of Tennessee,
-  * All rights reserved. This program and the accompanying materials
-  * are made available under the terms of the Eclipse Public License v1.0
-  * which accompanies this distribution, and is available at
-  * http://www.eclipse.org/legal/epl-v10.html
-  *
-  * Contributors:
-  *    Benjamin Lindner (ben@benlabs.net) - initial implementation (bug 316671)
+/*******************************************************************************
+ * Copyright (c) 2010 The University of Tennessee,
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Benjamin Lindner (ben@benlabs.net) - initial implementation (bug 316671)
 
-  *******************************************************************************/
+ *******************************************************************************/
 
 package org.eclipse.ptp.rm.proxy.core;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.ptp.core.attributes.*;
+import org.eclipse.ptp.core.attributes.BooleanAttribute;
+import org.eclipse.ptp.core.attributes.BooleanAttributeDefinition;
+import org.eclipse.ptp.core.attributes.DateAttribute;
+import org.eclipse.ptp.core.attributes.DateAttributeDefinition;
+import org.eclipse.ptp.core.attributes.DoubleAttribute;
+import org.eclipse.ptp.core.attributes.DoubleAttributeDefinition;
+import org.eclipse.ptp.core.attributes.IAttributeDefinition;
+import org.eclipse.ptp.core.attributes.IllegalValueException;
+import org.eclipse.ptp.core.attributes.IntegerAttribute;
+import org.eclipse.ptp.core.attributes.IntegerAttributeDefinition;
+import org.eclipse.ptp.core.attributes.StringAttribute;
+import org.eclipse.ptp.core.attributes.StringAttributeDefinition;
 
 /**
  * @since 2.0
@@ -24,41 +35,41 @@ public class AttributeDefinitionSerializer {
 
 	private List<String> attribute_elements;
 
-	public AttributeDefinitionSerializer(IAttributeDefinition<?,?,?> attr) {
+	public AttributeDefinitionSerializer(IAttributeDefinition<?, ?, ?> attr) {
 		if (attr instanceof DateAttributeDefinition) {
-			create_from_date((DateAttributeDefinition)attr);
+			create_from_date((DateAttributeDefinition) attr);
 		}
 		if (attr instanceof BooleanAttributeDefinition) {
-			create_from_boolean((BooleanAttributeDefinition)attr);
+			create_from_boolean((BooleanAttributeDefinition) attr);
 		}
 		if (attr instanceof StringAttributeDefinition) {
-			create_from_string((StringAttributeDefinition)attr);
+			create_from_string((StringAttributeDefinition) attr);
 		}
 		if (attr instanceof IntegerAttributeDefinition) {
-			create_from_integer((IntegerAttributeDefinition)attr);
+			create_from_integer((IntegerAttributeDefinition) attr);
 		}
 		if (attr instanceof DoubleAttributeDefinition) {
-			create_from_double((DoubleAttributeDefinition)attr);
+			create_from_double((DoubleAttributeDefinition) attr);
 		}
-		
-		
+
 	}
-	
+
 	private void create_from_date(DateAttributeDefinition attr) {
 		attribute_elements = new ArrayList<String>();
-			
+
 		// set default attribute elements common to all
 		attribute_elements.add(attr.getId());
-		attribute_elements.add("DATE");
+		attribute_elements.add("DATE"); //$NON-NLS-1$
 		attribute_elements.add(attr.getName());
 		attribute_elements.add(attr.getDescription());
 		if (attr.getDisplay()) {
-			attribute_elements.add("TRUE");			
+			attribute_elements.add("TRUE"); //$NON-NLS-1$
 		} else {
-			attribute_elements.add("FALSE");			
+			attribute_elements.add("FALSE"); //$NON-NLS-1$
 		}
-		
-		// we need to use a "trick" to get the default value for the attribute definition:
+
+		// we need to use a "trick" to get the default value for the attribute
+		// definition:
 		DateAttribute tmpdate = null;
 		try {
 			tmpdate = attr.create();
@@ -67,76 +78,79 @@ public class AttributeDefinitionSerializer {
 			e.printStackTrace();
 		}
 		attribute_elements.add(tmpdate.toString());
-		
+
 		// extra element for the given data type:
-		
-		// broken: have to find out how date_style and time_style and locale are determined
-		// 
-		attribute_elements.add("0");
-		attribute_elements.add("0");
-		attribute_elements.add("0");
+
+		// broken: have to find out how date_style and time_style and locale are
+		// determined
+		//
+		attribute_elements.add("0"); //$NON-NLS-1$
+		attribute_elements.add("0"); //$NON-NLS-1$
+		attribute_elements.add("0"); //$NON-NLS-1$
 
 		attribute_elements.add(attr.getMinDate().toString());
 		attribute_elements.add(attr.getMaxDate().toString());
-		
-	}
 
+	}
 
 	private void create_from_string(StringAttributeDefinition attr) {
 		attribute_elements = new ArrayList<String>();
-		
+
 		// set default attribute elements common to all
 		attribute_elements.add(attr.getId());
-		attribute_elements.add("STRING");
+		attribute_elements.add("STRING"); //$NON-NLS-1$
 		attribute_elements.add(attr.getName());
 		attribute_elements.add(attr.getDescription());
 		if (attr.getDisplay()) {
-			attribute_elements.add("TRUE");			
+			attribute_elements.add("TRUE"); //$NON-NLS-1$
 		} else {
-			attribute_elements.add("FALSE");			
+			attribute_elements.add("FALSE"); //$NON-NLS-1$
 		}
 
-		// we need to use a "trick" to get the default value for the attribute definition:
+		// we need to use a "trick" to get the default value for the attribute
+		// definition:
 		StringAttribute tmpattr;
 		tmpattr = attr.create();
-		attribute_elements.add(tmpattr.toString());		
+		attribute_elements.add(tmpattr.toString());
 	}
-	
+
 	private void create_from_boolean(BooleanAttributeDefinition attr) {
 		attribute_elements = new ArrayList<String>();
-		
+
 		// set default attribute elements common to all
 		attribute_elements.add(attr.getId());
-		attribute_elements.add("BOOLEAN");
+		attribute_elements.add("BOOLEAN"); //$NON-NLS-1$
 		attribute_elements.add(attr.getName());
 		attribute_elements.add(attr.getDescription());
 		if (attr.getDisplay()) {
-			attribute_elements.add("TRUE");			
+			attribute_elements.add("TRUE"); //$NON-NLS-1$
 		} else {
-			attribute_elements.add("FALSE");			
+			attribute_elements.add("FALSE"); //$NON-NLS-1$
 		}
 
-		// we need to use a "trick" to get the default value for the attribute definition:
+		// we need to use a "trick" to get the default value for the attribute
+		// definition:
 		BooleanAttribute tmpattr;
 		tmpattr = attr.create();
-		attribute_elements.add(tmpattr.toString());		
-	}	
+		attribute_elements.add(tmpattr.toString());
+	}
 
 	private void create_from_integer(IntegerAttributeDefinition attr) {
 		attribute_elements = new ArrayList<String>();
-		
+
 		// set default attribute elements common to all
 		attribute_elements.add(attr.getId());
-		attribute_elements.add("INTEGER");
+		attribute_elements.add("INTEGER"); //$NON-NLS-1$
 		attribute_elements.add(attr.getName());
 		attribute_elements.add(attr.getDescription());
 		if (attr.getDisplay()) {
-			attribute_elements.add("TRUE");			
+			attribute_elements.add("TRUE"); //$NON-NLS-1$
 		} else {
-			attribute_elements.add("FALSE");			
+			attribute_elements.add("FALSE"); //$NON-NLS-1$
 		}
 
-		// we need to use a "trick" to get the default value for the attribute definition:
+		// we need to use a "trick" to get the default value for the attribute
+		// definition:
 		IntegerAttribute tmpattr = null;
 		try {
 			tmpattr = attr.create();
@@ -144,28 +158,29 @@ public class AttributeDefinitionSerializer {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		attribute_elements.add(tmpattr.toString());	
-		
+		attribute_elements.add(tmpattr.toString());
+
 		attribute_elements.add(attr.getMinValue().toString());
 		attribute_elements.add(attr.getMaxValue().toString());
-				
+
 	}
 
 	private void create_from_double(DoubleAttributeDefinition attr) {
 		attribute_elements = new ArrayList<String>();
-		
+
 		// set default attribute elements common to all
 		attribute_elements.add(attr.getId());
-		attribute_elements.add("DOUBLE");
+		attribute_elements.add("DOUBLE"); //$NON-NLS-1$
 		attribute_elements.add(attr.getName());
 		attribute_elements.add(attr.getDescription());
 		if (attr.getDisplay()) {
-			attribute_elements.add("TRUE");			
+			attribute_elements.add("TRUE"); //$NON-NLS-1$
 		} else {
-			attribute_elements.add("FALSE");			
+			attribute_elements.add("FALSE"); //$NON-NLS-1$
 		}
 
-		// we need to use a "trick" to get the default value for the attribute definition:
+		// we need to use a "trick" to get the default value for the attribute
+		// definition:
 		DoubleAttribute tmpattr = null;
 		try {
 			tmpattr = attr.create();
@@ -173,34 +188,34 @@ public class AttributeDefinitionSerializer {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		attribute_elements.add(tmpattr.toString());		
+		attribute_elements.add(tmpattr.toString());
 
 		attribute_elements.add(attr.getMinValue().toString());
 		attribute_elements.add(attr.getMaxValue().toString());
 	}
 
 	// yet to implement: arrays and enums
-	
+
 	public String str() {
 
-		String totalstring = " ";
-		
+		String totalstring = " "; //$NON-NLS-1$
+
 		Integer size = attribute_elements.size();
 		totalstring += size.toString();
-		
+
 		for (String ae : attribute_elements) {
-			totalstring += " \" \" " + ae;
+			totalstring += " \" \" " + ae; //$NON-NLS-1$
 		}
-		
+
 		return totalstring;
 	}
 
 	public String[] strList() {
 
 		List<String> tmplist = new ArrayList<String>();
-		
+
 		// number of attribute definitions:
-		tmplist.add("1");
+		tmplist.add("1"); //$NON-NLS-1$
 
 		Integer size = attribute_elements.size();
 		tmplist.add(size.toString());
@@ -208,7 +223,7 @@ public class AttributeDefinitionSerializer {
 		for (String ae : attribute_elements) {
 			tmplist.add(ae);
 		}
-		
+
 		return tmplist.toArray(new String[tmplist.size()]);
 	}
 
