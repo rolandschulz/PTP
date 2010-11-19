5aa799001c95364d1761e17622c198118e249d3c
crecoskie <crecoskie>
2010-11-19 18:27:19 +0000

    Bug 330604 - remoteMakeBuilder's post-build refresh should report progress and be cancelable

---
+++ org.eclipse.ptp.rdt.core/src/org/eclipse/ptp/internal/rdt/core/remotemake/Messages.java
@@ -0,0 +1,16 @@
+package org.eclipse.ptp.internal.rdt.core.remotemake;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.ptp.internal.rdt.core.remotemake.messages"; //$NON-NLS-1$
+	public static String ResourceRefreshJob_0;
+	public static String ResourceRefreshJob_1;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
