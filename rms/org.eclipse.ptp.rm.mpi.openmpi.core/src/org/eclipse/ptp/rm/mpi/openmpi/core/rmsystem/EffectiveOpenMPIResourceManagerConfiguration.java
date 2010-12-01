1e2a1112d462dece783c68a392c7f2871c445a9e
Greg Watson <g.watson@computer.org>
2010-12-01 16:37:53 +0000

    Fix handling of open mpi versions. See bug 331561.

--- rms/org.eclipse.ptp.rm.mpi.openmpi.core/src/org/eclipse/ptp/rm/mpi/openmpi/core/rmsystem/EffectiveOpenMPIResourceManagerConfiguration.java
+++ rms/org.eclipse.ptp.rm.mpi.openmpi.core/src/org/eclipse/ptp/rm/mpi/openmpi/core/rmsystem/EffectiveOpenMPIResourceManagerConfiguration.java
@@ -11,11 +11,8 @@
 package org.eclipse.ptp.rm.mpi.openmpi.core.rmsystem;
 
 import org.eclipse.core.runtime.Path;
-import org.eclipse.ptp.core.Preferences;
 import org.eclipse.ptp.rm.core.rmsystem.AbstractEffectiveToolRMConfiguration;
 import org.eclipse.ptp.rm.core.rmsystem.IToolRMConfiguration;
-import org.eclipse.ptp.rm.mpi.openmpi.core.OpenMPIPlugin;
-import org.eclipse.ptp.rm.mpi.openmpi.core.OpenMPIPreferenceManager;
 
 /**
  * Represents an Open MPI Resource Manager configuration.
@@ -30,54 +27,11 @@ import org.eclipse.ptp.rm.mpi.openmpi.core.OpenMPIPreferenceManager;
 public class EffectiveOpenMPIResourceManagerConfiguration extends AbstractEffectiveToolRMConfiguration {
 
 	private static final String BINDIR = "bin"; //$NON-NLS-1$
-	private static final String EMPTY_STR = ""; //$NON-NLS-1$
 
 	public EffectiveOpenMPIResourceManagerConfiguration(IToolRMConfiguration configuration) {
 		super(configuration);
 		IOpenMPIResourceManagerConfiguration conf = (IOpenMPIResourceManagerConfiguration) configuration;
-		String launchCmd = null;
-		String debugCmd = null;
-		String discoverCmd = null;
-		String remoteInstallPath = null;
-
-		if (conf.getUseToolDefaults()) {
-			String version = conf.getDetectedVersion();
-			if (version.equals(IOpenMPIResourceManagerConfiguration.VERSION_UNKNOWN)
-					&& conf.getVersionId().equals(IOpenMPIResourceManagerConfiguration.VERSION_AUTO)) {
-				discoverCmd = Preferences.getString(OpenMPIPlugin.getUniqueIdentifier(), OpenMPIPreferenceManager.PREFIX_AUTO
-						+ OpenMPIPreferenceManager.PREFS_DISCOVER_CMD);
-			} else if (version.equals(IOpenMPIResourceManagerConfiguration.VERSION_12)) {
-				launchCmd = Preferences.getString(OpenMPIPlugin.getUniqueIdentifier(), OpenMPIPreferenceManager.PREFIX_12
-						+ OpenMPIPreferenceManager.PREFS_LAUNCH_CMD);
-				debugCmd = Preferences.getString(OpenMPIPlugin.getUniqueIdentifier(), OpenMPIPreferenceManager.PREFIX_12
-						+ OpenMPIPreferenceManager.PREFS_DEBUG_CMD);
-				discoverCmd = Preferences.getString(OpenMPIPlugin.getUniqueIdentifier(), OpenMPIPreferenceManager.PREFIX_12
-						+ OpenMPIPreferenceManager.PREFS_DISCOVER_CMD);
-			} else if (version.equals(IOpenMPIResourceManagerConfiguration.VERSION_13)) {
-				launchCmd = Preferences.getString(OpenMPIPlugin.getUniqueIdentifier(), OpenMPIPreferenceManager.PREFIX_13
-						+ OpenMPIPreferenceManager.PREFS_LAUNCH_CMD);
-				debugCmd = Preferences.getString(OpenMPIPlugin.getUniqueIdentifier(), OpenMPIPreferenceManager.PREFIX_13
-						+ OpenMPIPreferenceManager.PREFS_DEBUG_CMD);
-				discoverCmd = Preferences.getString(OpenMPIPlugin.getUniqueIdentifier(), OpenMPIPreferenceManager.PREFIX_13
-						+ OpenMPIPreferenceManager.PREFS_DISCOVER_CMD);
-			} else if (version.equals(IOpenMPIResourceManagerConfiguration.VERSION_14)) {
-				launchCmd = Preferences.getString(OpenMPIPlugin.getUniqueIdentifier(), OpenMPIPreferenceManager.PREFIX_14
-						+ OpenMPIPreferenceManager.PREFS_LAUNCH_CMD);
-				debugCmd = Preferences.getString(OpenMPIPlugin.getUniqueIdentifier(), OpenMPIPreferenceManager.PREFIX_14
-						+ OpenMPIPreferenceManager.PREFS_DEBUG_CMD);
-				discoverCmd = Preferences.getString(OpenMPIPlugin.getUniqueIdentifier(), OpenMPIPreferenceManager.PREFIX_14
-						+ OpenMPIPreferenceManager.PREFS_DISCOVER_CMD);
-			} else {
-				assert false;
-			}
-		} else {
-			launchCmd = conf.getLaunchCmd();
-			debugCmd = conf.getDebugCmd();
-			discoverCmd = conf.getDiscoverCmd();
-		}
-
-		remoteInstallPath = conf.getRemoteInstallPath();
-		applyValues(launchCmd, debugCmd, discoverCmd, null, 0, null, remoteInstallPath);
+		applyValues(conf.getLaunchCmd(), conf.getDebugCmd(), conf.getDiscoverCmd(), null, 0, null, conf.getRemoteInstallPath());
 	}
 
 	/*
