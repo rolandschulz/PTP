2af3463f9f1cf02a67faa3f8ed9dea12d0057a62
gwatson <gwatson>
2010-12-14 23:18:21 +0000

    Additional tests.

--- core/org.eclipse.ptp.remote.core.tests/src/org/eclipse/ptp/core/tests/RemoteToolsTests.java
+++ core/org.eclipse.ptp.remote.core.tests/src/org/eclipse/ptp/core/tests/RemoteToolsTests.java
@@ -1,8 +1,10 @@
 package org.eclipse.ptp.core.tests;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 import junit.framework.TestCase;
 
@@ -23,8 +25,7 @@ import org.eclipse.ptp.remotetools.exception.RemoteConnectionException;
 import org.eclipse.ptp.remotetools.exception.RemoteExecutionException;
 
 public class RemoteToolsTests extends TestCase {
-	private static final String USERNAME = "user"; //$NON-NLS-1$
-	private static final String PASSWORD = "password"; //$NON-NLS-1$
+	private static final String USERNAME = "greg"; //$NON-NLS-1$
 	private static final String HOST = "localhost"; //$NON-NLS-1$
 
 	private ITargetControl fTargetControl;
@@ -33,8 +34,8 @@ public class RemoteToolsTests extends TestCase {
 	private TargetTypeElement fTarget;
 	private TargetElement fElement;
 
-	public void testCopy() {
-		for (int i = 0; i < 50; i++) {
+	public void testScriptExecute() {
+		for (int i = 0; i < 10; i++) {
 			IRemoteScript script = fExecutionTools.createScript();
 			script.setScript("echo hi there");
 			RemoteProcess p = null;
@@ -58,93 +59,126 @@ public class RemoteToolsTests extends TestCase {
 			} catch (IOException e) {
 				fail();
 			}
-			// try {
-			// p.waitFor();
-			// } catch (InterruptedException e) {
-			// }
+			try {
+				p.waitFor();
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+
+	public void testScriptIO() {
+
+		IRemoteScript script = fExecutionTools.createScript();
+		script.setScript("cat > /tmp/xxx.script");
+		RemoteProcess p = null;
+		try {
+			p = fExecutionTools.executeProcess(script);
+		} catch (RemoteExecutionException e) {
+			fail();
+		} catch (RemoteConnectionException e) {
+			fail();
+		} catch (CancelException e) {
+			fail();
+		}
+		assertNotNull(p);
+		BufferedWriter b = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
+		try {
+			b.write("this is a test\n");
+			b.write("this is a test2\n");
+			b.close();
+		} catch (IOException e) {
+			fail();
+		}
+		try {
+			p.waitFor();
+		} catch (InterruptedException e) {
 		}
 
-		// IRemoteScript script = fExecutionTools.createScript();
-		// script.setScript("cat > /tmp/xxx.script");
-		// RemoteProcess p = null;
-		// try {
-		// p = fExecutionTools.executeProcess(script);
-		// } catch (RemoteExecutionException e) {
-		// fail();
-		// } catch (RemoteConnectionException e) {
-		// fail();
-		// } catch (CancelException e) {
-		// fail();
-		// }
-		// assertNotNull(p);
-		// BufferedWriter b = new BufferedWriter(new
-		// OutputStreamWriter(p.getOutputStream()));
-		// try {
-		// b.write("this is a test\n");
-		// b.write("this is a test2\n");
-		// b.close();
-		// } catch (IOException e) {
-		// fail();
-		// }
-		// try {
-		// p.waitFor();
-		// } catch (InterruptedException e) {
-		// }
-		//
-		// script = fExecutionTools.createScript();
-		// script.setScript("cat >> /tmp/xxx.script");
-		// p = null;
-		// try {
-		// p = fExecutionTools.executeProcess(script);
-		// } catch (RemoteExecutionException e) {
-		// fail();
-		// } catch (RemoteConnectionException e) {
-		// fail();
-		// } catch (CancelException e) {
-		// fail();
-		// }
-		// assertNotNull(p);
-		// b = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
-		// try {
-		// b.write("this is a test4\n");
-		// b.write("this is a test5\n");
-		// b.close();
-		// } catch (IOException e) {
-		// fail();
-		// }
-		// try {
-		// p.waitFor();
-		// } catch (InterruptedException e) {
-		// }
-		//
-		// script = fExecutionTools.createScript();
-		// script.setScript("cat < /tmp/xxx.script");
-		// p = null;
-		// try {
-		// p = fExecutionTools.executeProcess(script);
-		// } catch (RemoteExecutionException e) {
-		// fail();
-		// } catch (RemoteConnectionException e) {
-		// fail();
-		// } catch (CancelException e) {
-		// fail();
-		// }
-		// assertNotNull(p);
-		// BufferedReader br = new BufferedReader(new
-		// InputStreamReader(p.getInputStream()));
-		// try {
-		// String line;
-		// while ((line = br.readLine()) != null) {
-		// System.out.println("read \""+line+"\"");
-		// }
-		// } catch (IOException e) {
-		// fail();
-		// }
-		// try {
-		// p.waitFor();
-		// } catch (InterruptedException e) {
-		// }
+		script = fExecutionTools.createScript();
+		script.setScript("cat >> /tmp/xxx.script");
+		p = null;
+		try {
+			p = fExecutionTools.executeProcess(script);
+		} catch (RemoteExecutionException e) {
+			fail();
+		} catch (RemoteConnectionException e) {
+			fail();
+		} catch (CancelException e) {
+			fail();
+		}
+		assertNotNull(p);
+		b = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
+		try {
+			b.write("this is a test4\n");
+			b.write("this is a test5\n");
+			b.close();
+		} catch (IOException e) {
+			fail();
+		}
+		try {
+			p.waitFor();
+		} catch (InterruptedException e) {
+		}
 
+		script = fExecutionTools.createScript();
+		script.setScript("cat < /tmp/xxx.script");
+		p = null;
+		try {
+			p = fExecutionTools.executeProcess(script);
+		} catch (RemoteExecutionException e) {
+			fail();
+		} catch (RemoteConnectionException e) {
+			fail();
+		} catch (CancelException e) {
+			fail();
+		}
+		assertNotNull(p);
+		BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		try {
+			String line;
+			int count = 0;
+			while ((line = br.readLine()) != null) {
+				System.out.println("read \"" + line + "\"");
+				count++;
+			}
+			assertTrue(count == 4);
+		} catch (IOException e) {
+			fail();
+		}
+		try {
+			p.waitFor();
+		} catch (InterruptedException e) {
+		}
+
+	}
+
+	public void testScriptCancel() {
+
+		IRemoteScript script = fExecutionTools.createScript();
+		script.setScript("while true; do sleep 1; done");
+		RemoteProcess p = null;
+		try {
+			p = fExecutionTools.executeProcess(script);
+		} catch (RemoteExecutionException e) {
+			fail(e.getLocalizedMessage());
+		} catch (RemoteConnectionException e) {
+			fail(e.getLocalizedMessage());
+		} catch (CancelException e) {
+			fail(e.getLocalizedMessage());
+		}
+		assertNotNull(p);
+		/*
+		 * Give script time to start
+		 */
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e1) {
+		}
+		p.destroy();
+		try {
+			p.waitFor();
+		} catch (InterruptedException e) {
+		}
 	}
 
 	/*
@@ -158,7 +192,6 @@ public class RemoteToolsTests extends TestCase {
 		ITargetConfig config = factory.createTargetConfig();
 		config.setConnectionAddress(HOST);
 		config.setLoginUsername(USERNAME);
-		config.setLoginPassword(PASSWORD);
 		config.setPasswordAuth(true);
 
 		fTarget = RemoteToolsServices.getTargetTypeElement();
