417a8478540132930c3e58cc9ab7cf50e0c1aef5
rschulz <rschulz>
2010-12-20 20:23:39 +0000

    Start work for remote sync (see: http://wiki.eclipse.org/PTP/designs/remote/sync) The only thing that works so far is adding a nature to an new project. It's all been cloned directly from the RDT code.

---
+++ org.eclipse.ptp.rdt.sync.ui/src/org/eclipse/ptp/rdt/sync/ui/wizards/NewRemoteProjectWizard.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ptp.rdt.sync.ui.wizards;
+
+import org.eclipse.cdt.core.CCProjectNature;
+import org.eclipse.cdt.core.CProjectNature;
+import org.eclipse.cdt.ui.wizards.CDTCommonProjectWizard;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.ptp.internal.rdt.sync.ui.RDTPluginImages;
+import org.eclipse.ptp.rdt.sync.core.resources.RemoteSyncNature;
+
+/**
+ * A wizard for creating new Remote C/C++ Projects
+ * <strong>EXPERIMENTAL</strong>. This class or interface has been added as part
+ * of a work in progress. There is no guarantee that this API will work or that
+ * it will remain the same. Please do not use this API without consulting with
+ * the RDT team.
+ * 
+ * @author crecoskie
+ * @since 2.0
+ * 
+ */
+public class NewRemoteProjectWizard extends CDTCommonProjectWizard {
+	private static final String wz_title = "New Remote Synchronized Project"; //$NON-NLS-1$
+	private static final String wz_desc = "Create remote project of the selected type"; //$NON-NLS-1$
+
+	/**
+	 * 
+	 */
+	public NewRemoteProjectWizard() {
+		super(wz_title, wz_desc);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.cdt.ui.wizards.CDTCommonProjectWizard#continueCreation(org
+	 * .eclipse.core.resources.IProject)
+	 */
+
+	@Override
+	protected IProject continueCreation(IProject prj) {
+		try {
+			CProjectNature.addCNature(prj, new NullProgressMonitor());
+			CCProjectNature.addCCNature(prj, new NullProgressMonitor());
+			RemoteSyncNature.addRemoteNature(prj, new NullProgressMonitor());
+		} catch (CoreException e) {
+		}
+		return prj;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.cdt.ui.wizards.CDTCommonProjectWizard#getNatures()
+	 */
+	@Override
+	public String[] getNatures() {
+		return new String[] { CProjectNature.C_NATURE_ID, CCProjectNature.CC_NATURE_ID, RemoteSyncNature.REMOTE_NATURE_ID };
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.wizards.newresource.BasicNewResourceWizard#
+	 * initializeDefaultPageImageDescriptor()
+	 */
+	@Override
+	protected void initializeDefaultPageImageDescriptor() {
+		setDefaultPageImageDescriptor(RDTPluginImages.DESC_WIZBAN_NEW_REMOTE_C_PROJ);
+	}
+
+}
