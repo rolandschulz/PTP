512991f15494295e4cafdefe8f47003ca6f51e98
Beth Tibbitts <tibbitts@us.ibm.com>
2011-01-25 00:53:23 +0000

    extension point mechanism for fragments to specify HTML dirs to override original location of HTML files

--- tools/pldt/org.eclipse.ptp.pldt.common/src/org/eclipse/ptp/pldt/common/editorHelp/CHelpResourceDescriptorImpl.java
+++ tools/pldt/org.eclipse.ptp.pldt.common/src/org/eclipse/ptp/pldt/common/editorHelp/CHelpResourceDescriptorImpl.java
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (c) 2005,2010IBM Corporation.
+ * Copyright (c) 2005,2010 IBM Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,57 +14,109 @@ package org.eclipse.ptp.pldt.common.editorHelp;
 import org.eclipse.cdt.ui.ICHelpBook;
 import org.eclipse.cdt.ui.ICHelpResourceDescriptor;
 import org.eclipse.cdt.ui.IFunctionSummary;
+import org.eclipse.cdt.ui.IFunctionSummary.IFunctionPrototypeSummary;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.help.IHelpResource;
+import org.eclipse.ptp.pldt.common.CommonPlugin;
+import org.osgi.framework.Bundle;
 
-public class CHelpResourceDescriptorImpl implements ICHelpResourceDescriptor
-{
-    ICHelpBook  book;
-    String        name;
-    String        label;
-    String        href;
-    IHelpResource [] resources;
+public class CHelpResourceDescriptorImpl implements ICHelpResourceDescriptor {
+	ICHelpBook book;
+	String name;
+	String label;
+	String href;
+	IHelpResource[] resources;
+	static boolean traceOn = false;
 
-    /**
-     * This is where the location of the html help file (to be displayed for an API) is located
-     * @param helpBook
-     * @param functionSummary
-     * @param pluginId
-     */
-    public CHelpResourceDescriptorImpl(ICHelpBook helpBook, IFunctionSummary functionSummary, String pluginId)
-    {
-        book = helpBook;
-        name = functionSummary.getName();
-        //href = "/"+pluginId + "/html/" + name + ".html";
-        StringBuffer buf=new StringBuffer();
-        buf.append("/").append(pluginId).append("/html/").append(name).append(".html"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-        href=buf.toString();
-        //System.out.println("looking for help file: "+href);
-        // href="file://c:/temp/foo.html";      
-        label = functionSummary.getPrototype().getPrototypeString(false);
-        resources = new IHelpResource[1];
-        resources[0] = new IHelpResource() {
-            public String getHref()
-            {
-                return href;
-            }
+	/**
+	 * This is where the location of the html help file (to be displayed for an
+	 * API) is located
+	 * 
+	 * @param helpBook
+	 * @param functionSummary
+	 * @param pluginId
+	 */
+	public CHelpResourceDescriptorImpl(ICHelpBook helpBook, IFunctionSummary functionSummary, String pluginId) {
+		book = helpBook;
+		name = functionSummary.getName();
+		// href = "/"+pluginId + "/html/" + name + ".html";
+		StringBuffer buf = new StringBuffer();
 
-            public String getLabel()
-            {
-                return label;
-            }
-        };
-    }
+		// Find where the html dir is located - could vary e.g. if a fragment provides "extra" info
+		String htmlLocn = findHTMLdir(pluginId);
 
-    public ICHelpBook getCHelpBook()
-    {
-        return book;
-    }
+		buf.append("/").append(pluginId).append("/").append(htmlLocn).append("/").append(name).append(".html"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		href = buf.toString();
+		// System.out.println("looking for help file: "+href);
+		IFunctionPrototypeSummary proto = functionSummary.getPrototype();
+		if (proto != null) {
+			label = functionSummary.getPrototype().getPrototypeString(false);
+		} else {
+			label = functionSummary.getName();
+		}
+		resources = new IHelpResource[1];
+		resources[0] = new IHelpResource() {
+			public String getHref() {
+				return href;
+			}
 
-    public IHelpResource[] getHelpResources()
-    {
-        return resources;
-    }
-    public String toString() {
-    	return name+" -> "+href; //$NON-NLS-1$
-    }
+			public String getLabel() {
+				if (label == null) {
+					return "NO SUCH LABEL BETH";
+				}
+				return label;
+			}
+		};
+	}
+
+	/**
+	 * Find the HTML directory that holds the html files that dynamic help will
+	 * use. Normally this is in the 'html' directory but if there is a fragment
+	 * plug-in that e.g. adds "extra" information for the html files (via an Eclipse extension), then we
+	 * want to use that one.
+	 * 
+	 * @param pluginId
+	 *            the plugin/bundle that we want the HTML directory for
+	 * @return the name of the directory to use for the HTML files
+	 */
+	private String findHTMLdir(String pluginId) {
+		String result = "html";
+		String pid = CommonPlugin.PLUGIN_ID;
+		String extid = "althelp";
+		if(traceOn)System.out.println("Looking for HTML dir for "+pluginId);
+		IExtension[] extensions = Platform.getExtensionRegistry().getExtensionPoint(pid, extid).getExtensions();
+		for (int i = 0; i < extensions.length; i++) {
+			IExtension extn = extensions[i];
+			String extLabel = extn.getLabel();
+			if (traceOn)
+				System.out.println("  Found extension for " + extLabel + "  id=" + extn.getUniqueIdentifier()); //$NON-NLS-1$ //$NON-NLS-2$
+			String uid = extn.getUniqueIdentifier();
+			if (uid.startsWith(pluginId)) {
+				IConfigurationElement[] configElements = extensions[i].getConfigurationElements();
+				for (int j = 0; j < configElements.length; j++) {
+					IConfigurationElement ice = configElements[j];
+					result = ice.getAttribute("dirname"); //$NON-NLS-1$
+					if(traceOn)System.out.println("    dirname=" + result); //$NON-NLS-1$
+					//we can stop looking now
+					return result;
+				}
+			}
+		}
+		// Didn't find anything better, so stick with what we had
+		return result;
+	}
+
+	public ICHelpBook getCHelpBook() {
+		return book;
+	}
+
+	public IHelpResource[] getHelpResources() {
+		return resources;
+	}
+
+	public String toString() {
+		return name + " -> " + href; //$NON-NLS-1$
+	}
 }
