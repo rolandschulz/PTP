65b3b11197d1c6a19af51f88646e58d88d286183
gwatson <gwatson>
2011-01-25 01:47:54 +0000

    Reformatted

--- debug/org.eclipse.ptp.debug.core/src/org/eclipse/ptp/debug/core/event/IPDebugEvent.java
+++ debug/org.eclipse.ptp.debug.core/src/org/eclipse/ptp/debug/core/event/IPDebugEvent.java
@@ -18,7 +18,6 @@
  *******************************************************************************/
 package org.eclipse.ptp.debug.core.event;
 
-
 /**
  * @author Clement
  */
@@ -26,111 +25,111 @@ public interface IPDebugEvent {
 	/**
 	 * Resume event kind.
 	 */
-	public static final int RESUME= 0x0001;
+	public static final int RESUME = 0x0001;
 
 	/**
 	 * Suspend event kind.
 	 */
-	public static final int SUSPEND= 0x0002;
+	public static final int SUSPEND = 0x0002;
 
 	/**
 	 * Create event kind.
 	 */
-	public static final int CREATE= 0x0004;
+	public static final int CREATE = 0x0004;
 
 	/**
 	 * Terminate event kind.
 	 */
-	public static final int TERMINATE= 0x0008;
-	
+	public static final int TERMINATE = 0x0008;
+
 	/**
 	 * Change event kind.
 	 */
-	public static final int CHANGE= 0x0010;
+	public static final int CHANGE = 0x0010;
 
 	/**
 	 * Error event kind or detail
 	 */
-	public static final int ERROR= 0x0040;
+	public static final int ERROR = 0x0040;
 
 	/**
 	 * Process specific event detail.
 	 * 
 	 * @since 2.1.2
 	 */
-	//public static final int MODEL_SPECIFIC= 0x0020;
-	public static final int PROCESS_SPECIFIC= 0x0020;
+	// public static final int MODEL_SPECIFIC= 0x0020;
+	public static final int PROCESS_SPECIFIC = 0x0020;
 
 	/**
-	 * Step start detail. Indicates a thread was resumed by a step
-	 * into action.
+	 * Step start detail. Indicates a thread was resumed by a step into action.
+	 * 
 	 * @since 2.0
 	 */
-	public static final int STEP_INTO= 0x0001;
-	
-	/**
-	 * Step start detail. Indicates a thread was resumed by a step
-	 * over action.
-	 * @since 2.0
-	 */
-	public static final int STEP_OVER= 0x0002;
-	
-	/**
-	 * Step start detail. Indicates a thread was resumed by a step
-	 * return action.
-	 * @since 2.0
-	 */
-	public static final int STEP_RETURN= 0x0004;
+	public static final int STEP_INTO = 0x0001;
 
 	/**
-	 * Step end detail. Indicates a thread was suspended due
-	 * to the completion of a step action.
+	 * Step start detail. Indicates a thread was resumed by a step over action.
+	 * 
+	 * @since 2.0
 	 */
-	public static final int STEP_END= 0x0008;
-	
+	public static final int STEP_OVER = 0x0002;
+
+	/**
+	 * Step start detail. Indicates a thread was resumed by a step return
+	 * action.
+	 * 
+	 * @since 2.0
+	 */
+	public static final int STEP_RETURN = 0x0004;
+
+	/**
+	 * Step end detail. Indicates a thread was suspended due to the completion
+	 * of a step action.
+	 */
+	public static final int STEP_END = 0x0008;
+
 	/**
 	 * Breakpoint detail. Indicates a thread was suspended by a breakpoint.
 	 */
-	public static final int BREAKPOINT= 0x0010;
-	
+	public static final int BREAKPOINT = 0x0010;
+
 	/**
-	 * Client request detail. Indicates a thread was suspended due to a client request.
+	 * Client request detail. Indicates a thread was suspended due to a client
+	 * request.
 	 */
-	public static final int CLIENT_REQUEST= 0x0020;
-	
+	public static final int CLIENT_REQUEST = 0x0020;
+
 	/**
-	 * Evaluation detail. Indicates that a thread was resumed or
-	 * suspended to perform an expression evaluation.
+	 * Evaluation detail. Indicates that a thread was resumed or suspended to
+	 * perform an expression evaluation.
 	 * 
 	 * @since 2.0
 	 */
 	public static final int EVALUATION = 0x0040;
-	
+
 	/**
-	 * Evaluation detail. Indicates that a thread was resumed or
-	 * suspended to perform an implicit expression evaluation.
-	 * An implicit evaluation is an evaluation that is performed
-	 * as an indirect result of a user action.
-	 * Clients may use this detail event to decide whether or not
-	 * to alert the user that an evaluation is taking place..
+	 * Evaluation detail. Indicates that a thread was resumed or suspended to
+	 * perform an implicit expression evaluation. An implicit evaluation is an
+	 * evaluation that is performed as an indirect result of a user action.
+	 * Clients may use this detail event to decide whether or not to alert the
+	 * user that an evaluation is taking place..
 	 * 
 	 * @since 2.0
 	 */
 	public static final int EVALUATION_IMPLICIT = 0x0080;
 
 	/**
-	 * State change detail. Indicates the state of a single 
-	 * debug element has changed. Only valid for <code>CHANGE</code>
-	 * events.
+	 * State change detail. Indicates the state of a single debug element has
+	 * changed. Only valid for <code>CHANGE</code> events.
 	 * 
 	 * @since 2.0
 	 */
 	public static final int STATE = 0x0100;
-	
+
 	/**
-	 * Content change detail. Indicates the content of a debug element
-	 * (and potentially its children) has changed. Only valid for
-	 * <code>CHANGE</code> events.
+	 * Content change detail. Indicates the content of a debug element (and
+	 * potentially its children) has changed. Only valid for <code>CHANGE</code>
+	 * events.
 	 * 
 	 * @since 2.0
 	 */
@@ -144,28 +143,50 @@ public interface IPDebugEvent {
 	public static final int DEBUGGER = 0x0400;
 
 	/**
-	 * Register detail. Indicates the registered process is created or terminated
+	 * Register detail. Indicates the registered process is created or
+	 * terminated
 	 * 
 	 * @since 2.0
 	 */
 	public static final int REGISTER = 0x0800;
-	
+
 	/**
-	 * Constant indicating that the kind or detail of a debug
-	 * event is unspecified.
+	 * Constant indicating that the kind or detail of a debug event is
+	 * unspecified.
 	 */
 	public static final int UNSPECIFIED = 0;
 
-	public static final int ERR_NORMAL= 0x0001;
-	public static final int ERR_WARNING= 0x0002;
-	public static final int ERR_FATAL= 0x0004;		
+	public static final int ERR_NORMAL = 0x0001;
+	public static final int ERR_WARNING = 0x0002;
+	public static final int ERR_FATAL = 0x0004;
 
-	int getKind();
-	int getDetail();
-	IPDebugInfo getInfo();
-	boolean isStepStart();
-	boolean isEvaluation(); 
-	
-	Object getSource();
+	/**
+	 * @return
+	 */
+	public int getDetail();
+
+	/**
+	 * @return
+	 */
+	public IPDebugInfo getInfo();
+
+	/**
+	 * @return
+	 */
+	public int getKind();
+
+	/**
+	 * @return
+	 */
+	public Object getSource();
+
+	/**
+	 * @return
+	 */
+	public boolean isEvaluation();
+
+	/**
+	 * @return
+	 */
+	public boolean isStepStart();
 }
-
