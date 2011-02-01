c6b1d612da82796e41fb8aa1f64fb83e2f64aed2
vkong <vkong>
2011-02-01 18:49:54 +0000

    Bug 335958 - Duplicate remote C/C++ search result

--- org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/internal/rdt/ui/search/RemoteSearchMatchAdapter.java
+++ org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/internal/rdt/ui/search/RemoteSearchMatchAdapter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2010 QNX Software Systems and others.
+ * Copyright (c) 2006, 2011 QNX Software Systems and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -43,6 +43,6 @@ public class RemoteSearchMatchAdapter extends Match {
 		RemoteSearchMatchAdapter other = (RemoteSearchMatchAdapter)obj;
 		return getElement().equals(other.getElement())
 			&& getOffset() == other.getOffset()
-			&& getLength() == other.getLength();
+			&& getLength() <= other.getLength();
 	}
 }
