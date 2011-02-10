a8b8c0cc12220fe2a64e2e3b28e0617eb74979e5
vkong <vkong>
2011-02-10 20:09:24 +0000

    Bug 336867 - Strings in Remote Service property page should be externalized for translation

--- rdt/org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/rdt/ui/wizards/RSECIndexServiceProviderContributer.java
+++ rdt/org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/rdt/ui/wizards/RSECIndexServiceProviderContributer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2010 IBM Corporation and others.
+ * Copyright (c) 2009, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -71,7 +71,7 @@ public class RSECIndexServiceProviderContributer implements IServiceProviderCont
 		container.setLayout(new GridLayout(1, false));
 
 		Group connectionGroup = new Group(container, SWT.NONE);
-		connectionGroup.setText("Connection"); //$NON-NLS-1$
+		connectionGroup.setText(Messages.getString("RSECIndexServiceProviderContributer.ConnectionGroupName")); //$NON-NLS-1$
 		connectionGroup.setLayout(new GridLayout(1, false));
 		connectionGroup.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 
