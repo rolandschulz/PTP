980bc97428fa9e4477aa562e510d783de0207229
arossi <arossi>
2011-02-14 22:36:34 +0000

    Beginnings of JAXB RM Junit tests.

---
+++ rms/org.eclipse.ptp.rm.jaxb.tests/src/org/eclipse/ptp/rm/jaxb/tests/RMVariableTest.java
@@ -0,0 +1,34 @@
+package org.eclipse.ptp.rm.jaxb.tests;
+
+import junit.framework.TestCase;
+
+import org.eclipse.ptp.rm.jaxb.core.IJAXBNonNLSConstants;
+import org.eclipse.ptp.rm.jaxb.core.variables.RMVariableMap;
+import org.eclipse.ptp.rm.jaxb.core.xml.JAXBUtils;
+
+public class RMVariableTest extends TestCase implements IJAXBNonNLSConstants {
+
+	private static final String xml = DATA + "rm-pbs-torque_2.3.7.xml"; //$NON-NLS-1$
+
+	@Override
+	public void setUp() {
+
+	}
+
+	@Override
+	public void tearDown() {
+
+	}
+
+	public void testJAXBLoadVariables() {
+		try {
+			JAXBUtils.initializeRMData(xml);
+			String exp = RMVariableMap.dereference("${rm:stagein#description}");
+			System.out.println(exp);
+			assertEquals("The list of files to be staged in prior to job execution.", exp);
+		} catch (Throwable t) {
+			t.printStackTrace();
+			assert (t != null);
+		}
+	}
+}
