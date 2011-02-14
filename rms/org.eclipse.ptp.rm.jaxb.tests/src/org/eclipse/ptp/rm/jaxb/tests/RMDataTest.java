aa1465a8ec4fa0fba11b6bcc79a0108ce3ef7046
Albert L. Rossi <arossi@ncsa.uiuc.edu>
2011-02-14 22:36:35 +0000

    Beginnings of JAXB RM Junit tests.

---
+++ rms/org.eclipse.ptp.rm.jaxb.tests/src/org/eclipse/ptp/rm/jaxb/tests/RMDataTest.java
@@ -0,0 +1,36 @@
+package org.eclipse.ptp.rm.jaxb.tests;
+
+import junit.framework.TestCase;
+
+import org.eclipse.ptp.rm.jaxb.core.IJAXBNonNLSConstants;
+import org.eclipse.ptp.rm.jaxb.core.data.ResourceManagerData;
+import org.eclipse.ptp.rm.jaxb.core.variables.RMVariableMap;
+import org.eclipse.ptp.rm.jaxb.core.xml.JAXBUtils;
+
+public class RMDataTest extends TestCase implements IJAXBNonNLSConstants {
+
+	private static final String xml = DATA + "rm-pbs-torque_2.3.7.xml"; //$NON-NLS-1$
+
+	@Override
+	public void setUp() {
+
+	}
+
+	@Override
+	public void tearDown() {
+
+	}
+
+	public void testJAXBRMInstantiation() {
+		ResourceManagerData rmdata = null;
+		try {
+			rmdata = JAXBUtils.initializeRMData(xml);
+			if (rmdata != null)
+				RMVariableMap.getInstance().print();
+		} catch (Throwable t) {
+			t.printStackTrace();
+			assert (t != null);
+		}
+	}
+
+}
