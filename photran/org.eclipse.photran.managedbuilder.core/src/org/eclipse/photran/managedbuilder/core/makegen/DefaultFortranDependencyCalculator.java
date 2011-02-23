383eb6437d76e2d4dbe23ee904034b790321f4c7
joverbey <joverbey>
2011-02-23 04:43:20 +0000

    Files were not being closed -- Bug 334796

--- photran/org.eclipse.photran.managedbuilder.core/src/org/eclipse/photran/managedbuilder/core/makegen/DefaultFortranDependencyCalculator.java
+++ photran/org.eclipse.photran.managedbuilder.core/src/org/eclipse/photran/managedbuilder/core/makegen/DefaultFortranDependencyCalculator.java
@@ -14,6 +14,7 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -44,6 +45,7 @@ import org.eclipse.photran.internal.cdtinterface.core.FortranLanguage;
  *  This class implements the Dependency Manager and Output Name Provider interfaces
  *  @author Unknown
  *  @author Timofey Yuvashev 2009
+ *  @author Jeff Overbey -- files were not being closed (Bug 334796)
  */
 public class DefaultFortranDependencyCalculator implements IManagedDependencyGenerator,
 														   IManagedOutputNameProvider
@@ -55,6 +57,8 @@ public class DefaultFortranDependencyCalculator implements IManagedDependencyGen
 	 */
 	private String[] findUsedModuleNames(File file) {
 		ArrayList names = new ArrayList();
+		InputStream in = null;
+		Reader r = null;
 		try {
 		/*
 			InputStream in = new BufferedInputStream(new FileInputStream(file));
@@ -71,8 +75,8 @@ public class DefaultFortranDependencyCalculator implements IManagedDependencyGen
 				}
 			}
 		*/
-			InputStream in = new BufferedInputStream(new FileInputStream(file));
-			Reader r = new BufferedReader(new InputStreamReader(in));
+			in = new BufferedInputStream(new FileInputStream(file));
+			r = new BufferedReader(new InputStreamReader(in));
 			StreamTokenizer st = new StreamTokenizer(r);
 			st.commentChar('!');
 			st.eolIsSignificant(false);
@@ -110,6 +114,16 @@ public class DefaultFortranDependencyCalculator implements IManagedDependencyGen
 		catch (Exception e) {
 			return new String[0];
 		}
+		finally {
+			try {
+				if (r != null)
+					r.close();
+				else if (in != null)
+					in.close();
+			}
+			catch (IOException e) {
+			}
+		}
 		return (String[]) names.toArray(new String[names.size()]);
 	}
 	
@@ -118,6 +132,8 @@ public class DefaultFortranDependencyCalculator implements IManagedDependencyGen
 	 */
 	private String[] findModuleNames(File file) {
 		ArrayList names = new ArrayList();
+		InputStream in = null;
+		Reader r = null;
 		try {
 			/*
 			InputStream in = new BufferedInputStream(new FileInputStream(file));
@@ -137,8 +153,8 @@ public class DefaultFortranDependencyCalculator implements IManagedDependencyGen
 				}
 			}
 			*/
-			InputStream in = new BufferedInputStream(new FileInputStream(file));
-			Reader r = new BufferedReader(new InputStreamReader(in));
+			in = new BufferedInputStream(new FileInputStream(file));
+			r = new BufferedReader(new InputStreamReader(in));
 			StreamTokenizer st = new StreamTokenizer(r);
 			st.commentChar('!');
 			st.eolIsSignificant(false);
@@ -162,6 +178,16 @@ public class DefaultFortranDependencyCalculator implements IManagedDependencyGen
 		}
 		catch (Exception e) {
 			return new String[0];
+		}
+		finally {
+			try {
+				if (r != null)
+					r.close();
+				else if (in != null)
+					in.close();
+			}
+			catch (IOException e) {
+			}
 		}
 		return (String[]) names.toArray(new String[names.size()]);
 	}
