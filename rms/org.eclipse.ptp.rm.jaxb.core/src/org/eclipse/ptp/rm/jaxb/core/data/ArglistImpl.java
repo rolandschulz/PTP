33458f2faab16c923da83e5b4b61891746b84eef
arossi <arossi>
2011-02-27 03:02:02 +0000

    ManagedFile and Script write implementations proceed ...
    Dynamic Argument Append

--- rms/org.eclipse.ptp.rm.jaxb.core/src/org/eclipse/ptp/rm/jaxb/core/data/ArglistImpl.java
+++ rms/org.eclipse.ptp.rm.jaxb.core/src/org/eclipse/ptp/rm/jaxb/core/data/ArglistImpl.java
@@ -1,11 +1,24 @@
 package org.eclipse.ptp.rm.jaxb.core.data;
 
-public class ArglistImpl {
+import org.eclipse.ptp.rm.jaxb.core.IJAXBNonNLSConstants;
+import org.eclipse.ptp.rm.jaxb.core.variables.RMVariableMap;
+
+public class ArglistImpl implements IJAXBNonNLSConstants {
 
 	private final Arglist args;
+	private final RMVariableMap map;
 
 	public ArglistImpl(Arglist args) {
 		this.args = args;
+		this.map = RMVariableMap.getInstance();
+	}
+
+	public void toString(StringBuffer buffer) {
+		if (args.dynamicAppend) {
+			composeDynamicArgs(buffer);
+		} else {
+			composeStandardArgs(buffer);
+		}
 	}
 
 	/*
@@ -14,7 +27,35 @@ public class ArglistImpl {
 	 * given dynamic attribute name and value (of undefined position i in the
 	 * list).
 	 */
-	public void toString(StringBuffer buffer) {
+	private void composeDynamicArgs(StringBuffer buffer) {
+		for (String name : map.getDiscovered().keySet()) {
+			for (Arg arg : args.getArg()) {
+				String content = arg.getContent();
+				content = content.replaceAll(AMP, name + PD);
+				content = map.getString(content);
+				String undefined = arg.getIsUndefinedIfEquals();
+				if (undefined != null && undefined.equals(content)) {
+					continue;
+				}
+				if (buffer.length() > 0) {
+					buffer.append(SP);
+				}
+				buffer.append(content);
+			}
+		}
+	}
 
+	private void composeStandardArgs(StringBuffer buffer) {
+		for (Arg arg : args.getArg()) {
+			String dereferenced = map.getString(arg.getContent());
+			String undefined = arg.getIsUndefinedIfEquals();
+			if (undefined != null && undefined.equals(dereferenced)) {
+				continue;
+			}
+			if (buffer.length() > 0) {
+				buffer.append(SP);
+			}
+			buffer.append(dereferenced);
+		}
 	}
 }
