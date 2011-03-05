a19cd4bb96178f251647b94b63dd7f94b9c3da85
Albert L. Rossi <arossi@ncsa.uiuc.edu>
2011-03-05 19:13:56 +0000

    Still not right; saving and reworking design again.

--- rms/org.eclipse.ptp.rm.jaxb.core/src/org/eclipse/ptp/rm/jaxb/core/data/MatchGroup.java
+++ rms/org.eclipse.ptp.rm.jaxb.core/src/org/eclipse/ptp/rm/jaxb/core/data/MatchGroup.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
@@ -29,8 +30,16 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element ref="{}command-ref" maxOccurs="unbounded"/>
+ *         &lt;element ref="{}match" maxOccurs="unbounded"/>
  *       &lt;/sequence>
+ *       &lt;attribute name="type" default="jobAttribute">
+ *         &lt;simpleType>
+ *           &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *             &lt;enumeration value="jobAttribute"/>
+ *             &lt;enumeration value="property"/>
+ *           &lt;/restriction>
+ *         &lt;/simpleType>
+ *       &lt;/attribute>
  *     &lt;/restriction>
  *   &lt;/complexContent>
  * &lt;/complexType>
@@ -39,40 +48,67 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "", propOrder = { "commandRef" })
-@XmlRootElement(name = "on-shut-down")
-public class OnShutDown {
+@XmlType(name = "", propOrder = { "match" })
+@XmlRootElement(name = "match-group")
+public class MatchGroup {
 
-	@XmlElement(name = "command-ref", required = true)
-	protected List<String> commandRef;
+	@XmlElement(required = true)
+	protected List<Match> match;
+	@XmlAttribute
+	protected String type;
 
 	/**
-	 * Gets the value of the commandRef property.
+	 * Gets the value of the match property.
 	 * 
 	 * <p>
 	 * This accessor method returns a reference to the live list, not a
 	 * snapshot. Therefore any modification you make to the returned list will
 	 * be present inside the JAXB object. This is why there is not a
-	 * <CODE>set</CODE> method for the commandRef property.
+	 * <CODE>set</CODE> method for the match property.
 	 * 
 	 * <p>
 	 * For example, to add a new item, do as follows:
 	 * 
 	 * <pre>
-	 * getCommandRef().add(newItem);
+	 * getMatch().add(newItem);
 	 * </pre>
 	 * 
 	 * 
 	 * <p>
-	 * Objects of the following type(s) are allowed in the list {@link String }
+	 * Objects of the following type(s) are allowed in the list {@link Match }
 	 * 
 	 * 
 	 */
-	public List<String> getCommandRef() {
-		if (commandRef == null) {
-			commandRef = new ArrayList<String>();
+	public List<Match> getMatch() {
+		if (match == null) {
+			match = new ArrayList<Match>();
 		}
-		return this.commandRef;
+		return this.match;
+	}
+
+	/**
+	 * Gets the value of the type property.
+	 * 
+	 * @return possible object is {@link String }
+	 * 
+	 */
+	public String getType() {
+		if (type == null) {
+			return "jobAttribute"; //$NON-NLS-1$
+		} else {
+			return type;
+		}
+	}
+
+	/**
+	 * Sets the value of the type property.
+	 * 
+	 * @param value
+	 *            allowed object is {@link String }
+	 * 
+	 */
+	public void setType(String value) {
+		this.type = value;
 	}
 
 }
