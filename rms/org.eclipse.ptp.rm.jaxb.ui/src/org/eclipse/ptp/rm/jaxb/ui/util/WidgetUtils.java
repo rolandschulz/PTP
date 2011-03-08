5cca6e2d7cc3ed7a2fd7950340854a2ec3457a1f
Albert L. Rossi <arossi@ncsa.uiuc.edu>
2011-03-08 14:33:27 +0000

    Added missing copyright headers

--- rms/org.eclipse.ptp.rm.jaxb.ui/src/org/eclipse/ptp/rm/jaxb/ui/util/WidgetUtils.java
+++ rms/org.eclipse.ptp.rm.jaxb.ui/src/org/eclipse/ptp/rm/jaxb/ui/util/WidgetUtils.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2010 University of Illinois 
- * All rights reserved. This program and the accompanying materials are made 
- * available under the terms of the Eclipse Public License v1.0 which accompanies 
- * this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2011 University of Illinois All rights reserved. This program
+ * and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html 
  * 	
  * Contributors: 
  * 	Albert L. Rossi - design and implementation
- *                  - modified, eliminated unused methods 05/11/2010
  ******************************************************************************/
+
 package org.eclipse.ptp.rm.jaxb.ui.util;
 
 import java.io.PrintWriter;
--- rms/org.eclipse.ptp.rm.jaxb.ui/src/org/eclipse/ptp/rm/jaxb/ui/wizards/JAXBRMControlConfigurationWizardPage.java
+++ rms/org.eclipse.ptp.rm.jaxb.ui/src/org/eclipse/ptp/rm/jaxb/ui/wizards/JAXBRMControlConfigurationWizardPage.java
@@ -1,25 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2006 The Regents of the University of California and others.
- * This material was produced under U.S. Government contract W-7405-ENG-36
- * for Los Alamos National Laboratory, which is operated by the University
- * of California for the U.S. Department of Energy. The U.S. Government has
- * rights to use, reproduce, and distribute this software. NEITHER THE
- * GOVERNMENT NOR THE UNIVERSITY MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR
- * ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE. If software is modified
- * to produce derivative works, such modified software should be clearly marked,
- * so as not to confuse it with the version available from LANL.
- * 
- * Additionally, this program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * LA-CC 04-115
- * Contributors:
- *    Albert L. Rossi (NCSA)  -- modified to disable proxy path for
- *    							 automatically deployed RMs
- *    						  -- modified to allow subclasses to expose extra properties/widgets (2010/11/04)
- *******************************************************************************/
+ * Copyright (c) 2011 University of Illinois All rights reserved. This program
+ * and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html 
+ * 	
+ * Contributors: 
+ * 	Albert L. Rossi - design and implementation
+ ******************************************************************************/
+
 package org.eclipse.ptp.rm.jaxb.ui.wizards;
 
 import org.eclipse.ptp.remote.core.IRemoteProxyOptions;
