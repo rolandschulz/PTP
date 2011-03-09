97efc4c4a5548bbfc750c26f8df4f3917af64b50
arossi <arossi>
2011-03-09 17:50:47 +0000

    more modifications

--- rms/org.eclipse.ptp.rm.jaxb.ui/src/org/eclipse/ptp/rm/jaxb/ui/data/LaunchTabBuilder.java
+++ rms/org.eclipse.ptp.rm.jaxb.ui/src/org/eclipse/ptp/rm/jaxb/ui/data/LaunchTabBuilder.java
@@ -1,34 +1,25 @@
 package org.eclipse.ptp.rm.jaxb.ui.data;
 
 import java.util.List;
-import java.util.Map;
 
-import org.eclipse.ptp.rm.jaxb.core.data.LaunchTab;
+import org.eclipse.ptp.rm.jaxb.core.data.Group;
 import org.eclipse.ptp.rm.jaxb.core.data.TabController;
+import org.eclipse.ptp.rm.jaxb.core.data.TabFolder;
 import org.eclipse.ptp.rm.jaxb.ui.IJAXBUINonNLSConstants;
-import org.eclipse.ptp.rm.jaxb.ui.listener.JAXBRMLaunchTabWidgetListener;
+import org.eclipse.ptp.rm.jaxb.ui.launch.JAXBRMLaunchConfigurationDynamicTab;
 import org.eclipse.swt.widgets.Composite;
 
 public class LaunchTabBuilder implements IJAXBUINonNLSConstants {
 
-	private final LaunchTab launchTabData;
-	private final JAXBRMLaunchTabWidgetListener listener;
-	private final Map<Object, String> widgetToValueIndex;
+	private final JAXBRMLaunchConfigurationDynamicTab fLaunchTab;
 
-	public LaunchTabBuilder(LaunchTab launchTabData, JAXBRMLaunchTabWidgetListener listener, Map<Object, String> widgetToValueIndex) {
-		this.launchTabData = launchTabData;
-		this.listener = listener;
-		this.widgetToValueIndex = widgetToValueIndex;
+	public LaunchTabBuilder(JAXBRMLaunchConfigurationDynamicTab launchTab) {
+
+		this.fLaunchTab = launchTab;
 	}
 
 	public void build(Composite parent) throws Throwable {
-		if (launchTabData.isAdvancedModeEnabled()) {
-			addCustomScriptButton();
-		}
-		List<TabController> controllers = launchTabData.getTabController();
-		for (TabController c : controllers) {
-			addTabController(parent, c);
-		}
+
 	}
 
 	private void addCustomScriptButton() {
@@ -37,6 +28,7 @@ public class LaunchTabBuilder implements IJAXBUINonNLSConstants {
 	}
 
 	private void addTabController(Composite parent, TabController c) {
+
 		/*
 		 * create the controller as in:
 		 * org.eclipse.ptp.rm.ui.launch.ExtendableRMLaunchConfigurationDynamicTab
@@ -44,8 +36,13 @@ public class LaunchTabBuilder implements IJAXBUINonNLSConstants {
 		 */
 		c.getTitle();
 
-		c.getGroup();
-		c.getTabFolder();
+		List<Object> children = c.getGroupOrTabFolder();
+		for (Object child : children) {
+			if (child instanceof TabFolder) {
+			} else if (child instanceof Group) {
+
+			}
+		}
 
 	}
 
