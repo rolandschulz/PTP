4c4988b69e3813df0bd6228acc0568136f98a3db
Albert L. Rossi <arossi@ncsa.uiuc.edu>
2011-03-17 06:48:48 +0000

    Proceeding withl builder for tab.  Adding -descriptor to UI data items in XSD to avoid namespace clashes

--- rms/org.eclipse.ptp.rm.jaxb.ui/src/org/eclipse/ptp/rm/jaxb/ui/util/LaunchTabBuilder.java
+++ rms/org.eclipse.ptp.rm.jaxb.ui/src/org/eclipse/ptp/rm/jaxb/ui/util/LaunchTabBuilder.java
@@ -6,11 +6,13 @@ import java.util.Map;
 import org.eclipse.ptp.rm.jaxb.core.data.GroupDescriptor;
 import org.eclipse.ptp.rm.jaxb.core.data.TabController;
 import org.eclipse.ptp.rm.jaxb.core.data.TabFolderDescriptor;
+import org.eclipse.ptp.rm.jaxb.core.data.TabItemDescriptor;
 import org.eclipse.ptp.rm.jaxb.core.data.Widget;
 import org.eclipse.ptp.rm.jaxb.core.variables.RMVariableMap;
 import org.eclipse.ptp.rm.jaxb.ui.IJAXBUINonNLSConstants;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.TabFolder;
 
 public class LaunchTabBuilder implements IJAXBUINonNLSConstants {
 
@@ -40,10 +42,24 @@ public class LaunchTabBuilder implements IJAXBUINonNLSConstants {
 	}
 
 	private void addFolder(TabFolderDescriptor fd, Composite parent) {
+		TabFolder folder = new TabFolder(parent, WidgetBuilderUtils.getStyle(fd.getStyle()));
+		List<TabItemDescriptor> items = fd.getItem();
+		for (TabItemDescriptor i : items) {
+			addItem(folder, i);
+		}
 	}
 
-	private void addGroup(GroupDescriptor group, Composite parent) {
+	private void addGroup(GroupDescriptor grp, Composite parent) {
+		grp.getGridData();
+		grp.getGridLayout();
+		grp.getStyle();
+		grp.getTitle();
+
+	}
+
+	private void addItem(TabFolder folder, TabItemDescriptor i) {
 		// TODO Auto-generated method stub
 
 	}
+
 }
