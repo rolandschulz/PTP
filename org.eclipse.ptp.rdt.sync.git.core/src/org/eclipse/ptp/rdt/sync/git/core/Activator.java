1cf76450f959fe6982130679ea9a7a19415c46ad
rschulz <rschulz>
2011-04-01 07:17:14 +0000

    Some improvements to the initial implementation in bug 336976. It now works correctly on remote sites with older GIT versions and remote commits are more efficient, using "git ls-files" to add and delete specific files and commit them only as needed.
    It has also been modified to work with JGit 0.12 (see bug 336749). Thus the
    temporary getsession patch included with bug 336976 is no longer needed.
    
    JGit 0.12 is required and can be downloaded within eclipse from:
    http://download.eclipse.org/egit/updates-nightly/
    
    by: John Eblen bug-id: 341484

--- org.eclipse.ptp.rdt.sync.git.core/src/org/eclipse/ptp/rdt/sync/git/core/Activator.java
+++ org.eclipse.ptp.rdt.sync.git.core/src/org/eclipse/ptp/rdt/sync/git/core/Activator.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2011 The University of Tennessee and others.
+ * Copyright (c) 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    John Eblen - initial implementation
+ *    IBM Corporation - initial API and implementation
  *******************************************************************************/
 package org.eclipse.ptp.rdt.sync.git.core;
 
