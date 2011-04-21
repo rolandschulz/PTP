fdba06b947d46a72d1d347a30ebea549fc7f07b6
John D Eblen <jdeblen@comcast.net>
2011-04-21 11:53:21 -0400

    Add copyright statements

--- org.eclipse.ptp.rdt.core/src/org/eclipse/ptp/rdt/core/BuildConfigurationManager.java
+++ org.eclipse.ptp.rdt.core/src/org/eclipse/ptp/rdt/core/BuildConfigurationManager.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Oak Ridge National Laboratory and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    John Eblen - initial implementation
+ *******************************************************************************/
 package org.eclipse.ptp.rdt.core;
 
 import java.io.BufferedReader;
