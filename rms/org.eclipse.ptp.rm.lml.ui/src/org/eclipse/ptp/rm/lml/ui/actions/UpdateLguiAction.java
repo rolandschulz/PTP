dfdd46c59a3b71d44a1178ce71115bcef94a5f18
Claudia Knobloch <c.knobloch@fz-juelich.de>
2011-05-06 11:20:41 +0000

    Update changes

--- rms/org.eclipse.ptp.rm.lml.ui/src/org/eclipse/ptp/rm/lml/ui/actions/UpdateLguiAction.java
+++ rms/org.eclipse.ptp.rm.lml.ui/src/org/eclipse/ptp/rm/lml/ui/actions/UpdateLguiAction.java
@@ -6,9 +6,10 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ptp.rm.lml.core.ILMLManager;
 import org.eclipse.ptp.rm.lml.core.LMLCorePlugin;
 import org.eclipse.ptp.rm.lml.ui.messages.Messages;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 
-public class UpdateLguiAction extends Action {
+public class UpdateLguiAction extends Action{
 
 	private final Shell shell;
 
@@ -20,13 +21,12 @@ public class UpdateLguiAction extends Action {
 	public void dispose() {
 	}
 
-	@Override
 	public void run() {
 		ILMLManager lmlManager = LMLCorePlugin.getDefault().getLMLManager();
 		lmlManager.update();
 	}
 
 	public void selectionChanged(IAction action, ISelection selection) {
-
+	
 	}
 }
