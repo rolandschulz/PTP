e4cae4ae77bf5906bbe7abdc58fbcd78653a2ef3
Roland Schulz <roland@utk.edu>
2011-05-11 02:12:14 -0400

    Started ProgressMonitor improvements + some cleanups
    Done:
     - Monitor is passed everywhere it is needed
     - Converted to jgit monitor for jgit transport
     - Should allow cancel in most places (see Missing)
    
    Missing:
     - Monitor needs to be used correctly in CommandRunner (depends on Issue #13)
     - Tasks have to be created and updated to have moving progress bar
    
    Cleanups:
     - removed large number of unnessary catch blocks
     - moved NL-strings to messages

---
+++ org.eclipse.ptp.rdt.sync.git.core/src/org/eclipse/ptp/rdt/sync/git/core/messages/Messages.java
@@ -0,0 +1,27 @@
+package org.eclipse.ptp.rdt.sync.git.core.messages;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.ptp.rdt.sync.git.core.messages.messages"; //$NON-NLS-1$
+	public static String CommandRunner_0;
+	public static String GitRemoteSyncConnection_0;
+	public static String GitRemoteSyncConnection_1;
+	public static String GitRemoteSyncConnection_2;
+	public static String GitRemoteSyncConnection_3;
+	public static String GitRemoteSyncConnection_4;
+	public static String GitRemoteSyncConnection_5;
+	public static String GitServiceProvider_0;
+	public static String GitServiceProvider_1;
+	public static String GitServiceProvider_2;
+	public static String GitServiceProvider_3;
+	public static String GitServiceProvider_4;
+	public static String GitServiceProvider_5;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
