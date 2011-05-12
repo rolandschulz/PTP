e77df2d388388f343631bc3cedab9dfd99242885
Roland Schulz <roland@utk.edu>
2011-05-11 23:56:27 -0400

    OPEN - task 10: Better Progress Monitoring
    Progress bar is OK now. It isn't great, but JGit doesn't report a
    detailed status. Egit also doesn't show continous progress.
    
    Still open because #13 has to be fixed before cancel can be implemented
    there.
    
    http://github.com/rolandschulz/PTP/issues/issue/10

--- org.eclipse.ptp.rdt.sync.core/src/org/eclipse/ptp/rdt/sync/core/remotemake/RemoteMakeBuilder.java
+++ org.eclipse.ptp.rdt.sync.core/src/org/eclipse/ptp/rdt/sync/core/remotemake/RemoteMakeBuilder.java
@@ -233,7 +233,7 @@ public class RemoteMakeBuilder extends MakeBuilder {
 				if (last == null) {
 					last = new Integer(100);
 				}
-				StreamMonitor streamMon = new StreamMonitor(new SubProgressMonitor(monitor, 100), cos, last.intValue());
+				StreamMonitor streamMon = new StreamMonitor(new SubProgressMonitor(monitor, 80), cos, last.intValue());
 				ErrorParserManager epm = new ErrorParserManager(getProject(), workingDirectory, this, configuration.getErrorParserList());
 				epm.setOutputStream(streamMon);
 				final OutputStream stdout = epm.getOutputStream();
@@ -373,7 +373,7 @@ public class RemoteMakeBuilder extends MakeBuilder {
 					
 					// Synchronize before building
 					if (syncProvider != null) {
-						syncProvider.synchronize(null, monitor, SyncFlag.FORCE);
+						syncProvider.synchronize(null, new SubProgressMonitor(monitor, 10), SyncFlag.FORCE);
 					}
 					
 					// Before launching give visual cues via the monitor
@@ -452,7 +452,7 @@ public class RemoteMakeBuilder extends MakeBuilder {
 						
 						// Synchronize after building
 						if (syncProvider != null) {
-							syncProvider.synchronize(null, monitor, SyncFlag.FORCE);
+							syncProvider.synchronize(null, new SubProgressMonitor(monitor, 10), SyncFlag.FORCE);
 						}
 
 						// create a Job for the refresh
