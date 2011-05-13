da520b562021dca0714921be605a2d5b07d1ec4e
Beth Tibbitts <tibbitts@us.ibm.com>
2011-05-13 19:43:09 +0000

    move visit(IAstExpression) up to pldt.common so all subclasses (mpi,omp,lapi,upc,etc) can share in the logic (esp for artifacts found in macros)

--- tools/pldt/org.eclipse.ptp.pldt.mpi.core/src/org/eclipse/ptp/pldt/mpi/core/analysis/MpiCASTVisitor.java
+++ tools/pldt/org.eclipse.ptp.pldt.mpi.core/src/org/eclipse/ptp/pldt/mpi/core/analysis/MpiCASTVisitor.java
@@ -12,12 +12,6 @@ package org.eclipse.ptp.pldt.mpi.core.analysis;
 
 import java.util.List;
 
-import org.eclipse.cdt.core.dom.ast.IASTExpression;
-import org.eclipse.cdt.core.dom.ast.IASTFunctionCallExpression;
-import org.eclipse.cdt.core.dom.ast.IASTIdExpression;
-import org.eclipse.cdt.core.dom.ast.IASTLiteralExpression;
-import org.eclipse.cdt.core.dom.ast.IASTName;
-import org.eclipse.cdt.core.dom.ast.IBinding;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.ptp.pldt.common.CommonPlugin;
 import org.eclipse.ptp.pldt.common.ScanReturn;
@@ -58,59 +52,6 @@ public class MpiCASTVisitor extends PldtAstVisitor {
 		if (traceOn)
 			System.out.println("MpiCASTVisitor.ctor: traceOn=" + traceOn); //$NON-NLS-1$
 
-	}
-
-	/**
-	 * Visit an expression node.
-	 * 
-	 * @see org.eclipse.cdt.core.dom.ast.ASTVisitor#visit(org.eclipse.cdt.core.dom.ast.IASTExpression)
-	 */
-	public int visit(IASTExpression expression) {
-		if (expression instanceof IASTFunctionCallExpression) {
-			// in stdmake this is a CASTFunctionCallExpression but implements
-			// IASTFUnctionCallExpr, so ok
-			IASTExpression astExpr = ((IASTFunctionCallExpression) expression)
-					.getFunctionNameExpression();
-			String signature = astExpr.getRawSignature();
-			// note: getRawSig is the name BEFORE being processed by
-			// preprocessor!
-			// but it seems to be empty if it's different.
-			// IASTFunctionCallExpression
-			// fce=(IASTFunctionCallExpression)expression;
-			// IASTExpression fne=fce.getFunctionNameExpression();
-
-			// can we get post-pre-processor name here?
-			if (astExpr instanceof IASTIdExpression) {
-				IASTName tempFN = ((IASTIdExpression) astExpr).getName();
-				IBinding tempBIND = tempFN.resolveBinding();
-				String tempNAME = tempBIND.getName();
-				if (traceOn)
-					System.out
-							.println("MCAV name: " + tempNAME + " rawsig: " + signature); //$NON-NLS-1$ //$NON-NLS-2$
-				// if e.g. preprocessor substitution used, use that for function
-				// name
-				boolean preProcUsed = !signature.equals(tempNAME);
-				if (preProcUsed) {
-					signature = tempNAME;
-				}
-			}
-			if (signature.startsWith(PREFIX)) {
-				if (astExpr instanceof IASTIdExpression) {
-					IASTName funcName = ((IASTIdExpression) astExpr).getName();
-					// IBinding binding = funcName.resolveBinding();
-					// String name=binding.getName();// name ok for stdMake
-					processFuncName(funcName, astExpr);
-				}
-			}
-		} else if (expression instanceof IASTLiteralExpression) {
-			processMacroLiteral((IASTLiteralExpression) expression);
-		} else {
-			// Other possibilities? Do we care? Assume not.
-			// if(expression instanceof CASTUnaryExpression)
-			// if(expression instanceof CASTIdExpression)
-
-		}
-		return PROCESS_CONTINUE;
 	}
 
 	/**
@@ -157,6 +98,11 @@ public class MpiCASTVisitor extends PldtAstVisitor {
 	@Override
 	protected String getTypeName() {
 		return ARTIFACT_NAME;
+	}
+
+	@Override
+	public boolean matchesPrefix(String name) {
+		return name.startsWith(PREFIX);
 	}
 
 }
