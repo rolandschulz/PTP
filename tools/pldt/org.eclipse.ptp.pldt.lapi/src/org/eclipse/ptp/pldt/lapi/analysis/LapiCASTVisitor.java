d9e338061fef13b41bf1b5ba7e0af5d985103d65
Beth Tibbitts <tibbitts@us.ibm.com>
2011-05-13 19:22:33 +0000

    move visit(IAstExpression) up to pldt.common so all subclasses (mpi,omp,lapi,upc,etc) can share in the logic (esp for artifacts found in macros) - fixes bug 345485 for LAPI

--- tools/pldt/org.eclipse.ptp.pldt.lapi/src/org/eclipse/ptp/pldt/lapi/analysis/LapiCASTVisitor.java
+++ tools/pldt/org.eclipse.ptp.pldt.lapi/src/org/eclipse/ptp/pldt/lapi/analysis/LapiCASTVisitor.java
@@ -20,8 +20,8 @@ import org.eclipse.cdt.core.dom.ast.IASTName;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.ptp.pldt.common.ScanReturn;
 import org.eclipse.ptp.pldt.common.analysis.PldtAstVisitor;
-import org.eclipse.ptp.pldt.lapi.IDs;
 import org.eclipse.ptp.pldt.lapi.Activator;
+import org.eclipse.ptp.pldt.lapi.IDs;
 import org.eclipse.ptp.pldt.lapi.messages.Messages;
 
 /**
@@ -37,7 +37,12 @@ import org.eclipse.ptp.pldt.lapi.messages.Messages;
  * 
  */
 public class LapiCASTVisitor extends PldtAstVisitor {
+	/**
+	 * Note that LAPI APIs and constants differ as to whether they use
+	 * "lapi_" or "LAPI_" so we handle that in matchesPrefix() method
+	 */
 	private static final String PREFIX = "LAPI_"; //$NON-NLS-1$
+	boolean traceOn = false;
 
 	{
 		this.shouldVisitExpressions = true;
@@ -52,19 +57,17 @@ public class LapiCASTVisitor extends PldtAstVisitor {
 		ARTIFACT_CONSTANT = Messages.LapiCASTVisitor_lapi_constant;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.cdt.core.dom.ast.ASTVisitor#visit(org.eclipse.cdt.core.dom
-	 * .ast.IASTExpression)
+	/**
+	 * Unused now; the version in PldtAstVisitor is called instead.
 	 */
-	@Override
-	public int visit(IASTExpression expression) {
+	// @Override
+	public int visitORIG(IASTExpression expression) {
+
 		if (expression instanceof IASTFunctionCallExpression) {
 			IASTExpression astExpr = ((IASTFunctionCallExpression) expression).getFunctionNameExpression();
 			String signature = astExpr.getRawSignature();
-			// System.out.println("func signature=" + signature);
+			if (traceOn)
+				System.out.println("LAPI func signature=" + signature);
 			if (signature.startsWith(PREFIX)) {
 				if (astExpr instanceof IASTIdExpression) {
 					IASTName funcName = ((IASTIdExpression) astExpr).getName();
@@ -73,8 +76,22 @@ public class LapiCASTVisitor extends PldtAstVisitor {
 			}
 		} else if (expression instanceof IASTLiteralExpression) {
 			processMacroLiteral((IASTLiteralExpression) expression);
+
+			// calls w/i macro expansion will only fall below here
+		} else if (expression instanceof IASTIdExpression) {
+
+			IASTIdExpression idExpr = (IASTIdExpression) expression;
+			IASTName name = idExpr.getName(); // LAPI_Amsend
+			System.out.println("LAPI idexpr " + name);
+			String rawName = name.getRawSignature(); // CHECK() macro call
+			String artName = name.toString();
+			if (artName.startsWith(PREFIX)) {
+				System.out.println("   found " + artName);
+			}
+			System.out.println("   rawName: " + rawName);
+			processFuncName(name, expression);
 		}
-		return PROCESS_CONTINUE;
+		return PROCESS_CONTINUE;// Continue with traversing the children of this node.
 	}
 
 	/**
@@ -98,4 +115,17 @@ public class LapiCASTVisitor extends PldtAstVisitor {
 	protected IPreferenceStore getPreferenceStore() {
 		return Activator.getDefault().getPreferenceStore();
 	}
+
+	/**
+	 * Note we need to actually DO this test now because artifacts hidden inside preprocessor expansions
+	 * otherwise bring along all their "friends" of fn calls and ids within the macro expansion that aren't
+	 * really artifacts.<br>
+	 * Note that LAPI APIs and constants differ as to whether they use "lapi_" or "LAPI_" so we handle that here.
+	 */
+	@Override
+	public boolean matchesPrefix(String name) {
+		name = name.toUpperCase();
+		return name.startsWith(PREFIX);
+	}
+
 }
