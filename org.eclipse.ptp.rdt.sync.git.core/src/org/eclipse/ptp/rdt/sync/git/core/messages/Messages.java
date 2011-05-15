e54f5470a921e2315141dc3bf1122544e94a5a2a
Roland Schulz <roland@utk.edu>
2011-05-15 16:35:20 -0400

    added copyright headers
    Conflicts:
    
    	rdt/org.eclipse.ptp.rdt.sync.git.core/src/org/eclipse/ptp/rdt/sync/git/core/messages/messages.properties

--- org.eclipse.ptp.rdt.sync.git.core/src/org/eclipse/ptp/rdt/sync/git/core/messages/Messages.java
+++ org.eclipse.ptp.rdt.sync.git.core/src/org/eclipse/ptp/rdt/sync/git/core/messages/Messages.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 University of Tennessee and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     University of Tennessee (Roland Schulz) - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.ptp.rdt.sync.git.core.messages;
 
 import org.eclipse.osgi.util.NLS;
