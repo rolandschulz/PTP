f791ccea2c88e05cd8ba5b16117388713d96555e
Greg Watson <g.watson@computer.org>
2011-05-16 14:02:38 +0000

    Initial commit

---
+++ rms/org.eclipse.ptp.rm.lml.da.server/src/org/eclipse/ptp/rm/lml/da/server/core/LMLDAServer.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.ptp.rm.lml.da.server.core;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.ptp.remote.core.server.AbstractRemoteServerRunner;
+import org.eclipse.ptp.rm.lml.da.server.messages.Messages;
+
+public class LMLDAServer extends AbstractRemoteServerRunner {
+	public static String SERVER_ID = "org.eclipse.ptp.rm.lml.da.server"; //$NON-NLS-1$;
+
+	public LMLDAServer() {
+		super(Messages.LMLDAServer_Name);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ptp.remote.launch.core.AbstractRemoteServerRunner#doFinishServer
+	 * ()
+	 */
+	@Override
+	protected void doServerFinished(IProgressMonitor monitor) {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ptp.remote.launch.core.AbstractRemoteServerRunner#doRestartServer
+	 * ()
+	 */
+	@Override
+	protected boolean doServerStarting(IProgressMonitor monitor) {
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ptp.remote.launch.core.AbstractRemoteServerRunner#doStartServer
+	 * ()
+	 */
+	@Override
+	protected boolean doServerStarted(IProgressMonitor monitor) {
+		return true;
+	}
+
+}
---
+++ rms/org.eclipse.ptp.rm.lml.da.server/src/org/eclipse/ptp/rm/lml/da/server/internal/core/DebugUtil.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2010 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.ptp.rm.lml.da.server.internal.core;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.ptp.remote.core.PTPRemoteCorePlugin;
+
+public class DebugUtil {
+	public static boolean SERVER_TRACING = false;
+
+	private static final String SERVER_TRACING_OPTION = "org.eclipse.ptp.rm.lml.da.server/debug/tracing"; //$NON-NLS-1$
+
+	public static void configurePluginDebugOptions() {
+		if (PTPRemoteCorePlugin.getDefault().isDebugging()) {
+			String option = Platform.getDebugOption(SERVER_TRACING_OPTION);
+			if (option != null) {
+				SERVER_TRACING = option.equalsIgnoreCase("true"); //$NON-NLS-1$
+			}
+		}
+	}
+}
