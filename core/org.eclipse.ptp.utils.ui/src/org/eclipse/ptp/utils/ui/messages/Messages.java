af04c3cdb0c8b07602d8c88a671c053c4e040487
Greg Watson <g.watson@computer.org>
2011-05-18 12:17:44 +0000

    Add frame for proxy

--- core/org.eclipse.ptp.utils.ui/src/org/eclipse/ptp/utils/ui/messages/Messages.java
+++ core/org.eclipse.ptp.utils.ui/src/org/eclipse/ptp/utils/ui/messages/Messages.java
@@ -18,11 +18,9 @@ public class Messages extends NLS {
 	public static String BrowseButtonText;
 	public static String Frame_0;
 	public static String Frame_1;
-	public static String Frame_ExpandButtonLabel;
 	public static String Frame_NoDescriptionSet;
 	public static String Frame_NoEnclosingGroup;
 	public static String Frame_NoExpandButton;
-	public static String Frame_ShrinkButtonLable;
 	public static String GenericControlGroup_NoControlGroupButton;
 	public static String GenericControlGroup_NoControlGroupLabel;
 	public static String AuthenticationFrameMold_Button_HideAdvanced;
