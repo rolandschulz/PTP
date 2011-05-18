ae4d9efbd16418e26b6bedf34e38ede6be858d1d
joliu <joliu>
2011-05-18 15:06:48 +0000

    fix to 346270, https://bugs.eclipse.org/bugs/show_bug.cgi?id=346270

--- org.eclipse.ptp.rdt.core/src/org/eclipse/ptp/internal/rdt/core/index/IndexBuildSequenceController.java
+++ org.eclipse.ptp.rdt.core/src/org/eclipse/ptp/internal/rdt/core/index/IndexBuildSequenceController.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 IBM Corporation and others.
+ * Copyright (c) 2010, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -31,7 +31,6 @@ import org.eclipse.core.runtime.QualifiedName;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.ptp.rdt.core.resources.RemoteNature;
 import org.eclipse.ptp.rdt.core.services.IRDTServiceConstants;
 import org.eclipse.ptp.services.core.IService;
 import org.eclipse.ptp.services.core.IServiceConfiguration;
@@ -85,7 +84,7 @@ public class IndexBuildSequenceController implements IResourceChangeListener {
 							BUILD_STATUS_KEY));
 	
 			if (index_after_build_option == null) {
-				setIndexAfterBuildOption();
+				unsetIndexAfterBuildOption();
 			}
 			if (index_status == null) {
 
