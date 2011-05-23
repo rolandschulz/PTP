18a7200ba6f4b1e2b1cabb3d46f19f6fd460dcff
John D Eblen <jdeblen@comcast.net>
2011-05-23 12:14:58 -0400

    Open and write to file at same time, to minimize chances of losing data

--- org.eclipse.ptp.rdt.sync.core/src/org/eclipse/ptp/rdt/sync/core/BuildConfigurationManager.java
+++ org.eclipse.ptp.rdt.sync.core/src/org/eclipse/ptp/rdt/sync/core/BuildConfigurationManager.java
@@ -382,9 +382,6 @@ public class BuildConfigurationManager {
 	 * 						on problems writing configuration data to file
 	 */
 	public synchronized void saveConfigurationData() throws IOException {
-		IPath savePath = ServicesCorePlugin.getDefault().getStateLocation().append(DEFAULT_SAVE_FILE_NAME);
-		File saveFile = savePath.toFile();
-		BufferedWriter writer = new BufferedWriter(new FileWriter(saveFile));
 		XMLMemento rootMemento = XMLMemento.createWriteRoot(BUILD_CONFIGURATION_ELEMENT_NAME);
 		
 		// First, save template service configurations
@@ -416,7 +413,9 @@ public class BuildConfigurationManager {
 			}
 		}
 		
-		rootMemento.save(writer);
+		IPath savePath = ServicesCorePlugin.getDefault().getStateLocation().append(DEFAULT_SAVE_FILE_NAME);
+		File saveFile = savePath.toFile();
+		rootMemento.save(new FileWriter(saveFile));
 	}
 	
 	/**
