1c14ff79fdaad8d15f4d2bb5a4eac4dd679c46d8
Albert L. Rossi <arossi@ncsa.uiuc.edu>
2011-05-23 14:10:13 +0000

    multiple @since tags missing on constructors in proxy protocol

--- core/org.eclipse.ptp.proxy.protocol/src/org/eclipse/ptp/proxy/packet/ProxyPacket.java
+++ core/org.eclipse.ptp.proxy.protocol/src/org/eclipse/ptp/proxy/packet/ProxyPacket.java
@@ -64,47 +64,17 @@ public class ProxyPacket {
 	/* bit set in flag byte to indicate presence of compression table */
 	private static final int COMPRESSION_TABLE_FLAG = 0x10;
 
-	/**
-	 * Return some default values for the Huffman frequency table based on
-	 * experimentation.
-	 */
-	public static int[] getDefaultHuffmanTable() {
-		final int frequencyTable[] = { 422, 420, 418, 416, 414, 412, 410, 408,
-				406, 404, 402, 400, 398, 396, 394, 392, 390, 388, 386, 384,
-				382, 380, 378, 376, 374, 372, 370, 368, 366, 364, 362, 1802,
-				450, 448, 446, 444, 442, 440, 438, 436, 434, 432, 430, 428,
-				426, 424, 1652, 1550, 1500, 1450, 1400, 1350, 1300, 1250, 1200,
-				1150, 1100, 1050, 1000, 950, 900, 850, 800, 750, 4500, 2950,
-				2900, 2850, 4400, 2800, 2750, 2700, 4300, 2700, 2650, 2600,
-				2550, 2500, 4200, 2450, 2400, 2350, 2300, 2250, 2200, 2150,
-				2100, 2050, 2000, 1950, 1900, 1850, 1800, 500, 1700, 1650,
-				5000, 4000, 3950, 3900, 4900, 3850, 3800, 3750, 4800, 3700,
-				3650, 3600, 3550, 3500, 4700, 3450, 3400, 3350, 3300, 3250,
-				4600, 3200, 3150, 3100, 3050, 3000, 750, 700, 650, 600, 550,
-				360, 358, 356, 354, 352, 350, 348, 346, 344, 342, 340, 338,
-				336, 334, 332, 330, 328, 326, 324, 322, 320, 318, 316, 314,
-				312, 310, 308, 306, 304, 302, 300, 298, 296, 294, 292, 290,
-				288, 286, 284, 282, 280, 278, 276, 274, 272, 270, 268, 266,
-				264, 262, 260, 258, 256, 254, 252, 250, 248, 246, 244, 242,
-				240, 238, 236, 234, 232, 230, 228, 226, 224, 222, 220, 218,
-				216, 214, 212, 210, 208, 206, 204, 202, 200, 198, 196, 194,
-				192, 190, 188, 186, 184, 182, 180, 178, 176, 174, 172, 170,
-				168, 166, 164, 162, 160, 158, 156, 154, 152, 150, 148, 146,
-				144, 142, 140, 138, 136, 134, 132, 130, 128, 126, 124, 122,
-				120, 118, 116, 114, 112, 110, 108, 106, 104 };
-		return frequencyTable;
-	}
-
 	private boolean debug = false;
 
 	private int fPacketFlags;
+
 	private int fPacketID;
 	private int fPacketTransID;
 	private String[] fPacketArgs;
 	private HuffmanByteCompress compressor;
 	private HuffmanByteUncompress uncompressor;
-	
 	private final Charset fCharset = Charset.forName("US-ASCII"); //$NON-NLS-1$
+
 	private final CharsetEncoder encoder = fCharset.newEncoder();
 	private final CharsetDecoder decoder = fCharset.newDecoder();
 
@@ -112,6 +82,9 @@ public class ProxyPacket {
 		fPacketFlags = 0;
 	}
 
+	/**
+	 * @since 5.0
+	 */
 	public ProxyPacket(IDecoder uncomp) {
 		this();
 		if (!(uncomp instanceof HuffmanByteUncompress)) {
@@ -120,6 +93,9 @@ public class ProxyPacket {
 		uncompressor = (HuffmanByteUncompress) uncomp;
 	}
 
+	/**
+	 * @since 5.0
+	 */
 	public ProxyPacket(IEncoder comp) {
 		this();
 		if (!(comp instanceof HuffmanByteCompress)) {
@@ -135,6 +111,9 @@ public class ProxyPacket {
 		fPacketArgs = cmd.getArguments();
 	}
 
+	/**
+	 * @since 5.0
+	 */
 	public ProxyPacket(IProxyCommand cmd, IDecoder uncomp) {
 		this(cmd);
 		if (!(uncomp instanceof HuffmanByteUncompress)) {
@@ -143,6 +122,9 @@ public class ProxyPacket {
 		uncompressor = (HuffmanByteUncompress) uncomp;
 	}
 
+	/**
+	 * @since 5.0
+	 */
 	public ProxyPacket(IProxyCommand cmd, IEncoder comp) {
 		this(cmd);
 		if (!(comp instanceof HuffmanByteCompress)) {
@@ -161,6 +143,9 @@ public class ProxyPacket {
 		}
 	}
 
+	/**
+	 * @since 5.0
+	 */
 	public ProxyPacket(IProxyEvent event, IDecoder uncomp) {
 		this(event);
 		if (!(uncomp instanceof HuffmanByteUncompress)) {
@@ -169,81 +154,15 @@ public class ProxyPacket {
 		uncompressor = (HuffmanByteUncompress) uncomp;
 	}
 
+	/**
+	 * @since 5.0
+	 */
 	public ProxyPacket(IProxyEvent event, IEncoder comp) {
 		this(event);
 		if (!(comp instanceof HuffmanByteCompress)) {
 			throw new RuntimeException(Messages.getString("ProxyPacket_6")); //$NON-NLS-1$
 		}
 		compressor = (HuffmanByteCompress) comp;
-	}
-
-	/**
-	 * Compresses the given buffers using registered compressor.
-	 * 
-	 * @param buffers
-	 *            The buffers to compress
-	 * @param len
-	 *            The length of the input buffer
-	 * @return ArrayList containing compressed ByteBuffers.
-	 */
-	private ArrayList<ByteBuffer> compressPacket(ArrayList<ByteBuffer> buffers, int len) {
-		ByteBuffer compressedPacket;
-		final ByteBuffer bb = ByteBuffer.allocate(len);
-		List<ByteBuffer> subList;
-		int flagByte;
-		int limit;
-		boolean updated = false;
-
-		if (compressor == null) {
-			return buffers;
-		}
-
-		flagByte = buffers.get(1).get();
-		buffers.get(1).rewind();
-
-		subList = buffers.subList(2, buffers.size());
-		for (final ByteBuffer b : subList) {
-			bb.put(b);
-			b.rewind();
-		}
-		limit = bb.limit();
-
-		final int tableUpdateCount = compressor.getBytesAccumulated();
-		if (tableUpdateCount > COMPRESSION_UPDATE) {
-			if (len > LARGE_PACKET
-					|| tableUpdateCount > (COMPRESSION_UPDATE << 1)) {
-				compressor.updateHuffmanTable();
-			}
-		}
-
-		/* compress the packet */
-
-		updated = compressor.getIncludeTableFlag();
-		compressedPacket = compressor.apply(bb);
-
-		/*
-		 * If compression is not good enough, return original packet. If
-		 * frequency table is updated, send compressed packet anyway.
-		 */
-		if (limit - compressedPacket.limit() > COMPRESSION_DIFF || updated) {
-			flagByte |= COMPRESSION_FLAG;
-			if (updated) {
-				flagByte |= COMPRESSION_TABLE_FLAG;
-			}
-			compressedPacket.rewind();
-			buffers = new ArrayList<ByteBuffer>(3);
-			buffers.add(ByteBuffer.allocate(4));
-			buffers.add(ByteBuffer.allocate(1));
-			buffers.add(compressedPacket);
-			/* 1 byte for flag */
-			buffers.get(0).putInt(compressedPacket.limit() + 1).rewind();
-			buffers.get(1).put((byte) flagByte).rewind();
-			if (debug) {
-				System.out.println("Original size: " + bb.limit()); //$NON-NLS-1$
-				System.out.println("New size:" + compressedPacket.limit()); //$NON-NLS-1$
-			}
-		}
-		return buffers;
 	}
 
 	/**
@@ -262,60 +181,6 @@ public class ProxyPacket {
 	 */
 	public CharsetEncoder encoder() {
 		return encoder;
-	}
-
-	/**
-	 * Read a full buffer from the socket. Guaranteed to read buf.remaining()
-	 * bytes from the channel.
-	 * 
-	 * FIXME: Can this block if there is nothing available on the channel? If
-	 * so, then there should be some kind of timeout to prevent the UI from
-	 * hanging.
-	 * 
-	 * @param buf
-	 *            buffer containing the result of the read
-	 * @throws IOException
-	 *             if EOF
-	 */
-	private void fullRead(ReadableByteChannel channel, ByteBuffer buf) throws IOException {
-		buf.clear();
-		while (buf.hasRemaining()) {
-			int n = channel.read(buf);
-			if (n < 0) {
-				throw new IOException(Messages.getString("ProxyPacket_2")); //$NON-NLS-1$
-			}
-		}
-		buf.flip();
-	}
-
-	/**
-	 * Write an array of buffers to the socket. Guarantees to write the whole
-	 * buffer array.
-	 * 
-	 * NOTE: Originally used GatherByteChannel but the performance is pathetic.
-	 * This implementation requires a buffer copy, which should be eliminated.
-	 * 
-	 * FIXME: Can this block? If so, then there should be some kind of timeout
-	 * to prevent the UI from hanging.
-	 * 
-	 * @param bufs
-	 *            array of buffers to write
-	 * @param len
-	 *            total number of bytes to write
-	 * @throws IOException
-	 */
-	private void fullWrite(WritableByteChannel channel, List<ByteBuffer> bufs, long len) throws IOException {
-		long n;
-		ByteBuffer bb = ByteBuffer.allocate((int) len);
-		for (ByteBuffer b : bufs) {
-			bb.put(b);
-		}
-		bb.flip();
-		while ((n = channel.write(bb)) < len) {
-			if (n < 0) {
-				throw new IOException(Messages.getString("ProxyPacket_2")); //$NON-NLS-1$
-			}
-		}
 	}
 
 	/**
@@ -491,5 +356,150 @@ public class ProxyPacket {
 	 */
 	public void setDebug(boolean debug) {
 		this.debug = debug;
+	}
+
+	/**
+	 * Compresses the given buffers using registered compressor.
+	 * 
+	 * @param buffers
+	 *            The buffers to compress
+	 * @param len
+	 *            The length of the input buffer
+	 * @return ArrayList containing compressed ByteBuffers.
+	 */
+	private ArrayList<ByteBuffer> compressPacket(ArrayList<ByteBuffer> buffers, int len) {
+		ByteBuffer compressedPacket;
+		final ByteBuffer bb = ByteBuffer.allocate(len);
+		List<ByteBuffer> subList;
+		int flagByte;
+		int limit;
+		boolean updated = false;
+
+		if (compressor == null) {
+			return buffers;
+		}
+
+		flagByte = buffers.get(1).get();
+		buffers.get(1).rewind();
+
+		subList = buffers.subList(2, buffers.size());
+		for (final ByteBuffer b : subList) {
+			bb.put(b);
+			b.rewind();
+		}
+		limit = bb.limit();
+
+		final int tableUpdateCount = compressor.getBytesAccumulated();
+		if (tableUpdateCount > COMPRESSION_UPDATE) {
+			if (len > LARGE_PACKET || tableUpdateCount > (COMPRESSION_UPDATE << 1)) {
+				compressor.updateHuffmanTable();
+			}
+		}
+
+		/* compress the packet */
+
+		updated = compressor.getIncludeTableFlag();
+		compressedPacket = compressor.apply(bb);
+
+		/*
+		 * If compression is not good enough, return original packet. If
+		 * frequency table is updated, send compressed packet anyway.
+		 */
+		if (limit - compressedPacket.limit() > COMPRESSION_DIFF || updated) {
+			flagByte |= COMPRESSION_FLAG;
+			if (updated) {
+				flagByte |= COMPRESSION_TABLE_FLAG;
+			}
+			compressedPacket.rewind();
+			buffers = new ArrayList<ByteBuffer>(3);
+			buffers.add(ByteBuffer.allocate(4));
+			buffers.add(ByteBuffer.allocate(1));
+			buffers.add(compressedPacket);
+			/* 1 byte for flag */
+			buffers.get(0).putInt(compressedPacket.limit() + 1).rewind();
+			buffers.get(1).put((byte) flagByte).rewind();
+			if (debug) {
+				System.out.println("Original size: " + bb.limit()); //$NON-NLS-1$
+				System.out.println("New size:" + compressedPacket.limit()); //$NON-NLS-1$
+			}
+		}
+		return buffers;
+	}
+
+	/**
+	 * Read a full buffer from the socket. Guaranteed to read buf.remaining()
+	 * bytes from the channel.
+	 * 
+	 * FIXME: Can this block if there is nothing available on the channel? If
+	 * so, then there should be some kind of timeout to prevent the UI from
+	 * hanging.
+	 * 
+	 * @param buf
+	 *            buffer containing the result of the read
+	 * @throws IOException
+	 *             if EOF
+	 */
+	private void fullRead(ReadableByteChannel channel, ByteBuffer buf) throws IOException {
+		buf.clear();
+		while (buf.hasRemaining()) {
+			int n = channel.read(buf);
+			if (n < 0) {
+				throw new IOException(Messages.getString("ProxyPacket_2")); //$NON-NLS-1$
+			}
+		}
+		buf.flip();
+	}
+
+	/**
+	 * Write an array of buffers to the socket. Guarantees to write the whole
+	 * buffer array.
+	 * 
+	 * NOTE: Originally used GatherByteChannel but the performance is pathetic.
+	 * This implementation requires a buffer copy, which should be eliminated.
+	 * 
+	 * FIXME: Can this block? If so, then there should be some kind of timeout
+	 * to prevent the UI from hanging.
+	 * 
+	 * @param bufs
+	 *            array of buffers to write
+	 * @param len
+	 *            total number of bytes to write
+	 * @throws IOException
+	 */
+	private void fullWrite(WritableByteChannel channel, List<ByteBuffer> bufs, long len) throws IOException {
+		long n;
+		ByteBuffer bb = ByteBuffer.allocate((int) len);
+		for (ByteBuffer b : bufs) {
+			bb.put(b);
+		}
+		bb.flip();
+		while ((n = channel.write(bb)) < len) {
+			if (n < 0) {
+				throw new IOException(Messages.getString("ProxyPacket_2")); //$NON-NLS-1$
+			}
+		}
+	}
+
+	/**
+	 * Return some default values for the Huffman frequency table based on
+	 * experimentation.
+	 * 
+	 * @since 5.0
+	 */
+	public static int[] getDefaultHuffmanTable() {
+		final int frequencyTable[] = { 422, 420, 418, 416, 414, 412, 410, 408, 406, 404, 402, 400, 398, 396, 394, 392, 390, 388,
+				386, 384, 382, 380, 378, 376, 374, 372, 370, 368, 366, 364, 362, 1802, 450, 448, 446, 444, 442, 440, 438, 436, 434,
+				432, 430, 428, 426, 424, 1652, 1550, 1500, 1450, 1400, 1350, 1300, 1250, 1200, 1150, 1100, 1050, 1000, 950, 900,
+				850, 800, 750, 4500, 2950, 2900, 2850, 4400, 2800, 2750, 2700, 4300, 2700, 2650, 2600, 2550, 2500, 4200, 2450,
+				2400, 2350, 2300, 2250, 2200, 2150, 2100, 2050, 2000, 1950, 1900, 1850, 1800, 500, 1700, 1650, 5000, 4000, 3950,
+				3900, 4900, 3850, 3800, 3750, 4800, 3700, 3650, 3600, 3550, 3500, 4700, 3450, 3400, 3350, 3300, 3250, 4600, 3200,
+				3150, 3100, 3050, 3000, 750, 700, 650, 600, 550, 360, 358, 356, 354, 352, 350, 348, 346, 344, 342, 340, 338, 336,
+				334, 332, 330, 328, 326, 324, 322, 320, 318, 316, 314, 312, 310, 308, 306, 304, 302, 300, 298, 296, 294, 292, 290,
+				288, 286, 284, 282, 280, 278, 276, 274, 272, 270, 268, 266, 264, 262, 260, 258, 256, 254, 252, 250, 248, 246, 244,
+				242, 240, 238, 236, 234, 232, 230, 228, 226, 224, 222, 220, 218, 216, 214, 212, 210, 208, 206, 204, 202, 200, 198,
+				196, 194, 192, 190, 188, 186, 184, 182, 180, 178, 176, 174, 172, 170, 168, 166, 164, 162, 160, 158, 156, 154, 152,
+				150, 148, 146, 144, 142, 140, 138, 136, 134, 132, 130, 128, 126, 124, 122, 120, 118, 116, 114, 112, 110, 108, 106,
+				104 };
+		return frequencyTable;
 	}
 }
