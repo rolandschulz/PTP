0611fd3d461bcd989fd2a693bd0c5024b896cff0
Greg Watson <g.watson@computer.org>
2011-05-24 15:48:00 +0000

    No longer use hierarchy

--- core/org.eclipse.ptp.ui/src/org/eclipse/ptp/internal/ui/adapters/PResourceManagerWorkbenchAdapter.java
+++ core/org.eclipse.ptp.ui/src/org/eclipse/ptp/internal/ui/adapters/PResourceManagerWorkbenchAdapter.java
@@ -20,96 +20,16 @@ package org.eclipse.ptp.internal.ui.adapters;
 
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.ptp.core.elements.IPMachine;
-import org.eclipse.ptp.core.elements.IPQueue;
 import org.eclipse.ptp.core.elements.IPResourceManager;
 import org.eclipse.ptp.internal.ui.ParallelImages;
 import org.eclipse.ptp.rmsystem.IResourceManager;
 import org.eclipse.ptp.ui.IRuntimeModelPresentation;
 import org.eclipse.ptp.ui.PTPUIPlugin;
-import org.eclipse.ptp.ui.messages.Messages;
 import org.eclipse.ptp.utils.ui.ImageImageDescriptor;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.ui.model.IWorkbenchAdapter;
-import org.eclipse.ui.model.IWorkbenchAdapter2;
 import org.eclipse.ui.model.WorkbenchAdapter;
 
 public class PResourceManagerWorkbenchAdapter extends WorkbenchAdapter {
-
-	private class ChildContainer extends WorkbenchAdapter implements IAdaptable {
-
-		private final Object parent;
-
-		private final String name;
-
-		private final Object[] children;
-
-		public ChildContainer(Object parent, String name, Object[] children) {
-			this.parent = parent;
-			this.name = name;
-			this.children = children;
-		}
-
-		@SuppressWarnings("rawtypes")
-		public Object getAdapter(Class adapter) {
-			if (adapter == IWorkbenchAdapter.class || adapter == IWorkbenchAdapter2.class) {
-				return this;
-			}
-			return null;
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * org.eclipse.ui.model.WorkbenchAdapter#getChildren(java.lang.Object)
-		 */
-		@Override
-		public Object[] getChildren(Object object) {
-			return children.clone();
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see org.eclipse.ui.model.WorkbenchAdapter#getLabel(java.lang.Object)
-		 */
-		@Override
-		public String getLabel(Object object) {
-			return name;
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * org.eclipse.ui.model.WorkbenchAdapter#getParent(java.lang.Object)
-		 */
-		@Override
-		public Object getParent(Object object) {
-			return parent;
-		}
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.model.WorkbenchAdapter#getChildren(java.lang.Object)
-	 */
-	@Override
-	public Object[] getChildren(Object parentElement) {
-		IPResourceManager rm = getResourceManager(parentElement);
-		if (rm != null) {
-			if (!rm.getResourceManager().getState().equals(IResourceManager.STARTED_STATE)) {
-				return new Object[0];
-			}
-			IPMachine[] machines = rm.getMachines();
-			IPQueue[] queues = rm.getQueues();
-			return new Object[] { makeChildContainer(parentElement, Messages.ResourceManagerWorkbenchAdapter_0, machines),
-					makeChildContainer(parentElement, Messages.ResourceManagerWorkbenchAdapter_1, queues) };
-		}
-		return null;
-	}
 
 	/**
 	 * @param parentElement
@@ -180,10 +100,4 @@ public class PResourceManagerWorkbenchAdapter extends WorkbenchAdapter {
 		}
 		return null;
 	}
-
-	private ChildContainer makeChildContainer(Object parent, String name, final Object[] children) {
-		final ChildContainer container = new ChildContainer(parent, name, children);
-		return container;
-	}
-
 }
