be3f4ddb9046c7061794318f681508e516dfd0a8
John D Eblen <jdeblen@comcast.net>
2011-06-09 14:02:11 -0400

    Disallow modifications to the Workspace configuration on properties page (but still display correct information)

--- org.eclipse.ptp.rdt.sync.ui/src/org/eclipse/ptp/rdt/sync/ui/messages/Messages.java
+++ org.eclipse.ptp.rdt.sync.ui/src/org/eclipse/ptp/rdt/sync/ui/messages/Messages.java
@@ -17,6 +17,7 @@ public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.ptp.rdt.sync.ui.messages.messages"; //$NON-NLS-1$
 	public static String NewRemoteSyncProjectWizardPage_syncProvider;
 	public static String WizardProjectConversion_windowLabel;
+	public static String WorkspaceConfigName;
 	public static String ConvertToRemoteWizardPage_0;
 	public static String ConvertToSyncProjectWizardPage_0;
 	public static String ConvertToSyncProjectWizardPage_convertingToSyncProject;
