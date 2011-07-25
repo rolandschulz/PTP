bae059cc3a4ecfed63853b918d78d14a24f714ce
John D Eblen <jdeblen@comcast.net>
2011-07-25 13:41:00 -0400

    Start adding layer between sync calls and sync activation (to support sync'ing of all configurations as needed)

--- org.eclipse.ptp.rdt.sync.core/src/org/eclipse/ptp/internal/rdt/sync/core/SyncCommand.java
+++ org.eclipse.ptp.rdt.sync.core/src/org/eclipse/ptp/internal/rdt/sync/core/SyncCommand.java
@@ -10,12 +10,33 @@
  *******************************************************************************/
 package org.eclipse.ptp.internal.rdt.sync.core;
 
+import java.util.EnumSet;
 import java.util.Map;
 
+import org.eclipse.cdt.managedbuilder.core.IConfiguration;
+import org.eclipse.cdt.managedbuilder.core.ManagedBuildManager;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.ptp.rdt.sync.core.BuildConfigurationManager;
+import org.eclipse.ptp.rdt.sync.core.RDTSyncCorePlugin;
+import org.eclipse.ptp.rdt.sync.core.SyncFlag;
+import org.eclipse.ptp.rdt.sync.core.messages.Messages;
+import org.eclipse.ptp.rdt.sync.core.serviceproviders.ISyncServiceProvider;
+import org.eclipse.ptp.rdt.sync.core.services.IRemoteSyncServiceConstants;
+import org.eclipse.ptp.services.core.IService;
+import org.eclipse.ptp.services.core.IServiceConfiguration;
+import org.eclipse.ptp.services.core.IServiceModelManager;
+import org.eclipse.ptp.services.core.ServiceModelManager;
 import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.commands.IElementUpdater;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -26,8 +47,43 @@ public class SyncCommand extends AbstractHandler implements IElementUpdater {
 		ACTIVE, ALL, NONE
 	};
 
+	private static final IServiceModelManager serviceModel = ServiceModelManager.getInstance();
+	private static final IService syncService = serviceModel.getService(IRemoteSyncServiceConstants.SERVICE_SYNC);
 	private static SYNC_MODE syncMode = SYNC_MODE.ACTIVE;
 	private static final String SYNC_COMMAND_PARAMETER_ID = "org.eclipse.ptp.internal.rdt.sync.core.syncCommand.syncModeParameter"; //$NON-NLS-1$
+
+	private static class SynchronizeJob extends Job {
+		private final ISyncServiceProvider fSyncProvider;
+		private final IResourceDelta fDelta;
+		private final EnumSet<SyncFlag> fSyncFlags;
+
+		public SynchronizeJob(IResourceDelta delta, ISyncServiceProvider provider, EnumSet<SyncFlag> syncFlags) {
+			super(Messages.ResourceChangeListener_jobName);
+			fDelta = delta;
+			fSyncProvider = provider;
+			fSyncFlags = syncFlags;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see org.eclipse.core.runtime.jobs.Job#run(org.eclipse.core.runtime.
+		 * IProgressMonitor)
+		 */
+		@Override
+		protected IStatus run(IProgressMonitor monitor) {
+			SubMonitor progress = SubMonitor.convert(monitor, 100);
+			try {
+				fSyncProvider.synchronize(fDelta, progress.newChild(100), fSyncFlags);
+			} catch (CoreException e) {
+				System.out.println("sync failed: " + e.getLocalizedMessage()); //$NON-NLS-1$
+				e.printStackTrace();
+			} finally {
+				monitor.done();
+			}
+			return Status.OK_STATUS;
+		}
+	};
 
 	public static SYNC_MODE getSyncMode() {
 		return syncMode;
@@ -82,6 +138,24 @@ public class SyncCommand extends AbstractHandler implements IElementUpdater {
 			} else {
 				element.setChecked(false);
 			}
+		}
+	}
+
+	public static void sync(IResourceDelta delta, IProject project, EnumSet<SyncFlag> syncFlags) {
+		BuildConfigurationManager bcm = BuildConfigurationManager.getInstance();
+		if (!(bcm.isInitialized(project))) {
+			return;
+		}
+		IConfiguration buildConfig = ManagedBuildManager.getBuildInfo(project).getDefaultConfiguration();
+		IServiceConfiguration serviceConfig = bcm.getConfigurationForBuildConfiguration(buildConfig);
+		if (serviceConfig != null) {
+			ISyncServiceProvider provider = (ISyncServiceProvider) serviceConfig.getServiceProvider(syncService);
+			if (provider != null) {
+				SynchronizeJob job = new SynchronizeJob(delta, provider, syncFlags);
+				job.schedule();
+			}
+		} else {
+			RDTSyncCorePlugin.log(Messages.RCL_NoServiceConfigError);
 		}
 	}
 }
