cc8c7aba37e271b953975305470d388bab25c894
Vivian Kong <vivkong@ca.ibm.com>
2011-07-27 15:21:28 +0000

    Bug 353212 - Fix IndexIncludeValue and LocalSearchService for local include browser and local C/C++ search

--- rdt/org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/internal/rdt/ui/search/LocalSearchService.java
+++ rdt/org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/internal/rdt/ui/search/LocalSearchService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2010 IBM Corporation and others.
+ * Copyright (c) 2008, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,14 +14,11 @@ package org.eclipse.ptp.internal.rdt.ui.search;
 import org.eclipse.cdt.core.model.ICElement;
 import org.eclipse.cdt.core.model.ISourceReference;
 import org.eclipse.cdt.core.model.ITranslationUnit;
-import org.eclipse.cdt.utils.EFSExtensionManager;
+import org.eclipse.cdt.internal.ui.search.PDOMSearchElementQuery;
+import org.eclipse.cdt.internal.ui.search.PDOMSearchPatternQuery;
+import org.eclipse.cdt.internal.ui.search.PDOMSearchTextSelectionQuery;
 import org.eclipse.jface.text.ITextSelection;
-import org.eclipse.ptp.internal.rdt.core.miners.RemoteLocationConverter;
 import org.eclipse.ptp.internal.rdt.core.model.Scope;
-import org.eclipse.ptp.internal.rdt.core.search.RemoteSearchElementQuery;
-import org.eclipse.ptp.internal.rdt.core.search.RemoteSearchPatternQuery;
-import org.eclipse.ptp.internal.rdt.core.search.RemoteSearchTextSelectionQuery;
-import org.eclipse.ptp.internal.rdt.ui.search.actions.RemoteSearchTextSelectionQueryAdapter;
 import org.eclipse.search.ui.ISearchQuery;
 
 /**
@@ -29,18 +26,17 @@ import org.eclipse.search.ui.ISearchQuery;
  */
 public class LocalSearchService implements ISearchService {
 	public ISearchQuery createSearchPatternQuery(Scope indexScope, ICElement[] searchScope, String scopeDescription, String patternStr, boolean isCaseSensitive, int searchFlags) {
-		RemoteSearchPatternQuery query = new RemoteSearchPatternQuery(searchScope, scopeDescription, patternStr, isCaseSensitive, searchFlags);
-		return new RemoteSearchPatternQueryAdapter(null, indexScope, query);
+		PDOMSearchPatternQuery localQuery = new PDOMSearchPatternQuery(searchScope, scopeDescription, patternStr, isCaseSensitive, searchFlags);
+		return localQuery;
 	}
 
 	public ISearchQuery createSearchElementQuery(Scope indexScope, ICElement[] searchScope, ISourceReference object, int limitTo) {
-		String path = EFSExtensionManager.getDefault().getPathFromURI(object.getTranslationUnit().getLocationURI());		
-		RemoteSearchElementQuery query = new RemoteSearchElementQuery(searchScope, object, path, limitTo);
-		return new RemoteSearchElementQueryAdapter(null, indexScope, query);
+		PDOMSearchElementQuery localQuery = new PDOMSearchElementQuery(searchScope, object, limitTo);
+		return localQuery;
 	}
 
 	public ISearchQuery createSearchTextSelectionQuery(Scope indexScope, ICElement[] searchScope, ITranslationUnit element, ITextSelection selNode, int limitTo) {
-		RemoteSearchTextSelectionQuery query = new RemoteSearchTextSelectionQuery(searchScope, element, selNode.getText(), selNode.getOffset(), selNode.getLength(), limitTo);
-		return new RemoteSearchTextSelectionQueryAdapter(null, indexScope, query);
+		PDOMSearchTextSelectionQuery localQuery = new PDOMSearchTextSelectionQuery(searchScope, element, selNode, limitTo);
+		return localQuery;
 	}
 }
