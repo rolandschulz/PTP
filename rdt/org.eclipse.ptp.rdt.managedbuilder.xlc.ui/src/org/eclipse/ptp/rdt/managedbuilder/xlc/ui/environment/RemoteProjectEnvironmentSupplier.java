63cee0d0f98ffcccb24d6fda0d0dd5567d0498aa
Vivian Kong <vivkong@ca.ibm.com>
2011-08-09 17:21:56 +0000

    Bug 353345 - Fixes for local service provider

--- rdt/org.eclipse.ptp.rdt.managedbuilder.xlc.ui/src/org/eclipse/ptp/rdt/managedbuilder/xlc/ui/environment/RemoteProjectEnvironmentSupplier.java
+++ rdt/org.eclipse.ptp.rdt.managedbuilder.xlc.ui/src/org/eclipse/ptp/rdt/managedbuilder/xlc/ui/environment/RemoteProjectEnvironmentSupplier.java
@@ -50,9 +50,12 @@ public class RemoteProjectEnvironmentSupplier implements
 	public IBuildEnvironmentVariable getVariable(String variableName,
 			IManagedProject project, IEnvironmentVariableProvider provider) {
 		Map<String, String> envMap = getRemoteEnvironment(project);
-		String value = envMap.get(variableName) == null ? new String() : envMap.get(variableName);
-		IBuildEnvironmentVariable envVar = new BuildEnvVar(variableName, value);
-		return envVar;
+		if (envMap != null) {
+			String value = envMap.get(variableName) == null ? new String() : envMap.get(variableName);
+			IBuildEnvironmentVariable envVar = new BuildEnvVar(variableName, value);
+			return envVar;
+		}
+		return null;
 	}
 
 	/* (non-Javadoc)
