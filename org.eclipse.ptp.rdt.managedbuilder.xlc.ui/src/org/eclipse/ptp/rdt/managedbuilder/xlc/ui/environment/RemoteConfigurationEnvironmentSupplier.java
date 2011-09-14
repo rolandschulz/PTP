3bdad1713f14aeecad2913f9d6c5edaf1561f120
gwatson <gwatson>
2011-09-14 13:12:21 +0000

    Fix incorrect versions

--- org.eclipse.ptp.rdt.managedbuilder.xlc.ui/src/org/eclipse/ptp/rdt/managedbuilder/xlc/ui/environment/RemoteConfigurationEnvironmentSupplier.java
+++ org.eclipse.ptp.rdt.managedbuilder.xlc.ui/src/org/eclipse/ptp/rdt/managedbuilder/xlc/ui/environment/RemoteConfigurationEnvironmentSupplier.java
@@ -43,7 +43,7 @@ import org.eclipse.ptp.services.core.ServiceModelManager;
  * acts the same as the RemoteProjectEnvironmentSupplier at the moment.
  * 
  * @author crecoskie
- * @since 2.0
+ * @since 3.1
  *
  */
 @SuppressWarnings("restriction")
