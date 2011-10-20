1718d1dd4ad6172336171fedf330c63f67e08256
cvs2svn <ptp-dev@eclipse.org>
2011-10-20 12:48:59 +0000

    This commit was manufactured by cvs2svn to create branch 'ptp_5_0'.
    Cherrypick from master 2011-10-20 12:48:57 UTC Greg Watson <g.watson@computer.org> 'Initial commit':
        rdt/org.eclipse.ptp.rdt.sync.core.tests/.classpath
        rdt/org.eclipse.ptp.rdt.sync.core.tests/.project
        rdt/org.eclipse.ptp.rdt.sync.core.tests/.settings/org.eclipse.jdt.core.prefs
        rdt/org.eclipse.ptp.rdt.sync.core.tests/META-INF/MANIFEST.MF
        rdt/org.eclipse.ptp.rdt.sync.core.tests/about.html
        rdt/org.eclipse.ptp.rdt.sync.core.tests/build.properties
        rdt/org.eclipse.ptp.rdt.sync.core.tests/src/org/eclipse/ptp/rdt/sync/core/tests/BasicGitSyncTests.java
        rdt/org.eclipse.ptp.rdt.sync.core.tests/src/org/eclipse/ptp/rdt/sync/core/tests/TemporaryGitRemoteSyncConnection.java
        rdt/org.eclipse.ptp.rdt.sync.core.tests/src/org/eclipse/ptp/rdt/sync/core/tests/remotehost.properties.sample
        releng/org.eclipse.ptp.rm.jaxb.contrib-feature/.project
        releng/org.eclipse.ptp.rm.jaxb.contrib-feature/build.properties
        releng/org.eclipse.ptp.rm.jaxb.contrib-feature/feature.properties
        releng/org.eclipse.ptp.rm.jaxb.contrib-feature/feature.xml
        releng/org.eclipse.ptp.rm.jaxb.contrib-feature/pom.xml
        rms/org.eclipse.ptp.rm.jaxb.contrib/.project
        rms/org.eclipse.ptp.rm.jaxb.contrib/META-INF/MANIFEST.MF
        rms/org.eclipse.ptp.rm.jaxb.contrib/about.html
        rms/org.eclipse.ptp.rm.jaxb.contrib/about.ini
        rms/org.eclipse.ptp.rm.jaxb.contrib/about.mappings
        rms/org.eclipse.ptp.rm.jaxb.contrib/about.properties
        rms/org.eclipse.ptp.rm.jaxb.contrib/build.properties
        rms/org.eclipse.ptp.rm.jaxb.contrib/data/edu.illinois.ncsa.forge.pbs.batch.mvapich2.xml
        rms/org.eclipse.ptp.rm.jaxb.contrib/data/edu.illinois.ncsa.forge.pbs.batch.openmpi.xml
        rms/org.eclipse.ptp.rm.jaxb.contrib/data/edu.utk.nics.keeneland.pbs.batch.xml
        rms/org.eclipse.ptp.rm.jaxb.contrib/data/edu.utk.nics.kraken.pbs.batch.xml
        rms/org.eclipse.ptp.rm.jaxb.contrib/plugin.properties
        rms/org.eclipse.ptp.rm.jaxb.contrib/plugin.xml
        rms/org.eclipse.ptp.rm.jaxb.contrib/ptp_logo_icon32.png

---
+++ rdt/org.eclipse.ptp.rdt.sync.core.tests/src/org/eclipse/ptp/rdt/sync/core/tests/TemporaryGitRemoteSyncConnection.java
@@ -0,0 +1,155 @@
+/*******************************************************************************
+ * Copyright (c) 2011 University of Tennessee and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Roland Schulz - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.ptp.rdt.sync.core.tests;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.eclipse.ptp.rdt.sync.git.core.GitRemoteSyncConnection;
+import org.eclipse.ptp.rdt.sync.git.core.SyncFileFilter;
+import org.eclipse.ptp.remote.core.IRemoteConnection;
+import org.eclipse.ptp.remote.core.IRemoteConnectionManager;
+import org.eclipse.ptp.remote.core.IRemoteFileManager;
+import org.eclipse.ptp.remote.core.IRemoteServices;
+import org.eclipse.ptp.remote.core.PTPRemoteCorePlugin;
+import org.eclipse.ptp.remotetools.environment.generichost.core.ConfigFactory;
+import org.junit.rules.ExternalResource;
+import org.junit.rules.TemporaryFolder;
+
+import org.eclipse.core.filesystem.EFS;
+
+@SuppressWarnings("restriction")
+public class TemporaryGitRemoteSyncConnection extends ExternalResource {
+	private GitRemoteSyncConnection fGITConn;
+	private IRemoteConnection fRemoteConnection;
+	private TemporaryFolder localFolder;
+	private static Random random = new Random();
+	private IRemoteFileManager fileManager;
+	private BasicGitSyncTests test;
+	private String remoteFolder;
+	private IRemoteConnectionManager connMgr;
+	
+	public TemporaryGitRemoteSyncConnection(BasicGitSyncTests basicGitSyncTests) {
+		test = basicGitSyncTests;
+	}
+
+	@Override
+	protected void before() throws Throwable {
+		create();
+	}
+
+	@Override
+	protected void after() {
+		delete();
+	}
+	
+	public GitRemoteSyncConnection getGITConn() {
+		return fGITConn;
+	}
+	
+	public TemporaryFolder getLocalFolder() {
+		return localFolder;
+	}
+	
+	public String getRemoteFolder() {
+		return remoteFolder;
+	}
+
+	public IRemoteFileManager getFileManager() {
+		return fileManager;
+	}
+
+	private void create() throws Exception {
+		long n = random.nextInt(1000000);
+		remoteFolder = test.remoteBaseDir + "/junit" + n;
+		
+		/* setup remote connection */
+		IRemoteServices fRemoteServices;
+		
+		fRemoteServices = PTPRemoteCorePlugin.getDefault().getRemoteServices(
+				"org.eclipse.ptp.remote.RemoteTools"); //$NON-NLS-1$
+		assertNotNull(fRemoteServices);
+
+		connMgr = fRemoteServices.getConnectionManager();
+		assertNotNull(connMgr);
+
+		//TODO: understand why it is causes problem when all connections are called the same. Should be fine because 
+		// connections are deleted. There seems to be a problem in RemoteTools with creating a new connection with the same name
+		fRemoteConnection = connMgr.newConnection("test_connection"+n); //$NON-NLS-1$  
+
+		assertNotNull(fRemoteConnection);
+		fRemoteConnection.setAddress(test.host);
+		fRemoteConnection.setUsername(test.username);
+		if (test.privatekey == null) {
+			fRemoteConnection.setPassword(test.password);
+		} else {
+			fRemoteConnection.setAttribute(ConfigFactory.ATTR_KEY_PATH, test.privatekey);
+			fRemoteConnection.setAttribute(ConfigFactory.ATTR_KEY_PASSPHRASE, test.password);
+			fRemoteConnection.setAttribute(ConfigFactory.ATTR_IS_PASSWORD_AUTH, Boolean.toString(false));
+		}
+		
+		if (!fRemoteConnection.isOpen()) {
+			fRemoteConnection.open(null);
+		}
+		
+		fileManager = fRemoteConnection.getRemoteServices().getFileManager(fRemoteConnection);
+		
+		/*local folder*/
+		localFolder = new TemporaryFolder();
+		localFolder.create();
+		
+		/*remote folder (just delete - is created by GitRemoteSyncConnection)*/
+		fileManager.getResource(remoteFolder).delete(EFS.NONE, null);
+		
+		fGITConn = new GitRemoteSyncConnection(fRemoteConnection,
+				localFolder.getRoot().getPath(), remoteFolder,
+				new SyncFileFilter() {
+					public boolean shouldIgnore(String fileName) {
+						if (fileName.startsWith(GitRemoteSyncConnection.gitDir)) {
+							return true;
+						}
+						return false;
+					}
+				}, null);
+	}
+
+	private void delete()  {
+		String failMessage="";
+		
+		fGITConn.close();
+		localFolder.delete();
+		
+		try {
+			fileManager.getResource(remoteFolder).delete(EFS.NONE, null);
+		} catch (Exception e){
+			e.printStackTrace();
+			failMessage += e.getMessage(); //don't fail yet - try to clean up as much as possible
+		}
+		
+		fRemoteConnection.close();
+		try {
+			connMgr.removeConnection(fRemoteConnection);
+		} catch (Exception e){
+			e.printStackTrace();
+			failMessage += e.getMessage(); //don't fail yet - try to clean up as much as possible
+		}
+		
+		if (!failMessage.equals("")) {
+			fail(failMessage);
+		}
+	}
+
+	public IRemoteConnection getRemoteConn() {
+		return fRemoteConnection;
+	}
+}
