f1967fa31a3c3c4923ede8765b4422e5f8c897ac
John D Eblen <jeblen@acm.org>
2012-01-12 14:50:47 -0500

    Fix errors found during testing:
    1) Specify absolute path to the diff3 parser
    
    2) Stop sync'ing when there is a merge conflict
    
    3) Fix problem getting the sync service provider in tree viewer
       (also see previous commit reverting changes to BuildConfigurationManager)

--- rdt/org.eclipse.ptp.rdt.sync.ui/src/org/eclipse/ptp/rdt/sync/ui/SyncMergeFileTreeViewer.java
+++ rdt/org.eclipse.ptp.rdt.sync.ui/src/org/eclipse/ptp/rdt/sync/ui/SyncMergeFileTreeViewer.java
@@ -14,6 +14,8 @@ import java.util.ArrayList;
 import java.util.Set;
 import java.util.concurrent.locks.ReentrantLock;
 
+import org.eclipse.cdt.managedbuilder.core.IConfiguration;
+import org.eclipse.cdt.managedbuilder.core.ManagedBuildManager;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
@@ -24,7 +26,14 @@ import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.ptp.rdt.sync.core.BuildConfigurationManager;
+import org.eclipse.ptp.rdt.sync.core.RDTSyncCorePlugin;
+import org.eclipse.ptp.rdt.sync.ui.messages.Messages;
 import org.eclipse.ptp.rdt.sync.core.serviceproviders.ISyncServiceProvider;
+import org.eclipse.ptp.rdt.sync.core.services.IRemoteSyncServiceConstants;
+import org.eclipse.ptp.services.core.IService;
+import org.eclipse.ptp.services.core.IServiceConfiguration;
+import org.eclipse.ptp.services.core.IServiceModelManager;
+import org.eclipse.ptp.services.core.ServiceModelManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
@@ -39,7 +48,7 @@ public class SyncMergeFileTreeViewer extends ViewPart {
 	public void createPartControl(Composite parent) {
 		parentComposite = parent;
 		synchronized(treeLock) {
-			TreeViewer fileTree = new TreeViewer(parent);
+			fileTree = new TreeViewer(parent);
 			fileTree.getTree().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
 			fileTree.setContentProvider(new ConflictedFilesContentProvider());
 			if (project != null) {
@@ -83,17 +92,18 @@ public class SyncMergeFileTreeViewer extends ViewPart {
 			ArrayList<IResource> children = new ArrayList<IResource>();
 
 			if (element instanceof IProject && ((IProject) element).isAccessible()) {
-				ISyncServiceProvider provider = BuildConfigurationManager.getInstance().getProjectSyncProvider((IProject) element);
-				conflictedFiles = provider.getMergeConflictFiles();
-				
-				try {
-					for (IResource localChild : ((IProject) element).members()) {
-						if (shouldBeIncluded(localChild)) {
-							children.add(localChild);
+				ISyncServiceProvider provider = this.getSyncProvider((IProject) element);
+				if (provider != null) {
+					conflictedFiles = provider.getMergeConflictFiles();
+					try {
+						for (IResource localChild : ((IProject) element).members()) {
+							if (shouldBeIncluded(localChild)) {
+								children.add(localChild);
+							}
 						}
+					} catch (CoreException e) {
+						assert(false); // This should never happen, since we check for existence before accessing the project.
 					}
-				} catch (CoreException e) {
-					assert(false); // This should never happen, since we check for existence before accessing the project.
 				}
 			}
 
@@ -136,6 +146,24 @@ public class SyncMergeFileTreeViewer extends ViewPart {
 			} else {
 				return true;
 			}
+		}
+		
+		// Low-level access to core to get sync provider. Logs error message and returns null if unable to find a provider.
+		private ISyncServiceProvider getSyncProvider(IProject project) {
+			ISyncServiceProvider provider = null;
+			IConfiguration config = ManagedBuildManager.getBuildInfo(project).getDefaultConfiguration();
+			BuildConfigurationManager bcm = BuildConfigurationManager.getInstance();
+			IServiceConfiguration serviceConfig = bcm.getConfigurationForBuildConfiguration(config);
+			if (serviceConfig != null) {
+				IServiceModelManager serviceModel = ServiceModelManager.getInstance();
+				IService syncService = serviceModel.getService(IRemoteSyncServiceConstants.SERVICE_SYNC);
+				provider = (ISyncServiceProvider) serviceConfig.getServiceProvider(syncService);
+			}
+			
+			if (provider == null) {
+				RDTSyncUIPlugin.getDefault().logErrorMessage(Messages.SyncMergeFileTreeViewer_0 + config.getName());
+			}
+			return provider;
 		}
 	}
 }
