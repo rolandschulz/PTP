c5f96320543fb547c0e112c89619c8e11ae03acd
Jeffrey Overbey <jeffreyoverbey@acm.org>
2012-01-19 10:53:04 -0600

    Eliminated PLDT tests' dependency on org.eclipse.cdt.core.tests (at Beth's request)

---
+++ tools/pldt/org.eclipse.ptp.pldt.tests/src/org/eclipse/ptp/pldt/tests/infrastructure/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The classes in this package originated from org.eclipse.cdt.core.tests.
+ * <p>
+ * They are used by PLDT's unit tests to avoid a dependency on org.eclipse.cdt.core.tests.
+ */
+package org.eclipse.ptp.pldt.tests.infrastructure;
