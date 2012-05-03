d220a6a41953c6b4076abae4737a31f59be019cf
John Eblen <jeblen@acm.org>
2012-05-03 10:21:25 -0400

    Disable POST_BUILD sync for auto builds
    This was causing an infinite loop when a sync was requested during a merge conflict

--- rdt/org.eclipse.ptp.rdt.sync.ui/src/org/eclipse/ptp/rdt/sync/ui/ResourceChangeListener.java
+++ rdt/org.eclipse.ptp.rdt.sync.ui/src/org/eclipse/ptp/rdt/sync/ui/ResourceChangeListener.java
@@ -18,6 +18,7 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
 import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.dialogs.MessageDialogWithToggle;
@@ -117,7 +118,9 @@ public class ResourceChangeListener {
 					try {
 						// Post-build event
 						// Force a sync in order to download any new remote files but no need to sync if sync'ing is disabled.
-						if (event.getType() == IResourceChangeEvent.POST_BUILD ) {
+						// Ignore auto builds, which are triggered for every resource change.
+						if (event.getType() == IResourceChangeEvent.POST_BUILD &&
+								event.getBuildKind() != IncrementalProjectBuilder.AUTO_BUILD) {
 							if (!syncOn || syncMode == SYNC_MODE.UNAVAILABLE) {
 								continue;
 							} else if (syncMode == SYNC_MODE.ALL) {
