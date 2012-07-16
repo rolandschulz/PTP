f1a72d0478af41b307849dba1bc6b181925f358a
John Eblen <jeblen@acm.org>
2012-07-16 16:50:25 -0400

    Throttle error messages when connection requested repeatedly. (For example, "Build All" with several projects that use the missing connection.)

--- rdt/org.eclipse.ptp.rdt.sync.ui/src/org/eclipse/ptp/rdt/sync/ui/CommonMissingConnectionHandler.java
+++ rdt/org.eclipse.ptp.rdt.sync.ui/src/org/eclipse/ptp/rdt/sync/ui/CommonMissingConnectionHandler.java
@@ -16,11 +16,19 @@ import org.eclipse.ptp.rdt.sync.core.IMissingConnectionHandler;
 import org.eclipse.ptp.remote.core.IRemoteServices;
 
 public class CommonMissingConnectionHandler implements IMissingConnectionHandler {
+	private static long lastMissingConnectiontDialogTimeStamp = 0;
+	private static final long timeBetweenDialogs = 5000; // 5 seconds
+
 	@Override
 	public void handle(final IRemoteServices remoteServices, final String connectionName) {
 		RDTSyncUIPlugin.getStandardDisplay().syncExec(new Runnable() {
 			@Override
 			public void run() {
+				// Avoid flooding the display with missing connection dialogs
+				if (System.currentTimeMillis() - lastMissingConnectiontDialogTimeStamp <= timeBetweenDialogs) {
+					return;
+				}
+				lastMissingConnectiontDialogTimeStamp = System.currentTimeMillis();
 				String[] buttonLabels = new String[1];
 				buttonLabels[0] = IDialogConstants.OK_LABEL;
 				MessageDialog dialog = new MessageDialog(null, "Missing Connection", null, "Connection does not exist: " + //$NON-NLS-1$ //$NON-NLS-2$
