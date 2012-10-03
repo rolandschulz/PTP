9ea13411dbc71c2aee2d57d176f9f63ee67a59d2
Vivian Kong <vivkong@ca.ibm.com>
2012-10-03 13:04:25 -0400

    Bug 391038 - Cannot build/clean projects created by previous versions of ptp

--- rdt/org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/internal/rdt/ui/scannerinfo/RemoteSymbolTab.java
+++ rdt/org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/internal/rdt/ui/scannerinfo/RemoteSymbolTab.java
@@ -10,11 +10,6 @@
  *******************************************************************************/ 
 package org.eclipse.ptp.internal.rdt.ui.scannerinfo;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.cdt.core.settings.model.ICLanguageSetting;
-import org.eclipse.cdt.core.settings.model.ICResourceDescription;
 import org.eclipse.cdt.ui.newui.SymbolTab;
 import org.eclipse.ptp.internal.rdt.ui.RDTHelpContextIds;
 
@@ -23,22 +18,5 @@ public class RemoteSymbolTab extends SymbolTab {
 	public String getHelpContextId() {
 		return RDTHelpContextIds.REMOTE_SYMBOL_TAB;
 	}
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.cdt.ui.newui.AbstractLangsListTab#getLangSetting(org.eclipse.cdt.core.settings.model.ICResourceDescription)
-	 */
-	@Override
-	public ICLanguageSetting[] getLangSetting(ICResourceDescription rcDes) {
-		ICLanguageSetting[] langSettings_orig = super.getLangSetting(rcDes);
-		List<ICLanguageSetting> langSettings = new ArrayList<ICLanguageSetting>();
-		
-		// bug 378579 - remove the old Remote XL C++ Compiler's C source file input type in case 
-		// this is a project created before the fix for bug 378579
-		for (int i = 0; i < langSettings_orig.length; i++) {
-			if (langSettings_orig[i].getId().indexOf("org.eclipse.ptp.rdt.managedbuilder.xlc.ui.cpp.c.compiler.input") == -1) { //$NON-NLS-1$
-				langSettings.add(langSettings_orig[i]);
-			}
-		}
-		return langSettings.toArray(new ICLanguageSetting[langSettings.size()]);
-	}
+
 }
