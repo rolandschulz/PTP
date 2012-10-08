bfeac1693b6bcaa3911209d5193f220b99fcb4a1
Carsten Karbach <c.karbach@fz-juelich.de>
2012-10-08 11:02:18 +0200

    Added license text to new classes

--- rms/org.eclipse.ptp.rm.lml.core/src/org/eclipse/ptp/rm/lml/internal/core/model/LayoutTraverser.java
+++ rms/org.eclipse.ptp.rm.lml.core/src/org/eclipse/ptp/rm/lml/internal/core/model/LayoutTraverser.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2012 Forschungszentrum Juelich GmbH
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * 		Carsten Karbach, FZ Juelich
+ */
 package org.eclipse.ptp.rm.lml.internal.core.model;
 
 import java.util.List;
