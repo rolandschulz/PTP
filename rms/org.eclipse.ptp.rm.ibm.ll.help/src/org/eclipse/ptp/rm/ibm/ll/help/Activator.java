04304232182ccae58fef347cb9c64d554ae150c7
Greg Watson <g.watson@computer.org>
2013-02-08 16:30:01 -0500

    Update copyright.

--- rms/org.eclipse.ptp.rm.ibm.ll.help/src/org/eclipse/ptp/rm/ibm/ll/help/Activator.java
+++ rms/org.eclipse.ptp.rm.ibm.ll.help/src/org/eclipse/ptp/rm/ibm/ll/help/Activator.java
@@ -1,10 +1,10 @@
-/*
+/*******************************************************************************
  * Copyright (c) 2007 IBM Corporation
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- */
+ ******************************************************************************/
 package org.eclipse.ptp.rm.ibm.ll.help;
 
 import org.eclipse.jface.resource.ImageDescriptor;
@@ -21,7 +21,7 @@ public class Activator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -30,8 +30,10 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
@@ -39,8 +41,10 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
@@ -48,7 +52,7 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
@@ -58,8 +62,9 @@ public class Activator extends AbstractUIPlugin {
 	/**
 	 * Returns an image descriptor for the image file at the given
 	 * plug-in relative path
-	 *
-	 * @param path the path
+	 * 
+	 * @param path
+	 *            the path
 	 * @return the image descriptor
 	 */
 
