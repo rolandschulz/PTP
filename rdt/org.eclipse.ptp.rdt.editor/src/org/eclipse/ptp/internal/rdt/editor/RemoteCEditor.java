1e30c8ecdd9125450dec8f49714e5340da0cf467
Mike Kucera <mkucera@ca.ibm.com>
2013-02-12 13:55:21 -0500

    Bug 400614 - Add keybinding context to RemoteCEditor

--- rdt/org.eclipse.ptp.rdt.editor/src/org/eclipse/ptp/internal/rdt/editor/RemoteCEditor.java
+++ rdt/org.eclipse.ptp.rdt.editor/src/org/eclipse/ptp/internal/rdt/editor/RemoteCEditor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2012 IBM Corporation and others.
+ * Copyright (c) 2008, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -444,5 +444,10 @@ public class RemoteCEditor extends CEditor implements HelpListener {
 				firePropertyChange(PROP_DIRTY);
 			}
 		});
+	}
+	
+	@Override
+	protected void initializeKeyBindingScopes() {
+		setKeyBindingScopes(new String [] { "org.eclipse.ptp.rdt.editor.RemoteCEditorScope" }); //$NON-NLS-1$
 	}
 }
