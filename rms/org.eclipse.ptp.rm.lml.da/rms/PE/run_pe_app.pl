e5406344780b46c917cd403ae4961ad0ca7480c8
dwootton <dwootton@us.ibm.com>
2013-03-21 09:49:07 -0400

    Use poe UNIX domain socket to get PE runtime task mapping info

--- rms/org.eclipse.ptp.rm.lml.da/rms/PE/run_pe_app.pl
+++ rms/org.eclipse.ptp.rm.lml.da/rms/PE/run_pe_app.pl
@@ -10,17 +10,16 @@
 use strict;
 use File::Basename;
 use Cwd;
+use Sys::Hostname;
+use IO::Socket::UNIX;
 
 my $patint  = "([\\+\\-\\d]+)";              # Pattern for Integer number
 my $patnode = "([\^\\s]+(\\.[\^\\s]*)*)";    # Pattern for domain name (a.b.c)
-my $patbl = "\\s*";    # Pattern for blank space (variable length)
 
 my $portbase     = 50000;
 my $portrange    = 10000;
-my $verbose      = 0;
 my $ROUTING_FILE = "routing_file";
 my $TOTAL_PROCS  = 0;
-my @JOB;
 
 my $pid;
 my $hpcrun;
@@ -29,39 +28,6 @@ my $debuggerId;
 my $debuggerLauncher;
 my @child_pids;
 
-# Waits for the attach.cfg file to appear, then checks that the whole
-# file has been written. Returns when the file is available.
-sub wait_for_cfg_file {
-	my ($file) = @_;
-	my $line;
-	my $ntasks = -1;
-	my $nlines = 0;
-
-	while (1) {
-		while ( !open( CFG, "<", $file ) ) {
-			sleep(1);
-		}
-
-		# Ignore first line
-		$line = <CFG>;
-
-		# Second line has number of tasks
-		chomp( $line = <CFG> );
-		if ( $line =~ /$patint$patbl;/ ) {
-			$ntasks = $1;
-		}
-		# Count the number of lines in the rest of the file
-		$nlines = 0;
-		while ( $line = <CFG> ) {
-			$nlines++;
-		}
-		close(CFG);
-		if ($ntasks == $nlines) {
-			return;
-		}
-		sleep(1);
-	}
-}
 
 #####################################################################
 #
@@ -85,41 +51,69 @@ sub wait_for_cfg_file {
 #####################################################################
 sub generate_routing_file {
 	my ($pid, $file) = @_;
-	my $line;
 	my $ntasks;
-	my $fd;
 	my $cfg_file = "/tmp/.ppe.$pid.attach.cfg";
+	my $hname = hostname;
 
-	wait_for_cfg_file($cfg_file);
-	
-	if ( open(CFG, $cfg_file) ) {
-
-		open( OUT, "> $file" ) || die "cannot open file $file";
-
-		# Ignore first line
-		$line = <CFG>;
-
-		# Second line has number of tasks
-		chomp( $line = <CFG> );
-		if ( $line =~ /$patint$patbl;/ ) {
-			$ntasks = $1;
-			printf(OUT "%d\n", $ntasks);
-		}
-		while ( $line = <CFG> ) {
-			my ( $taskid, $nodeid );
-			chomp($line);
-			if ( $line =~ /$patint$patbl$patint$patbl$patnode/ ) {
-				( $taskid, $nodeid ) = ( $1, $3 );
-			} elsif ( $line =~ /$patint$patbl$patnode/ ) {
-				( $taskid, $nodeid ) = ( $1, $2 );
-			}
-			printf( OUT "%d %s %d\n",
-				$taskid, $nodeid, $portbase + int( rand($portrange) ) );
-		}
-
-		close(CFG);
-		close(OUT);
+	# get the location of the socket file
+	my $taskinfodir = $ENV{'MP_DBG_TASKINFO_DIR'};
+	if ( $taskinfodir == "" ) {
+		$taskinfodir = "/tmp";
 	}
+
+	my $socket_file = "$taskinfodir/.ppe.$hname.$pid.socket";
+	my $socket_addr = sockaddr_un($socket_file);
+	my $sb;
+
+	while ( $sb == "" ) {
+		# check the socket file
+		$sb = stat($socket_file);
+	}
+
+	my $clntsocket = IO::Socket::UNIX->new(Type => SOCK_STREAM,
+	                                       Peer => $socket_file) || die "IO::Socket->new: $!\n";
+
+	# register for notifications
+	my $register_for_notifications = "[TASK:%t,%p,%H]-1\n";
+	$clntsocket->syswrite($register_for_notifications) || die "syswrite: $!\n";
+
+	my @msgs;
+	my $msg;
+	my $chr;
+	my $bytesread = 0;
+	my $msgdone = 0;
+
+	while ($bytesread = $clntsocket->sysread($chr, 1)) {
+		if ( $chr eq "\n" ) {
+			if ( $msgdone eq 1 ) {
+				last;
+			} else {
+				$msgdone = 1;
+				$msg = $msg . $chr;
+				push( @msgs, $msg );
+				$msg = "";
+			}
+		} else {
+			$msgdone = 0;
+			$msg = $msg . $chr;
+		}
+	}
+
+	$clntsocket->close;
+
+	# write the task configuration info to the routing file
+	open( OUT, ">$file") || die "open: $!\n";
+
+	printf(OUT "%d\n", scalar(@msgs));
+
+	foreach $msg (@msgs) {
+		if ( $msg =~ /$patint,$patint,$patint,$patnode/ ) {
+			my ( $taskid, $nodeid ) = ( $2, $4 );
+			printf(OUT "%d %s %d\n", $taskid, $nodeid, $portbase + int (rand($portrange)));
+		}
+	}
+
+	close(OUT);
 }
 
 sub start_sdm_master {
@@ -153,14 +147,17 @@ if ($launchMode eq 'debug') {
 
 $pid = fork();
 if ( $pid == 0 ) {
-	  # Unconditionally set MP_DEBUG_ATTACH to be sure attach.cfg file is created
-	$ENV{MP_DEBUG_ATTACH} = "yes";
+	# set MP_DBG_TASKINFO
+	$ENV{'MP_DEBUG_ATTACH'} = "no";
+	$ENV{'MP_DBG_TASKINFO'} = "yes";
+
 	printf( "::::PoePid=%d::::\n", $$ );
+
     if ($hpcrun eq 'yes') {
         exec('/opt/ibmhpc/ppedev.hpct/bin/hpcrun', '/usr/bin/poe', @ARGV);
     }
     else {
-        exec('poe', @ARGV);
+        exec('/usr/bin/poe', @ARGV);
     }
 	exit(1);
 }
