c30522151dbfe7633ae7bb83e60b556aa33bcbeb
John Eblen <jeblen@acm.org>
2013-05-09 22:18:31 -0400

    More work to fix conversion wizards

--- rdt/org.eclipse.ptp.rdt.sync.cdt.ui/src/org/eclipse/ptp/internal/rdt/sync/cdt/ui/messages/Messages.java
+++ rdt/org.eclipse.ptp.rdt.sync.cdt.ui/src/org/eclipse/ptp/internal/rdt/sync/cdt/ui/messages/Messages.java
@@ -28,6 +28,10 @@ public class Messages extends NLS {
 	public static String NewRemoteSyncProjectWizardPage_syncProvider;
 	public static String WizardProjectConversion_windowLabel;
 	public static String ConvertFromRemoteCToSyncProjectWizardPage_3;
+	public static String ConvertLocalToSyncProjectWizard_0;
+	public static String ConvertLocalToSyncProjectWizard_1;
+	public static String ConvertLocalToSyncProjectWizard_2;
+	public static String ConvertLocalToSyncProjectWizard_3;
 	public static String ConvertLocalToSyncProjectWizard_Convert_nonexistent_project_error;
 	public static String ConvertLocalToSyncProjectWizardPage_2;
 	public static String ConvertLocalToSyncProjectWizardPage_3;
