3725c9d92da32472dcf1f4a2106744c5d00a816c
John Eblen <jeblen@acm.org>
2013-05-18 21:48:56 -0400

    Fix copyright headers

--- rdt/org.eclipse.ptp.rdt.sync.cdt.ui/src/org/eclipse/ptp/internal/rdt/sync/cdt/ui/wizards/SynchronizeWizardExtension.java
+++ rdt/org.eclipse.ptp.rdt.sync.cdt.ui/src/org/eclipse/ptp/internal/rdt/sync/cdt/ui/wizards/SynchronizeWizardExtension.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2013 IBM Corporation and others.
+ * Copyright (c) 2013 Oak Ridge National Laboratory and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    IBM Corporation - Initial API and implementation
+ *     John Eblen - initial implementation
  *******************************************************************************/
 package org.eclipse.ptp.internal.rdt.sync.cdt.ui.wizards;
 
