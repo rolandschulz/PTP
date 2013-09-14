1af263d9987f6c4e5b09e3528707d1d2474b2b06
Carsten Karbach <c.karbach@fz-juelich.de>
2013-09-14 17:11:43 -0500

    [393148] Fix PBS scripts

--- rms/org.eclipse.ptp.rm.lml.da/rms/PBS/da_system_info_LML.pl
+++ rms/org.eclipse.ptp.rm.lml.da/rms/PBS/da_system_info_LML.pl
@@ -11,6 +11,10 @@
 #*******************************************************************************/ 
 use strict;
 
+use FindBin;
+use lib "$FindBin::RealBin/../../lib";
+use LML_da_util;
+
 my $srcDir = $0;
 if($srcDir =~ /\// ){
 	$srcDir =~ s/[^\/]+\.pl$//;
@@ -73,7 +77,7 @@ printf(OUT "<objects>\n");
 $count=0;
 foreach $sysinfoid (sort(keys(%sysinfo))) {
     $count++;$sysinfonr{$sysinfoid}=$count;
-    printf(OUT "<object id=\"sys%06d\" name=\"%s\" type=\"system\"/>\n",$count,$sysinfoid);
+    printf(OUT "<object id=\"sys%06d\" name=\"%s\" type=\"system\"/>\n",$count,&LML_da_util::escapeForXML($sysinfoid));
 }
 printf(OUT "</objects>\n");
 printf(OUT "<information>\n");
@@ -85,7 +89,7 @@ foreach $sysinfoid (sort(keys(%sysinfo))) {
 
 		$value=&modify($key,$mapping{$key},$sysinfo{$sysinfoid}{$key});
 		if(defined($value)) {
-		    printf(OUT " <data %-20s value=\"%s\"/>\n","key=\"".$mapping{$key}."\"",$value);
+		    printf(OUT " <data %-20s value=\"%s\"/>\n","key=\"".$mapping{$key}."\"",&LML_da_util::escapeForXML($value));
 		}
 	    } else {
 		$notmappedkeys{$key}++;
