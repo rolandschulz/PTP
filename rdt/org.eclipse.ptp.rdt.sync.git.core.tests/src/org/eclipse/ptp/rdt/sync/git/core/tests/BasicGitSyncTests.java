38b929852d8c0a03b402363d0774eddf56a68bf3
John Eblen <jeblen@acm.org>
2014-04-17 18:06:22 -0400

    Finish up and polish initial sync testing
    Change-Id: I140581d03b63414caa8a0f25429029892a665210

--- rdt/org.eclipse.ptp.rdt.sync.git.core.tests/src/org/eclipse/ptp/rdt/sync/git/core/tests/BasicGitSyncTests.java
+++ rdt/org.eclipse.ptp.rdt.sync.git.core.tests/src/org/eclipse/ptp/rdt/sync/git/core/tests/BasicGitSyncTests.java
@@ -7,7 +7,7 @@
  *
  * Contributors:
  *     Roland Schulz - initial API and implementation
- *     John Eblen - update and augment for Luna
+ *     John Eblen - update and augment for Eclipse Luna
  *******************************************************************************/
 package org.eclipse.ptp.rdt.sync.git.core.tests;
 
@@ -16,16 +16,16 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DecimalFormat;
 import java.util.Properties;
 
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.ptp.internal.rdt.sync.core.RDTSyncCorePlugin;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.ptp.internal.rdt.sync.git.core.GitRepo;
+import org.eclipse.ptp.internal.rdt.sync.git.core.JGitRepo;
 import org.eclipse.ptp.rdt.sync.core.RemoteLocation;
+import org.eclipse.ptp.rdt.sync.core.exceptions.MissingConnectionException;
 import org.eclipse.remote.core.IRemoteConnection;
 import org.eclipse.remote.core.IRemoteConnectionManager;
 import org.eclipse.remote.core.IRemoteConnectionWorkingCopy;
@@ -34,38 +34,51 @@ import org.eclipse.remote.core.RemoteServices;
 import org.eclipse.remote.core.exception.RemoteConnectionException;
 import org.junit.Test;
 
+/**
+ * JUnit tests for synchronized project's git sync service
+ *
+ * Tests are configured in "remotehost.properties." Basic sync operations are performed on and between the given local and remote
+ * directories. Timing information is also logged, which is useful for identifying bottlenecks for a particular local/remote pair.
+ */
 public class BasicGitSyncTests {
 	private static String remoteServicesProvider = "org.eclipse.remote.JSch";
-	private static String testProjectName = "testSyncProject";
 	private static String testConnectionName = "testSyncConnection";
-	private static String testRemoteDirName = "junit";
 
-	String host = null;
-	String username = null;
-	String password = null;
-	String remoteBaseDir = null;
+	private String host = null;
+	private String username = null;
+	private String password = null;
+	private String localBaseDir = null;
+	private String remoteBaseDir = null;
+	private int steps = 1;
 
+	/**
+	 * Constructor - reads in test configuration
+	 */
 	public BasicGitSyncTests() {
 		Properties prop = new Properties();
 		InputStream in = BasicGitSyncTests.class.getResourceAsStream("remotehost.properties");
 		try {
 			prop.load(in);
 		} catch (IOException e) {
-			RDTSyncCorePlugin.log("Unable to load testing properties", e);
+			log("Unable to load testing properties", e);
 		}
 		try {
 			in.close();
 		} catch (IOException e) {
-			RDTSyncCorePlugin.log("Unable to close test properties file", e);
+			log("Unable to close test properties file", e);
 		}
 
 		host = prop.getProperty("host");
 		username = prop.getProperty("username");
 		password = prop.getProperty("password");
+		localBaseDir = prop.getProperty("localBaseDir");
 		remoteBaseDir = prop.getProperty("remoteBaseDir");
+		String numSteps = prop.getProperty("steps");
+		if (numSteps != null) {
+			steps = Integer.parseInt(numSteps);
+		}
 		// TODO: Change so that password is not required in config file.
 		assertTrue(host != null && password != null);
-
 		if (username == null || username.equals("")) {
 			username = System.getProperty("user.name");
 		}
@@ -74,45 +87,69 @@ public class BasicGitSyncTests {
 		}
 	}
 
+	/**
+	 * Testing code. Performs basic sync operations and reports timing information.
+	 * @throws CoreException 
+	 * @throws GitAPIException 
+	 * @throws IOException 
+	 * @throws MissingConnectionException 
+	 */
 	@Test
-	public void basicGitSyncTest() {
-		IProject project;
+	public void basicGitSyncTest() throws CoreException, GitAPIException, IOException, MissingConnectionException {
 		IRemoteConnection conn;
 
 		try {
 			// TODO: Pop up a dialog for password if it is not specified.
 			conn = createTestConnection(host, username, password, remoteBaseDir);
 		} catch (RemoteConnectionException e) {
-			RDTSyncCorePlugin.log("Unable to create test connection to host: " + host + " directory: " + remoteBaseDir, e);
-			return;
-		}
-		try {
-			project = createTestProject();
-		} catch (CoreException e) {
-			RDTSyncCorePlugin.log("Unable to create test project", e);
-			deleteConnection(conn);
+			log("Unable to create test connection to host: " + host + " directory: " + remoteBaseDir, e);
 			return;
 		}
 
 		RemoteLocation remoteLocation = new RemoteLocation();
 		remoteLocation.setConnection(conn);
-		remoteLocation.setLocation(remoteBaseDir + testRemoteDirName);
+		remoteLocation.setLocation(remoteBaseDir);
 		remoteLocation.setRemoteServicesId(conn.getRemoteServices().getId());
 
-		RDTSyncCorePlugin.log("I am now thoroughly testing Sync!");
+		Timer stepTimer = new Timer();
+		Timer totalTimer = new Timer();
+		stepTimer.reset();
+		totalTimer.reset();
 
-		deleteRemoteFiles(remoteLocation, remoteBaseDir);
+		JGitRepo localRepo = new JGitRepo(new Path(localBaseDir), null);
+		log("Created local JGit repository", stepTimer);
+
+		GitRepo remoteRepo = new GitRepo(remoteLocation, null);
+		log("Created remote Git repository", stepTimer);
+
+		for (int i=0; i<steps; i++) {
+			localRepo.commit(null);
+			log("Commit local files", stepTimer);
+
+			remoteRepo.uploadFilter(localRepo, null);
+			log("Uploaded file filter", stepTimer);
+
+			remoteRepo.commitRemoteFiles(null);
+			log("Committed remote files", stepTimer);
+
+			localRepo.fetch(remoteLocation, null);
+			log("Fetched remote files", stepTimer);
+
+			localRepo.merge(null);
+			log("Local merged in remote changes", stepTimer);
+
+			localRepo.push(remoteLocation, null);
+			log("Pushed local files to remote", stepTimer);
+
+			remoteRepo.merge(null);
+			log("Remote merged in local changes", stepTimer);
+
+			log("-------------------------------------------------");
+			log("Total elapsed time for sync " + Integer.toString(i), totalTimer);
+			log("\n");
+			stepTimer.reset();
+		}
 		deleteConnection(conn);
-		deleteProject(project);
-	}
-
-	private static IProject createTestProject() throws CoreException {
-		IProgressMonitor progressMonitor = new NullProgressMonitor();
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		IProject project = root.getProject(testProjectName);
-		project.create(progressMonitor);
-		project.open(progressMonitor);
-		return project;
 	}
 
 	private static IRemoteConnection createTestConnection(String host, String username, String password, String remoteBaseDir)
@@ -134,25 +171,13 @@ public class BasicGitSyncTests {
 		return conn;
 	}
 
-	private static void deleteRemoteFiles(RemoteLocation rl, String dir) {
-		// TODO: Figure out a safe way to remove remote testing directory
-	}
-
 	private static void deleteConnection(IRemoteConnection conn) {
 		IRemoteConnectionManager connMgr = getRemoteConnectionManager();
 		try {
 			conn.close();
 			connMgr.removeConnection(conn);
 		} catch (RemoteConnectionException e) {
-			RDTSyncCorePlugin.log("Unable to delete connection", e);
-		}
-	}
-
-	private static void deleteProject(IProject project) {
-		try {
-			project.delete(true, true, null);
-		} catch (CoreException e) {
-			RDTSyncCorePlugin.log("Unable to delete project " + testProjectName, e);
+			log("Unable to delete connection", e);
 		}
 	}
 
@@ -161,5 +186,27 @@ public class BasicGitSyncTests {
 		assertNotNull(remoteServices);
 
 		return remoteServices.getConnectionManager();
+	}
+
+	// Report event and elapsed time, according to passed timer, to the log.
+	// Note that time is recorded first and timer is reset last to minimize logging overhead.
+	private static void log(String event, Timer timer) {
+		double elapsed = timer.getElapsed() / Math.pow(10, 9);
+		DecimalFormat df = new DecimalFormat("0.00");
+		System.out.print(event + ":");
+		for (int i=event.length(); i < 40; i++) {
+			System.out.print(" ");
+		}
+		System.out.println(df.format(elapsed) + " sec");
+		timer.reset();
+	}
+
+	private static void log(String event) {
+		System.out.println(event);
+	}
+
+	private static void log(String event, Exception e) {
+		System.out.println(event);
+		e.printStackTrace();
 	}
 }
